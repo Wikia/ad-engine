7634376b8ed6cadfcf956ab59e14f6d484a4d101
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2018-07-12 11:33:29 +0200

    ADEN-7353 Prepare test page

---
+++ examples/utils/cmp/script.js
@@ -0,0 +1,38 @@
+import { context, utils, trackingOptIn } from '@wikia/ad-engine';
+
+context.set('options.trackingOptIn', null);
+
+const isCmpDefined = typeof utils.queryString.get('cmp') !== 'undefined',
+	optIn = utils.queryString.get('cmp') === '1';
+console.log(isCmpDefined, optIn,  utils.queryString.get('cmp-consent'));
+if (isCmpDefined) {
+	window.__cmp = function (cmd, param, cb) {
+		if (cmd === 'getConsentData') {
+			cb({
+				consentData: optIn ? 'BOQu5jyOQu5jyCNABAPLBR-AAAAeCAFgAUABYAIAAaABFACY' : 'BOQu5naOQu5naCNABAPLBRAAAAAeCAAA',
+				gdprApplies: true,
+				hasGlobalScope: false
+			}, true);
+		} else if (cmd === 'getVendorConsents') {
+			cb({
+				metadata: 'BOQu5naOQu5naCNABAAABRAAAAAAAA',
+				purposeConsents: Array.from({length: 5}).reduce((map, val, i) => {
+					map[i + 1] = optIn;
+					return map;
+				}, {}),
+				vendorConsents: Array.from({length: 500}).reduce((map, val, i) => {
+					map[i + 1] = optIn;
+					return map;
+				}, {})
+			}, true);
+		} else {
+			cb(null, false);
+		}
+	};
+}
+
+trackingOptIn.readConsent();
+
+const preElement = document.getElementById('consent');
+
+preElement.innerText = trackingOptIn.isOptedIn() ? 'yes' : 'no';
