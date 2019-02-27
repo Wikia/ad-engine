60dc88a0a3aa2d29ff830d31d3c41d96db6b292f
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-02-27 13:47:22 +0100

    Use sh

--- maintenance/build-bundle.sh
+++ maintenance/build-bundle.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
 cat ./dist/ad-engine.global.js <(echo) ./dist/ad-products.global.js <(echo) ./dist/ad-bidders.global.js <(echo) ./dist/ad-services.global.js <(echo) ./lib/prebid.min.js > ./dist/global-bundle.js
