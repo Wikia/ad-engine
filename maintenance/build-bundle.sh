8b29432f73cc1a5c2e916646825bab040044717e
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-02-27 14:37:14 +0100

    Fix build step

--- maintenance/build-bundle.sh
+++ maintenance/build-bundle.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
-cat ./dist/ad-engine.global.js \
-    ./dist/ad-products.global.js \
-    ./dist/ad-bidders.global.js \
-    ./dist/ad-services.global.js \
-    ./lib/prebid.min.js \
-    > ./dist/global-bundle.js
+FILES=(
+    './dist/ad-engine.global.js'
+    './dist/ad-products.global.js'
+    './dist/ad-bidders.global.js'
+    './dist/ad-services.global.js'
+    './lib/prebid.min.js'
+)
+
+rm -f ./dist/global-bundle.js
+for file in "${FILES[@]}"; do (cat ${file}; echo) >> ./dist/global-bundle.js; done
