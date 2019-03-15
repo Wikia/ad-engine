2b180fe116859d04fa29f327f5f1221effc2c907
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-03-15 11:28:06 +0100

    ADEN-8483 CR fixes

--- maintenance/tslint-rules/src/noBlacklistedPathsRule.ts
+++ maintenance/tslint-rules/src/noBlacklistedPathsRule.ts
@@ -2,33 +2,31 @@ import * as Lint from 'tslint';
 import * as ts from 'typescript';
 
 export class Rule extends Lint.Rules.AbstractRule {
-	static FAILURE_STRING = 'Cannot import from blacklisted children/sibling package.';
+	static FAILURE_STRING = 'Cannot import from blacklisted paths:';
 
 	apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-		return this.applyWithWalker(new NoCrossImportsWalker(sourceFile, this.getOptions()));
+		return this.applyWithWalker(new NoBlacklistedPathsWalker(sourceFile, this.getOptions()));
 	}
 }
 
-class NoCrossImportsWalker extends Lint.RuleWalker {
+class NoBlacklistedPathsWalker extends Lint.RuleWalker {
 	visitImportDeclaration(node: ts.ImportDeclaration): void {
 		const [configuration] = this.getOptions();
-		const sourceFile = this.getSourceFile();
 
 		if (configuration) {
 			const { blacklist } = configuration;
 			const importText = node.getText();
+			const failureString = `${Rule.FAILURE_STRING} ${blacklist.join(', ')}.`;
 
 			if (this.isForbiddenImport(importText, blacklist)) {
-				this.addFailureAt(sourceFile.getEnd(), sourceFile.getWidth(), Rule.FAILURE_STRING);
+				this.addFailureAtNode(node, failureString);
 			}
 		}
 	}
 
 	private isForbiddenImport(importText: string, blacklist: string[]): boolean {
-		return (
-			blacklist.filter((forbiddenImport: string) => {
-				return importText.indexOf(`/${forbiddenImport}`) !== -1;
-			}).length > 0
-		);
+		return !!blacklist.some((forbiddenImport: string) => {
+			return importText.indexOf(`/${forbiddenImport}`) !== -1;
+		});
 	}
 }
