79d49b704c823e7a1603b073efa30645f19a37af
Katarzyna Basiukajc <kbasiukajc@wikia-inc.com>
2019-04-29 15:01:13 +0200

    ADEN-8704 | Delay panel disappearing

--- src/ad-products/templates/interface/video/toggle-ui.ts
+++ src/ad-products/templates/interface/video/toggle-ui.ts
@@ -4,9 +4,11 @@ const overlayTimeout = 5000;
 
 function add(video, container, params) {
 	let timeout = null;
+	let isFading = false;
 
 	const isMobile = utils.client.isSmartphone() || utils.client.isTablet();
 	const overlay = document.createElement('div');
+	const panel = document.getElementsByClassName('dynamic-panel')[0] as HTMLElement;
 	const setAutomaticToggle = () => {
 		timeout = setTimeout(() => {
 			if (video.isPlaying()) {
@@ -14,6 +16,36 @@ function add(video, container, params) {
 			}
 		}, overlayTimeout);
 	};
+
+	function fadeOut(): void {
+		isFading = true;
+		setTimeout(() => fadeOutEffect(overlay), 3000);
+		setTimeout(() => fadeOutEffect(panel), 3000);
+		setTimeout(() => {
+			if (isFading && overlay.style.opacity === '0' && panel.style.opacity === '0') {
+				video.container.classList.remove('ui-visible');
+			}
+		}, 4000);
+	}
+
+	function fadeOutEffect(element: HTMLElement): void {
+		const fadeEffect = setInterval(() => {
+			if (!element.style.opacity) {
+				element.style.opacity = '1';
+			}
+			if (isFading && parseFloat(element.style.opacity) > 0) {
+				element.style.opacity = (parseFloat(element.style.opacity) - 0.01).toString();
+			} else {
+				clearInterval(fadeEffect);
+			}
+		}, 10);
+	}
+
+	function resetFadeOut(): void {
+		isFading = false;
+		overlay.style.opacity = '1';
+		panel.style.opacity = '1';
+	}
 
 	overlay.classList.add('toggle-ui-overlay');
 	if (isMobile) {
@@ -27,9 +59,10 @@ function add(video, container, params) {
 	} else {
 		video.container.addEventListener('mouseenter', () => {
 			video.container.classList.add('ui-visible');
+			resetFadeOut();
 		});
 		video.container.addEventListener('mouseleave', () => {
-			video.container.classList.remove('ui-visible');
+			fadeOut();
 		});
 		overlay.addEventListener('click', () => {
 			top.open(params.clickThroughURL, '_blank');
