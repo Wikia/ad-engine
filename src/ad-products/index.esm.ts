67a106731fce1283c9f81cb12dbc49d1f0141361
Damian Bielecki <bielik20@gmail.com>
2019-05-02 16:17:53 +0200

    Add one export to rule them all

---
+++ src/ad-products/index.esm.ts
@@ -0,0 +1,10 @@
+// This file exist to provide different name for utils
+
+import './styles/styles.scss';
+import * as productsUtils from './utils';
+
+export * from './common';
+export * from './templates';
+export * from './tracking';
+export * from './video';
+export { productsUtils };
