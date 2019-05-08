62c4f83cc8b4366b191d2da014e8d9fa1c77584f
Damian Bielecki <bielik20@gmail.com>
2019-05-08 17:40:02 +0200

    Use one export for ad-engine

--- src/ad-engine/index.esm.ts
+++
@@ -1,7 +0,0 @@
-export * from './ad-engine';
-export * from './listeners';
-export * from './models';
-export * from './providers';
-export * from './services';
-export * from './video';
-export * from './utils';
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { logVersion } from './ad-engine/log-version';
-
-logVersion();
-
-export * from './ad-engine/index.esm';
+export * from './ad-engine';
 export * from './ad-bidders';
 export * from './ad-products/index.esm';
 export * from './ad-services';
