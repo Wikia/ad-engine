333d088d2844856c35417b5af3e955e3b7d79309
Damian Bielecki <bielik20@gmail.com>
2019-05-09 12:55:19 +0200

    Add automatic generation of pure instances

--- maintenance/rollup-plugins.js
+++ maintenance/rollup-plugins.js
@@ -1,5 +1,26 @@
 import { resolvePaths } from 'tscpaths';
 
+/**
+ * Applies regexp from https://regexr.com/4dlu6 to allow tree shaking
+ * @returns {{transform(string): *}|{code: string, map: null}}
+ */
+export function instanceToPure() {
+	return {
+		transform(code) {
+			return {
+				code: code.replace(/^(export const .+ = )new/gm, (match) =>
+					match.replace('new', '/*@__PURE__*/ new'),
+				),
+				map: null,
+			};
+		},
+	};
+}
+
+/**
+ * Replaces TypeScript @class comment with webpack @__PURE__ to allow tree shaking
+ * @returns {{renderChunk(string): *}|{code: string, map: null}}
+ */
 export function classToPure() {
 	return {
 		renderChunk(code) {
@@ -11,6 +32,11 @@ export function classToPure() {
 	};
 }
 
+/**
+ * Rewrites paths using settings from tsconfig.json
+ * @param options
+ * @returns {{generateBundle(): void}}
+ */
 export function tscpaths(options) {
 	options.project = options.project || 'tsconfig.json';
 	options.src = options.src || 'src';
@@ -23,6 +49,12 @@ export function tscpaths(options) {
 	};
 }
 
+/**
+ * Fills ad-engine name and version
+ * @param name
+ * @param version
+ * @returns {{renderChunk(string): *}|{code: string, map: null}}
+ */
 export function adEngineVersion(name, version) {
 	return {
 		renderChunk(code) {
