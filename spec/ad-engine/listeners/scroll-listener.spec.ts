e2039d5be4a2c1d403526e3cd5af18bb79411264
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-05-14 10:07:41 +0200

    ADEN-8686 Change interface of scrollListener.addSlot.
    Add async "ready" property to Ads module.

---
+++ spec/ad-engine/listeners/scroll-listener.spec.ts
@@ -0,0 +1,60 @@
+import { ScrollListener } from '@wikia/ad-engine';
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+
+describe('ScrollListener', () => {
+	let scrollListener: ScrollListener;
+	let getElementByIdStub;
+	let adStack: any[];
+	let addCallbackStub;
+
+	beforeEach(() => {
+		scrollListener = new ScrollListener();
+		adStack = [];
+		getElementByIdStub = sinon.stub(document, 'getElementById');
+		getElementByIdStub.callsFake(() => 'fakeNode');
+		addCallbackStub = sinon.stub(scrollListener, 'addCallback');
+		addCallbackStub.callsFake(() => {});
+	});
+
+	afterEach(() => {
+		getElementByIdStub.restore();
+		addCallbackStub.restore();
+	});
+
+	describe('addSlot', () => {
+		it('should not call addCallback if node with provided ID cannot be found', () => {
+			getElementByIdStub.callsFake(() => null);
+
+			scrollListener.addSlot(adStack, 'nodeId', { threshold: 5 });
+			scrollListener.addSlot(adStack, 'nodeId', { distancefromtop: 10 });
+
+			expect(scrollListener.addCallback.called).to.be.false;
+		});
+
+		it('should not call addCallback if both threshold and distanceFromTop are missing', () => {
+			scrollListener.addSlot(adStack, 'nodeId');
+			scrollListener.addSlot(adStack, 'nodeId', {});
+
+			expect(scrollListener.addCallback.called).to.be.false;
+		});
+
+		it('should not call addCallback if both threshold and distanceFromTop are supplied', () => {
+			scrollListener.addSlot(adStack, 'nodeId', { threshold: 20, distanceFromTop: 10 });
+
+			expect(scrollListener.addCallback.called).to.be.false;
+		});
+
+		it('should call scrollListener.addCallback if only distanceFromTop is supplied', () => {
+			scrollListener.addSlot(adStack, 'nodeId', { distanceFromTop: 10 });
+
+			expect(scrollListener.addCallback.called).to.be.true;
+		});
+
+		it('should call scrollListener.addCallback if only threshold is supplied', () => {
+			scrollListener.addSlot(adStack, 'nodeId', { threshold: 10 });
+
+			expect(scrollListener.addCallback.called).to.be.true;
+		});
+	});
+});
