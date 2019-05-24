b58569ffad9d04869c91530f4f652aeb40f1814a
Damian Bielecki <bielik20@gmail.com>
2019-05-24 12:34:07 +0200

    Remove webpack examples

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,27 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const merge = require('webpack-merge');
-const examples = require('./webpack.examples.js');
+const common = require('./webpack.common.js');
+const { examplePages } = require('./maintenance/examples-pages');
 
 const development = {
+	mode: 'development',
+
+	entry: examplePages,
+
+	output: {
+		path: path.resolve(__dirname, 'examples'),
+		filename: '[name]/dist/bundle.js',
+	},
+
+	plugins: [
+		new MiniCssExtractPlugin({ filename: '[name]/dist/styles.css' }),
+		new CopyWebpackPlugin([
+			{ from: path.resolve(__dirname, 'lib/prebid.min.js'), to: 'vendor/dist/prebid.min.js' },
+		]),
+	],
+
 	optimization: {
 		splitChunks: {
 			cacheGroups: {
@@ -15,4 +35,4 @@ const development = {
 	},
 };
 
-module.exports = merge(examples, development);
+module.exports = merge(common('tsconfig.misc.json'), development);
