a1746548038be6549a9abeb7d5145a53a16ddb2d
Damian Bielecki <bielik20@gmail.com>
2019-05-31 16:07:46 +0200

    Add old common

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,53 @@
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const StringReplacePlugin = require('string-replace-webpack-plugin');
+const fs = require('fs');
+const path = require('path');
+const get = require('lodash/get');
+const pkg = require('./package.json');
+
+const common = {
+	mode: 'development',
+	context: __dirname,
+	module: {
+		rules: [
+			{
+				test: /\.(js|ts)$/,
+				use: 'babel-loader',
+				include: [
+					path.resolve(__dirname, 'src'),
+					path.resolve(__dirname, 'spec'),
+					path.resolve(__dirname, 'examples'),
+				],
+			},
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+				type: 'javascript/auto',
+				exclude: [path.resolve(__dirname, 'node_modules')],
+			},
+			{
+				test: /\.s?css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				exclude: [path.resolve(__dirname, 'node_modules')],
+			},
+			{
+				test: path.resolve(__dirname, 'src/ad-engine/log-version.ts'),
+				loader: StringReplacePlugin.replace({
+					replacements: [
+						{
+							pattern: /<\?=[ \t]*PACKAGE\(([\w\-_.]*?)\)[ \t]*\?>/gi,
+							replacement: (match, p1) => get(pkg, p1),
+						},
+					],
+				}),
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json'],
+	},
+};
 
 const adEngine = {
 	mode: 'production',
@@ -70,9 +118,9 @@ const adServices = {
 
 module.exports = function() {
 	return [
-		merge(common('tsconfig.json'), adEngine),
-		merge(common('tsconfig.json'), adProducts),
-		merge(common('tsconfig.json'), adBidders),
-		merge(common('tsconfig.json'), adServices),
+		merge(common, adEngine),
+		merge(common, adProducts),
+		merge(common, adBidders),
+		merge(common, adServices),
 	];
 };
