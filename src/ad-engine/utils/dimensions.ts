baf87dc73ba3ba7fe957b0840cc609612e4af5c5
Damian Bielecki <bielik20@gmail.com>
2019-06-13 11:11:16 +0200

    Add getElementOffsetFromDocument helper method

--- src/ad-engine/utils/dimensions.ts
+++ src/ad-engine/utils/dimensions.ts
@@ -58,6 +58,18 @@ export function getLeftOffset(element: HTMLElement): number {
 }
 
 /**
+ * The result of this helpers is equal to jQuery's $.offset()
+ * @see https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+ */
+export function getElementOffsetFromDocument(element: HTMLElement): { top: number; left: number } {
+	const rect = element.getBoundingClientRect();
+	const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+	const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+	return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
+
+/**
  * Returns client's viewport height
  */
 export function getViewportHeight(): number {
