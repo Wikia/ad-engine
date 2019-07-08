54f84d3fae7bf9bfb6855e466530b682d7f446c9
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-07-08 15:31:00 +0200

    ADEN-9021 Add unit tests to getPrebidBestPrice.

--- spec/ad-bidders/prebid/price-helper.spec.ts
+++ spec/ad-bidders/prebid/price-helper.spec.ts
@@ -1,5 +1,13 @@
 import { expect } from 'chai';
-import { transformPriceFromCpm } from '../../../src/ad-bidders/prebid/price-helper';
+import * as sinon from 'sinon';
+import { PrebidBid } from '../../../src/ad-bidders/prebid';
+import { BaseAdapter } from '../../../src/ad-bidders/prebid/adapters';
+import { adaptersRegistry } from '../../../src/ad-bidders/prebid/adapters-registry';
+import {
+	getPrebidBestPrice,
+	transformPriceFromCpm,
+} from '../../../src/ad-bidders/prebid/price-helper';
+import { PrebidBidFactory } from './prebid-bid.factory';
 
 describe('transformPriceFromCpm', () => {
 	it('should return bucket from price converted to string with two decimal places', () => {
@@ -36,5 +44,94 @@ describe('transformPriceFromCpm', () => {
 
 	it('should cap to 20 even if maxCpm is lower than 20', () => {
 		expect(transformPriceFromCpm(21, 10)).to.equal('20.00');
+	});
+});
+
+describe('getPrebidBestPrice', () => {
+	let bids: PrebidBid[];
+	let adapters: Map<string, BaseAdapter>;
+	let sandbox;
+	const bidderName = 'bidderA';
+
+	beforeEach(() => {
+		sandbox = sinon.createSandbox();
+		bids = [];
+		window.pbjs = {
+			getBidResponsesForAdUnitCode() {
+				return { bids };
+			},
+		};
+		adapters = new Map();
+		sandbox.stub(adaptersRegistry, 'getAdapters').returns(adapters);
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	it('should return empty object if window.pbjs does not exist', () => {
+		delete window.pbjs;
+
+		const result = getPrebidBestPrice('bidderA');
+
+		expect(result).to.deep.equal({});
+	});
+
+	it('should return empty string if there is no price for bidder', () => {
+		adapters.set(bidderName, { bidderName } as BaseAdapter);
+		bids = [];
+
+		const result = getPrebidBestPrice('bidderA');
+
+		expect(result).to.deep.equal({ [bidderName]: '' });
+	});
+
+	it('should round to 2 decimal places', () => {
+		adapters.set(bidderName, { bidderName } as BaseAdapter);
+		const bid = PrebidBidFactory.getBid({ bidderCode: bidderName, cpm: 1 });
+		bids = [bid];
+
+		const result = getPrebidBestPrice(bidderName);
+
+		expect(result).to.deep.equal({ [bidderName]: '1.00' });
+	});
+
+	it('should not take rendered bids into consideration', () => {
+		adapters.set(bidderName, { bidderName } as BaseAdapter);
+		const bid = PrebidBidFactory.getBid({ bidderCode: bidderName, status: 'rendered' });
+		bids = [bid];
+
+		const result = getPrebidBestPrice(bidderName);
+
+		expect(result).to.deep.equal({ [bidderName]: '' });
+	});
+
+	it('should select highest price', () => {
+		adapters.set(bidderName, { bidderName } as BaseAdapter);
+		bids = [
+			PrebidBidFactory.getBid({ bidderCode: bidderName, cpm: 1 }),
+			PrebidBidFactory.getBid({ bidderCode: bidderName, cpm: 0.5 }),
+			PrebidBidFactory.getBid({ bidderCode: bidderName, cpm: 20 }),
+			PrebidBidFactory.getBid({ bidderCode: bidderName, cpm: 8 }),
+		];
+
+		const result = getPrebidBestPrice(bidderName);
+
+		expect(result).to.deep.equal({ [bidderName]: '20.00' });
+	});
+
+	it('should round cpm', () => {
+		adapters.set(bidderName, { bidderName } as BaseAdapter);
+		bids = [PrebidBidFactory.getBid({ bidderCode: bidderName, cpm: 0.03 })];
+
+		let result = getPrebidBestPrice(bidderName);
+
+		expect(result).to.deep.equal({ [bidderName]: '0.01' });
+
+		bids = [PrebidBidFactory.getBid({ bidderCode: bidderName, cpm: 19.99 })];
+
+		result = getPrebidBestPrice(bidderName);
+
+		expect(result).to.deep.equal({ [bidderName]: '19.50' });
 	});
 });
