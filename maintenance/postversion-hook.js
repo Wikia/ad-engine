6c0dcb55b856cd17929a43a273cd958b0c0f07e5
Damian Bielecki <bielik20@gmail.com>
2019-07-10 15:24:27 +0200

    remove failed message

--- maintenance/postversion-hook.js
+++ maintenance/postversion-hook.js
@@ -2,9 +2,7 @@ const { IncomingWebhook } = require('@slack/client');
 const { version } = require('../package.json');
 const { version: previousVersion } = require('./previous-package.json');
 
-const failed = process.argv[2] === 'failure';
 const repositoryName = 'ad-engine';
-const buildUrl = process.env.BUILD_URL;
 const webhookUrl = process.env.SLACK_INCOMING_WEBHOOK;
 
 if (!webhookUrl) {
@@ -19,46 +17,42 @@ const jenkinsUrl = 'http://jenkins.wikia-prod:8080';
 let text;
 let attachments = [];
 
-if (failed) {
-	text = `:x: <!here|here>: Something went wrong during *${repositoryName}* release (<${buildUrl}|show log>)`;
-} else {
-	const releaseLink = `<https://github.com/Wikia/${repositoryName}/releases/tag/v${version}|Release v${version}>`;
-	let diffLink = '';
+const releaseLink = `<https://github.com/Wikia/${repositoryName}/releases/tag/v${version}|Release v${version}>`;
+let diffLink = '';
 
-	if (previousVersion) {
-		diffLink = `(<https://github.com/Wikia/${repositoryName}/compare/v${previousVersion}...v${version}|show diff>)`;
-	}
-
-	text = `New version of *${repositoryName}* has been released: ${releaseLink} ${diffLink}`;
-	attachments = [
-		{
-			color: '#00acac',
-			text: 'Install new version in:',
-			actions: [
-				{
-					type: 'button',
-					text: 'app',
-					url: `${jenkinsUrl}/job/update_dependencies_app/parambuild/?adengine_version=v${version}`,
-				},
-				{
-					type: 'button',
-					text: 'f2',
-					url: `${jenkinsUrl}/job/update_dependencies_f2/parambuild/?adengine_version=v${version}`,
-				},
-				{
-					type: 'button',
-					text: 'mobile-wiki',
-					url: `${jenkinsUrl}/job/update_dependencies_mobilewiki/parambuild/?adengine_version=v${version}`,
-				},
-				{
-					type: 'button',
-					text: 'gamepedia-ads',
-					url: `${jenkinsUrl}/job/update_dependencies_gamepedia_ads/parambuild/?adengine_version=v${version}`,
-				},
-			],
-		},
-	];
+if (previousVersion) {
+	diffLink = `(<https://github.com/Wikia/${repositoryName}/compare/v${previousVersion}...v${version}|show diff>)`;
 }
+
+text = `New version of *${repositoryName}* has been released: ${releaseLink} ${diffLink}`;
+attachments = [
+	{
+		color: '#00acac',
+		text: 'Install new version in:',
+		actions: [
+			{
+				type: 'button',
+				text: 'app',
+				url: `${jenkinsUrl}/job/update_dependencies_app/parambuild/?adengine_version=v${version}`,
+			},
+			{
+				type: 'button',
+				text: 'f2',
+				url: `${jenkinsUrl}/job/update_dependencies_f2/parambuild/?adengine_version=v${version}`,
+			},
+			{
+				type: 'button',
+				text: 'mobile-wiki',
+				url: `${jenkinsUrl}/job/update_dependencies_mobilewiki/parambuild/?adengine_version=v${version}`,
+			},
+			{
+				type: 'button',
+				text: 'gamepedia-ads',
+				url: `${jenkinsUrl}/job/update_dependencies_gamepedia_ads/parambuild/?adengine_version=v${version}`,
+			},
+		],
+	},
+];
 
 webhook.send(
 	{
