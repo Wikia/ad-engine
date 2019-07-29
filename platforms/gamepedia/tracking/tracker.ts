dec8cb3666eddb1af674d535914de945f1b66711
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-07-29 09:26:15 +0200

    ADEN-8839 Move postmessage-tracker.ts content to tracker.ts.

--- platforms/gamepedia/tracking/tracker.ts
+++ platforms/gamepedia/tracking/tracker.ts
@@ -3,10 +3,14 @@ import {
 	eventService,
 	playerEvents,
 	porvataTracker,
+	PostmessageTracker,
 	slotBiddersTrackingMiddleware,
 	slotPropertiesTrackingMiddleware,
 	slotTracker,
 	slotTrackingMiddleware,
+	TrackingMessage,
+	trackingPayloadValidationMiddleware,
+	TrackingTarget,
 	viewabilityPropertiesTrackingMiddleware,
 	viewabilityTracker,
 	viewabilityTrackingMiddleware,
@@ -51,3 +55,24 @@ export const registerViewabilityTracker = () => {
 			dataWarehouseTracker.track(data, viewabilityUrl);
 		});
 };
+
+export function registerPostmessageTrackingTracker(): void {
+	const postmessageTracker = new PostmessageTracker(['payload', 'target']);
+
+	postmessageTracker
+		.add(trackingPayloadValidationMiddleware)
+		.register<TrackingMessage>((message) => {
+			const { target, payload } = message;
+
+			switch (target) {
+				case TrackingTarget.GoogleAnalytics:
+					break; // There is no tracking to Google Analytics yet
+				case TrackingTarget.DataWarehouse:
+					const dataWarehouseTracker = new DataWarehouseTracker();
+					dataWarehouseTracker.track(payload);
+					break;
+				default:
+					break;
+			}
+		});
+}
