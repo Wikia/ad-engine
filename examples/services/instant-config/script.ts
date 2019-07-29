0442346dd5fa356f4664d097233a546be229ca91
Damian Bielecki <bielik20@gmail.com>
2019-07-29 15:34:05 +0200

    fix example (part 1)

--- examples/services/instant-config/script.ts
+++ examples/services/instant-config/script.ts
@@ -1,6 +1,5 @@
 import { context, InstantConfigService, utils } from '@wikia/ad-engine';
 import { set } from 'lodash';
-import { instantConfigLoader } from '../../../src/ad-services/instant-config/instant-config.loader';
 import adContext from '../../context';
 
 const breakConfigButton = document.getElementById('break-config');
@@ -10,22 +9,18 @@ const testParamsButton = document.getElementById('test-params');
 context.extend(adContext);
 
 set(window, 'fallbackConfig', {
-	thisIsValueFromFallbackConfig: true,
+	thisIsValueFromFallbackConfig: [{ regions: ['XX'] }],
+	wgAdDriverThisIsValueFromFallbackConfig: true,
 });
 
 if (utils.queryString.isUrlParamSet('break-config')) {
 	context.set('services.instantConfig.endpoint', '//example.com');
 }
 
-instantConfigLoader.getConfig().then((config) => {
-	configPlaceholder.innerText = JSON.stringify(config, null, '\t');
-	set(window, 'exposedInstantConfig', config);
+InstantConfigService.init().then((config) => {
+	configPlaceholder.innerText = JSON.stringify(config['repository'], null, '\t');
+	set(window, 'exposedInstantConfig', config['repository']);
 });
-
-// InstantConfigService.init().then((config) => {
-// 	configPlaceholder.innerText = JSON.stringify(config['repository'], null, '\t');
-// 	set(window, 'exposedInstantConfig', config['repository']);
-// });
 
 breakConfigButton.addEventListener('click', () => {
 	window.location.href = `?break-config=1`;
@@ -33,12 +28,12 @@ breakConfigButton.addEventListener('click', () => {
 
 testParamsButton.addEventListener('click', () => {
 	const testParams = [
-		'InstantGlobals.boolean=false',
-		'InstantGlobals.string=exampleString',
-		'InstantGlobals.strings=["strings","in","the","array"]',
-		'InstantGlobals.number=42',
-		'InstantGlobals.numbers=[4, 8, 15, 16, 23, 42]',
-		'InstantGlobals.object={"this":{"is":{"more":{"complex":"json","is it?": 1}}}}',
+		'InstantGlobals.wgAdDriverBoolean=false',
+		'InstantGlobals.wgAdDriverString=exampleString',
+		'InstantGlobals.wgAdDriverStrings=["strings","in","the","array"]',
+		'InstantGlobals.wgAdDriverNumber=42',
+		'InstantGlobals.wgAdDriverNumbers=[4, 8, 15, 16, 23, 42]',
+		'InstantGlobals.wgAdDriverObject={"this":{"is":{"more":{"complex":"json","is it?": 1}}}}',
 	].join('&');
 
 	window.location.href = `?${testParams}`;
