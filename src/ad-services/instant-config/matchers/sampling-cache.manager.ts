f60d86023c07e509578f9696013d55b3781cea5f
Damian Bielecki <bielik20@gmail.com>
2019-08-01 12:44:11 +0200

    add sampling

--- src/ad-services/instant-config/matchers/sampling-cache.manager.ts
+++ src/ad-services/instant-config/matchers/sampling-cache.manager.ts
@@ -1,18 +1,58 @@
+import { sessionCookie, UniversalStorage } from '@ad-engine/core';
 import { InstantConfigSamplingCache } from '../instant-config.models';
 
+interface CacheDictionary {
+	[key: string]: CacheData;
+}
+
+interface CacheData {
+	name: string;
+	group: 'A' | 'B';
+	limit: number;
+	result: boolean;
+	withCookie: boolean;
+}
+
 export class SamplingCacheManager {
-	apply(id: string, cache: InstantConfigSamplingCache, predicate: () => boolean): boolean {
-		// if is in the cookie
-		// // return value from cookie
+	private readonly cookieStorage = new UniversalStorage(sessionCookie);
+	private readonly cacheStorage: CacheDictionary;
+	private readonly precision = 10 ** 6;
+
+	constructor() {
+		this.cacheStorage = this.cookieStorage.getItem('basset') || {};
+	}
+
+	apply(id: string, samplingCache: InstantConfigSamplingCache, predicate: () => boolean): boolean {
+		if (typeof this.cacheStorage[id] !== 'undefined') {
+			return this.cacheStorage[id].result;
+		}
 
 		const value = predicate();
 
-		// if sampling
-		// // sample
+		if (typeof samplingCache.sampling !== 'number' || value === false) {
+			return value;
+		}
 
-		// if cache
-		// // cache
+		const samplingResult = this.getSamplingResult(samplingCache.sampling);
 
-		return value;
+		const cacheData: CacheData = {
+			name: id,
+			result: samplingResult,
+			withCookie: samplingCache.samplingCache,
+			group: samplingResult ? 'B' : 'A',
+			limit: samplingResult ? samplingCache.sampling : 100 - samplingCache.sampling,
+		};
+
+		// cache
+		//
+
+		return samplingResult;
+	}
+
+	private getSamplingResult(sampling: number): boolean {
+		const randomValue: number = Math.round(Math.random() * 100 * this.precision);
+		const samplingValue: number = Math.round(sampling * this.precision);
+
+		return samplingValue > randomValue;
 	}
 }
