e86a0d0b3bc2fffa695e677bcfb6d91228c6ff26
Katarzyna Basiukajc <kbasiukajc@wikia-inc.com>
2019-08-06 09:15:01 +0200

    ADEN-8997 | Fix tests

--- spec/ad-engine/services/scroll-speed-calculator.spec.ts
+++ spec/ad-engine/services/scroll-speed-calculator.spec.ts
@@ -4,20 +4,20 @@ import { scrollSpeedCalculator } from '../../../src/ad-engine/services/scroll-sp
 describe('Scroll speed calculator', () => {
 	it('session scroll speed and page views are 0 at the beginning', () => {
 		expect(scrollSpeedCalculator.getAverageSessionScrollSpeed()).to.equal(0);
-		expect(scrollSpeedCalculator.getCurrentValidScrollSpeedPageViews()).to.equal(0);
+		expect(scrollSpeedCalculator.getScrollSpeedRecordsNumber()).to.equal(0);
 	});
 
 	it('scroll speed and page views are calculated properly', () => {
-		scrollSpeedCalculator.setAverageSessionScrollSpeed(325);
+		scrollSpeedCalculator.setAverageSessionScrollSpeed([300, 350]);
 		expect(scrollSpeedCalculator.getAverageSessionScrollSpeed()).to.equal(325);
-		expect(scrollSpeedCalculator.getCurrentValidScrollSpeedPageViews()).to.equal(1);
+		expect(scrollSpeedCalculator.getScrollSpeedRecordsNumber()).to.equal(1);
 
-		scrollSpeedCalculator.setAverageSessionScrollSpeed(0);
+		scrollSpeedCalculator.setAverageSessionScrollSpeed([0]);
 		expect(scrollSpeedCalculator.getAverageSessionScrollSpeed()).to.equal(163);
-		expect(scrollSpeedCalculator.getCurrentValidScrollSpeedPageViews()).to.equal(2);
+		expect(scrollSpeedCalculator.getScrollSpeedRecordsNumber()).to.equal(2);
 
-		scrollSpeedCalculator.setAverageSessionScrollSpeed(-13);
+		scrollSpeedCalculator.setAverageSessionScrollSpeed([-13]);
 		expect(scrollSpeedCalculator.getAverageSessionScrollSpeed()).to.equal(104);
-		expect(scrollSpeedCalculator.getCurrentValidScrollSpeedPageViews()).to.equal(3);
+		expect(scrollSpeedCalculator.getScrollSpeedRecordsNumber()).to.equal(3);
 	});
 });
