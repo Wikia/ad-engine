acc85e00a3a19d964a5c6ed53888d0b480fc5fa6
emilmadry <emadry@wikia-inc.com>
2019-08-08 14:18:51 +0200

    ADEN-8839 Test made independent

--- tests/specs/tracking/postmessage-tracker.desktop.test.js
+++ tests/specs/tracking/postmessage-tracker.desktop.test.js
@@ -4,28 +4,27 @@ import { timeouts } from '../../common/timeouts';
 describe('PostmessageTracker', () => {
 	const page = postmessageTrackerPage;
 
-	before(() => {
+	beforeEach(() => {
 		browser.url(page.pageLink);
-	});
-
-	it('should handle correct message (raw object)', () => {
 		browser.waitUntil(
 			() => page.getMessageCount() === '0',
 			timeouts.standard,
 			'Page set up incorrectly',
 		);
+	});
 
+	it('should handle correct message (raw object)', () => {
 		$(page.correctButtonSelector).click();
 		browser.waitUntil(() => page.getMessageCount() === '1', timeouts.standard);
 	});
 
 	it('should handle correct message (serialized object)', () => {
 		$(page.correctSerializedButtonSelector).click();
-		browser.waitUntil(() => page.getMessageCount() === '2', timeouts.standard);
+		browser.waitUntil(() => page.getMessageCount() === '1', timeouts.standard);
 	});
 
 	it('should not catch incorrect message', () => {
 		$(page.incorrectButtonSelector).click();
-		browser.waitUntil(() => page.getMessageCount() === '2', timeouts.standard);
+		browser.waitUntil(() => page.getMessageCount() === '0', timeouts.standard);
 	});
 });
