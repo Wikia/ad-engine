3a801e2b8c2555da4c6ae2517be78689a29da867
Damian Bielecki <bielik20@gmail.com>
2019-08-23 11:11:58 +0200

    Add targeting interface

--- platforms/mutfut/targeting.ts
+++ platforms/mutfut/targeting.ts
@@ -1,9 +1,9 @@
-import { Dictionary, utils } from '@wikia/ad-engine';
+import { Targeting, utils } from '@wikia/ad-engine';
 
-export function getPageLevelTargeting(): Dictionary<string> {
+export function getPageLevelTargeting(): Partial<Targeting> {
 	const domain = getDomain();
 	const cid = utils.queryString.get('cid');
-	const targeting: Dictionary<string> = {
+	const targeting: Partial<Targeting> = {
 		ae3: '1',
 		skin: 'turf',
 		uap: 'none',
