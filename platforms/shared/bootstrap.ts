0785a3012864cab8cca53b262eba36cfa17abe4e
Damian Sobczak <dsobczak@wikia-inc.com>
2019-08-28 11:59:04 +0200

    ADEN-9185 CR fixes

--- platforms/shared/bootstrap.ts
+++ platforms/shared/bootstrap.ts
@@ -2,9 +2,9 @@ import { utils } from '@wikia/ad-engine';
 import { cmpWrapper } from './cmp/cmp-wrapper';
 
 export async function bootstrapAndGetCmpConsent(): Promise<boolean> {
-	const geo = utils.geoService.setUpGeoData().country;
+	const countryCode = utils.geoService.setUpGeoData().country;
 
-	await cmpWrapper.init(geo);
+	await cmpWrapper.init(countryCode);
 
-	return cmpWrapper.getConsent(geo);
+	return cmpWrapper.getConsent();
 }
--- platforms/shared/cmp/cmp-wrapper.ts
+++ platforms/shared/cmp/cmp-wrapper.ts
@@ -1,18 +1,8 @@
 import { context, utils } from '@wikia/ad-engine';
-import * as Cookies from 'js-cookie';
 
-const CMP_LIBRARY_URL = '//cdm.cursecdn.com/js/cmp/tracking-opt-in.min.js';
+const trackingOptInLibraryUrl = '//cdm.cursecdn.com/js/cmp/tracking-opt-in.min.js';
 const optOutConsentString = 'BOl8T5MOl8T5MCNACAENCiAAAAAp6A';
 const logGroup = 'cmp-wrapper';
-
-/**
- * Constants for controlling the display of the CMP modal/banner
- */
-export enum CmpPolicy {
-	always,
-	never,
-	detect,
-}
 
 /**
  * Wraps all functionality for the consent management system
@@ -20,19 +10,14 @@ export enum CmpPolicy {
 class CmpWrapper {
 	cmpReady = false;
 	cmpModal: any;
+	cmpStored: WindowCMP = null;
 	gdprConsent = false;
-	gdprConsentPolicy: CmpPolicy = CmpPolicy.detect;
 
 	/**
 	 * Initialize the CMP system
 	 * Returns a Promise fulfilled when the CMP library is ready for use
 	 */
 	init(country: string): Promise<void> {
-		const consentRequired = this.geoRequiresConsent(country);
-
-		context.set('custom.isCMPEnabled', consentRequired);
-		context.set('options.geoRequiresConsent', consentRequired);
-
 		return new Promise<void>((resolve, reject) => {
 			// In case it fails to load, we'll resolve after 2s
 			setTimeout(() => {
@@ -40,8 +25,8 @@ class CmpWrapper {
 				resolve();
 			}, 2000);
 
-			utils.scriptLoader.loadScript(CMP_LIBRARY_URL).then(() => {
-				utils.logger(logGroup, 'CMP Loaded');
+			utils.scriptLoader.loadScript(trackingOptInLibraryUrl).then(() => {
+				utils.logger(logGroup, 'Modal library loaded');
 
 				this.cmpReady = true;
 				this.cmpModal = window.trackingOptIn.default({
@@ -49,13 +34,20 @@ class CmpWrapper {
 					onAcceptTracking: () => {
 						utils.logger(logGroup, 'GDPR Consent');
 						this.gdprConsent = true;
+						this.restoreCmp();
 					},
 					onRejectTracking: () => {
 						utils.logger(logGroup, 'GDPR Non-consent');
 						this.gdprConsent = false;
+						this.restoreCmp();
 					},
 					zIndex: 9999999,
 				});
+
+				const consentRequired = this.cmpModal.geoRequiresTrackingConsent();
+
+				context.set('custom.isCMPEnabled', consentRequired);
+				context.set('options.geoRequiresConsent', consentRequired);
 
 				resolve();
 			});
@@ -66,10 +58,8 @@ class CmpWrapper {
 	 * Returns a promise fulfilled when either the geo does not require consent, CMP is disabled,
 	 * or the geo requires consent that has either been previously set in a cookie or via interaction
 	 * with the CMP UI.
-	 *
-	 * @param geo The geo to check for GDPR consent
 	 */
-	getConsent(geo: string): Promise<boolean> {
+	getConsent(): Promise<boolean> {
 		return new Promise<boolean>((resolve, reject) => {
 			// Nothing is needed if cookies are disabled
 			if (!window.navigator.cookieEnabled) {
@@ -79,18 +69,14 @@ class CmpWrapper {
 			}
 
 			// Nothing is needed if the geo does not require consent
-			if (!this.geoRequiresConsent(geo)) {
+			if (!this.cmpModal.geoRequiresTrackingConsent()) {
 				this.gdprConsent = true;
 				resolve(true);
 				return;
 			}
 
-			if (!Cookies.get('euconsent')) {
-				utils.logger(logGroup, 'No consent data found. Showing consent tool');
-				window.__cmp('showConsentTool');
-
+			if (this.cmpModal.hasUserConsented() === undefined) {
 				this.overwriteCmp();
-
 				resolve(false);
 				return;
 			}
@@ -100,134 +86,31 @@ class CmpWrapper {
 				return;
 			}
 
-			// Populate hidden div read by Prebid
-			window.__cmp('getConsentData', null, (result: any) => {
-				utils.logger(logGroup, 'Populating Prebid consent data');
+			this.gdprConsent = this.cmpModal.hasUserConsented();
+			utils.logger(logGroup, `User consent: ${this.gdprConsent}`);
 
-				const body = document.getElementsByTagName('body')[0];
-				const consentDataDiv = document.createElement('div');
-
-				consentDataDiv.id = 'iab';
-				consentDataDiv.innerText = JSON.stringify(result, null, 2);
-				consentDataDiv.style['display'] = 'none';
-
-				body.appendChild(consentDataDiv);
-				utils.logger(logGroup, consentDataDiv.innerText);
-			});
-
-			const maxPurposes = 5;
-
-			// Get fine-grained information about the consent details for calculation of a policy to send DFP
-			window.__cmp('getVendorConsents', null, (result: any) => {
-				const stringifiedResult = JSON.stringify(result, null, 2);
-				utils.logger(logGroup, `getVendorConsents callback result:\n${stringifiedResult}`);
-
-				// Should not happen
-				if (!result || !result.purposeConsents) {
-					this.gdprConsent = false;
-					return;
-				}
-
-				// Consider global consent to be consenting to all purposes
-				let consentFlag = true;
-
-				for (let i = 0; i < maxPurposes; i++) {
-					// Variable purposeConsents is not an array, but an object with properties named '1'-'5'
-					if (!result.purposeConsents[String(i + 1)]) {
-						consentFlag = false;
-						break;
-					}
-				}
-
-				this.gdprConsent = consentFlag;
-
-				utils.logger(logGroup, `Consent from vendorConsent purposes ${this.gdprConsent}`);
-
-				resolve(this.gdprConsent);
-				return;
-			});
+			resolve(this.gdprConsent);
+			return;
 		});
 	}
 
 	/**
-	 * Returns whether a given geo requires GDPR consent
-	 *
-	 * @param geo The geo to check for GDPR consent
+	 * Restore original CMP version
 	 */
-	private geoRequiresConsent(geo: string): boolean {
-		const upperGeo = geo.toUpperCase();
-		const euGeos: string[] = [
-			'AT',
-			'BE',
-			'BG',
-			'CY',
-			'CZ',
-			'DE',
-			'DK',
-			'EE',
-			'ES',
-			'FI',
-			'FR',
-			'GR',
-			'HR',
-			'HU',
-			'IE',
-			'IT',
-			'LT',
-			'LU',
-			'LV',
-			'MT',
-			'NL',
-			'PL',
-			'PT',
-			'RO',
-			'SE',
-			'SI',
-			'SK',
-			'GB', // for now
-			'UK', // for now
-		];
-		const eeaGeos: string[] = ['CH', 'IS', 'LI', 'NO'];
-
-		// Check debugging / feature flags
-		if (this.gdprConsentPolicy === CmpPolicy.never) {
-			utils.logger(logGroup, 'GDPR Consent disabled');
-			this.gdprConsent = true;
-			return false;
+	private restoreCmp(): void {
+		if (this.cmpStored) {
+			utils.logger(logGroup, 'Restoring original CMP module');
+			window.__cmp = this.cmpStored;
 		}
-
-		if (this.gdprConsentPolicy === CmpPolicy.always) {
-			utils.logger(logGroup, 'GDPR Consent explicity required');
-			return true;
-		}
-
-		const consentRequired =
-			geo === 'undefined' || euGeos.indexOf(upperGeo) > -1 || eeaGeos.indexOf(upperGeo) > -1;
-
-		if (consentRequired) {
-			utils.logger(logGroup, `GDPR Consent required for ${geo}`);
-			return true;
-		}
-
-		utils.logger(logGroup, `GDPR Consent not required`);
-		this.gdprConsent = true;
-
-		return false;
 	}
 
 	/**
 	 * Store and replace original CMP with temporary opting-out version
 	 */
 	private overwriteCmp(): void {
-		let cmpStored: WindowCMP;
-
-		window.__cmp('getConsentData', null, () => {
-			utils.logger(logGroup, 'Restoring original CMP module');
-			window.__cmp = cmpStored;
-		});
-
 		utils.logger(logGroup, 'Overwriting original CMP module');
-		cmpStored = window.__cmp;
+		this.cmpStored = window.__cmp;
+
 		// @ts-ignore
 		window.__cmp = (method: string, data: any, callback: any) => {
 			const consentData = {
@@ -258,7 +141,7 @@ class CmpWrapper {
 
 			callback(consentData);
 		};
-		window.__cmp.receiveMessage = cmpStored.receiveMessage;
+		window.__cmp.receiveMessage = this.cmpStored.receiveMessage;
 	}
 }
 
