eac63262cfe341e95cca6b11aef73cf0ee5fbdc8
Damian Bielecki <bielik20@gmail.com>
2019-09-05 15:04:35 +0200

    use setupBidders

--- platforms/shared/bidders/setup-bidders.ts
+++ platforms/shared/bidders/setup-bidders.ts
@@ -10,6 +10,10 @@ export function setupBidders(context: Context, instantConfig: InstantConfigServi
 	context.set('bidders.a9.enabled', instantConfig.get('icA9Bidder'));
 	context.set('bidders.a9.dealsEnabled', instantConfig.get('icA9Deals'));
 	context.set('bidders.a9.videoEnabled', instantConfig.get('icA9VideoBidder') && hasFeaturedVideo);
+	context.set(
+		'bidders.a9.bidsRefreshing.enabled',
+		instantConfig.get('icA9BidRefreshing') && context.get('options.slotRepeater'),
+	);
 
 	if (instantConfig.get('icPrebid')) {
 		context.set('bidders.prebid.enabled', true);
