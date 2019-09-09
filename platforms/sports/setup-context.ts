d8e2803ef34993f194966b1eb82fee26bb3b9819
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-09-09 10:20:58 +0200

    ADEN-9289 Leave uap restriction on gp only

--- platforms/sports/setup-context.ts
+++ platforms/sports/setup-context.ts
@@ -90,7 +90,7 @@ class ContextSetup {
 
 		this.injectIncontentPlayer();
 
-		uapHelper.configureUap(this.instantConfig);
+		uapHelper.configureUap();
 		slotsContext.setupStates();
 
 		this.updateWadContext();
