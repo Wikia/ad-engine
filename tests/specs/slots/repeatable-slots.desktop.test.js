51594bb1dc20768d96da7ac3cccf7a31f7ceae56
emilmadry <emadry@wikia-inc.com>
2019-10-17 17:58:14 +0200

    Repeatable slots refactored

--- tests/specs/slots/repeatable-slots.desktop.test.js
+++ tests/specs/slots/repeatable-slots.desktop.test.js
@@ -1,49 +1,47 @@
 import { expect } from 'chai';
 import { repeatableSlots } from '../../pages/repeatable-slots.page';
-import { timeouts } from '../../common/timeouts';
 import { helpers } from '../../common/helpers';
 import { queryStrings } from '../../common/query-strings';
 import { adSlots } from '../../common/ad-slots';
 import { slots } from '../../common/slot-registry';
 
 describe('Repeatable slots ads', () => {
-	let adStatus;
-
 	before(() => {
 		helpers.navigateToUrl(repeatableSlots.pageLink);
-		adStatus = slots.repeatableBoxad1.status;
 	});
 
 	beforeEach(() => {
-		$(repeatableSlots.getRepeatableSlotSelector(1)).waitForDisplayed(timeouts.standard);
+		slots.repeatableBoxad1.waitForDisplayed();
 	});
 
-	it('Check if first boxad is visible', () => {
-		expect(adStatus.inViewport, 'Not in viewport').to.be.true;
+	it('Check if first repeatable slot is visible', () => {
+		expect(slots.repeatableBoxad1.isDisplayedInViewport(), 'Not in viewport').to.be.true;
 	});
 
 	it('Check if last slot is visible with a limit to 4', () => {
 		const numberOfSlots = 4;
-		const lengthOfContent = 5;
+		const lengthOfContent = 10;
+
+		const expectedSlots = [
+			slots.repeatableBoxad1,
+			slots.repeatableBoxad2,
+			slots.repeatableBoxad3,
+			slots.repeatableBoxad4,
+		];
 
 		helpers.navigateToUrl(
 			repeatableSlots.pageLink,
-			queryStrings.getLimitOfSlots(numberOfSlots),
 			queryStrings.getLengthOfContent(lengthOfContent),
+			queryStrings.getLimitOfSlots(numberOfSlots),
 		);
-		$(repeatableSlots.getRepeatableSlotSelector(1)).waitForDisplayed(timeouts.standard);
-		for (let i = 1; i < numberOfSlots; i += 1) {
-			repeatableSlots.scrollBetweenBoxads(repeatableSlots.getRepeatableSlotSelector(i));
-			expect(
-				$(repeatableSlots.getRepeatableSlotSelector(i + 1)).isDisplayed(),
-				`Slot number ${i + 1} is not visible`,
-			).to.be.true;
-		}
-		repeatableSlots.scrollBetweenBoxads(repeatableSlots.getRepeatableSlotSelector(numberOfSlots));
-		expect(
-			$(repeatableSlots.getRepeatableSlotSelector(numberOfSlots + 1)).isDisplayed(),
-			'Slot not visible',
-		).to.be.false;
+
+		expectedSlots.forEach((slot) => {
+			slot.scrollIntoView();
+			slot.waitForDisplayed();
+			helpers.mediumScroll(2250); //move to variable
+		});
+
+		expect(slots.repeatableBoxad5.isDisplayed(), '9th slot is visible').to.be.false;
 	});
 
 	it('Check if 8th boxad is visible', () => {
