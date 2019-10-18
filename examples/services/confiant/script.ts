9a9a882cebeacad58987d472f7973ee06271fbc1
Tomasz Felczak <tfelczak@wikia-inc.com>
2019-10-18 08:45:15 +0200

    ADEN-9450 import fix in confiant example page

--- examples/services/confiant/script.ts
+++ examples/services/confiant/script.ts
@@ -1,11 +1,10 @@
 import { AdEngine, confiant, context, utils } from '@wikia/ad-engine';
-import adContext  from '../../context';
-import customContext  from '../../context';
+import customContext from '../../context';
 
 const isConfiantEnabled = utils.queryString.get('confiant-disabled') !== '1';
 
 customContext.targeting.artid = '553';
-context.extend(adContext);
+context.extend(customContext);
 context.set('services.confiant.enabled', isConfiantEnabled);
 
 confiant.call();
