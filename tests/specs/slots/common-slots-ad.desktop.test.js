f6030a3dcc360be51093a85a43924244008ddfb5
emilmadry <emadry@wikia-inc.com>
2019-10-24 11:14:41 +0200

    Common slots refactored

--- tests/specs/slots/common-slots-ad.desktop.test.js
+++ tests/specs/slots/common-slots-ad.desktop.test.js
@@ -1,51 +1,30 @@
 import { expect } from 'chai';
 import { commonAds } from '../../pages/common-ad.page';
-import { adSlots } from '../../common/ad-slots';
 import { slots } from '../../common/slot-registry';
 import { timeouts } from '../../common/timeouts';
 import { helpers } from '../../common/helpers';
 
 describe('Common slots: top leaderboard', () => {
-	let adStatus;
-
 	before(() => {
 		helpers.navigateToUrl(commonAds.pageLink);
 		slots.topLeaderboard.waitForDisplayed();
-		adStatus = slots.topLeaderboard.status;
 	});
 
 	it('Check if dimensions are correct', () => {
-		const dimensions = adSlots.checkSlotSize(
-			adSlots.topLeaderboard,
-			adSlots.leaderboardWidth,
-			adSlots.leaderboardHeight,
-		);
-
-		expect(dimensions.status, dimensions.capturedErrors).to.be.true;
+		expect(slots.topLeaderboard.size.height).to.equal(commonAds.topLeaderboardData.height);
+		expect(slots.topLeaderboard.size.width).to.equal(commonAds.topLeaderboardData.width);
 	});
 
 	it('Check if slot is visible in viewport', () => {
-		expect(adStatus.inViewport, 'Not in viewport').to.be.true;
-	});
-
-	it('Check if slot has default classes', () => {
-		const classAssertions = adSlots.checkSlotClasses(adSlots.topLeaderboard, [
-			'i-am-the-default-class-added-on-create',
-		]);
-
-		expect(classAssertions.status, classAssertions.capturedErrors).to.be.true;
+		expect(slots.topLeaderboard.isDisplayedInViewport(), 'Not in viewport').to.be.true;
 	});
 
 	it('Check if line item id is from the inhouse campaign', () => {
 		slots.topLeaderboard.waitForLineItemIdAttribute();
 		expect(slots.topLeaderboard.lineItemId).to.equal(
-			adSlots.inhouseLineItemId,
+			commonAds.topLeaderboardData.lineItemId,
 			'Line item ID mismatch',
 		);
-	});
-
-	it('Check if redirect on click works properly', () => {
-		expect(helpers.adRedirect(adSlots.topLeaderboard), 'Wrong link after redirect').to.be.true;
 	});
 });
 
@@ -53,16 +32,12 @@ describe('Common slots: top boxad', () => {
 	before(() => {
 		helpers.navigateToUrl(commonAds.pageLink);
 		slots.topBoxad.waitForDisplayed();
+		slots.topBoxad.scrollIntoView();
 	});
 
 	it('Check if dimensions are correct', () => {
-		const dimensions = adSlots.checkSlotSize(
-			adSlots.topBoxad,
-			adSlots.boxadWidth,
-			adSlots.boxadHeight,
-		);
-
-		expect(dimensions.status, dimensions.capturedErrors).to.be.true;
+		expect(slots.topBoxad.size.height).to.equal(commonAds.topBoxadData.height);
+		expect(slots.topBoxad.size.width).to.equal(commonAds.topBoxadData.width);
 	});
 
 	it('Check if slot is visible in viewport', () => {
@@ -71,62 +46,50 @@ describe('Common slots: top boxad', () => {
 
 	it('Check if line item id is from the inhouse campaign', () => {
 		slots.topBoxad.waitForLineItemIdAttribute();
-		expect(slots.topBoxad.lineItemId).to.equal(adSlots.inhouseLineItemId, 'Line item ID mismatch');
-	});
-
-	it('Check if redirect on click works', () => {
-		expect(helpers.adRedirect(adSlots.topBoxad), 'Wrong link after redirect').to.be.true;
+		expect(slots.topBoxad.lineItemId).to.equal(
+			commonAds.topBoxadData.lineItemId,
+			'Line item ID mismatch',
+		);
 	});
 });
 
 describe('Common slots: rail module', () => {
 	before(() => {
 		helpers.navigateToUrl(commonAds.pageLink);
-		helpers.slowScroll(150);
 		$(commonAds.railModule).waitForDisplayed(timeouts.standard);
+		$(commonAds.railModule).scrollIntoView();
 	});
 
 	it('Check if dimensions are correct', () => {
-		const dimensions = adSlots.checkSlotSize(
-			commonAds.railModule,
-			adSlots.railModuleWidth,
-			adSlots.railModuleHeight,
-		);
-
-		expect(dimensions.status, dimensions.capturedErrors).to.be.true;
+		expect($(commonAds.railModule).getSize('height')).to.equal(commonAds.railData.height);
+		expect($(commonAds.railModule).getSize('width')).to.equal(commonAds.railData.width);
 	});
 
-	it('Check if module is visible', () => {
-		expect($(commonAds.railModule).isDisplayedInViewport(), 'Not visible').to.be.true;
+	it('Check if slot is visible in viewport', () => {
+		expect($(commonAds.railModule).isDisplayedInViewport(), 'Not in viewport').to.be.true;
 	});
 });
 
 describe('Common slots: incontent boxad', () => {
 	before(() => {
 		helpers.navigateToUrl(commonAds.pageLink);
-		helpers.mediumScroll(500);
-		slots.incontentBoxad.waitForDisplayed();
 		slots.incontentBoxad.scrollIntoView();
+		slots.incontentBoxad.waitForDisplayed();
 	});
 
 	it('Check if dimensions are correct', () => {
-		const dimensions = adSlots.checkSlotSize(
-			adSlots.incontentBoxad,
-			adSlots.boxadWidth,
-			adSlots.boxadHeight,
-		);
-
-		expect(dimensions.status, dimensions.capturedErrors).to.be.true;
+		expect(slots.incontentBoxad.size.height).to.equal(commonAds.incontentBoxadData.height);
+		expect(slots.incontentBoxad.size.width).to.equal(commonAds.incontentBoxadData.width);
 	});
 
 	it('Check if slot is visible in viewport', () => {
-		expect(slots.topBoxad.isDisplayedInViewport(), 'Not in viewport').to.be.true;
+		expect(slots.incontentBoxad.isDisplayedInViewport(), 'Not in viewport').to.be.true;
 	});
 
 	it('Check if line item id is from the inhouse campaign', () => {
 		slots.incontentBoxad.waitForLineItemIdAttribute();
 		expect(slots.incontentBoxad.lineItemId).to.equal(
-			adSlots.inhouseLineItemId,
+			commonAds.incontentBoxadData.lineItemId,
 			'Line item ID mismatch',
 		);
 	});
@@ -137,31 +100,23 @@ describe('Common slots: bottom leaderboard', () => {
 		helpers.navigateToUrl(commonAds.pageLink);
 		slots.bottomLeaderboard.scrollIntoView();
 		slots.bottomLeaderboard.waitForDisplayed();
+		helpers.mediumScroll(100);
 	});
 
 	it('Check if dimensions are correct', () => {
-		const dimensions = adSlots.checkSlotSize(
-			adSlots.bottomLeaderboard,
-			adSlots.leaderboardWidth,
-			adSlots.leaderboardHeight,
-		);
-
-		expect(dimensions.status, dimensions.capturedErrors).to.be.true;
+		expect(slots.bottomLeaderboard.size.height).to.equal(commonAds.bottomLeaderboardData.height);
+		expect(slots.bottomLeaderboard.size.width).to.equal(commonAds.bottomLeaderboardData.width);
 	});
 
-	it('Check if slot is visible', () => {
-		expect(slots.bottomLeaderboard.isDisplayed(), 'Not displayed').to.be.true;
+	it('Check if slot is visible in viewport', () => {
+		expect(slots.bottomLeaderboard.isDisplayedInViewport(), 'Not in viewport').to.be.true;
 	});
 
 	it('Check if line item id is from the inhouse campaign', () => {
 		slots.bottomLeaderboard.waitForLineItemIdAttribute();
 		expect(slots.bottomLeaderboard.lineItemId).to.equal(
-			adSlots.inhouseLineItemId,
+			commonAds.bottomLeaderboardData.lineItemId,
 			'Line item ID mismatch',
 		);
-	});
-
-	it('Check if redirect on click works properly', () => {
-		expect(helpers.adRedirect(adSlots.bottomLeaderboard), 'Wrong link after redirect').to.be.true;
 	});
 });
