29cb0d1e13694dc626c36ca5acf5deaab8107650
emilmadry <emadry@wikia-inc.com>
2019-10-24 15:33:41 +0200

    Floating ad test refactored

--- tests/specs/templates/non-hivi/floating-ad.desktop.test.js
+++ tests/specs/templates/non-hivi/floating-ad.desktop.test.js
@@ -1,17 +1,19 @@
 import { expect } from 'chai';
 import { floatingAd } from '../../../pages/floating-ad.page';
-import { adSlots } from '../../../common/ad-slots';
 import { helpers } from '../../../common/helpers';
+import { slots } from '../../../common/slot-registry';
 
 describe('Floating ad page: incontent boxad', () => {
 	before(() => {
 		helpers.navigateToUrl(floatingAd.pageLink);
-		helpers.slowScroll(1000);
+		helpers.mediumScroll(1000);
 		slots.incontentBoxad.waitForDisplayed();
 	});
 
 	it('Check if slot scrolls with the page', () => {
-		helpers.slowScroll(2500);
-		expect($(`${adSlots.incontentBoxad}${floatingAd.pinnedTopClass}`).isExisting()).to.be.false;
+		helpers.mediumScroll(2500);
+		expect($(`${slots.incontentBoxad.selector}${floatingAd.pinnedTopClass}`).isExisting()).to.be
+			.false;
+		expect(slots.incontentBoxad.isDisplayedInViewport()).to.be.true;
 	});
 });
