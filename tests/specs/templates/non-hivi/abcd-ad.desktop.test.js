0736d270f0712992c5be9fe0cec6cc15403ede21
emilmadry <emadry@wikia-inc.com>
2019-10-24 14:58:42 +0200

    Abcd fixed

--- tests/specs/templates/non-hivi/abcd-ad.desktop.test.js
+++ tests/specs/templates/non-hivi/abcd-ad.desktop.test.js
@@ -1,33 +1,25 @@
 import { expect } from 'chai';
 import { abcdAd } from '../../../pages/abcd-ad.page';
-import { adSlots } from '../../../common/ad-slots';
 import { slots } from '../../../common/slot-registry';
 import { timeouts } from '../../../common/timeouts';
 import { helpers } from '../../../common/helpers';
 
 describe('ABCD ads page: top leaderboard', () => {
-	let adStatus;
-
 	before(() => {
 		helpers.navigateToUrl(abcdAd.pageLink);
 	});
 
 	beforeEach(() => {
 		slots.topLeaderboard.waitForDisplayed();
-		adStatus = slots.topLeaderboard.status;
+		helpers.waitForAnimations();
 	});
 
 	it('Check if slot is visible in viewport', () => {
-		expect(adStatus.inViewport, 'Not in viewport').to.be.true;
+		expect(slots.topLeaderboard.isDisplayedInViewport(), 'Not in viewport').to.be.true;
 	});
 
 	it('Check if dimensions are correct', () => {
-		const dimensions = adSlots.checkUAPSizeSlotRatio(
-			adSlots.topLeaderboard,
-			abcdAd.abcdLeaderboardRatio,
-		);
-
-		expect(dimensions.status, dimensions.capturedErrors).to.be.true;
+		expect(slots.topLeaderboard.calculateApectRatio()).to.equal(abcdAd.abcdLeaderboardRatio);
 	});
 
 	it('Check if line item id is from the proper campaign', () => {
@@ -61,7 +53,7 @@ describe('ABCD ads page: video player in leaderboard', () => {
 
 	it('Check if unmuting the video works properly', () => {
 		$(`${slots.topLeaderboard.selector} ${abcdAd.videoPlayer}`).moveTo();
-		browser.pause(timeouts.hover);
+		helpers.waitForAnimations();
 		$(abcdAd.unmuteButton).click();
 		$(`${abcdAd.unmuteButton}${abcdAd.buttonIsOnClass}`).waitForExist(timeouts.standard, true);
 	});
