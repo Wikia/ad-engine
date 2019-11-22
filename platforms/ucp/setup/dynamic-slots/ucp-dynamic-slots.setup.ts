1ad03a492cd733ef8edd83a3285d67846f303d68
Tomasz Felczak <tfelczak@wikia-inc.com>
2019-11-22 16:34:55 +0100

    ADEN-9633 injecting slot dynamically

--- platforms/ucp/setup/dynamic-slots/ucp-dynamic-slots.setup.ts
+++ platforms/ucp/setup/dynamic-slots/ucp-dynamic-slots.setup.ts
@@ -1,37 +1,19 @@
 import { DynamicSlotsSetup } from '@platforms/shared';
+import { context, insertNewSlot } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
 
 @Injectable()
 export class UcpDynamicSlotsSetup implements DynamicSlotsSetup {
 	configureDynamicSlots(): void {
-		this.injectTopLeaderboardPlaceholder();
-		this.injectTopBoxadPlaceholder();
-		this.injectBottomLeaderboardPlaceholder();
+		this.injectSlots();
 	}
 
-	private injectTopLeaderboardPlaceholder(): void {
-		const container = document.querySelector('.WikiaPage');
-		const topLeadeboardWrapper = document.createElement('div');
-		const parentElement = container.parentNode;
-
-		topLeadeboardWrapper.id = 'top_leaderboard';
-		parentElement.insertBefore(topLeadeboardWrapper, container);
-	}
-
-	private injectTopBoxadPlaceholder(): void {
-		const container = document.querySelector('.rcs-container');
-		const topBoxadWrapper = document.createElement('div');
-		const parentElement = container.parentNode;
-
-		topBoxadWrapper.id = 'top_boxad';
-		parentElement.insertBefore(topBoxadWrapper, container);
-	}
-
-	private injectBottomLeaderboardPlaceholder(): void {
-		const container = document.querySelector('.WikiaMainContentContainer');
-		const bottomLeadeboardWrapper = document.createElement('div');
-
-		bottomLeadeboardWrapper.id = 'bottom_leaderboard';
-		container.appendChild(bottomLeadeboardWrapper);
+	private injectSlots(): void {
+		const slots = context.get('slots');
+		Object.values(slots).forEach((slot) => {
+			if (slot.nextSibling) {
+				insertNewSlot(slot.adProduct, document.querySelector(slot.nextSibling), false);
+			}
+		});
 	}
 }
