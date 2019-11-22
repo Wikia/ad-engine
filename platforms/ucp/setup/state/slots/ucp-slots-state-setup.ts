833595958b813920319a69381378977afb465525
Tomasz Felczak <tfelczak@wikia-inc.com>
2019-11-22 09:57:18 +0100

    ADEN-9633 added top_boxad with styling

--- platforms/ucp/setup/state/slots/ucp-slots-state-setup.ts
+++ platforms/ucp/setup/state/slots/ucp-slots-state-setup.ts
@@ -6,7 +6,7 @@ export class UcpSlotsStateSetup implements SlotsStateSetup {
 	configureSlotsState(): void {
 		slotsContext.setState('hivi_leaderboard', true);
 		slotsContext.setState('top_leaderboard', true);
-		slotsContext.setState('top_boxad', false);
+		slotsContext.setState('top_boxad', true);
 		slotsContext.setState('incontent_boxad_1', false);
 		slotsContext.setState('bottom_leaderboard', false);
 		slotsContext.setState('incontent_player', false);
