18c151db87c0686158f03832fe2889b0c1d363ec
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2019-12-24 10:16:36 +0100

    ADEN-9699 JWPlayer ads on UCP

--- platforms/ucp/index.ts
+++ platforms/ucp/index.ts
@@ -1,12 +1,17 @@
 import { bootstrapAndGetConsent, PlatformStartup } from '@platforms/shared';
 import { context } from '@wikia/ad-engine';
 import { Container } from '@wikia/dependency-injection';
+import { Communicator, setupPostQuecast } from '@wikia/post-quecast';
 import { basicContext } from './ad-context';
 import { setupUcpIoc } from './setup-ucp-ioc';
 import './styles.scss';
 
+setupPostQuecast();
+
 window.RLQ = window.RLQ || [];
 window.RLQ.push(async () => {
+	const communicator = new Communicator();
+
 	context.extend(basicContext);
 
 	const [container]: [Container, ...any[]] = await Promise.all([
@@ -16,5 +21,8 @@ window.RLQ.push(async () => {
 	const platformStartup = container.get(PlatformStartup);
 
 	platformStartup.configure({ isMobile: false });
+
+	communicator.dispatch({ type: '[AdEngine] Configured' });
+
 	platformStartup.run();
 });
