6799322e53b553ec4da00dc2e277f26ac03d757d
Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>
2019-12-29 12:13:07 +0100

    ADEN-9559 Add ofState operator. Update navbar position when scrolling in impact state.
    Signed-off-by: Jacek Bronski-Jankowski <jjankowski@wikia-inc.com>

--- src/ad-products/templates/uap/themes/hivi/hivi-bfaa-2.ts
+++ src/ad-products/templates/uap/themes/hivi/hivi-bfaa-2.ts
@@ -4,7 +4,7 @@ import * as EventEmitter from 'eventemitter3';
 import { isUndefined, mapValues } from 'lodash';
 import { BehaviorSubject, fromEvent, Observable, Subject } from 'rxjs';
 import { filter, skip, switchMap, take, takeUntil, withLatestFrom } from 'rxjs/operators';
-import { FSM, ReduxExtensionConnector } from 'state-charts';
+import { FSM, ReduxExtensionConnector, State } from 'state-charts';
 import {
 	BigFancyAdAboveConfig,
 	PorvataPlayer,
@@ -28,8 +28,6 @@ import { BigFancyAdHiviTheme } from './hivi-theme';
 const HIVI_RESOLVED_THRESHOLD = 0.995;
 export const MOVE_NAVBAR = '[UAP HiVi BFAA] move navbar';
 export const SET_BODY_PADDING_TOP = '[UAP HiVi BFAA] set body padding top';
-
-type State = any;
 
 const STATES = {
 	IMPACT: 'impact',
@@ -89,6 +87,7 @@ const bfaaFsm = new FSM(
 new ReduxExtensionConnector(bfaaFsm, '[UAP BFAA] ');
 
 function createScrollObservable(): Observable<any> {
+	// TODO: Create observable monitoring subscriptions
 	return new Observable((observer) => {
 		const listenerId = scrollListener.addCallback(() => {
 			observer.next();
@@ -109,6 +108,11 @@ bfaaEmitter.on(FSM.events.leave, (state: State) => {
 	leaving$.next(state);
 });
 
+function ofState(stateName: string) {
+	// TODO: Add return type
+	return filter((state: State) => state.name === stateName);
+}
+
 export class BfaaHiviTheme2 extends BigFancyAdTheme {
 	protected platformConfig: BigFancyAdAboveConfig;
 	protected gamConfig: UapConfig;
@@ -122,112 +126,108 @@ export class BfaaHiviTheme2 extends BigFancyAdTheme {
 		this.gamConfig = params.config;
 
 		// ENTER - UI
-		bfaaEmitter.on(FSM.events.enter, (state: State) => {
-			if (state.name === STATES.INITIAL) {
-				this.startStickiness();
-			}
-			if (state.name === STATES.RESOLVED) {
-				slotTweaker.makeResponsive(this.adSlot, this.gamConfig.aspectRatio.resolved);
-				this.ui.switchImagesInAd(this.params, true);
-				this.adSlot.addClass(CSS_CLASSNAME_THEME_RESOLVED);
-
-				this.updateAdSizes();
-
-				this.setBodyPaddingTop(`${100 / this.currentAspectRatio}%`);
-				this.moveNavbar(0, 0);
-			}
-
-			if (state.name === STATES.IMPACT) {
-				this.adSlot.addClass(CSS_CLASSNAME_IMPACT_BFAA);
-				slotTweaker.makeResponsive(this.adSlot, this.gamConfig.aspectRatio.default);
-				this.ui.switchImagesInAd(this.params, false);
-
-				this.updateAdSizes();
-				// TODO: Update body padding
-
-				createScrollObservable()
-					.pipe(takeUntil(leaving$))
-					.subscribe(() => {
-						// TODO: Update body padding
-						this.updateAdSizes();
-					});
-			}
-			if (state.name === STATES.STICKY) {
-				this.adSlot.addClass(CSS_CLASSNAME_STICKY_BFAA);
-				this.stickNavbar();
-			}
+		entering$.pipe(ofState(STATES.INITIAL)).subscribe(() => {
+			this.startStickiness();
 		});
 
-		// LEAVE - UI
-		bfaaEmitter.on(FSM.events.leave, (state: State) => {
-			if (state.name === STATES.RESOLVED) {
-				this.adSlot.removeClass(CSS_CLASSNAME_THEME_RESOLVED);
-			}
+		entering$.pipe(ofState(STATES.RESOLVED)).subscribe(() => {
+			slotTweaker.makeResponsive(this.adSlot, this.gamConfig.aspectRatio.resolved);
+			this.ui.switchImagesInAd(this.params, true);
+			this.adSlot.addClass(CSS_CLASSNAME_THEME_RESOLVED);
 
-			if (state.name === STATES.STICKY) {
-				this.adSlot.removeClass(CSS_CLASSNAME_STICKY_BFAA);
-			}
+			this.updateAdSizes();
 
-			if (state.name === STATES.IMPACT) {
-				this.adSlot.removeClass(CSS_CLASSNAME_IMPACT_BFAA);
-			}
+			this.setBodyPaddingTop(`${100 / this.currentAspectRatio}%`);
+			this.moveNavbar(0, 0);
 		});
 
-		// ENTER - STATE
-		bfaaEmitter.on(FSM.events.enter, (state: State) => {
-			if (state.name === STATES.RESOLVED) {
-				// Stick on scroll only if not viewable and not timeout
-				this.viewableAndTimeoutRunning$
-					.pipe(
-						filter((running) => !!running),
-						switchMap(() => {
-							return createScrollObservable().pipe(
-								takeUntil(this.viewableAndTimeoutRunning$.pipe(filter((running) => !running))),
-								takeUntil(leaving$),
-							);
-						}),
-					)
-					.subscribe(() => bfaaFsm.dispatch(ACTIONS.STICK));
-			}
+		entering$.pipe(ofState(STATES.IMPACT)).subscribe(() => {
+			this.adSlot.addClass(CSS_CLASSNAME_IMPACT_BFAA);
+			slotTweaker.makeResponsive(this.adSlot, this.gamConfig.aspectRatio.default);
+			this.ui.switchImagesInAd(this.params, false);
 
-			if (state.name === STATES.STICKY) {
-				// Unstick on scroll only if viewable and timeout
-				this.viewableAndTimeoutRunning$
-					.pipe(
-						filter((running) => !running),
-						takeUntil(leaving$),
-						switchMap(() => createScrollObservable()),
-						take(1),
-					)
-					.subscribe(() => bfaaFsm.dispatch(ACTIONS.RESOLVE));
-			}
+			this.updateAdSizes().then((element) => {
+				this.moveNavbar(element.offsetHeight);
+			});
+			// TODO: Update body padding
 
-			if (state.name === STATES.IMPACT) {
-				// When smaller than HIVI_RESOLVED_THRESHOLD
-				createScrollObservable()
-					.pipe(
-						takeUntil(leaving$),
-						filter(() => this.currentState >= HIVI_RESOLVED_THRESHOLD),
-						take(1),
-						withLatestFrom(this.viewableAndTimeoutRunning$),
-					)
-					.subscribe(([_, running]) => {
-						if (running) {
-							// if not timeout and not viewable
-							bfaaFsm.dispatch(ACTIONS.STICK);
-						} else {
-							// if timeout and viewable
-							bfaaFsm.dispatch(ACTIONS.RESOLVE);
-						}
+			createScrollObservable()
+				.pipe(takeUntil(leaving$))
+				.subscribe(() => {
+					// TODO: Update body padding
+					this.updateAdSizes().then((element) => {
+						this.moveNavbar(element.offsetHeight);
 					});
-			}
+				});
+		});
+
+		entering$.pipe(ofState(STATES.STICKY)).subscribe(() => {
+			this.adSlot.addClass(CSS_CLASSNAME_STICKY_BFAA);
+			this.stickNavbar();
+		});
+
+		leaving$.pipe(ofState(STATES.RESOLVED)).subscribe(() => {
+			this.adSlot.removeClass(CSS_CLASSNAME_THEME_RESOLVED);
+		});
+		leaving$.pipe(ofState(STATES.STICKY)).subscribe(() => {
+			// TODO: Animation of scrolling the ad up to resolved
+			// But how do I know it's scrolling to resolved and not restarted?
+			this.adSlot.removeClass(CSS_CLASSNAME_STICKY_BFAA);
+		});
+		leaving$.pipe(ofState(STATES.IMPACT)).subscribe(() => {
+			this.adSlot.removeClass(CSS_CLASSNAME_IMPACT_BFAA);
+		});
+
+		entering$.pipe(ofState(STATES.RESOLVED)).subscribe(() => {
+			// Stick on scroll only if not viewable and not timeout
+			this.viewableAndTimeoutRunning$
+				.pipe(
+					filter((running) => !!running),
+					switchMap(() => {
+						return createScrollObservable().pipe(
+							takeUntil(this.viewableAndTimeoutRunning$.pipe(filter((running) => !running))),
+							takeUntil(leaving$),
+						);
+					}),
+				)
+				.subscribe(() => bfaaFsm.dispatch(ACTIONS.STICK));
+		});
+
+		entering$.pipe(ofState(STATES.STICKY)).subscribe(() => {
+			// Unstick on scroll only if viewable and timeout
+			this.viewableAndTimeoutRunning$
+				.pipe(
+					filter((running) => !running),
+					takeUntil(leaving$),
+					switchMap(() => createScrollObservable()),
+					take(1),
+				)
+				.subscribe(() => bfaaFsm.dispatch(ACTIONS.RESOLVE));
+		});
+
+		entering$.pipe(ofState(STATES.IMPACT)).subscribe(() => {
+			// When smaller than HIVI_RESOLVED_THRESHOLD
+			createScrollObservable()
+				.pipe(
+					takeUntil(leaving$),
+					filter(() => this.currentState >= HIVI_RESOLVED_THRESHOLD),
+					take(1),
+					withLatestFrom(this.viewableAndTimeoutRunning$),
+				)
+				.subscribe(([_, running]) => {
+					if (running) {
+						// if not timeout and not viewable
+						bfaaFsm.dispatch(ACTIONS.STICK);
+					} else {
+						// if timeout and viewable
+						bfaaFsm.dispatch(ACTIONS.RESOLVE);
+					}
+				});
 		});
 
 		// LEAVE - STATE
-		bfaaEmitter.on(FSM.events.leave, (state: State) => {
-			if (state.name === STATES.STICKY) {
-				communicator.dispatch({ type: MOVE_NAVBAR, payload: { height: 0, time: SLIDE_OUT_TIME } });
-			}
+		leaving$.pipe(ofState(STATES.STICKY)).subscribe(() => {
+			communicator.dispatch({ type: MOVE_NAVBAR, payload: { height: 0, time: SLIDE_OUT_TIME } });
 		});
 
 		bfaaFsm.init();
