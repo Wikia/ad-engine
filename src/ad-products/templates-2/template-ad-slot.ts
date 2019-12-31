89445eb6cbbd69c1b63afd21e3eadfec424f2b3b
Damian Bielecki <bielik20@gmail.com>
2019-12-31 08:20:56 +0100

    add docs

--- src/ad-products/templates-2/template-ad-slot.ts
+++ src/ad-products/templates-2/template-ad-slot.ts
@@ -1,3 +1,6 @@
 import { AdSlot } from '@ad-engine/core';
 
+/**
+ * AdSlot to be used in TemplateStateHandler
+ */
 export class TemplateAdSlot extends AdSlot {}
--- src/ad-products/templates-2/template-params.ts
+++ src/ad-products/templates-2/template-params.ts
@@ -1,5 +1,9 @@
 import { Dictionary } from '@ad-engine/core';
 
+/**
+ * TemplateParams to be used in TemplateStateHandler
+ * They are delivered from GAM
+ */
 export class TemplateParams implements Dictionary {
 	templateName: string;
 }
