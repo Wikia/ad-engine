107bf44368f9716f7aaea4d9e59a249a0229c762
Damian Bielecki <bielik20@gmail.com>
2020-01-10 15:11:09 +0100

    add TemplateStateStub

---
+++ spec/ad-engine/services/templates-registry/template-state.stub.ts
@@ -0,0 +1,18 @@
+import { TemplateState } from '@wikia/ad-engine/services/templates-registry/template-state';
+import { Observable, Subject } from 'rxjs';
+import { SinonSandbox, SinonStub } from 'sinon';
+
+export type TemplateStateStub = {
+	[key in keyof Omit<TemplateState<string>, 'transition$'>]: SinonStub & TemplateState<string>[key]
+} & { transition$: Observable<string>; transitionSubject$: Subject<string> };
+
+export function createTemplateStaterStub(sandbox: SinonSandbox): TemplateStateStub {
+	const transitionSubject$ = new Subject<string>();
+
+	return {
+		transitionSubject$,
+		transition$: transitionSubject$.asObservable(),
+		enter: sandbox.stub(),
+		leave: sandbox.stub(),
+	};
+}
