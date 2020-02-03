dc3398ae03d62c2e1595adf281dec7fc83aaa1dc
Damian Bielecki <bielik20@gmail.com>
2020-02-03 11:54:39 +0100

    reorder

--- platforms/shared/tracking/data-warehouse.ts
+++ platforms/shared/tracking/data-warehouse.ts
@@ -12,7 +12,87 @@ export interface TimeBasedParams {
 }
 
 export class DataWarehouseTracker {
-	private static getTimeBasedParams(): TimeBasedParams {
+	/**
+	 * Call all of the setup trackers
+	 */
+	track(options: TrackingParams, trackingURL?: string): void {
+		const params: TrackingParams = {
+			...this.getDataWarehouseParams(),
+			...options,
+		};
+
+		if (trackingURL) {
+			this.sendCustomEvent(params, trackingURL);
+		} else {
+			this.sendTrackEvent(params);
+		}
+	}
+
+	/**
+	 * Get additional params only for DataWarehouse trackingParams
+	 *
+	 * @see https://wikia-inc.atlassian.net/wiki/display/FT/Analytics+Data+Model
+	 * @see https://docs.google.com/spreadsheets/d/1_C--1KGTuj3cs1un4UM_UnCWUBLhhGkGZKbabNwPWFU/edit#gid=932083905
+	 */
+	private getDataWarehouseParams(): TrackingParams {
+		return {
+			session_id: window.sessionId || 'unknown',
+			pv_number: window.pvNumber,
+			pv_number_global: window.pvNumberGlobal,
+			pv_unique_id: window.pvUID,
+			beacon: window.beacon_id || 'unknown',
+			ck: context.get('wiki.dsSiteKey') || 'unknown',
+			lc: context.get('wiki.wgUserLanguage') || 'unknown',
+			s: context.get('targeting.skin') || 'unknown',
+			ua: window.navigator.userAgent,
+			u: trackingOptIn.isOptedIn() ? context.get('userId') || 0 : -1,
+			a: context.get('targeting.artid') || -1,
+		};
+	}
+
+	/**
+	 * Send single get request to Data Warehouse using custom route name (defined in config)
+	 */
+	private sendCustomEvent(options: TrackingParams, trackingURL: string): void {
+		const params: TrackingParams & TimeBasedParams = {
+			...options,
+			...this.getTimeBasedParams(),
+		};
+		const url = this.buildDataWarehouseUrl(params, trackingURL);
+
+		this.sendRequest(url, params);
+	}
+
+	/**
+	 * Send the get request to  Data Warehouse
+	 */
+	private sendTrackEvent(options: TrackingParams, type = 'Event'): void {
+		const params: TrackingParams & TimeBasedParams = {
+			...options,
+			...this.getTimeBasedParams(),
+		};
+
+		// Prefix action, label, and category and remove unprefixed properties
+		params.ga_category = params.category || '';
+		params.ga_label = params.label || '';
+		params.ga_action = params.action || '';
+		params.ga_value = params.value || '';
+		delete params.category;
+		delete params.label;
+		delete params.action;
+		delete params.value;
+
+		let url = this.buildDataWarehouseUrl(params, eventUrl);
+		this.sendRequest(url, params, type);
+
+		// For video player events, send same data to additional endpoint
+		if (params.eventName === 'videoplayerevent') {
+			url = this.buildDataWarehouseUrl(params, videoEventUrl);
+			this.sendRequest(url, params, type);
+		}
+	}
+
+	private getTimeBasedParams(): TimeBasedParams {
 		return {
 			cb: Math.floor(Math.random() * 99999),
 			url: document.URL,
@@ -22,7 +102,7 @@ export class DataWarehouseTracker {
 	/**
 	 * Build a URL to send to Data Warehouse
 	 */
-	private static buildDataWarehouseUrl(params: object, baseUrl: string): string {
+	private buildDataWarehouseUrl(params: object, baseUrl: string): string {
 		if (!baseUrl) {
 			const msg = `Error building DW tracking URL`;
 			utils.logger(logGroup, msg);
@@ -48,85 +128,5 @@ export class DataWarehouseTracker {
 		);
 
 		request.send();
-	}
-
-	/**
-	 * Send the get request to  Data Warehouse
-	 */
-	private sendTrackEvent(options: TrackingParams, type = 'Event'): void {
-		const params: TrackingParams & TimeBasedParams = {
-			...options,
-			...DataWarehouseTracker.getTimeBasedParams(),
-		};
-
-		// Prefix action, label, and category and remove unprefixed properties
-		params.ga_category = params.category || '';
-		params.ga_label = params.label || '';
-		params.ga_action = params.action || '';
-		params.ga_value = params.value || '';
-		delete params.category;
-		delete params.label;
-		delete params.action;
-		delete params.value;
-
-		let url = DataWarehouseTracker.buildDataWarehouseUrl(params, eventUrl);
-		this.sendRequest(url, params, type);
-
-		// For video player events, send same data to additional endpoint
-		if (params.eventName === 'videoplayerevent') {
-			url = DataWarehouseTracker.buildDataWarehouseUrl(params, videoEventUrl);
-			this.sendRequest(url, params, type);
-		}
-	}
-
-	/**
-	 * Send single get request to Data Warehouse using custom route name (defined in config)
-	 */
-	private sendCustomEvent(options: TrackingParams, trackingURL: string): void {
-		const params: TrackingParams & TimeBasedParams = {
-			...options,
-			...DataWarehouseTracker.getTimeBasedParams(),
-		};
-		const url = DataWarehouseTracker.buildDataWarehouseUrl(params, trackingURL);
-
-		this.sendRequest(url, params);
-	}
-
-	/**
-	 * Call all of the setup trackers
-	 */
-	track(options: TrackingParams, trackingURL?: string): void {
-		const params: TrackingParams = {
-			...this.getDataWarehouseParams(),
-			...options,
-		};
-
-		if (trackingURL) {
-			this.sendCustomEvent(params, trackingURL);
-		} else {
-			this.sendTrackEvent(params);
-		}
-	}
-
-	/**
-	 * Get additional params only for datawarehouse trackingParams
-	 *
-	 * @see https://wikia-inc.atlassian.net/wiki/display/FT/Analytics+Data+Model
-	 * @see https://docs.google.com/spreadsheets/d/1_C--1KGTuj3cs1un4UM_UnCWUBLhhGkGZKbabNwPWFU/edit#gid=932083905
-	 */
-	private getDataWarehouseParams(): TrackingParams {
-		return {
-			session_id: window.sessionId || 'unknown',
-			pv_number: window.pvNumber,
-			pv_number_global: window.pvNumberGlobal,
-			pv_unique_id: window.pvUID,
-			beacon: window.beacon_id || 'unknown',
-			ck: context.get('wiki.dsSiteKey') || 'unknown',
-			lc: context.get('wiki.wgUserLanguage') || 'unknown',
-			s: context.get('targeting.skin') || 'unknown',
-			ua: window.navigator.userAgent,
-			u: trackingOptIn.isOptedIn() ? context.get('userId') || 0 : -1,
-			a: context.get('targeting.artid') || -1,
-		};
 	}
 }
