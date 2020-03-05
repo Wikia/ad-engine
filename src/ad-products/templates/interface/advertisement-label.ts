26b16106b344d676e8b0a0be0f44adfac86a6393
Paulina Gralak <pgralak@wikia-inc.com>
2020-03-05 10:46:17 +0100

    [ADEN-9965] dark UAP label and X button

--- src/ad-products/templates/interface/advertisement-label.ts
+++ src/ad-products/templates/interface/advertisement-label.ts
@@ -3,9 +3,7 @@ import { UiComponent } from './ui-component';
 
 export class AdvertisementLabel extends UiComponent {
 	render(): HTMLDivElement {
-		const { isDarkTheme } = this.props;
 		const label = document.createElement('div');
-		const classNames = this.getClassNames();
 
 		label.innerText = getTranslation('labels', 'advertisement');
 		label.className = 'advertisement-label';
