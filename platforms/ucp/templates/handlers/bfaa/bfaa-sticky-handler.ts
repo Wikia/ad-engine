53657da7610ae311f7b1db35c53c761d4ed88c66
Damian Bielecki <bielik20@gmail.com>
2020-03-18 17:59:37 +0100

    extract setStickyBodyPadding

--- platforms/ucp/templates/handlers/bfaa/bfaa-sticky-handler.ts
+++ platforms/ucp/templates/handlers/bfaa/bfaa-sticky-handler.ts
@@ -1,15 +1,9 @@
-import {
-	AdSlot,
-	DomListener,
-	NAVBAR,
-	TEMPLATE,
-	TemplateStateHandler,
-	UapParams,
-} from '@wikia/ad-engine';
+import { AdSlot, DomListener, NAVBAR, TEMPLATE, TemplateStateHandler } from '@wikia/ad-engine';
 import { Inject, Injectable } from '@wikia/dependency-injection';
 import { Subject } from 'rxjs';
 import { startWith, takeUntil, tap } from 'rxjs/operators';
 import { BfaaDomManager } from '../../helpers/bfaa-dom-manager';
+import { BfaaDomReader } from '../../helpers/bfaa-dom-reader';
 import { DomManipulator } from '../../helpers/manipulators/dom-manipulator';
 
 @Injectable()
@@ -18,11 +12,11 @@ export class BfaaStickyHandler implements TemplateStateHandler {
 
 	constructor(
 		@Inject(TEMPLATE.SLOT) private adSlot: AdSlot,
-		@Inject(TEMPLATE.PARAMS) params: UapParams,
-		@Inject(NAVBAR) navbar: HTMLElement,
+		@Inject(NAVBAR) private navbar: HTMLElement,
 		private domListener: DomListener,
 		private manipulator: DomManipulator,
 		private manager: BfaaDomManager,
+		private reader: BfaaDomReader,
 	) {}
 
 	async onEnter(): Promise<void> {
@@ -35,11 +29,18 @@ export class BfaaStickyHandler implements TemplateStateHandler {
 					this.manager.setResolvedAdHeight();
 					this.manager.setAdFixedPosition();
 					this.manager.setNavbarFixedPosition();
-					this.manager.setStickyBodyPadding();
+					this.setStickyBodyPadding();
 				}),
 				takeUntil(this.unsubscribe$),
 			)
 			.subscribe();
+	}
+
+	private setStickyBodyPadding(): void {
+		const adHeight = this.reader.getResolvedAdHeight();
+		const adAndNavHeight = adHeight + this.navbar.offsetHeight;
+
+		this.manipulator.element(document.body).setProperty('paddingTop', `${adAndNavHeight}px`);
 	}
 
 	async onLeave(): Promise<void> {
