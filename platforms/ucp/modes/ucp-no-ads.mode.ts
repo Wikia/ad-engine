43ecdbaecccbe4478df7448a8a0ce43e8156c3e5
Paulina Gralak <pgralak@wikia-inc.com>
2020-03-18 13:44:40 +0100

    [ADEN-9940] track AdEngine status on UCP

--- platforms/ucp/modes/ucp-no-ads.mode.ts
+++ platforms/ucp/modes/ucp-no-ads.mode.ts
@@ -1,4 +1,5 @@
-import { NoAdsMode } from '@platforms/shared';
+import { NoAdsMode, PageTracker } from '@platforms/shared';
+import { context, utils } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
 import { Communicator } from '@wikia/post-quecast';
 
@@ -6,6 +7,41 @@ import { Communicator } from '@wikia/post-quecast';
 export class UcpNoAdsMode implements NoAdsMode {
 	handleNoAds(): void {
 		this.dispatchJWPlayerSetupAction();
+		this.trackAdEngineStatus();
+	}
+
+	private trackAdEngineStatus(): void {
+		if (context.get('state.showAds')) {
+			PageTracker.trackProp('adengine', `on_${window.ads.adEngineVersion}`);
+		} else {
+			PageTracker.trackProp('adengine', `off_${this.getReasonForNoAds()}`);
+		}
+	}
+
+	private getReasonForNoAds(): string {
+		const reasonFromBackend = window.ads.context.opts.noAdsReason || null;
+		const pageType = window.ads.context.opts.pageType || null;
+
+		if (reasonFromBackend === 'no_ads_user' && pageType === 'homepage_logged') {
+			return null;
+		}
+
+		if (reasonFromBackend !== null) {
+			return reasonFromBackend;
+		}
+
+		const possibleFrontendReasons = {
+			noads_querystring: !!utils.queryString.get('noads'),
+			noexternals_querystring: !!utils.queryString.get('noexternals'),
+			steam_browser: context.get('state.isSteam'),
+			ad_stack_disabled: context.get('options.disableAdStack'),
+		};
+
+		const reasons = Object.keys(possibleFrontendReasons).filter((key) => {
+			return possibleFrontendReasons[key] === true;
+		});
+
+		return reasons.length > 0 ? reasons[0] : null;
 	}
 
 	private dispatchJWPlayerSetupAction(): void {
