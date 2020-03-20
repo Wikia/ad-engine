c759b740f873c828d7c9c1bb5881d8e3cf1093cb
Paulina Gralak <pgralak@wikia-inc.com>
2020-03-20 13:18:01 +0100

    [ADEN-10048] UCP: change A9 context

--- platforms/ucp/setup/context/a9/a9.ts
+++ platforms/ucp/setup/context/a9/a9.ts
@@ -10,26 +10,20 @@ export class UcpA9ConfigSetup implements A9ConfigSetup {
 
 	getA9Context(): object {
 		return {
-			dealsEnabled: false,
-			enabled: false,
-			videoEnabled: false,
-			amazonId: '3115',
-			slots: {
-				bottom_leaderboard: {
-					sizes: [[728, 90], [970, 250]],
-				},
-				incontent_boxad_1: {
-					sizes: [[300, 250], [300, 600]],
-				},
-				top_leaderboard: {
-					sizes: [[728, 90], [970, 250]],
-				},
-				top_boxad: {
-					sizes: [[300, 250], [300, 600]],
-				},
-				featured: {
-					type: 'video',
-				},
+			bottom_leaderboard: {
+				sizes: [[728, 90], [970, 250]],
+			},
+			incontent_boxad_1: {
+				sizes: [[300, 250], [300, 600]],
+			},
+			top_leaderboard: {
+				sizes: [[728, 90], [970, 250]],
+			},
+			top_boxad: {
+				sizes: [[300, 250], [300, 600]],
+			},
+			featured: {
+				type: 'video',
 			},
 		};
 	}
