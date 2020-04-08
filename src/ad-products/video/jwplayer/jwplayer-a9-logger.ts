5be13b92aa2f535e5e298b9710c911d7914d0ebb
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2020-04-08 13:29:14 +0200

    CR fixes

--- src/ad-products/video/jwplayer/jwplayer-a9-logger.ts
+++ src/ad-products/video/jwplayer/jwplayer-a9-logger.ts
@@ -6,13 +6,13 @@ import { JWPlayerEventParams } from './external-types/jwplayer';
  *
  * TODO: Remove it and its usage after finished A9 debugging
  */
-class JWPlayerA9Logger {
-	log(payload: JWPlayerEventParams['adError']) {
+export class JWPlayerA9Logger {
+	static log(payload: JWPlayerEventParams['adError']) {
 		const bid = context.get('slots.featured.targeting.amznbid');
 		const bidId = context.get('slots.featured.targeting.amzniid');
-		const errorCodes: number[] = context.get('options.jwplayerA9LoggerErrorCodes');
+		const errorCodes: number[] = context.get('options.jwplayerA9LoggerErrorCodes') || [];
 
-		if (bid && bidId && errorCodes && errorCodes.length && errorCodes.includes(payload.code)) {
+		if (bid && bidId && errorCodes.includes(payload.code)) {
 			const vastUrl = `//aax.amazon-adsystem.com/e/dtb/vast?b=${bidId}&pp=${bid}&rnd=${Math.random()}`;
 
 			fetch(vastUrl)
@@ -30,5 +30,3 @@ class JWPlayerA9Logger {
 		}
 	}
 }
-
-export const jwplayerA9Logger = new JWPlayerA9Logger();
