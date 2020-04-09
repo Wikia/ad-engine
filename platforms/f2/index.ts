11e416b58a5ddfe5e7a4267f80ce364901a5e26e
Paulina Gralak <pgralak@wikia-inc.com>
2020-04-09 17:33:09 +0200

    [ADEN-10081] f2: wait for platform to setup

--- platforms/f2/index.ts
+++ platforms/f2/index.ts
@@ -1,19 +1,28 @@
 import { adEngineConfigured, bootstrapAndGetConsent, PlatformStartup } from '@platforms/shared';
 import { context } from '@wikia/ad-engine';
 import { Container } from '@wikia/dependency-injection';
-import { Communicator, setupPostQuecast } from '@wikia/post-quecast';
+import { Action, Communicator, setupPostQuecast } from '@wikia/post-quecast';
+import { take } from 'rxjs/operators';
+import { action } from 'ts-action';
+import { ofType } from 'ts-action-operators';
 import { basicContext } from './ad-context';
 import { setupF2Ioc } from './setup-f2-ioc';
 import './styles.scss';
 
 setupPostQuecast();
 
-function checkIff2IsMobile(): boolean {
-	return false;
+export const f2ready = action('[F2] Configured');
+
+function onF2Configured(): Promise<Action> {
+	const communicator = new Communicator();
+
+	return communicator.actions$.pipe(ofType(f2ready), take(1)).toPromise();
 }
 
 const load = async () => {
 	const communicator = new Communicator();
+	const f2Action = await onF2Configured();
+	const f2Payload = f2Action.payload;
 
 	context.extend(basicContext);
 
@@ -23,9 +32,7 @@ const load = async () => {
 	]);
 	const platformStartup = container.get(PlatformStartup);
 
-	const isf2Mobile = await checkIff2IsMobile();
-
-	platformStartup.configure({ isMobile: isf2Mobile });
+	platformStartup.configure({ isMobile: f2Payload.isMobile });
 
 	communicator.dispatch(adEngineConfigured());
 
