a66d3b43d1a00da950c0b4d07a2edf62176fc1a9
Damian Bielecki <bielik20@gmail.com>
2020-04-15 09:32:23 +0200

    unify reader for video and slot

--- platforms/ucp/templates/helpers/uap-dom-reader.ts
+++ platforms/ucp/templates/helpers/uap-dom-reader.ts
@@ -2,6 +2,12 @@ import { AdSlot, NAVBAR, TEMPLATE, UapParams } from '@wikia/ad-engine';
 import { Inject, Injectable } from '@wikia/dependency-injection';
 import { isUndefined } from 'util';
 
+export interface UapVideoSize {
+	width: number;
+	height: number;
+	margin: number;
+}
+
 @Injectable({ autobind: false })
 export class UapDomReader {
 	constructor(
@@ -32,17 +38,66 @@ export class UapDomReader {
 		return this.getSlotHeightSmall();
 	}
 
+	getVideoSizeImpact(): UapVideoSize {
+		return this.calculateVideoSize(this.getSlotHeightBig(), this.getVideoMultiplierImpact());
+	}
+
+	getVideoSizeResolved(): UapVideoSize {
+		return this.calculateVideoSize(this.getSlotHeightSmall(), this.getVideoMultiplierResolved());
+	}
+
+	getVideoSizeImpactToResolved(): UapVideoSize {
+		return this.calculateVideoSize(
+			this.getSlotHeightBigToSmall(),
+			this.getVideoMultiplierImpactToResolved(),
+		);
+	}
+
+	private calculateVideoSize(slotHeight: number, videoMultiplier: number): UapVideoSize {
+		const margin = (100 - videoMultiplier) / 2;
+		const height = (slotHeight * videoMultiplier) / 100;
+		const width = height * this.params.videoAspectRatio;
+
+		return { margin, height, width };
+	}
+
+	private getVideoMultiplierImpactToResolved(): number {
+		return (
+			this.getVideoMultiplierImpact() +
+			this.getProgressBigToSmall() *
+				(this.getVideoMultiplierResolved() - this.getVideoMultiplierImpact())
+		);
+	}
+
+	private getVideoMultiplierImpact(): number {
+		return this.params.config.state.height.default;
+	}
+
+	private getVideoMultiplierResolved(): number {
+		return this.params.config.state.height.resolved;
+	}
+
 	getSlotOffsetSmallToNone(): number {
 		return this.getNavbarOffsetSmallToNone() - this.getSlotHeightSmall();
 	}
 
 	getSlotHeightBigToSmall(): number {
-		const minHeight = this.getSlotHeightSmall();
-		const maxHeight = this.getSlotHeightBig();
-		const offset = window.scrollY || window.pageYOffset || 0;
-		const height = maxHeight - offset;
+		const smallHeight = this.getSlotHeightSmall();
+		const bigHeight = this.getSlotHeightBig();
+		const progress = this.getProgressBigToSmall();
 
-		return height < minHeight ? minHeight : height;
+		return bigHeight - (bigHeight - smallHeight) * progress;
+	}
+
+	/**
+	 * Progress changes between 0 (big, full height) to 1 (small size);
+	 */
+	private getProgressBigToSmall(): number {
+		const smallHeight = this.getSlotHeightSmall();
+		const bigHeight = this.getSlotHeightBig();
+		const progress = window.scrollY / (bigHeight - smallHeight);
+
+		return progress >= 1 ? 1 : progress;
 	}
 
 	getSlotHeightBig(): number {
