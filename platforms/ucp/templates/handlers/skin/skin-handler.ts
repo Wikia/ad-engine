cdc63843c5da4cca0deec872ed06bbfa0b121752
Damian Bielecki <bielik20@gmail.com>
2020-04-16 15:19:17 +0200

    remove background changer

--- platforms/ucp/templates/handlers/skin/skin-handler.ts
+++ platforms/ucp/templates/handlers/skin/skin-handler.ts
@@ -1,6 +1,5 @@
 import { TEMPLATE, TemplateStateHandler, utils } from '@wikia/ad-engine';
 import { Inject, Injectable } from '@wikia/dependency-injection';
-import { BackgroundOptions } from '../../helpers/background-changer';
 import { DomManipulator } from '../../helpers/manipulators/dom-manipulator';
 import { SkinParams } from './skin-params';
 
@@ -27,30 +26,15 @@ export class SkinHandler implements TemplateStateHandler {
 	}
 
 	async onEnter(): Promise<void> {
-		const adSkin = this.createAdSkin();
+		this.createAdSkin();
 
-		this.manipulator.element(adSkin).setProperty('display', 'inline');
 		this.manipulator.element(document.body).addClass('has-background-ad');
-
-		console.log('**', this.params);
-	}
-
-	private getBackgroundOptions(): BackgroundOptions {
-		return {
-			skinImage: this.params.skinImage,
-			skinImageWidth: 1700,
-			skinImageHeight: 800,
-			backgroundColor: `#${this.params.backgroundColor}`,
-			backgroundMiddleColor: `#${this.params.middleColor}`,
-			ten64: this.params.ten64,
-		};
 	}
 
 	private createAdSkin(): HTMLElement {
 		const parent = document.querySelector('.WikiaSiteWrapper');
-		const adSkin = document.createElement('dev');
+		const adSkin = document.createElement('div');
 
-		adSkin.style.display = 'none';
 		adSkin.id = 'ad-skin';
 		adSkin.style.position = 'fixed';
 		adSkin.style.height = '100%';
@@ -63,9 +47,8 @@ export class SkinHandler implements TemplateStateHandler {
 			utils.logger(`${this.name}-template`, 'click');
 			window.open(this.params.destUrl);
 		});
-		parent.appendChild(adSkin);
 
-		return adSkin;
+		return parent.appendChild(adSkin);
 	}
 
 	async onLeave(): Promise<void> {}
