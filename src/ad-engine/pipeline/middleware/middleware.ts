e2457d5d214ee680589728cb24747ce69143d882
Damian Bielecki <bielik20@gmail.com>
2020-04-23 16:33:23 +0200

    add todo

--- src/ad-engine/pipeline/middleware/middleware.ts
+++ src/ad-engine/pipeline/middleware/middleware.ts
@@ -1,6 +1,8 @@
 import { Middleware } from './middleware-types';
 import { MiddlewarePipeline } from './pipeline-middleware-adapter';
 
+// TODO: Could be replaced with MiddlewarePipeline, but would increase scope.
+// TODO: Best would be to replace it with FuncPipeline, but would require few adjustment on middleware side.
 export class MiddlewareService<T> {
 	private pipeline = new MiddlewarePipeline();
 
