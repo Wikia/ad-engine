dfe8012ef7adb04e522c38698055fdcc75dc17ca
Damian Bielecki <bielik20@gmail.com>
2020-04-28 14:42:44 +0200

    extract handlers to shared

--- platforms/ucp/templates/bfab-template.ts
+++ platforms/ucp/templates/bfab-template.ts
@@ -1,20 +1,22 @@
+import {
+	AdvertisementLabelHandler,
+	DebugTransitionHandler,
+	DomCleanupHandler,
+	DomManipulator,
+	PlayerRegistry,
+	SlotSizeImpactHandler,
+	SlotSizeResolvedHandler,
+	UapDomManager,
+	UapDomReader,
+	VideoBootstrapHandler,
+	VideoCompletedHandler,
+	VideoDomManager,
+	VideoSizeImpactHandler,
+	VideoSizeResolvedHandler,
+} from '@platforms/shared';
 import { TemplateAction, TemplateRegistry } from '@wikia/ad-engine';
 import { Observable } from 'rxjs';
-import { AdvertisementLabelHandler } from './handlers/advertisement-label-handler';
 import { BfabBootstrapHandler } from './handlers/bfab/bfab-bootstrap-handler';
-import { DebugTransitionHandler } from './handlers/debug-transition-handler';
-import { DomCleanupHandler } from './handlers/dom-cleanup-handler';
-import { SlotSizeImpactHandler } from './handlers/slot/slot-size-impact-handler';
-import { SlotSizeResolvedHandler } from './handlers/slot/slot-size-resolved-handler';
-import { VideoBootstrapHandler } from './handlers/video/video-bootstrap-handler';
-import { VideoCompletedHandler } from './handlers/video/video-completed-handler';
-import { VideoSizeImpactHandler } from './handlers/video/video-size-impact-handler';
-import { VideoSizeResolvedHandler } from './handlers/video/video-size-resolved-handler';
-import { DomManipulator } from './helpers/manipulators/dom-manipulator';
-import { PlayerRegistry } from './helpers/player-registry';
-import { UapDomManager } from './helpers/uap-dom-manager';
-import { UapDomReader } from './helpers/uap-dom-reader';
-import { VideoDomManager } from './helpers/video-dom-manager';
 
 export function registerBfabTemplate(registry: TemplateRegistry): Observable<TemplateAction> {
 	return registry.register(
--- platforms/ucp/templates/sticky-tlb-template.ts
+++ platforms/ucp/templates/sticky-tlb-template.ts
@@ -1,26 +1,28 @@
+import {
+	AdvertisementLabelHandler,
+	BodyOffsetResolvedHandler,
+	CloseButtonHelper,
+	CloseToHiddenButtonHandler,
+	DebugTransitionHandler,
+	DomCleanupHandler,
+	DomManipulator,
+	NavbarOffsetResolvedHandler,
+	NavbarOffsetResolvedToNoneHandler,
+	ScrollCorrector,
+	SlotDecisionTimeoutHandler,
+	SlotHiddenHandler,
+	SlotOffsetResolvedToNoneHandler,
+	SlotSizeResolvedHandler,
+	SlotTransitionHandler,
+	StickinessTimeout,
+	UapDomManager,
+	UapDomReader,
+} from '@platforms/shared';
 import { TemplateAction, TemplateRegistry, universalAdPackage } from '@wikia/ad-engine';
 import { Observable } from 'rxjs';
-import { AdvertisementLabelHandler } from './handlers/advertisement-label-handler';
-import { BodyOffsetResolvedHandler } from './handlers/body/body-offset-resolved-handler';
-import { CloseToHiddenButtonHandler } from './handlers/close-to-hidden-button-handler';
-import { DebugTransitionHandler } from './handlers/debug-transition-handler';
-import { DomCleanupHandler } from './handlers/dom-cleanup-handler';
-import { NavbarOffsetResolvedHandler } from './handlers/navbar/navbar-offset-resolved-handler';
-import { NavbarOffsetResolvedToNoneHandler } from './handlers/navbar/navbar-offset-resolved-to-none-handler';
-import { SlotDecisionTimeoutHandler } from './handlers/slot/slot-decision-timeout-handler';
-import { SlotHiddenHandler } from './handlers/slot/slot-hidden-handler';
-import { SlotOffsetResolvedToNoneHandler } from './handlers/slot/slot-offset-resolved-to-none-handler';
-import { SlotSizeResolvedHandler } from './handlers/slot/slot-size-resolved-handler';
-import { SlotTransitionHandler } from './handlers/slot/slot-transition-handler';
 import { StickyTlbBlockingHandler } from './handlers/sticky-tlb/sticky-tlb-blocking-handler';
 import { StickyTlbBootstrapHandler } from './handlers/sticky-tlb/sticky-tlb-bootstrap-handler';
 import { StickyTlbConfigHandler } from './handlers/sticky-tlb/sticky-tlb-config-handler';
-import { CloseButtonHelper } from './helpers/close-button-helper';
-import { DomManipulator } from './helpers/manipulators/dom-manipulator';
-import { ScrollCorrector } from './helpers/scroll-corrector';
-import { StickinessTimeout } from './helpers/stickiness-timeout';
-import { UapDomManager } from './helpers/uap-dom-manager';
-import { UapDomReader } from './helpers/uap-dom-reader';
 
 export function registerStickyTlbTemplate(registry: TemplateRegistry): Observable<TemplateAction> {
 	return registry.register(
