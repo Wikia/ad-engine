c10b344e62b15261e951727bb503c10bd45b3fbf
Damian Bielecki <bielik20@gmail.com>
2020-05-06 14:10:03 +0200

    add nielsen call

--- platforms/f2/modes/f2-ads.mode.ts
+++ platforms/f2/modes/f2-ads.mode.ts
@@ -1,4 +1,5 @@
 import { AdsMode, startAdEngine, wadRunner } from '@platforms/shared';
+import { context, nielsen } from '@wikia/ad-engine';
 
 export class F2AdsMode implements AdsMode {
 	handleAds(): void {
@@ -9,8 +10,15 @@ export class F2AdsMode implements AdsMode {
 
 	private callExternals(): Promise<any>[] {
 		const inhibitors: Promise<any>[] = [];
+		const targeting = context.get('targeting');
 
 		inhibitors.push(wadRunner.call());
+
+		nielsen.call({
+			type: 'static',
+			assetid: `fandom.com/news_and_stories/${targeting.s1}/${targeting.post_id}`,
+			section: `FANDOM NEWS AND STORIES NETWORK`,
+		});
 
 		return inhibitors;
 	}
