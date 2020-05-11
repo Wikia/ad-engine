d48376d82944c82984ee302ca765733f108687c5
Damian Bielecki <bielik20@gmail.com>
2020-05-11 14:55:46 +0200

    Add interstitial

--- platforms/ucp/templates/ucp-templates.setup.ts
+++ platforms/ucp/templates/ucp-templates.setup.ts
@@ -6,6 +6,7 @@ import { registerBfaaTemplate } from './bfaa-template';
 import { registerBfabTemplate } from './bfab-template';
 import { getOutstreamConfig } from './configs/outstream-config';
 import { registerFloorAdhesionTemplate } from './floor-adhesion-template';
+import { registerInterstitialTemplate } from './interstitial-template';
 import { registerRoadblockTemplate } from './roadblock-template';
 import { registerStickyTlbTemplate } from './sticky-tlb-template';
 
@@ -21,8 +22,9 @@ export class UcpTemplatesSetup implements TemplatesSetup {
 		const stickyTlb$ = registerStickyTlbTemplate(this.registry);
 		const roadblock$ = registerRoadblockTemplate(this.registry);
 		const floorAdhesion$ = registerFloorAdhesionTemplate(this.registry);
+		const interstitial$ = registerInterstitialTemplate(this.registry);
 
-		logTemplates(merge(bfaa$, bfab$, stickyTlb$, roadblock$, floorAdhesion$));
+		logTemplates(merge(bfaa$, bfab$, stickyTlb$, roadblock$, floorAdhesion$, interstitial$));
 
 		templateService.register(PorvataTemplate, getOutstreamConfig());
 	}
