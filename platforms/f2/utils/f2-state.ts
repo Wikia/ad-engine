63c027e1f65f841de1ae693344fe3193ad9a7f90
Damian Bielecki <bielik20@gmail.com>
2020-05-14 09:40:21 +0200

    Mark whole state as optional

--- platforms/f2/utils/f2-state.ts
+++ platforms/f2/utils/f2-state.ts
@@ -15,120 +15,120 @@ export interface F2State {
 }
 
 interface Ads {
-	adStack: any[];
-	runtime: any[];
+	adStack?: any[];
+	runtime?: any[];
 }
 
 interface Article {
-	featuredImage: string;
-	id: number;
-	slug: string;
-	tags: string[];
-	topics: ArticleTopic[];
-	topicNames: string[];
-	publishedTime: Date;
-	authorName: string;
-	isArticlePlus: boolean;
-	contentId: number;
+	featuredImage?: string;
+	id?: number;
+	slug?: string;
+	tags?: string[];
+	topics?: ArticleTopic[];
+	topicNames?: string[];
+	publishedTime?: Date;
+	authorName?: string;
+	isArticlePlus?: boolean;
+	contentId?: number;
 }
 
 interface ArticleTopic {
-	id: string;
-	name: string;
-	topicSlug: string;
-	pinned: boolean;
+	id?: string;
+	name?: string;
+	topicSlug?: string;
+	pinned?: boolean;
 }
 
 interface Topic {
-	id: string;
-	slug: string;
+	id?: string;
+	slug?: string;
 }
 
 interface Comscore {
-	id: string;
-	keyword: string;
-	url: string;
+	id?: string;
+	keyword?: string;
+	url?: string;
 }
 
 interface Config {
-	environment: Environment;
-	assetPath: string;
-	tracking: Tracking;
-	dsSpriteFile: string;
-	cookieDomain: string;
+	environment?: Environment;
+	assetPath?: string;
+	tracking?: Tracking;
+	dsSpriteFile?: string;
+	cookieDomain?: string;
 }
 
 interface Environment {
-	debug: boolean;
-	env: string;
-	locale: string;
-	siteType: string;
+	debug?: boolean;
+	env?: string;
+	locale?: string;
+	siteType?: string;
 }
 
 interface Tracking {
-	googleAnalytics: TrackingGoogleAnalytics;
-	dataWarehouse: DataWarehouse;
-	simpleReach: SimpleReach;
+	googleAnalytics?: TrackingGoogleAnalytics;
+	dataWarehouse?: DataWarehouse;
+	simpleReach?: SimpleReach;
 }
 
 interface DataWarehouse {
-	routes: Routes;
-	wikiId: number;
+	routes?: Routes;
+	wikiId?: number;
 }
 
 interface Routes {
-	adRenderEndedEvent: string;
-	adPageInfoPropEvent: string;
-	adVideoEvent: string;
-	adViewabilityEvent: string;
-	event: string;
-	videoEvent: string;
-	pageView: string;
+	adRenderEndedEvent?: string;
+	adPageInfoPropEvent?: string;
+	adVideoEvent?: string;
+	adViewabilityEvent?: string;
+	event?: string;
+	videoEvent?: string;
+	pageView?: string;
 }
 
 interface TrackingGoogleAnalytics {
-	accounts: Accounts;
+	accounts?: Accounts;
 }
 
 interface Accounts {
-	production: Development[];
-	development: Development[];
+	production?: Development[];
+	development?: Development[];
 }
 
 interface Development {
-	id: string;
-	name: string;
-	sampleRate: number;
+	id?: string;
+	name?: string;
+	sampleRate?: number;
 }
 
 interface SimpleReach {
-	pid: PID;
+	pid?: PID;
 }
 
 interface PID {
-	production: string;
-	development: string;
+	production?: string;
+	development?: string;
 }
 
 interface Endpoints {
-	getVideoPlaylist: string;
-	getKibanaLog: string;
+	getVideoPlaylist?: string;
+	getKibanaLog?: string;
 }
 
 interface Feed {
-	hotContentModuleUrl: string;
+	hotContentModuleUrl?: string;
 }
 
 interface F2StateGoogleAnalytics {
-	activeTrackerNames: string[];
+	activeTrackerNames?: string[];
 }
 
 interface Regions {
-	AU: string;
-	UK: string;
-	US: string;
+	AU?: string;
+	UK?: string;
+	US?: string;
 }
 
 interface User {
-	region: string;
+	region?: string;
 }
