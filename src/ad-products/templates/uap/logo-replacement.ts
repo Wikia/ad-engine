82d3bdc5b408ebacc53e968642febfd75536acfc
Paulina Gralak <pgralak@wikia-inc.com>
2020-05-15 13:23:09 +0200

    [ADEN-10259] templates:LogoReplacement - make new logo a link

--- src/ad-products/templates/uap/logo-replacement.ts
+++ src/ad-products/templates/uap/logo-replacement.ts
@@ -2,6 +2,7 @@ import { utils } from '@ad-engine/core';
 
 export interface LogoReplacementTemplateConfig {
 	logoImage: string;
+	clickThroughUrl: string;
 }
 
 export class LogoReplacement {
@@ -16,22 +17,25 @@ export class LogoReplacement {
 	init(params: LogoReplacementTemplateConfig): void {
 		utils.logger(LogoReplacement.getName(), 'init');
 
-		const logoSrc = params.logoImage;
-		this.replaceLogo(logoSrc);
+		this.replaceLogo(params);
 	}
 
-	private replaceLogo(logoSrc: string): void {
+	private replaceLogo(params: LogoReplacementTemplateConfig): void {
 		const parentElement = document.querySelector(
 			'#globalNavigation .wds-global-navigation__content-bar-left',
 		);
 		const fandomLogo = document.querySelector('.wds-global-navigation__logo');
 
 		if (parentElement && fandomLogo) {
-			const newLogo = document.createElement('img');
-			newLogo.src = logoSrc;
+			const newLogoAnchorElement = document.createElement('a');
+			newLogoAnchorElement.href = params.clickThroughUrl;
 
-			parentElement.insertBefore(newLogo, fandomLogo);
+			const newLogo = document.createElement('img');
+			newLogo.src = params.logoImage;
+
+			parentElement.insertBefore(newLogoAnchorElement, fandomLogo);
 			parentElement.removeChild(fandomLogo);
+			newLogoAnchorElement.appendChild(newLogo);
 		}
 	}
 }
