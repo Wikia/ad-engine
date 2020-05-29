0e3225671b82ac8bd484daa0c22819ba06d6b807
Paulina Gralak <pgralak@wikia-inc.com>
2020-05-29 10:00:01 +0200

    [ADEN-10273] futhead: LogoReplacement template

--- platforms/futhead/setup-futhead-ioc.ts
+++ platforms/futhead/setup-futhead-ioc.ts
@@ -20,9 +20,11 @@ import {
 	bidderTrackingMiddleware,
 	context,
 	InstantConfigService,
+	LogoReplacement,
 	slotBiddersTrackingMiddleware,
 	slotPropertiesTrackingMiddleware,
 	slotTrackingMiddleware,
+	templateService,
 } from '@wikia/ad-engine';
 import { Container } from '@wikia/dependency-injection';
 import { set } from 'lodash';
@@ -30,6 +32,7 @@ import * as fallbackInstantConfig from './fallback-config.json';
 import { FutheadPrebidConfigSetup } from './setup/context/prebid/futhead-prebid-config.setup';
 import { FutheadTargetingSetup } from './setup/context/targeting/futhead-targeting.setup';
 import { FutheadDynamicSlotsSetup } from './setup/dynamic-slots/futhead-dynamic-slots.setup';
+import { getLogoReplacementConfig } from './templates/logo-replacement-config';
 
 export async function setupFutheadIoc(): Promise<Container> {
 	const container = new Container();
@@ -38,6 +41,7 @@ export async function setupFutheadIoc(): Promise<Container> {
 	container.bind(InstantConfigService as any).value(await InstantConfigService.init());
 	container.bind(TargetingSetup).to(FutheadTargetingSetup);
 	container.bind(TemplatesSetup).to(SportsTemplatesSetup);
+	templateService.register(LogoReplacement, getLogoReplacementConfig());
 	container.bind(AdsMode).to(SportsAdsMode);
 	container.bind(SlotsContextSetup).to(CurseSlotsContextSetup);
 	container.bind(BiddersStateSetup).to(CommonBiddersStateSetup);
