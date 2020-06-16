ba0688fc19af88bd33fe7a7e6c85143e79827b20
Damian Bielecki <bielik20@gmail.com>
2020-06-16 10:04:20 +0200

    add jdoc description

--- src/ad-engine/services/communication/global-action.ts
+++ src/ad-engine/services/communication/global-action.ts
@@ -20,12 +20,25 @@ export function isGlobalActionCreator(input: ActionCreator): input is GlobalActi
 	return (input as any).__global === true;
 }
 
+/**
+ * Use it for external AdEngine actions.
+ * Those actions WILL be dispatched using PostQuecast.
+ * Those actions WILL be listened on PostQuecast stream.
+ * Changing this action introduces breaking change because it requires changes in different place than AdEngine.
+ * Do NOT use this action creator unless you need to.
+ */
 export function globalAction<T extends string>(type: T): ActionCreator<T, () => Typed<{}, T>>;
+/**
+ * @see globalAction
+ */
 export function globalAction<T extends string>(
 	type: T,
 	// tslint:disable-next-line:unified-signatures
 	config: { _as: 'empty' },
 ): ActionCreator<T, () => Typed<{}, T>>;
+/**
+ * @see globalAction
+ */
 export function globalAction<T extends string, P, M>(
 	type: T,
 	config: { _as: 'fsa'; _p: P; _m: M },
@@ -38,18 +51,30 @@ export function globalAction<T extends string, P, M>(
 		| Typed<{ error: false; meta?: M; payload: P }, T>
 		| Typed<{ error: true; meta?: M; payload: P }, T>
 >;
+/**
+ * @see globalAction
+ */
 export function globalAction<T extends string, P>(
 	type: T,
 	config: { _as: 'payload'; _p: P },
 ): ActionCreator<T, (payload: P) => Typed<{ payload: P }, T>>;
+/**
+ * @see globalAction
+ */
 export function globalAction<T extends string, P extends object>(
 	type: T,
 	config: { _as: 'props'; _p: P },
 ): ActionCreator<T, (props: P & NotTyped<P>) => Typed<P, T>>;
+/**
+ * @see globalAction
+ */
 export function globalAction<T extends string, C extends Creator>(
 	type: T,
 	creator: C & NotTyped<ReturnType<C>>,
 ): Typed<FunctionWithParametersType<ParametersType<C>, Typed<ReturnType<C>, T>>, T>;
+/**
+ * @see globalAction
+ */
 export function globalAction<T extends string>(
 	type: T,
 	config?: { _as: 'empty' } | { _as: 'fsa' } | { _as: 'payload' } | { _as: 'props' } | Creator,
