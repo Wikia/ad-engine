9a4b0ab7eeec5011c31f2b747e9c6d41508dcbe1
Damian Bielecki <bielik20@gmail.com>
2020-06-19 11:28:23 +0200

    fix rebase conflicts

--- platforms/f2/f2.actions.ts
+++ platforms/f2/f2.actions.ts
@@ -1,23 +1,24 @@
-import { action, props } from 'ts-action';
+import { globalAction } from '@wikia/ad-engine';
+import { props } from 'ts-action';
 
-export const f2FeedInsertTopBoxad = action(
+export const f2FeedInsertTopBoxad = globalAction(
 	'[F2 Feed] Insert Top Boxad',
 	props<{ hasRightRail: boolean }>(),
 );
 
-export const f2FeedInsertFeedBoxad = action(
+export const f2FeedInsertFeedBoxad = globalAction(
 	'[F2 Feed] Insert Feed Boxad',
 	props<{ hasRightRail: boolean }>(),
 );
 
-export const f2ArticleInsertTopBoxad = action(
+export const f2ArticleInsertTopBoxad = globalAction(
 	'[F2 Article] Insert Top Boxad',
 	props<{ hasRightRail: boolean; isVideo: boolean }>(),
 );
 
-export const f2ArticleInsertFeedBoxad = action(
+export const f2ArticleInsertFeedBoxad = globalAction(
 	'[F2 Article] Insert Feed Boxad',
 	props<{ hasRightRail: boolean }>(),
 );
 
-export const f2ArticleInsertIncontentBoxad = action('[F2 Article] Insert Incontent Boxad');
+export const f2ArticleInsertIncontentBoxad = globalAction('[F2 Article] Insert Incontent Boxad');
