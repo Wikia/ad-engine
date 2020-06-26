8701d2328d4f3e7741f0dc272b372e16f5ee5973
Damian Bielecki <bielik20@gmail.com>
2020-06-26 12:58:29 +0200

    fix F2FeedBoxadStickiness

--- platforms/f2/setup/dynamic-slots/f2-feed-boxad-stickiness.ts
+++ platforms/f2/setup/dynamic-slots/f2-feed-boxad-stickiness.ts
@@ -25,7 +25,7 @@ export class F2FeedBoxadStickiness {
 					if (this.isAboveSticky()) {
 						boxad.classList.remove(stickyClass);
 						boxad.classList.remove(bottomClass);
-					} else if (this.isBelowSticky(boxad.offsetHeight, rail)) {
+					} else if (this.isBelowSticky(this.calcElementHeight(boxad), rail)) {
 						boxad.classList.add(bottomClass);
 						boxad.classList.add(stickyClass);
 					} else {
@@ -43,13 +43,34 @@ export class F2FeedBoxadStickiness {
 		const windowTop = window.scrollY + 56; // nav height
 		const lowerMain: HTMLElement = document.querySelector('.feed-item[data-feed-index="10"]');
 
-		return windowTop < lowerMain.offsetTop;
+		return windowTop < this.calcElementOffset(lowerMain).top;
 	}
 
 	private isBelowSticky(boxadHeight: number, rail: HTMLElement): boolean {
 		const windowTop = window.scrollY;
-		const feedRailBottom = rail.offsetTop + rail.offsetHeight;
+		const { top: railOffsetTop } = this.calcElementOffset(rail);
+		const railHeight = this.calcElementHeight(rail);
+		const feedRailBottom = railOffsetTop + railHeight;
 
 		return windowTop + boxadHeight > feedRailBottom;
+	}
+
+	/**
+	 * Replaces jQuery `offset()`
+	 */
+	private calcElementOffset(element: HTMLElement): { top: number; left: number } {
+		const rect = element.getBoundingClientRect();
+
+		return {
+			top: rect.top + window.scrollY,
+			left: rect.left + window.scrollX,
+		};
+	}
+
+	/**
+	 * Replaces jQuery `height()`
+	 */
+	private calcElementHeight(element: HTMLElement): number {
+		return parseFloat(getComputedStyle(element, null).height.replace('px', ''));
 	}
 }
