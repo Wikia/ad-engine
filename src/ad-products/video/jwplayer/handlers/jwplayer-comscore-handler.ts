e2e03f742b24b188fb4920668a8634f98a6268ce
Damian Bielecki <bielik20@gmail.com>
2020-07-27 11:03:27 +0200

    Add comscore flag

--- src/ad-products/video/jwplayer/handlers/jwplayer-comscore-handler.ts
+++ src/ad-products/video/jwplayer/handlers/jwplayer-comscore-handler.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { context } from '@ad-engine/core';
+import { EMPTY, Observable } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
 import { PlayerReadyResult } from '../helpers/player-ready-result';
 import { JwpStream, ofJwpEvent } from '../streams/jwplayer-stream';
@@ -11,13 +12,38 @@ export class JwplayerComscoreHandler {
 	}
 
 	handle(): Observable<unknown> {
+		if (!this.isEnabled()) {
+			return EMPTY;
+		}
+
 		return this.stream$.pipe(
 			ofJwpEvent('adStarted'),
 			filter(({ state }) => state.adInVideo === 'preroll'),
 			tap(() => {
+				const payload = {
+					c1: '1',
+					c2: 6177433,
+					c5: '09',
+				};
 				// tslint:disable-next-line:no-console
-				console.log('** Preroll Comscore Track');
+				console.log('** Preroll Comscore Track', payload);
 			}),
 		);
+	}
+
+	private isEnabled(): boolean {
+		if (!context.get('options.video.comscoreJwpTracking')) {
+			return false;
+		}
+
+		if (context.get('options.geoRequiresConsent')) {
+			return context.get('options.trackingOptIn');
+		}
+
+		if (context.get('options.geoRequiresSignal')) {
+			return context.get('options.optOutSale');
+		}
+
+		return true;
 	}
 }
