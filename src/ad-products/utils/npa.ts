5ba0c757b66095ba9561e32fbe09223987eb3c43
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2020-07-31 15:12:10 +0200

    ADEN-10477 Restoring npa targeting keyval (#867)

--- src/ad-products/utils/npa.ts
+++ src/ad-products/utils/npa.ts
@@ -2,9 +2,7 @@ import { context, trackingOptIn } from '@ad-engine/core';
 
 // ToDo: cleanup after full TCFv2 rollout
 export function setupNpaContext(): void {
-	if (!context.get('custom.tcf2Enabled')) {
-		const optedOut = trackingOptIn.isOptedIn() ? 0 : 1;
+	const optedOut = trackingOptIn.isOptedIn() ? 0 : 1;
 
-		context.set('targeting.npa', optedOut.toString());
-	}
+	context.set('targeting.npa', optedOut.toString());
 }
