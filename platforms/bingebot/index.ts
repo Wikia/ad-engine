94ba7ec230ae4b8eec0d8bdb3c5d71604df3f2ae
Paulina Gralak <pgralak@wikia-inc.com>
2020-08-05 12:19:44 +0200

    [ADEN-10467] BingeBot: remove RLQ usage

--- platforms/bingebot/index.ts
+++ platforms/bingebot/index.ts
@@ -1,10 +1,7 @@
 import { Container } from '@wikia/dependency-injection';
 import { BingeBotPlatform } from './bingebot-platform';
 
-window.RLQ = window.RLQ || [];
-window.RLQ.push(async () => {
-	const container = new Container();
-	const platform = container.get(BingeBotPlatform);
+const container = new Container();
+const platform = container.get(BingeBotPlatform);
 
-	platform.execute();
-});
+platform.execute();
