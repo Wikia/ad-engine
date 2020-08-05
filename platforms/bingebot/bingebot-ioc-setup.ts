049303c55f20ff1f7cdabbe8ef58a2779ecbf3e6
Paulina Gralak <pgralak@wikia-inc.com>
2020-08-05 16:48:55 +0200

    [ADEN-10468] BingeBot: Instant Config Service

--- platforms/bingebot/bingebot-ioc-setup.ts
+++ platforms/bingebot/bingebot-ioc-setup.ts
@@ -1,5 +1,7 @@
-import { DiProcess } from '@wikia/ad-engine';
+import { context, DiProcess, InstantConfigService } from '@wikia/ad-engine';
 import { Container, Injectable } from '@wikia/dependency-injection';
+import { set } from 'lodash';
+import * as fallbackInstantConfig from './fallback-config.json';
 import { BingeBotTargetingSetup } from './setup/context/targeting/bingebot-targeting.setup';
 
 @Injectable()
@@ -7,6 +9,9 @@ export class BingeBotIocSetup implements DiProcess {
 	constructor(private container: Container) {}
 
 	async execute(): Promise<void> {
+		set(window, context.get('services.instantConfig.fallbackConfigKey'), fallbackInstantConfig);
+
+		this.container.bind(InstantConfigService).value(await InstantConfigService.init());
 		this.container.bind(BingeBotTargetingSetup.skin('bingebot'));
 	}
 }
