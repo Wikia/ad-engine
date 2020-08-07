8c0e3fed843233872e3a8f19b081ba8f2a6ba2d3
Paulina Gralak <pgralak@wikia-inc.com>
2020-08-07 10:46:23 +0200

    [ADEN-10468] BingeBot platform setup changes & start AdEngine

--- platforms/bingebot/bingebot-ioc-setup.ts
+++ platforms/bingebot/bingebot-ioc-setup.ts
@@ -1,7 +1,5 @@
-import { context, DiProcess, InstantConfigService } from '@wikia/ad-engine';
+import { DiProcess } from '@wikia/ad-engine';
 import { Container, Injectable } from '@wikia/dependency-injection';
-import { set } from 'lodash';
-import * as fallbackInstantConfig from './fallback-config.json';
 import { BingeBotTargetingSetup } from './setup/context/targeting/bingebot-targeting.setup';
 
 @Injectable()
@@ -9,9 +7,6 @@ export class BingeBotIocSetup implements DiProcess {
 	constructor(private container: Container) {}
 
 	async execute(): Promise<void> {
-		set(window, context.get('services.instantConfig.fallbackConfigKey'), fallbackInstantConfig);
-
-		this.container.bind(InstantConfigService).value(await InstantConfigService.init());
 		this.container.bind(BingeBotTargetingSetup.skin('bingebot'));
 	}
 }
