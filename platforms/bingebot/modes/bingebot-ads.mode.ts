79c96a1617d97bd329e8111f9b10d1a2043ceb5b
Paulina Gralak <pgralak@wikia-inc.com>
2020-08-10 17:52:34 +0200

    [ADEN-10468] BingeBot: inject sponsored logo POC

--- platforms/bingebot/modes/bingebot-ads.mode.ts
+++ platforms/bingebot/modes/bingebot-ads.mode.ts
@@ -1,9 +1,15 @@
 import { AdsMode, startAdEngine } from '@platforms/shared';
+import { context } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
 
 @Injectable()
 export class BingeBotAdsMode implements AdsMode {
 	execute(): void {
 		startAdEngine();
+		this.setAdStack();
+	}
+
+	private setAdStack(): void {
+		context.push('state.adStack', { id: 'sponsored_logo' });
 	}
 }
