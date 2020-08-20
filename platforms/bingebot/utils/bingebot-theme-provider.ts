b6446efc437398d350292f8c3ec3d30d6401e56f
Damian Bielecki <bielik20@gmail.com>
2020-08-20 11:56:43 +0200

    Simplify BingebotThemeProvider

--- platforms/bingebot/utils/bingebot-theme-provider.ts
+++ platforms/bingebot/utils/bingebot-theme-provider.ts
@@ -1,7 +1,6 @@
 import { communicationService, globalAction, ofType } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { map, shareReplay, take } from 'rxjs/operators';
 import { props } from 'ts-action';
 import { BingebotTheme } from './bingebot-theme';
 
@@ -12,18 +11,14 @@ export const setBingebotTheme = globalAction(
 
 @Injectable()
 export class BingebotThemeProvider {
-	private theme$: Observable<BingebotTheme>;
-
 	get theme(): Promise<BingebotTheme> {
-		return this.theme$.toPromise();
-	}
-
-	constructor() {
-		this.theme$ = communicationService.action$.pipe(
-			ofType(setBingebotTheme),
-			map(({ theme }) => theme),
-			shareReplay(1),
-		);
-		this.theme$.subscribe();
+		return communicationService.action$
+			.pipe(
+				ofType(setBingebotTheme),
+				map(({ theme }) => theme),
+				shareReplay(1), // take only newest value
+				take(1),
+			)
+			.toPromise();
 	}
 }
