d2b312717b3a48b6f2bb101eb6ea72fcac6b7e0f
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2020-12-15 16:40:05 +0100

    ADEN-10687 fix warnings from wiki context setup

--- platforms/shared/setup/wiki-context.setup.ts
+++ platforms/shared/setup/wiki-context.setup.ts
@@ -3,16 +3,20 @@ import { context, DiProcess } from '@wikia/ad-engine';
 export class WikiContextSetup implements DiProcess {
 	execute(): void {
 		const wikiContext = {
-			beaconId: window.beaconId || window.beacon_id,
-			pvNumber: window.pvNumber,
-			pvNumberGlobal: window.pvNumberGlobal,
-			pvUID: window.pvUID,
-			sessionId: window.sessionId || window.session_id,
+			beaconId: this.getMediaWikiVariable('beaconId') || window.beaconId || window.beacon_id,
+			pvNumber: this.getMediaWikiVariable('pvNumber') || window.pvNumber,
+			pvNumberGlobal: this.getMediaWikiVariable('pvNumberGlobal') || window.pvNumberGlobal,
+			pvUID: this.getMediaWikiVariable('pvUID') || window.pvUID,
+			sessionId: this.getMediaWikiVariable('sessionId') || window.sessionId || window.session_id,
 
 			...(window.mw && window.mw.config ? window.mw.config.values : {}),
 			...(window.ads ? window.ads.context : {}),
 		};
 
 		context.set('wiki', wikiContext);
+	}
+
+	private getMediaWikiVariable(variableName: string): any | null {
+		return window.mw && window.mw.config ? window.mw.config.get(variableName) : null;
 	}
 }
