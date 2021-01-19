ef28fa4494f755f959f0f2fea534918793a7dae0
Damian Sobczak <dsobczak@wikia-inc.com>
2021-01-19 13:48:56 +0100

    ADEN-10709 Integrating VerizonBidder alias

--- src/ad-bidders/prebid/adapters/index.ts
+++ src/ad-bidders/prebid/adapters/index.ts
@@ -15,5 +15,6 @@ export * from './rubicon';
 export * from './rubicon-display';
 export * from './telaria';
 export * from './triplelift';
+export * from './verizon';
 export * from './wikia';
 export * from './wikia-video';
