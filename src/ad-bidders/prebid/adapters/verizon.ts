75df8d4aa6f1fe64ed302193901d3684add438d8
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2021-01-25 16:28:21 +0100

    ADEN-10709 Integrating VerizonBidder alias (#958)
    Co-authored-by: Emil Madry <40793618+emadry@users.noreply.github.com>

---
+++ src/ad-bidders/prebid/adapters/verizon.ts
@@ -0,0 +1,36 @@
+import { PrebidAdapter } from '../prebid-adapter';
+
+export class Verizon extends PrebidAdapter {
+	static bidderName = 'verizon';
+	dcn: string;
+
+	get bidderName(): string {
+		return Verizon.bidderName;
+	}
+
+	constructor(options) {
+		super(options);
+
+		this.dcn = options.dcn;
+	}
+
+	prepareConfigForAdUnit(code, { sizes, pos }): PrebidAdUnit {
+		return {
+			code,
+			mediaTypes: {
+				banner: {
+					sizes,
+				},
+			},
+			bids: [
+				{
+					bidder: this.bidderName,
+					params: {
+						pos,
+						dcn: this.dcn,
+					},
+				},
+			],
+		};
+	}
+}
