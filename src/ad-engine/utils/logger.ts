b531e87ef923a0dd19c0afacf544ffff7c2cba60
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2021-04-16 13:12:02 +0200

    ADEN-10923 Adding communicator module (#994)
    Co-authored-by: Emil Madry <40793618+emadry@users.noreply.github.com>

--- src/ad-engine/utils/logger.ts
+++ src/ad-engine/utils/logger.ts
@@ -1,7 +1,14 @@
+import { communicationService, globalAction } from '@ad-engine/communication';
+import { props } from 'ts-action';
 import { debug } from '../services/debug';
 
 const debugGroup = debug.getDebugGroup() || '';
 const groups = debugGroup.split(',');
+
+interface CommunicatorPayload {
+	name: string;
+	state: string;
+}
 
 if (debugGroup !== '') {
 	window.console.info('AdEngine debug mode - groups:', debugGroup === '1' ? 'all' : groups);
@@ -25,4 +32,16 @@ export function warner(warnGroup: string, ...warnValues: any[]): void {
 	if (debugGroup === '1' || groups.indexOf(warnGroup) !== -1) {
 		window.console.warn(warnGroup, warnValues);
 	}
+}
+
+export function communicator(
+	communicatorEvent: string,
+	communicatorValues: CommunicatorPayload,
+): void {
+	const aeCommunicatorEvent = globalAction(
+		`[AdEngine] ${communicatorEvent}`,
+		props<CommunicatorPayload>(),
+	);
+
+	communicationService.dispatch(aeCommunicatorEvent(communicatorValues));
 }
