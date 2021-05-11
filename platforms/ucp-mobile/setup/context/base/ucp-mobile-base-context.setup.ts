504f3844c1d3fb4c46e4c5d20790047592f48169
Mariusz Walczak <62331720+Mariusz8Walczak@users.noreply.github.com>
2021-05-11 11:26:19 +0200

    adding default value

--- platforms/ucp-mobile/setup/context/base/ucp-mobile-base-context.setup.ts
+++ platforms/ucp-mobile/setup/context/base/ucp-mobile-base-context.setup.ts
@@ -20,7 +20,10 @@ export class UcpMobileBaseContextSetup extends BaseContextSetup {
 		// );
 
 		context.set('options.video.watchingThat.enabled', this.instantConfig.get('icWatchingThat'));
-		context.set('events.pushOnScroll.threshold', this.instantConfig.get('icPushOnScrollThreshold'));
+		context.set(
+			'events.pushOnScroll.threshold',
+			this.instantConfig.get('icPushOnScrollThreshold', 100),
+		);
 
 		if (context.get('wiki.opts.enableICLazyRequesting')) {
 			context.set('wiki.opts.enableICBPlaceholder', false);
