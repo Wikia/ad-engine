56ef3756d32c70810a92874270b4180ff0a8e5e8
preludek <tomasz.felczak89@gmail.com>
2021-05-17 23:51:42 +0200

    ADEN-10943 sending event with key-vals to GAM

--- src/ad-services/silver-surfer/index.ts
+++ src/ad-services/silver-surfer/index.ts
@@ -6,46 +6,43 @@ import { silverSurferServiceLoader } from './silver-surfer.loader';
 const logGroup = 'silver-surfer';
 
 class SilverSurferService {
+	private static isEnabled(): boolean {
+		return !!context.get('services.silverSurfer');
+	}
+
+	private static convertArrayToObject(array: []): {} {
+		return Object.assign({}, ...array);
+	}
+
 	async configureUserTargeting(): Promise<AdTags> {
-		if (!this.isEnabled()) {
+		if (!SilverSurferService.isEnabled()) {
 			utils.logger(logGroup, 'disabled');
 			return {};
 		}
-		// @ts-ignore
+
 		const targetingConfig = context.get('services.silverSurfer');
 		const userProfile: UserProfile = await silverSurferServiceLoader.getUserProfile();
 
-		return this.mapTargetingResults(targetingConfig, userProfile);
+		const userTargeting = this.mapTargetingResults(targetingConfig, userProfile);
+
+		Object.keys(userTargeting).forEach((key) => {
+			context.set(`targeting.${key}`, userTargeting[key]);
+		});
+		return userTargeting;
 	}
 
-	private mapTargetingResults(targetingConfig: string[] = [], userProfile = {}): AdTags {
+	private mapTargetingResults(targetingConfig: string[] = [], userProfile: UserProfile): AdTags {
 		if (!targetingConfig || !targetingConfig.length) {
 			return {};
 		}
 
-		const splitTargetingConfigKeyVals: string[][] = targetingConfig.map((keyVal: string) =>
-			keyVal.split(':'),
-		);
+		const splitTargetingConfigKeyVals: any = targetingConfig
+			.map((keyVal: string) => keyVal.split(':'))
+			.map(([configKeyVals, gamKeyVals]) => ({
+				[gamKeyVals]: userProfile[configKeyVals],
+			}));
 
-		const [configKeyVals, gamKeyVals] = this.partitionTargetingKeyValsArrays(
-			splitTargetingConfigKeyVals,
-		);
-
-		console.log({ targetingConfig, userProfile, configKeyVals, gamKeyVals });
-	}
-
-	private isEnabled(): boolean {
-		return !!context.get('services.silverSurfer');
-	}
-
-	private partitionTargetingKeyValsArrays(config: string[][]): string[][] {
-		const configKeyVals = [];
-		const gamKeyVals = [];
-		config.forEach(([a, b]) => {
-			configKeyVals.push(a);
-			gamKeyVals.push(b);
-		});
-		return [configKeyVals, gamKeyVals];
+		return SilverSurferService.convertArrayToObject(splitTargetingConfigKeyVals);
 	}
 }
 
