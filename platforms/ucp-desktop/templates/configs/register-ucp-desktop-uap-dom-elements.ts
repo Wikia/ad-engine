5ebf2657d8f9ad9789ae0e8215ea536f12e62e68
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2021-06-10 13:10:00 +0200

    ADEN-11044 Move UAP on FandomDesktop to the right of global nav (#1033)
    * ADEN-11044 Move UAP on FandomDesktop to the right of global nav
    
    * ADEN-11044 Fixing StickyTLB too
    
    * ADEN-11044 CR fixes
    
    * ADEN-11044 z-index fix
    
    * ADEN-11044 Another z-index fix
    
    Co-authored-by: soniackozubek <83713961+soniackozubek@users.noreply.github.com>

--- platforms/ucp-desktop/templates/configs/register-ucp-desktop-uap-dom-elements.ts
+++ platforms/ucp-desktop/templates/configs/register-ucp-desktop-uap-dom-elements.ts
@@ -3,7 +3,7 @@ import { TemplateDependency } from '@wikia/ad-engine';
 
 export function registerUcpDesktopUapDomElements(): TemplateDependency[] {
 	return [
-		{ bind: NAVBAR, value: document.querySelector('.global-navigation') },
+		{ bind: NAVBAR, value: document.querySelector('.fandom-sticky-header') },
 		{ bind: PAGE, value: document.body },
 		{ bind: FOOTER, value: document.querySelector('.wds-global-footer') },
 	];
