176075dd56537671d2a954ecc848dfe4e2af5ee2
preludek <tomasz.felczak89@gmail.com>
2021-06-13 10:06:13 +0200

    ADEN-11063 change transtion embeddedBig to embeddedResolved from 3s to video end

--- platforms/shared/templates/handlers/slot/slot-decision-embedded-big-to-embedded-resolved-handler.ts
+++ platforms/shared/templates/handlers/slot/slot-decision-embedded-big-to-embedded-resolved-handler.ts
@@ -1,20 +1,19 @@
 import { TemplateStateHandler, TemplateTransition } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
-import { Subject } from 'rxjs';
-import { filter, takeUntil, tap } from 'rxjs/operators';
-import { StickinessTimeout } from '../../helpers/stickiness-timeout';
+import { fromEvent, Subject } from 'rxjs';
+import { switchMap, takeUntil, tap } from 'rxjs/operators';
+import { PlayerRegistry } from '../../helpers/player-registry';
 
 @Injectable({ autobind: false })
 export class SlotDecisionEmbeddedBigToEmbeddedResolvedHandler implements TemplateStateHandler {
 	private unsubscribe$ = new Subject<void>();
 
-	constructor(private timeout: StickinessTimeout) {}
+	constructor(private playerRegistry: PlayerRegistry) {}
 
 	async onEnter(transition: TemplateTransition<'embeddedResolved'>): Promise<void> {
-		this.timeout
-			.isViewedAndDelayed()
+		this.playerRegistry.video$
 			.pipe(
-				filter((viewedAndDelayed) => viewedAndDelayed),
+				switchMap(({ player }) => fromEvent(player, 'wikiaAdCompleted')),
 				tap(() => transition('embeddedResolved')),
 				takeUntil(this.unsubscribe$),
 			)
