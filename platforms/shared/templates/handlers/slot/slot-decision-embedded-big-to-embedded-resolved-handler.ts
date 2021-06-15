17e88164199da6c544479b9b40389bfc0a4d7fe8
preludek <tomasz.felczak89@gmail.com>
2021-06-15 14:26:25 +0200

    ADEN-11063 fix 3s resolved for static uap

--- platforms/shared/templates/handlers/slot/slot-decision-embedded-big-to-embedded-resolved-handler.ts
+++ platforms/shared/templates/handlers/slot/slot-decision-embedded-big-to-embedded-resolved-handler.ts
@@ -1,19 +1,33 @@
 import { TemplateStateHandler, TemplateTransition } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
 import { fromEvent, Subject } from 'rxjs';
-import { switchMap, takeUntil, tap } from 'rxjs/operators';
+import { filter, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { PlayerRegistry } from '../../helpers/player-registry';
+import { StickinessTimeout } from '../../helpers/stickiness-timeout';
 
 @Injectable({ autobind: false })
 export class SlotDecisionEmbeddedBigToEmbeddedResolvedHandler implements TemplateStateHandler {
 	private unsubscribe$ = new Subject<void>();
+	private isVideo = false;
 
-	constructor(private playerRegistry: PlayerRegistry) {}
+	constructor(private timeout: StickinessTimeout, private playerRegistry: PlayerRegistry) {}
 
 	async onEnter(transition: TemplateTransition<'embeddedResolved'>): Promise<void> {
 		this.playerRegistry.video$
 			.pipe(
-				switchMap(({ player }) => fromEvent(player, 'wikiaAdCompleted')),
+				switchMap(({ player }) => {
+					this.isVideo = true;
+					return fromEvent(player, 'wikiaAdCompleted');
+				}),
+				tap(() => transition('embeddedResolved')),
+				takeUntil(this.unsubscribe$),
+			)
+			.subscribe();
+
+		this.timeout
+			.isViewedAndDelayed()
+			.pipe(
+				filter((viewedAndDelayed) => !this.isVideo && viewedAndDelayed),
 				tap(() => transition('embeddedResolved')),
 				takeUntil(this.unsubscribe$),
 			)
