a9dd3066cd2579f792f4e901542af3f936e0bf5c
Mariusz Walczak <62331720+Mariusz8Walczak@users.noreply.github.com>
2021-06-16 10:41:03 +0200

    fixes for scroll bug

---
+++ platforms/shared/templates/handlers/slot/slot-decision-timeout-handler-wihtout-scroll.ts
@@ -0,0 +1,39 @@
+import {
+	AdSlot,
+	DomListener,
+	TEMPLATE,
+	TemplateStateHandler,
+	TemplateTransition,
+	universalAdPackage,
+} from '@wikia/ad-engine';
+import { Inject, Injectable } from '@wikia/dependency-injection';
+import { Subject } from 'rxjs';
+import { filter, takeUntil, tap } from 'rxjs/operators';
+import { StickinessTimeout } from '../../helpers/stickiness-timeout';
+
+@Injectable({ autobind: false })
+export class SlotDecisionTimeoutHandler implements TemplateStateHandler {
+	private unsubscribe$ = new Subject<void>();
+
+	constructor(@Inject(TEMPLATE.SLOT) private adSlot: AdSlot, private timeout: StickinessTimeout) {}
+
+	async onEnter(transition: TemplateTransition<'transition'>): Promise<void> {
+		this.adSlot.emitEvent(universalAdPackage.SLOT_STICKED_STATE);
+
+		this.timeout
+			.isViewedAndDelayed()
+			.pipe(
+				filter((viewedAndDelayed) => viewedAndDelayed),
+				tap(() => {
+					this.adSlot.emitEvent(universalAdPackage.SLOT_UNSTICKED_STATE);
+					transition('transition');
+				}),
+				takeUntil(this.unsubscribe$),
+			)
+			.subscribe();
+	}
+
+	async onLeave(): Promise<void> {
+		this.unsubscribe$.next();
+	}
+}
