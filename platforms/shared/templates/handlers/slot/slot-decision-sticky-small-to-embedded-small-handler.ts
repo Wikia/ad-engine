7946f2c6a77f8930d65cfe688e3e3d0f9e3b938f
Paulina Gralak <pgralak@wikia-inc.com>
2021-06-17 17:27:03 +0200

    [ADEN-11079] ucp-mobile: UAP 3.0 - fix transitions between embedded and sticky states

--- platforms/shared/templates/handlers/slot/slot-decision-sticky-small-to-embedded-small-handler.ts
+++ platforms/shared/templates/handlers/slot/slot-decision-sticky-small-to-embedded-small-handler.ts
@@ -15,7 +15,7 @@ export class SlotDecisionStickySmallToEmbeddedSmallHandler implements TemplateSt
 			.pipe(
 				startWith({}),
 				filter(
-					() => window.scrollY < this.manager.getAdSlotTopOffset() + this.manager.getNavbarHeight(),
+					() => window.scrollY < this.manager.getAdSlotTopOffset() - this.manager.getNavbarHeight(),
 				),
 				tap(() => transition('embeddedSmall')),
 				takeUntil(this.unsubscribe$),
