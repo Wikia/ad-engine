44454ee12239822373243860e0ebf923c12b3c47
Paulina Gralak <pgralak@wikia-inc.com>
2021-06-21 16:16:32 +0200

    [ADEN-11079] ucp-mobile: UAP 3.0 CR addressed - rename file

--- platforms/shared/index.ts
+++ platforms/shared/index.ts
@@ -59,7 +59,7 @@ export * from './templates/handlers/slot/slot-decision-sticky-big-to-embedded-bi
 export * from './templates/handlers/slot/slot-decision-sticky-small-to-embedded-small-handler';
 export * from './templates/handlers/slot/slot-hidden-handler';
 export * from './templates/handlers/slot/slot-offset-resolved-to-none-handler';
-export * from './templates/handlers/slot/slot-position-embedded-big-handler';
+export * from './templates/handlers/slot/slot-position-embedded-handler';
 export * from './templates/handlers/slot/slot-position-sticky-big-handler';
 export * from './templates/handlers/slot/slot-position-sticky-small-handler';
 export * from './templates/handlers/slot/slot-size-impact-handler';
