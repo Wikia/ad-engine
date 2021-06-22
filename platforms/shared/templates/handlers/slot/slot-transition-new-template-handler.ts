50d271b97f30ef289ccb4ddaadc73dcbdb67c4e7
preludek <tomasz.felczak89@gmail.com>
2021-06-22 08:43:34 +0200

    ADEN-11077 WiP working animations

--- platforms/shared/templates/handlers/slot/slot-transition-new-template-handler.ts
+++ platforms/shared/templates/handlers/slot/slot-transition-new-template-handler.ts
@@ -38,13 +38,21 @@ export class SlotTransitionNewTemplateHandler implements TemplateStateHandler {
 	}
 
 	private animate(): Observable<unknown> {
-		const duration = universalAdPackage.SLIDE_OUT_TIME;
+		const duration = this.calcAnimationDuration();
 		this.manipulator
 			.element(this.adSlot.getElement())
 			.setProperty('transition', `all ${duration}ms ${universalAdPackage.CSS_TIMING_EASE_IN_CUBIC}`)
-			.setProperty('marginTop', `-${this.reader.getSlotHeightResolved() * 2}px`);
+			.setProperty('marginTop', `-${this.reader.getSlotHeightResolved()}px`);
 
 		return from(utils.wait(duration));
+	}
+
+	private calcAnimationDuration(): number {
+		const heightResolved = this.reader.getSlotHeightResolved();
+		const distance = this.reader.getNavbarOffsetResolvedToNone();
+		const distanceFraction = (heightResolved - distance) / heightResolved;
+
+		return distanceFraction * universalAdPackage.SLIDE_OUT_TIME;
 	}
 
 	async onLeave(): Promise<void> {
