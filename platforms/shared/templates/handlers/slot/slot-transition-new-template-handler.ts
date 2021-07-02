2b4ae8fa641ec513a34e0f09a256564f3c10592c
Paulina Gralak <pgralak@wikia-inc.com>
2021-07-02 12:30:33 +0200

    [ADEN-11092] ucp-mobile UAP 3.0 - PoC transition state

--- platforms/shared/templates/handlers/slot/slot-transition-new-template-handler.ts
+++ platforms/shared/templates/handlers/slot/slot-transition-new-template-handler.ts
@@ -1,53 +1,15 @@
-import {
-	AdSlot,
-	TEMPLATE,
-	TemplateStateHandler,
-	TemplateTransition,
-	universalAdPackage,
-	utils,
-} from '@wikia/ad-engine';
-import { Inject, Injectable } from '@wikia/dependency-injection';
-import { from, Observable, Subject } from 'rxjs';
-import { takeUntil, tap } from 'rxjs/operators';
-import { DomManipulator } from '../../helpers/manipulators/dom-manipulator';
-import { ScrollCorrector } from '../../helpers/scroll-corrector';
-import { UapDomReader } from '../../helpers/uap-dom-reader';
+import { TemplateStateHandler, TemplateTransition, universalAdPackage } from '@wikia/ad-engine';
+import { Injectable } from '@wikia/dependency-injection';
+import { UapDomManager } from '../../helpers/uap-dom-manager';
 
 @Injectable({ autobind: false })
 export class SlotTransitionNewTemplateHandler implements TemplateStateHandler {
-	private unsubscribe$ = new Subject<void>();
-
-	constructor(
-		@Inject(TEMPLATE.SLOT) private adSlot: AdSlot,
-		private scrollCorrector: ScrollCorrector,
-		private manipulator: DomManipulator,
-		private reader: UapDomReader,
-	) {}
+	constructor(private manager: UapDomManager) {}
 
 	async onEnter(transition: TemplateTransition<'embeddedResolved'>): Promise<void> {
-		this.animate()
-			.pipe(
-				tap(() => {
-					const correction = this.scrollCorrector.useScrollCorrection();
-
-					transition('embeddedResolved').then(correction);
-				}),
-				takeUntil(this.unsubscribe$),
-			)
-			.subscribe();
-	}
-
-	private animate(): Observable<unknown> {
-		const duration = universalAdPackage.SLIDE_OUT_TIME;
-		this.manipulator
-			.element(this.adSlot.getElement().parentElement)
-			.setProperty('transition', `top ${duration}ms ${universalAdPackage.CSS_TIMING_EASE_IN_CUBIC}`)
-			.setProperty('top', `-${this.reader.getSlotHeightResolved() * 2}px`);
-
-		return from(utils.wait(duration));
-	}
-
-	async onLeave(): Promise<void> {
-		this.unsubscribe$.next();
+		this.manager.addClassToAdSlotPlaceholder('bfaa-transition');
+		setTimeout(() => {
+			transition('embeddedResolved');
+		}, universalAdPackage.SLIDE_OUT_TIME);
 	}
 }
