bad7c42cc3cd254b949a043fc9836ea47bbc2c75
preludek <tomasz.felczak89@gmail.com>
2021-07-07 12:52:29 +0200

    ADEN-11112 moving things here and there

--- platforms/shared/templates/handlers/video/video-no-viewability-handler.ts
+++ platforms/shared/templates/handlers/video/video-no-viewability-handler.ts
@@ -1,11 +1,13 @@
 import { AdSlot, TEMPLATE, TemplateStateHandler } from '@wikia/ad-engine';
 import { Inject, Injectable } from '@wikia/dependency-injection';
-import { from, fromEvent, Observable } from 'rxjs';
+import { from, fromEvent, Observable, Subject } from 'rxjs';
 import { filter, mergeMap, take, tap } from 'rxjs/operators';
 import { PlayerRegistry } from '../../helpers/player-registry';
 
 @Injectable({ autobind: false })
 export class VideoNoViewabilityHandler implements TemplateStateHandler {
+	private unsubscribe$ = new Subject<void>();
+
 	constructor(
 		@Inject(TEMPLATE.SLOT) private adSlot: AdSlot,
 		private playerRegistry: PlayerRegistry,
@@ -38,5 +40,9 @@ export class VideoNoViewabilityHandler implements TemplateStateHandler {
 
 	private getViewabilityStream(): Observable<unknown> {
 		return from(this.adSlot.viewed);
+	}
+
+	async onLeave(): Promise<void> {
+		this.unsubscribe$.next();
 	}
 }
