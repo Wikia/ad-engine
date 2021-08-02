7cc2c80ce70c53fd0cf4072d693913d6e6a2b677
preludek <tomasz.felczak89@gmail.com>
2021-08-02 10:52:51 +0200

    ADEN-11119 change deprecated method for communication service

--- src/ad-tracking/click-tracker.ts
+++ src/ad-tracking/click-tracker.ts
@@ -1,38 +1,51 @@
-import { AdSlot, eventService, FuncPipeline, FuncPipelineStep, utils } from '@ad-engine/core';
+import { communicationService, globalAction, ofType } from '@ad-engine/communication';
+import { AdSlot, FuncPipeline, FuncPipelineStep, slotService, utils } from '@ad-engine/core';
+import { props } from 'ts-action';
+
+const logGroup = 'ad-click-tracker';
+
+const adSlotLoadedEvent = globalAction(
+	'[AdEngine] Ad Slot event',
+	props<{ adSlotName: string; status: string; event: string }>(),
+);
 
 class AdClickTracker {
 	private pipeline = new FuncPipeline<any>();
 
-	private logGroup = 'ad-click-tracker';
-
 	register(middleware: any): void {
-		eventService.on(AdSlot.SLOT_RENDERED_EVENT, (slot: AdSlot) => {
-			this.addClickTrackingListeners(middleware, slot);
-		});
+		communicationService.action$
+			.pipe(ofType(adSlotLoadedEvent))
+			.subscribe(async ({ event, adSlotName }) => {
+				if (event === AdSlot.SLOT_RENDERED_EVENT) {
+					this.addClickTrackingListeners(middleware, adSlotName);
+					utils.logger(logGroup, 'registered');
+				}
+			});
 	}
 
-	private addClickTrackingListeners(middleware: FuncPipelineStep<any>, slot: AdSlot): void {
-		const slotName = slot.getSlotName();
-		const iframeElement = slot.getIframe();
-		const slotElement = slot.getElement();
+	private addClickTrackingListeners(middleware: FuncPipelineStep<any>, slotName): void {
+		const adSlot = slotService.get(slotName);
+		const iframeElement: HTMLIFrameElement = adSlot.getIframe();
+		const slotElement: HTMLElement = adSlot.getElement();
 
-		if (!slot || !iframeElement) {
-			utils.logger(this.logGroup, `Slot ${slotName} has no iframe.`);
+		if (!adSlot || !iframeElement) {
+			utils.logger(logGroup, `Slot ${slotName} has no iframe.`);
 			return;
 		}
 
 		if (!typeof iframeElement.contentWindow) {
-			utils.logger(this.logGroup, `Slot ${slotName} is served in safeframe.`);
+			utils.logger(logGroup, `Slot ${slotName} is served in safeframe.`);
 			return;
 		}
+
 		const iframeBody: HTMLElement = iframeElement.contentWindow.document.body;
 
 		if (iframeBody && slotElement) {
 			slotElement.addEventListener('click', () => {
-				this.handleClickEvent(middleware, slot);
+				this.handleClickEvent(middleware, adSlot);
 			});
 			iframeBody.addEventListener('click', () => {
-				this.handleClickEvent(middleware, slot);
+				this.handleClickEvent(middleware, adSlot);
 			});
 		}
 	}
