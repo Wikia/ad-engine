5989f317e4045aa5a1d4652b429dc2f83e02a027
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2021-08-11 15:54:12 +0200

    ADEN-10876 Changing Instant Config endpoints (#1088)
    Co-authored-by: Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>

--- platforms/ucp/ad-context.ts
+++ platforms/ucp/ad-context.ts
@@ -91,7 +91,7 @@ export const basicContext = {
 			endpoint: '/wikia.php?controller=AdEngine&method=postLog',
 		},
 		instantConfig: {
-			endpoint: 'https://services.fandom.com/icbm/api/config?app=oasis',
+			endpoint: 'https://services.fandom.com/icbm/api/config?app=fandomdesktop',
 			fallback: fallbackInstantConfig,
 		},
 		nielsen: {
