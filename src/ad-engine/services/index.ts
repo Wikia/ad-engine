5f33b8e540639798a36ece3462517223b98616de
Szymon Nawrocki <snawrocki@fandom.com>
2021-10-20 09:32:40 +0200

    ADEN-11290 Add PlaceholderService path to services->index.ts

--- src/ad-engine/services/index.ts
+++ src/ad-engine/services/index.ts
@@ -12,6 +12,7 @@ export * from './message-bus';
 export * from './scroll-speed-calculator';
 export * from './session-cookie';
 export * from './cookie-storage-adapter';
+export * from './placeholder-service';
 export * from './slot-data-params-updater';
 export * from './slot-injector';
 export * from './slot-repeater';
