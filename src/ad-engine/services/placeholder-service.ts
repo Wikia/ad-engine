b94eec742c62198ccf7bd1fb59a7b9786bbbbe36
Szymon Nawrocki <snawrocki@fandom.com>
2021-10-20 09:28:11 +0200

    ADEN-11290 Create basic implementation of PlaceholderService

---
+++ src/ad-engine/services/placeholder-service.ts
@@ -0,0 +1,42 @@
+class PlaceholderService {
+	stopLoading = (slotName: string, withHide: string = ''): void => {
+		const className = `.${slotName.replace('_', '-')}`;
+		const placeholder: HTMLElement = document.querySelector(className);
+
+		placeholder?.classList.remove('is-loading');
+
+		if (withHide === 'placeholder') {
+			placeholder?.setAttribute('style', 'display: none');
+		} else if (withHide === 'parent') {
+			placeholder?.parentElement?.setAttribute('style', 'display: none');
+		}
+	};
+
+	addAdvertisementLabel = (className: string): void => {
+		const classElements: NodeListOf<HTMLElement> = document.querySelectorAll(className);
+
+		classElements?.forEach((element) => {
+			const div = document.createElement('div');
+			div.className = 'ae-translatable-label';
+			div.innerText = 'Advertisement';
+			element.appendChild(div);
+		});
+	};
+
+	removeAdLabel = (slotName: string): void => {
+		const parentElement =
+			slotName !== 'top_leaderboard'
+				? document.querySelector(`#${slotName}`).parentElement
+				: document.querySelector('.top-ads-container');
+
+		let adLabel: HTMLElement;
+		for (const child of parentElement.children as any) {
+			if (child.className.includes('ae-translatable-label')) {
+				adLabel = child;
+			}
+		}
+		adLabel?.classList.add('hide');
+	};
+}
+
+export const placeholderService = new PlaceholderService();
