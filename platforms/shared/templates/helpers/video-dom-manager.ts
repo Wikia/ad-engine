fb827a4019a9c572131f56a5395b4191f65fda96
Jakub Banaś <jbanas@fandom.com>
2021-10-28 09:34:37 +0200

    ADEN-11262 fix console errors

--- platforms/shared/templates/helpers/video-dom-manager.ts
+++ platforms/shared/templates/helpers/video-dom-manager.ts
@@ -38,6 +38,9 @@ export class VideoDomManager {
 
 	private setVideoSize(video: PorvataPlayer, props: UapVideoSize): void {
 		video.resize(props.width, props.height);
+		if (!props) {
+			return;
+		}
 
 		const videoOverlay = video.dom.getPlayerContainer().parentElement;
 		const thumbnail = this.params.thumbnail;
