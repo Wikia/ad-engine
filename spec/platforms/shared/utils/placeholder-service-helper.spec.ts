f1f403d3b4e81afed473b31b02b60e9a9e14d926
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2021-11-26 12:41:36 +0100

    ADEN-10981 first test for shouldAddMessageBox

--- spec/platforms/shared/utils/placeholder-service-helper.spec.ts
+++ spec/platforms/shared/utils/placeholder-service-helper.spec.ts
@@ -1,51 +1,69 @@
 import { expect } from 'chai';
+import { createSandbox } from 'sinon';
 
 import { PlaceholderServiceHelper } from '../../../../platforms/shared/utils/placeholder-service-helper';
 
 describe('placeholder service helper', () => {
-	function getMockElement(hidden = false): HTMLElement {
+	function getMockElement(containsClass = false): HTMLElement {
 		return {
 			classList: {
 				add: () => {},
-				contains: () => hidden,
+				contains: () => containsClass,
 				remove: () => {},
 			},
 		} as any;
 	}
 
-	let placeholderHelper;
+	describe('placeholder', () => {
+		let placeholderHelper;
 
-	beforeEach(() => {
-		placeholderHelper = new PlaceholderServiceHelper();
+		beforeEach(() => {
+			placeholderHelper = new PlaceholderServiceHelper();
+		});
+
+		it('shouldDisplayPlaceholder returns true when slot is rendered and forced success', () => {
+			const actionEventMock = 'slotRendered';
+			const actionPayloadMock = 'forced_success';
+
+			expect(
+				placeholderHelper.shouldDisplayPlaceholder(actionEventMock, actionPayloadMock),
+			).to.equal(true);
+		});
+
+		it('shouldDisplayPlaceholder returns false for rendered successfully slot', () => {
+			const actionEventMock = 'slotRendered';
+			const actionPayloadMock = 'success';
+
+			expect(
+				placeholderHelper.shouldDisplayPlaceholder(actionEventMock, actionPayloadMock),
+			).to.equal(false);
+		});
+
+		it('shouldHidePlaceholder returns true when placeholder does not contain the right class', () => {
+			const placeholderMock: HTMLElement = getMockElement(false);
+
+			expect(placeholderHelper.shouldHidePlaceholder(placeholderMock)).to.equal(true);
+		});
+
+		it('shouldHidePlaceholder returns true when placeholder does contain the right class', () => {
+			const placeholderMock: HTMLElement = getMockElement(true);
+
+			expect(placeholderHelper.shouldHidePlaceholder(placeholderMock)).to.equal(false);
+		});
 	});
 
-	it('shouldDisplayPlaceholder returns true when slot is rendered and forced success', () => {
-		const actionEventMock = 'slotRendered';
-		const actionPayloadMock = 'forced_success';
+	describe('message', () => {
+		it('should message box appear when it was not added before, slot is collapsed and is not a top_leaderboard', () => {
+			const sandbox = createSandbox();
+			const actionEventMock = 'collapse';
+			const placeholderHelper = new PlaceholderServiceHelper();
 
-		expect(placeholderHelper.shouldDisplayPlaceholder(actionEventMock, actionPayloadMock)).to.equal(
-			true,
-		);
-	});
+			sandbox.stub(placeholderHelper, 'isMessageBoxAlreadyAdded').returns(false);
+			sandbox.stub(placeholderHelper, 'isItTopLeaderboard').returns(false);
 
-	it('shouldDisplayPlaceholder returns false for rendered successfully slot', () => {
-		const actionEventMock = 'slotRendered';
-		const actionPayloadMock = 'success';
-
-		expect(placeholderHelper.shouldDisplayPlaceholder(actionEventMock, actionPayloadMock)).to.equal(
-			false,
-		);
-	});
-
-	it('shouldHidePlaceholder returns true when placeholder does not contain the right class', () => {
-		const placeholderMock: HTMLElement = getMockElement(false);
-
-		expect(placeholderHelper.shouldHidePlaceholder(placeholderMock)).to.equal(true);
-	});
-
-	it('shouldHidePlaceholder returns true when placeholder does contain the right class', () => {
-		const placeholderMock: HTMLElement = getMockElement(true);
-
-		expect(placeholderHelper.shouldHidePlaceholder(placeholderMock)).to.equal(false);
+			expect(placeholderHelper.shouldAddMessageBox(actionEventMock, getMockElement(true))).to.equal(
+				true,
+			);
+		});
 	});
 });
