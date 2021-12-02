861761d9ed174d60ee793ac4ee27500771e52aa8
Tomasz Felczak <43118980+preludek@users.noreply.github.com>
2021-12-02 13:16:33 +0100

    ADEN-11405 change code prio for native ads (#1193)

--- platforms/shared/templates/handlers/sticky-tlb/sticky-tlb-blocking-handler.ts
+++ platforms/shared/templates/handlers/sticky-tlb/sticky-tlb-blocking-handler.ts
@@ -1,7 +1,6 @@
 import {
 	AdSlot,
 	context,
-	events,
 	slotService,
 	TEMPLATE,
 	TemplateStateHandler,
@@ -22,7 +21,6 @@ export class StickyTlbBlockingHandler implements TemplateStateHandler {
 			this.logger('Disabling incontent_player and affiliate_slot');
 			slotService.disable('incontent_player', 'hivi-collapse');
 			slotService.disable('affiliate_slot', 'hivi-collapse');
-			this.adSlot.emitEvent(events.TOP_LEADERBOARD_STICKED);
 			transition('initial');
 		} else {
 			this.adSlot.emitEvent(universalAdPackage.SLOT_STICKINESS_DISABLED);
