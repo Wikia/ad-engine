acdba8cf028c69ccd69926a3eadc6b738e574dcc
Szymon Nawrocki <snawrocki@fandom.com>
2021-12-10 15:56:54 +0100

    ADEN-11412 Remove MessageBox tests from this file

--- spec/platforms/shared/utils/placeholder-service-helper.spec.ts
+++ spec/platforms/shared/utils/placeholder-service-helper.spec.ts
@@ -1,5 +1,4 @@
 import { expect } from 'chai';
-import { createSandbox } from 'sinon';
 
 import { PlaceholderServiceHelper } from '../../../../platforms/shared/utils/placeholder-service-helper';
 
@@ -49,58 +48,6 @@ describe('placeholder service helper', () => {
 			const placeholderMock: HTMLElement = getMockElement(true);
 
 			expect(placeholderHelper.shouldHidePlaceholder(placeholderMock)).to.equal(false);
-		});
-	});
-
-	describe('Message Box', () => {
-		const sandbox = createSandbox();
-		let placeholderHelper: PlaceholderServiceHelper;
-		let actionEventMock = 'collapse';
-
-		beforeEach(() => {
-			placeholderHelper = new PlaceholderServiceHelper();
-		});
-
-		afterEach(() => {
-			sandbox.restore();
-		});
-
-		it('is added - when it was not added before, slot is collapsed and is not a top_leaderboard', () => {
-			sandbox.stub(placeholderHelper, 'isMessageBoxAlreadyAdded').returns(false);
-			sandbox.stub(placeholderHelper, 'isItTopLeaderboard').returns(false);
-
-			expect(placeholderHelper.shouldAddMessageBox(actionEventMock, getMockElement(true))).to.equal(
-				true,
-			);
-		});
-
-		it("is not added - when slot status is different than 'collapse'", () => {
-			actionEventMock = 'success';
-
-			sandbox.stub(placeholderHelper, 'isMessageBoxAlreadyAdded').returns(false);
-			sandbox.stub(placeholderHelper, 'isItTopLeaderboard').returns(false);
-
-			expect(placeholderHelper.shouldAddMessageBox(actionEventMock, getMockElement(true))).to.equal(
-				false,
-			);
-		});
-
-		it('is not added - when message box already exists on the page', () => {
-			sandbox.stub(placeholderHelper, 'isMessageBoxAlreadyAdded').returns(true);
-			sandbox.stub(placeholderHelper, 'isItTopLeaderboard').returns(false);
-
-			expect(placeholderHelper.shouldAddMessageBox(actionEventMock, getMockElement(true))).to.equal(
-				false,
-			);
-		});
-
-		it('is not added - when collapsed slot is top_leaderboard', () => {
-			sandbox.stub(placeholderHelper, 'isMessageBoxAlreadyAdded').returns(false);
-			sandbox.stub(placeholderHelper, 'isItTopLeaderboard').returns(true);
-
-			expect(placeholderHelper.shouldAddMessageBox(actionEventMock, getMockElement(true))).to.equal(
-				false,
-			);
 		});
 	});
 
