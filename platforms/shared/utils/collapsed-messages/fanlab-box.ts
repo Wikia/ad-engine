5cb8e6f36f4107dca1feeda8b8e13af3ac82d830
Szymon Nawrocki <snawrocki@fandom.com>
2021-12-13 15:16:24 +0100

    ADEN-11412 Add FanLab class configuration

--- platforms/shared/utils/collapsed-messages/fanlab-box.ts
+++ platforms/shared/utils/collapsed-messages/fanlab-box.ts
@@ -1,7 +1,11 @@
 import { MessageBox } from './message-box';
 
-export class FanlabBox extends MessageBox {
+export class FanLabBox extends MessageBox {
 	constructor() {
 		super();
+		this.type = 'FANLAB';
+		this.redirectUrl = 'https://www.fandom.com/fanlab/';
+		this.buttonText = 'Sign Up';
+		this.messageText = 'Join our Fan Lab community.';
 	}
 }
