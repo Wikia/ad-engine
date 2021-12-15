e92fe97ec9f22c01957bd20e617f96bed368564d
Szymon Nawrocki <snawrocki@fandom.com>
2021-12-15 15:55:36 +0100

    ADEN-11412 Add tests for checking message box index

--- spec/platforms/shared/utils/collapsed-messages/message-box-service.spec.ts
+++ spec/platforms/shared/utils/collapsed-messages/message-box-service.spec.ts
@@ -1,3 +1,4 @@
+import { AdSlot } from '@wikia/ad-engine';
 import { expect } from 'chai';
 import { createSandbox } from 'sinon';
 import { MessageBoxService } from '../../../../../platforms/shared';
@@ -13,7 +14,7 @@ describe('Message Box Service', () => {
 		} as any;
 	}
 
-	describe('Test if message box should be added', () => {
+	describe('Test shouldAddMessageBox() method', () => {
 		const sandbox = createSandbox();
 		let messageBoxService: MessageBoxService;
 		let actionEventMock: string;
@@ -27,7 +28,7 @@ describe('Message Box Service', () => {
 			sandbox.restore();
 		});
 
-		it('is added - when slot is collapsed & is not a top/bottom leaderboard', () => {
+		it('MB is added - when slot is collapsed & is not a top/bottom leaderboard', () => {
 			sandbox.stub(messageBoxService, 'isTopLeaderboard').returns(false);
 			sandbox.stub(messageBoxService, 'isBottomLeaderoard').returns(false);
 
@@ -36,7 +37,7 @@ describe('Message Box Service', () => {
 			);
 		});
 
-		it("is not added - when slot status is different than 'collapse'", () => {
+		it("MB is not added - when slot status is different than 'collapse'", () => {
 			actionEventMock = 'success';
 
 			sandbox.stub(messageBoxService, 'isTopLeaderboard').returns(false);
@@ -47,7 +48,7 @@ describe('Message Box Service', () => {
 			);
 		});
 
-		it('is not added - when collapsed slot is top_leaderboard', () => {
+		it('MB is not added - when collapsed slot is top_leaderboard', () => {
 			sandbox.stub(messageBoxService, 'isTopLeaderboard').returns(true);
 			sandbox.stub(messageBoxService, 'isBottomLeaderoard').returns(false);
 
@@ -56,13 +57,31 @@ describe('Message Box Service', () => {
 			);
 		});
 
-		it('is not added - when collapsed slot is bottom_leaderboard', () => {
+		it('MB is not added - when collapsed slot is bottom_leaderboard', () => {
 			sandbox.stub(messageBoxService, 'isTopLeaderboard').returns(false);
 			sandbox.stub(messageBoxService, 'isBottomLeaderoard').returns(true);
 
 			expect(messageBoxService.shouldAddMessageBox(actionEventMock, getMockElement(true))).to.equal(
 				false,
 			);
+		});
+	});
+
+	describe('Test if message box indexes are changed correctly', () => {
+		const placeholderMock: HTMLElement = document.createElement('div');
+		const adSlotMock: AdSlot = new AdSlot({ id: 'top_leaderboard' });
+		const messageBoxService: MessageBoxService = new MessageBoxService();
+
+		it('index changes to 1 - when Register box is added', () => {
+			expect(messageBoxService.getCurrentTypeIndex()).to.equal(0);
+			messageBoxService.addMessageBox(placeholderMock, adSlotMock);
+			expect(messageBoxService.getCurrentTypeIndex()).to.equal(1);
+		});
+
+		it('index changes to 2 - when Fan Lab box is added', () => {
+			expect(messageBoxService.getCurrentTypeIndex()).to.equal(1);
+			messageBoxService.addMessageBox(placeholderMock, adSlotMock);
+			expect(messageBoxService.getCurrentTypeIndex()).to.equal(2);
 		});
 	});
 });
