6e17067f04f88af3e5aa8225fbc00f4b70da15f0
Szymon Nawrocki <snawrocki@fandom.com>
2021-12-20 15:27:02 +0100

    ADEN-11421 Add parameters to NewsletterBox class

--- platforms/shared/utils/collapsed-messages/newsletter-box.ts
+++ platforms/shared/utils/collapsed-messages/newsletter-box.ts
@@ -3,5 +3,25 @@ import { MessageBox } from './message-box';
 export class NewsletterBox extends MessageBox {
 	constructor() {
 		super();
+		this.type = 'NEWSLETTER';
+		this.redirectUrl = this.buildUrl();
+		this.buttonText = 'Sign up';
+		this.messageText = `Sign up today<br/>to receive weekly newsletters from Fandom`;
 	}
+
+	private buildUrl = (): string => {
+		const basicUrl = 'http://linkst.fandom.com/join/6n2/fandomemailsign-uppage-wikibar?';
+		const utm_source = 'fandom';
+		const utm_medium = 'ctr';
+		const utm_campaign = 'collapse_message_newsletter';
+		const utm_content = 'button';
+		const params = [
+			`utm_source=${utm_source}`,
+			`utm_medium=${utm_medium}`,
+			`utm_campaign=${utm_campaign}`,
+			`utm_content=${utm_content}`,
+		];
+
+		return basicUrl + params.join('&');
+	};
 }
