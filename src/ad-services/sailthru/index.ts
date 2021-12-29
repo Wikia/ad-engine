0175ce80554c3f55561494957bf646b5a2fa5abc
Szymon Nawrocki <snawrocki@fandom.com>
2021-12-29 16:06:18 +0100

    ADEN-11413 Move Sailthru url to variable

--- src/ad-services/sailthru/index.ts
+++ src/ad-services/sailthru/index.ts
@@ -1,8 +1,8 @@
 import { utils } from '@ad-engine/core';
 
 const ACCT_ID = 'c9322e218da57e71a4965ae18fecbefa';
-
 const logGroup = 'sailthru';
+export const packageUrl = 'https://ak.sail-horizon.com/spm/spm.v1.min.js';
 
 function loadScript(path: string): Promise<Event> {
 	return utils.scriptLoader.loadScript(path, 'text/javascript', true, 'first');
@@ -10,13 +10,11 @@ function loadScript(path: string): Promise<Event> {
 
 class Sailthru {
 	call(): Promise<void> {
-		const path = 'https://ak.sail-horizon.com/spm/spm.v1.min.js';
-
 		utils.logger(logGroup, 'loading');
 
 		window.Sailthru = window.Sailthru || {};
 
-		return loadScript(path).then(() => {
+		return loadScript(packageUrl).then(() => {
 			window.Sailthru.init({ customerId: ACCT_ID });
 			utils.logger(logGroup, 'ready');
 		});
