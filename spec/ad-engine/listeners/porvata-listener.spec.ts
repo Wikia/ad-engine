09582c90d5dbf81852a17f7f6019a48997aa921c
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-01-11 14:58:56 +0100

    ADEN-11483 CR change: use custom type instead of any

--- spec/ad-engine/listeners/porvata-listener.spec.ts
+++ spec/ad-engine/listeners/porvata-listener.spec.ts
@@ -1,9 +1,17 @@
-import { context, slotService } from '@wikia/ad-engine';
-import { PorvataListener } from '@wikia/ad-products/video/porvata/porvata-listener';
+import { context, slotService, VideoData, VideoEventListener } from '@wikia/ad-engine';
+import {
+	PorvataListener,
+	PorvataListenerParams,
+} from '@wikia/ad-products/video/porvata/porvata-listener';
 import { expect } from 'chai';
 import { createSandbox, spy } from 'sinon';
 
-function getListener(): any {
+interface PorvataEventTestListener extends VideoEventListener {
+	dispatchedEvents: [];
+	onEvent(eventName: string, params: PorvataListenerParams, data: VideoData): void;
+}
+
+function getListener(): PorvataEventTestListener {
 	return {
 		dispatchedEvents: [],
 		onEvent(eventName, params, data): void {
