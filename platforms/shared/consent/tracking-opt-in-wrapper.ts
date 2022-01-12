839693854b389ebbca06751dba4e4214dfaa2db6
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2022-01-12 14:12:38 +0100

    ADEN-11445 Removing CMP logic (moved to TrackingOptIn) (#1205)
    * ADEN-11445 Removing CMP logic (moved to TrackingOptIn)
    
    * ADEN-11445 Refactor after applying event service changes
    
    Co-authored-by: Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>

--- platforms/shared/consent/tracking-opt-in-wrapper.ts
+++ platforms/shared/consent/tracking-opt-in-wrapper.ts
@@ -1,39 +1,11 @@
-import * as installCMPStub from '@iabtcf/stub';
 import {
+	CcpaSignalPayload,
 	communicationService,
 	context,
-	globalAction,
-	InstantConfigService,
-	ofType,
-	utils,
+	eventsRepository,
+	GdprConsentPayload,
+	utils
 } from '@wikia/ad-engine';
-import { take } from 'rxjs/operators';
-import { props } from 'ts-action';
-
-interface OptInInstances {
-	gdpr: any;
-	ccpa: any;
-}
-
-interface GdprConsentPayload {
-	gdprConsent: boolean;
-	geoRequiresConsent: boolean;
-}
-
-interface CcpaSignalPayload {
-	ccpaSignal: boolean;
-	geoRequiresSignal: boolean;
-}
-
-const setOptIn = globalAction(
-	'[AdEngine OptIn] set opt in',
-	props<GdprConsentPayload & CcpaSignalPayload>(),
-);
-
-const setOptInInstances = globalAction(
-	'[AdEngine OptIn] set opt in instances',
-	props<OptInInstances>(),
-);
 
 const logGroup = 'tracking-opt-in-wrapper';
 
@@ -44,130 +16,27 @@ class TrackingOptInWrapper {
 	constructor() {
 		window.ads = window.ads || ({} as MediaWikiAds);
 
-		// Install temporary stub until full CMP will be ready
-		if (typeof window.__tcfapi === 'undefined') {
-			installCMPStub();
-		}
-
 		this.installConsentQueue();
 	}
 
 	async init(): Promise<void> {
-		if (!window.navigator.cookieEnabled) {
-			return this.handleDisabledCookies();
-		}
-
-		await this.loadTrackingOptInLibrary();
-		await this.handleLibraryLoaded();
-	}
-
-	private handleDisabledCookies(): void {
-		utils.logger(logGroup, 'Cookies are disabled. Ignoring CMP and USAPI consent checks.');
-		this.dispatchConsents({
-			gdprConsent: true,
-			geoRequiresConsent: true,
-			ccpaSignal: false,
-			geoRequiresSignal: true,
-		});
-	}
-
-	private async loadTrackingOptInLibrary(): Promise<void> {
-		const instantConfig = await InstantConfigService.init();
-
-		await utils.scriptLoader.loadScript(instantConfig.get('icTrackingOptInLibraryUrl'));
-	}
-
-	private async handleLibraryLoaded(): Promise<void> {
-		const optInInstances = await this.initInstances();
-		const gdpr = this.getConsent(optInInstances);
-		const ccpa = this.getSignal(optInInstances);
-
-		this.dispatchConsents({ ...gdpr, ...ccpa });
-		this.dispatchInstances(optInInstances);
-	}
-
-	private initInstances(): Promise<OptInInstances> {
 		return new Promise(async (resolve) => {
-			utils.logger(logGroup, 'Modal library loaded');
+			utils.logger(logGroup, 'Waiting for consents');
 
-			const optInInstances: OptInInstances = window.trackingOptIn.default({
-				isSubjectToCcpa: window.ads.context && window.ads.context.opts.isSubjectToCcpa,
-				onAcceptTracking: () => {
-					utils.logger(logGroup, 'GDPR Consent');
-					resolve(optInInstances);
-				},
-				onRejectTracking: () => {
-					utils.logger(logGroup, 'GDPR Non-consent');
-					resolve(optInInstances);
-				},
-				zIndex: 9999999,
+			communicationService.on(eventsRepository.AD_ENGINE_CONSENT_READY, (payload) => {
+				this.setConsents(payload);
+				resolve();
 			});
 		});
 	}
 
-	private getConsent(optInInstances: OptInInstances): GdprConsentPayload {
-		// Nothing is needed if the geo does not require any consent
-		if (!optInInstances.gdpr.geoRequiresTrackingConsent()) {
-			return {
-				gdprConsent: true,
-				geoRequiresConsent: false,
-			};
-		}
+	private setConsents(consents: GdprConsentPayload & CcpaSignalPayload): void {
+		utils.logger(logGroup, 'TrackingOptIn consents', consents);
 
-		if (optInInstances.gdpr.hasUserConsented() === undefined) {
-			return {
-				gdprConsent: false,
-				geoRequiresConsent: true,
-			};
-		}
-
-		const gdprConsent = optInInstances.gdpr.hasUserConsented();
-
-		utils.logger(logGroup, `User consent: ${gdprConsent}`);
-
-		return {
-			gdprConsent,
-			geoRequiresConsent: true,
-		};
-	}
-
-	private getSignal(optInInstances: OptInInstances): CcpaSignalPayload {
-		// Nothing is needed if the geo does not require any consent
-		if (!optInInstances.ccpa.geoRequiresUserSignal()) {
-			return {
-				ccpaSignal: false,
-				geoRequiresSignal: false,
-			};
-		}
-
-		if (optInInstances.ccpa.hasUserProvidedSignal() === undefined) {
-			return {
-				ccpaSignal: true,
-				geoRequiresSignal: true,
-			};
-		}
-
-		const ccpaSignal = optInInstances.ccpa.hasUserProvidedSignal();
-
-		utils.logger(logGroup, `User signal: ${ccpaSignal}`);
-
-		return {
-			ccpaSignal,
-			geoRequiresSignal: true,
-		};
-	}
-
-	private dispatchConsents(consents: GdprConsentPayload & CcpaSignalPayload): void {
 		context.set('options.trackingOptIn', consents.gdprConsent);
 		context.set('options.geoRequiresConsent', consents.geoRequiresConsent);
 		context.set('options.optOutSale', consents.ccpaSignal);
 		context.set('options.geoRequiresSignal', consents.geoRequiresSignal);
-
-		communicationService.dispatch(setOptIn(consents));
-	}
-
-	private dispatchInstances(optInInstances: OptInInstances): void {
-		communicationService.dispatch(setOptInInstances(optInInstances));
 	}
 
 	/**
@@ -183,7 +52,7 @@ class TrackingOptInWrapper {
 				window.ads.consentQueue.push(callback);
 			});
 
-		communicationService.action$.pipe(ofType(setOptIn), take(1)).subscribe((consents) => {
+		communicationService.on(eventsRepository.AD_ENGINE_CONSENT_READY, (consents) => {
 			window.ads.consentQueue.onItemFlush((callback) => {
 				console.warn(
 					`[AdEngine] You are using deprecated API to get consent.\nPlease use PostQuecast action "[AdEngine OptIn] set opt in" instead.`,
