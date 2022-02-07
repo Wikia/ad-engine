9728a4c4146bff1b2032441985cfb7d39226e377
Tomasz Felczak <43118980+preludek@users.noreply.github.com>
2022-02-07 13:42:01 +0100

    ADEN-11425 add Anyclip partner (#1254)

---
+++ src/ad-services/anyclip/index.ts
@@ -0,0 +1,42 @@
+import { context, utils } from '@ad-engine/core';
+
+const logGroup = 'Anyclip';
+
+class Anyclip {
+	get params(): Record<string, string> {
+		return {
+			pubname: context.get('services.anyclip.pubname'),
+			widgetname: context.get('services.anyclip.widgetname'),
+		};
+	}
+
+	isEnabled(): boolean {
+		return context.get('services.anyclip.enabled');
+	}
+
+	call(): Promise<void> {
+		if (!this.isEnabled()) {
+			utils.logger(logGroup, 'Anyclip player is disabled');
+
+			return Promise.resolve();
+		}
+
+		this.loadPlayerAsset();
+	}
+
+	private loadPlayerAsset() {
+		const libraryUrl = 'https://player.anyclip.com/anyclip-widget/lre-widget/prod/v1/src/lre.js';
+		const incontentPlayerContainer = document.getElementById('incontent_player');
+
+		utils.logger(logGroup, 'loading Anyclip asset', libraryUrl);
+
+		return utils.scriptLoader
+			.loadScript(libraryUrl, 'text/javascript', true, incontentPlayerContainer, this.params)
+			.then(() => {
+				incontentPlayerContainer.classList.remove('hide');
+				utils.logger(logGroup, 'ready');
+			});
+	}
+}
+
+export const anyclip = new Anyclip();
