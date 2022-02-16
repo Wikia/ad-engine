62d7901b216275830a93a03c739941344aa82ab4
preludek <tomasz.felczak89@gmail.com>
2022-02-16 15:39:53 +0100

    ADEN-lodash-test remove isEmpty

--- src/ad-services/silver-surfer/silver-surfer.loader.ts
+++ src/ad-services/silver-surfer/silver-surfer.loader.ts
@@ -1,5 +1,4 @@
 import { utils } from '@ad-engine/core';
-import isEmpty from 'lodash/isEmpty';
 
 const logGroup = 'silver-surfer';
 
@@ -22,7 +21,7 @@ class SilverSurferServiceLoader {
 		return window.SilverSurferSDK.getUserProfile()
 			.then(
 				(response: UserProfile) => {
-					if (!isEmpty(response)) {
+					if (Object.keys(response).length !== 0) {
 						utils.logger(logGroup, 'successful response');
 
 						return response;
