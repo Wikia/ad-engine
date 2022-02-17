85d37f7be68738c4ae5447d7b9dc69f1b18f165b
preludek <tomasz.felczak89@gmail.com>
2022-02-17 08:50:53 +0100

    ADEN-11597 branch name change, revert import because compiler

--- spec/ad-products/video/jwplayer/streams/jwplayer-stream-state.spec.ts
+++ spec/ad-products/video/jwplayer/streams/jwplayer-stream-state.spec.ts
@@ -6,8 +6,7 @@ import {
 } from '@wikia/ad-products/video/jwplayer/streams/jwplayer-stream-state';
 import { JwpStatelessEvent } from '@wikia/ad-products/video/jwplayer/streams/jwplayer-stream-stateless';
 import { expect } from 'chai';
-import uniq from 'lodash/uniq';
-import uniqBy from 'lodash/uniqBy';
+import { uniq, uniqBy } from 'lodash';
 import { Observable, Subject } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 import { createSandbox } from 'sinon';
