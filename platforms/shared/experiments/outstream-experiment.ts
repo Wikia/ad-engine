33aa093d8a66beb625ca7c8e9dd9d5f7c26683f4
Lukasz Szwajkowski <lszwajkowski@fandom.com>
2022-02-18 07:44:59 +0100

    ADEN-11560 added experiments base classes and tests

--- platforms/shared/experiments/outstream-experiment.ts
+++ platforms/shared/experiments/outstream-experiment.ts
@@ -1,27 +1,20 @@
-import { InstantConfigService } from '@wikia/ad-engine';
+import { InstantConfigServiceInterface } from '@wikia/ad-engine';
+import { MultivariantExperiment } from './experiment';
 
-export class OutstreamExperiment {
-	private readonly enabled: boolean;
-	private readonly chosenPlayer: string;
-
-	constructor(protected instantConfig: InstantConfigService) {
-		this.enabled = this.instantConfig.get('icOutstreamExperimentEnabled', false);
-		this.chosenPlayer = this.instantConfig.get('icOutstreamExperiment', 'distroscale');
+export class OutstreamExperiment extends MultivariantExperiment {
+	constructor(instantConfig: InstantConfigServiceInterface) {
+		super(instantConfig, 'icOutstreamExperimentEnabled', 'icOutstreamExperiment', 'distroscale');
 	}
 
 	isExco(): boolean {
-		return this.isPlayerEnabled('exco');
+		return this.isVariantSelected('exco');
 	}
 
 	isConnatix(): boolean {
-		return this.isPlayerEnabled('connatix');
+		return this.isVariantSelected('connatix');
 	}
 
 	isAnyclip(): boolean {
-		return this.isPlayerEnabled('anyclip');
-	}
-
-	private isPlayerEnabled(playerName: string): boolean {
-		return this.enabled && this.chosenPlayer === playerName;
+		return this.isVariantSelected('anyclip');
 	}
 }
