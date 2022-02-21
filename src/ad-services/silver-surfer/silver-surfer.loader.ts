8117ab18b6b9a540d58af6bedb41e6ad29d69da2
preludek <tomasz.felczak89@gmail.com>
2022-02-21 17:58:53 +0100

    ADEN-11597 removed uniq and uniqBy

--- src/ad-services/silver-surfer/silver-surfer.loader.ts
+++ src/ad-services/silver-surfer/silver-surfer.loader.ts
@@ -21,11 +21,7 @@ class SilverSurferServiceLoader {
 		return window.SilverSurferSDK.getUserProfile()
 			.then(
 				(response: UserProfile) => {
-					if (
-						typeof response === 'object' &&
-						response !== null &&
-						Object.keys(response).length !== 0
-					) {
+					if (response && typeof response === 'object' && Object.keys(response).length !== 0) {
 						utils.logger(logGroup, 'successful response');
 
 						return response;
