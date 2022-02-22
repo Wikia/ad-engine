659fc4e7e37104fb9ac4b6d90b7c142fa4de2ccf
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2022-02-22 18:51:10 +0100

    ADEN-11519 Cleanup after header and footer unification (#1282)
    Co-authored-by: soniackozubek <83713961+soniackozubek@users.noreply.github.com>

--- platforms/ucp-mobile/templates/configs/register-ucp-mobile-uap-dom-elements.ts
+++ platforms/ucp-mobile/templates/configs/register-ucp-mobile-uap-dom-elements.ts
@@ -3,10 +3,8 @@ import { TemplateDependency } from '@wikia/ad-engine';
 
 export function registerUcpMobileUapDomElements(): TemplateDependency[] {
 	return [
-		// ADEN-11573: Cleanup after switch
-		{ bind: NAVBAR, value: document.querySelector('.mobile-global-navigation,#globalNavigation') },
+		{ bind: NAVBAR, value: document.querySelector('.mobile-global-navigation') },
 		{ bind: PAGE, value: document.body },
-		// ADEN-11573: Cleanup after switch
-		{ bind: FOOTER, value: document.querySelector('.global-footer,.wds-global-footer') },
+		{ bind: FOOTER, value: document.querySelector('.global-footer') },
 	];
 }
