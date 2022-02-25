1ea0664f4a5e6f5723bdbcd2745b56f77b2178e4
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-02-25 16:55:32 +0100

    ADEN-11578 update the test after merging dev/origin

--- spec/ad-engine/providers/nativo/nativo-provider.spec.ts
+++ spec/ad-engine/providers/nativo/nativo-provider.spec.ts
@@ -1,17 +1,11 @@
 import { expect } from 'chai';
 import { adSlotFake } from '../../ad-slot-fake';
 import { NativoProvider } from '../../../../src/ad-engine/providers/nativo/nativo-provider';
-import { spy } from 'sinon';
 
 describe('Nativo Provider', () => {
 	beforeEach(() => {
 		window.ntv = {
 			cmd: ['mocked-element'],
-			Events: {
-				PubSub: {
-					subscribe: spy(),
-				},
-			},
 		} as NativoSdk;
 	});
 
@@ -20,7 +14,7 @@ describe('Nativo Provider', () => {
 		expect(nativo.getQueue()).to.equal(window.ntv.cmd);
 	});
 
-	it('filling slot subscribes to events and pushes SDK call to Nativo queue', () => {
+	it('filling slot pushes SDK call to Nativo command queue', () => {
 		const nativo = new NativoProvider(window.ntv);
 		nativo.fillIn(adSlotFake as any);
 		expect(nativo.getQueue().length).to.equal(2);
