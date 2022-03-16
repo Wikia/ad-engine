a8d3ac9003cac01a9ee834fbdbc5fcddc75b5343
Katarzyna Pudełko <bckatarzyna@gmail.com>
2022-03-16 14:38:37 +0100

    ADEN-11544 | Add Live Connect service (#1302)
    * ADEN-11544 | Add Live Connect service
    
    * ADEN-11544 | CR fixes
    
    Co-authored-by: Katarzyna Basiukajc <kbasiukajc@wikia-inc.com>
    Co-authored-by: Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
    Co-authored-by: Karol Wajs <90037542+kwfandom@users.noreply.github.com>
    Co-authored-by: soniackozubek <83713961+soniackozubek@users.noreply.github.com>

--- platforms/shared/setup/base-context.setup.ts
+++ platforms/shared/setup/base-context.setup.ts
@@ -163,6 +163,7 @@ export class BaseContextSetup implements DiProcess {
 		context.set('services.nativo.enabled', this.instantConfig.get('icNativo'));
 		context.set('services.sailthru.enabled', this.instantConfig.get('icSailthru'));
 		context.set('services.stroer.enabled', this.instantConfig.get('icStroer'));
+		context.set('services.liveConnect.enabled', this.instantConfig.get('icLiveConnect'));
 
 		this.setupOutstreamPlayers();
 	}
---
+++ spec/ad-services/live-connect/live-connect.spec.ts
@@ -0,0 +1,61 @@
+import { expect } from 'chai';
+import { createSandbox } from 'sinon';
+import { context, utils } from '../../../src/ad-engine';
+import { liveConnect } from '../../../src/ad-services';
+
+describe('LiveConnect', () => {
+	const sandbox = createSandbox();
+	let loadScriptStub;
+
+	beforeEach(() => {
+		loadScriptStub = sandbox
+			.stub(utils.scriptLoader, 'loadScript')
+			.returns(Promise.resolve({} as any));
+		context.set('services.liveConnect.enabled', true);
+		context.set('options.trackingOptIn', true);
+		context.set('options.optOutSale', false);
+		context.set('wiki.targeting.directedAtChildren', false);
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+	});
+
+	it('Live Connect is called', async () => {
+		await liveConnect.call();
+
+		expect(loadScriptStub.called).to.equal(true);
+	});
+
+	it('Live Connect can be disabled', async () => {
+		context.set('services.liveConnect.enabled', false);
+
+		await liveConnect.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('Live Connect not called when user is not opted in', async () => {
+		context.set('options.trackingOptIn', false);
+
+		await liveConnect.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('Live Connect not called when user has opted out sale', async () => {
+		context.set('options.optOutSale', true);
+
+		await liveConnect.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('Live Connect not called on kid wikis', async () => {
+		context.set('wiki.targeting.directedAtChildren', true);
+
+		await liveConnect.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+});
