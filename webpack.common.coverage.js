ca8107856f153e5fdcfce1200c32277e4eed2877
Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>
2022-03-25 19:25:09 +0100

    Update webpack.common.coverage.js
    Instead copying and pasting whole "webpack.common.js" file I think it would be better to merge them with "webpack-merge" package. 
    @Tomasz-at-Fandom what do you think?

--- webpack.common.coverage.js
+++ webpack.common.coverage.js
@@ -1,66 +1,27 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
-const { getTypeScriptLoader } = require('./configs/webpack-app.config');
-const { mergeCompilerOptionsPaths } = require('./configs/merge-compiler-options-paths');
-const pkg = require('./package.json');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-const include = [
-	path.resolve(__dirname, 'src'),
-	path.resolve(__dirname, 'platforms'),
-	path.resolve(__dirname, 'spec'),
-];
-
-const paths = mergeCompilerOptionsPaths([
-	path.resolve(__dirname, 'src/tsconfig.json'),
-	path.resolve(__dirname, 'platforms/tsconfig.json'),
-	path.resolve(__dirname, 'spec/tsconfig.json'),
-]);
-
-module.exports = () => ({
-	mode: 'development',
-
-	context: __dirname,
-
-	resolve: {
-		extensions: ['.ts', '.js', '.json'],
-		modules: [...include, 'node_modules'],
-		plugins: [new TsConfigPathsPlugin({ paths })],
-		fallback: {
-			util: require.resolve('util/'),
-		},
-	},
-
+const config = () => ({
 	module: {
 		rules: [
-			getTypeScriptLoader({
-				include,
-				paths,
-			}),
-			{
-				test: /\.s?css$/,
-				include,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-			{
-				test: path.resolve(__dirname, 'src/ad-engine/log-version.ts'),
-				loader: 'string-replace-loader',
-				options: {
-					search: '<?= PACKAGE(version) ?>',
-					replace: pkg.version,
-				},
-			},
-			// This is only for establishing tests code coverage
 			{
 				test: /\.ts$/,
-				enforce: 'post', // needed if you're using Babel
+				enforce: 'post',
 				include: [path.resolve(`src/`), path.resolve(`platforms/`)], // instrument only sources with Istanbul
 				exclude: [/node_modules/],
 				loader: 'istanbul-instrumenter-loader',
 				options: {
-					esModules: true, // needed if you're using Babel
+					esModules: true,
 				},
 			},
 		],
 	},
 });
+
+module.exports = () => {
+	return merge(
+		common(),
+		config(),
+	);
+};
