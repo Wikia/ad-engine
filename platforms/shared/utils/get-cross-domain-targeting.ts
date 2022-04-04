9a7d2bc33e4df766ef41cdb12543dd9bd91a946c
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2022-04-04 11:28:14 +0200

    ADEN-11693 CR change: rename the cookie name

--- platforms/shared/utils/get-cross-domain-targeting.ts
+++ platforms/shared/utils/get-cross-domain-targeting.ts
@@ -2,6 +2,6 @@ import { CookieStorageAdapter } from '@wikia/ad-engine';
 
 export function getCrossDomainTargeting(crossDomainStorage: CookieStorageAdapter) {
 	return {
-		'cortex-visitor': crossDomainStorage.getItem('adeng-cortex-rpg-visited') ? 'yes' : 'no',
+		'cortex-visitor': crossDomainStorage.getItem('cortex-rpg-visited') ? 'yes' : 'no',
 	};
 }
