ad5e7dd5aeb0869b9eaf0f14e579f6202c0b1745
FanTomek <78414581+Tomasz-at-Fandom@users.noreply.github.com>
2022-04-07 09:28:01 +0200

    ADEN-11701 fix: put UAP sequence on hold when FV is loaded (#1333)
    * ADEN-11701 fix: put UAP sequence on hold when FV is loaded
    
    * ADEN-11701 test: SequenceContinuationHandler UAP on FV
    
    * ADEN-11701 feat: recognize UAP SM base on SEQUENTIAL_MESSAGING_STARTED event payload
    
    Co-authored-by: Karol Wajs <90037542+kwfandom@users.noreply.github.com>

--- spec/platforms/shared/sequential-messaging/domain/sequence-start-handler.spec.ts
+++ spec/platforms/shared/sequential-messaging/domain/sequence-start-handler.spec.ts
@@ -1,13 +1,17 @@
 import { assert } from 'sinon';
 import { SequenceStartHandler } from '../../../../../platforms/shared/sequential-messaging/domain/sequence-start-handler';
 import { makeUserStateStoreSpy } from '../test_doubles/state-store.spy';
-import { UserSequentialMessageState } from '../../../../../platforms/shared/sequential-messaging/domain/data-structures/user-sequential-message-state';
+import {
+	SequenceState,
+	UserSequentialMessageState,
+} from '../../../../../platforms/shared/sequential-messaging/domain/data-structures/user-sequential-message-state';
 
 const sequenceId = '5928558921';
 const sampleWidth = 970;
 const sampleHeight = 250;
+const uap = true;
 const userState: UserSequentialMessageState = {
-	5928558921: { stepNo: 1, width: sampleWidth, height: sampleHeight },
+	5928558921: new SequenceState(1, sampleWidth, sampleHeight, uap),
 };
 
 describe('Sequence Start Handler', () => {
@@ -16,7 +20,7 @@ describe('Sequence Start Handler', () => {
 
 		const sh = new SequenceStartHandler(userStateStore);
 
-		sh.startSequence(sequenceId, sampleWidth, sampleHeight);
+		sh.startSequence(sequenceId, sampleWidth, sampleHeight, uap);
 
 		assert.calledOnce(userStateStore.set);
 		assert.calledWith(userStateStore.set, userState);
