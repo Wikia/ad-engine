2e75d9f5f1c38e01066733c479f78aadd967c083
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-04-14 15:53:20 +0200

    ADEN-11741 remove tests that will not work because of timing

--- src/ad-engine/providers/nativo/nativo.ts
+++ src/ad-engine/providers/nativo/nativo.ts
@@ -59,9 +59,12 @@ export class Nativo {
 			Nativo.log(logGroup, `Slot disabled: ${slotName}`);
 
 			// the Nativo ad server responses with a JS that searches for specific IDs that's why the removal here
-			if (this.context.get('targeting.skin') !== 'ucp_mobile') {
+			if (
+				this.context.get('targeting.skin') !== 'ucp_mobile' &&
+				document.getElementById(slotName)
+			) {
 				document.getElementById(slotName).id = '';
-			} else {
+			} else if (document.getElementById(slotName)) {
 				// on mobile the slots are injected before we know they're disabled, so we need to remove the node
 				document.getElementById(slotName).remove();
 			}
