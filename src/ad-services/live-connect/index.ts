e06a06ba21437c07fed3aeb23ac119b824dde4a6
Katarzyna Basiukajc <kbasiukajc@wikia-inc.com>
2022-04-29 13:18:50 +0200

    ADEN-11726 | Add logging to live connect

--- src/ad-services/live-connect/index.ts
+++ src/ad-services/live-connect/index.ts
@@ -15,6 +15,18 @@ class LiveConnect {
 		);
 	}
 
+	private logInfoWithSpecifiedQf(featureName: string, qf: number): void {
+		window.liQ.resolve(
+			(nonId) => {
+				console.log(`liveConnect ${featureName} with qf=${qf}:`, nonId[featureName]);
+			},
+			(err) => {
+				console.error(err);
+			},
+			{ qf: qf, resolve: featureName },
+		);
+	}
+
 	call(): void {
 		if (!this.isEnabled()) {
 			utils.logger(logGroup, 'disabled');
@@ -23,11 +35,20 @@ class LiveConnect {
 
 		if (!this.isLoaded) {
 			utils.logger(logGroup, 'loading');
+			const qf = parseFloat(utils.queryString.get('qf')) || 0.1;
 
 			utils.scriptLoader
 				.loadScript(liveConnectScriptUrl, 'text/javascript', true, 'first')
 				.then(() => {
 					utils.logger(logGroup, 'loaded');
+					window.liQ.resolve((nonId) => {
+						console.log('liveConnect nonId object', nonId);
+					});
+					this.logInfoWithSpecifiedQf('md5', qf);
+					this.logInfoWithSpecifiedQf('sha1', qf);
+					this.logInfoWithSpecifiedQf('sha2', qf);
+					this.logInfoWithSpecifiedQf('age', qf);
+					this.logInfoWithSpecifiedQf('gender', qf);
 				});
 
 			this.isLoaded = true;
