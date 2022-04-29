c4a4b39a2f467480e36fe4c096e2e33d0ddccb10
Jakub Banaś <jbanas@fandom.com>
2022-04-29 08:38:36 +0200

    add icbm config

--- src/ad-engine/services/slot-reloader.ts
+++ src/ad-engine/services/slot-reloader.ts
@@ -1,27 +1,52 @@
-import { communicationService } from '@ad-engine/communication';
+import { communicationService, eventsRepository, UapLoadStatus } from '@ad-engine/communication';
 import { AdSlot } from '../models';
 import { logger } from '../utils';
 import { context, GptProvider } from '@ad-engine/core';
 
 const logGroup = 'slot-reloader';
 
+interface Config {
+	timeoutMS?: number;
+	slots?: string[];
+}
+const defaultConfig: Config = {
+	timeoutMS: 30000,
+	slots: [],
+};
+
+async function isUAP(): Promise<boolean> {
+	return new Promise((resolve) => {
+		communicationService.on(eventsRepository.AD_ENGINE_UAP_LOAD_STATUS, (action: UapLoadStatus) => {
+			resolve(action.isLoaded);
+		});
+	});
+}
+
 class SlotReloader {
 	log(message) {
 		logger(logGroup, message);
 	}
-	init(): void {
-		if (context.get('services.durationMedia.enabled')) {
+
+	async init() {
+		const config: Config = {
+			...defaultConfig,
+			...context.get('services.slotRefresher.config') as Config,
+		};
+		debugger;
+		const enabled = config.slots.length > 0;
+		if (enabled && (await isUAP())) {
 			this.log('disabled');
 			return;
 		}
 
 		communicationService.onSlotEvent(AdSlot.SLOT_VIEWED_EVENT, ({ slot }) => {
+			if (!config.slots.includes(slot.getSlotName())) return;
 			setTimeout(() => {
 				if (slot.isEnabled()) {
 					this.log(`refreshing ${slot.getSlotName()}.`);
 					GptProvider.refreshSlot(slot);
 				}
-			}, 30000);
+			}, config.timeoutMS);
 		});
 
 		this.log('enabled');
