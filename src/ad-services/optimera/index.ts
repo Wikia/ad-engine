a089ffecda8bff49c815b3232fab25e194a868a0
Szymon Nawrocki <snawrocki@fandom.com>
2022-05-09 14:40:29 +0200

    ADEN-11695 Use AdEngine tools to load Optimer ascript

--- src/ad-services/optimera/index.ts
+++ src/ad-services/optimera/index.ts
@@ -1,10 +1,17 @@
 import { context, utils } from '@ad-engine/core';
-import { communicationService, eventsRepository } from '@ad-engine/communication';
 
 const logGroup = 'optimera';
 const CLIENT_ID = '82';
 
 class Optimera {
+	private oDevice = context.get('state.isMobile') ? 'mo' : 'de';
+	private oDv = [CLIENT_ID, 'top_leaderboard', 'top_boxad', 'bottom_leaderboard'];
+	private oVa: any = {
+		top_leaderboard: ['NULL'],
+		top_boxad: ['NULL'],
+		bottom_leaderboard: ['NULL'],
+	};
+
 	isEnabled(): boolean {
 		return context.get('services.optimera.enabled');
 	}
@@ -16,75 +23,91 @@ class Optimera {
 		}
 
 		try {
-			await this.loadScript();
-			utils.logger(logGroup, 'loaded');
-
+			this.loadGlobalVariablesScript();
+			await this.loadScoreFileScript();
+			await this.updateOvaVariable();
+			await this.loadOpsScript();
 			this.setTargeting();
 		} catch (e) {
 			utils.logger(logGroup, 'loading failed.', e.message);
 		}
 	}
 
-	getHardcodedScript(): HTMLScriptElement {
+	loadGlobalVariablesScript(): void {
 		const script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.innerHTML = `
-			var oDv = ['${CLIENT_ID}', 'top_leaderboard', 'top_boxad', 'bottom_leaderboard'];
-			var oVa = {'top_leaderboard':['NULL'],'top_boxad':['NULL'],'bottom_leaderboard':['NULL']};
-			var oDevice = ${JSON.stringify(context.get('state.isMobile') ? 'mo' : 'de')};
-			
-			//Do not edit
-			(function() {
-			var optimeraHost = window.location.host;
-			var optimeraPathName = window.location.pathname;
-			var optimeraScript = document.createElement('script');
-			optimeraScript.async = true;
-			optimeraScript.type = 'text/javascript'; 
-			/* IF AVAILABLE: PAGE AD MAP CONFIGURATION */
-			optimeraScript.onload = function() {
-			  if(typeof oDevice == 'string' && typeof oVa.device == 'object' && typeof oVa.device[oDevice] == 'object') {
-			   oVa = oVa.device[oDevice];
-			  }
-			}
-			
-			optimeraScript.src="https://dyv1bugovvq1g.cloudfront.net/"+oDv[0]+"/"+optimeraHost+optimeraPathName+".js";
-			var node = document.getElementsByTagName('script')[0];
-			node.parentNode.insertBefore(optimeraScript, node);
-			
-			var optimeraOpsScript = document.createElement('script');
-			optimeraOpsScript.async = true;
-			optimeraOpsScript.type = 'text/javascript';
-			optimeraOpsScript.src="https://d15kdpgjg3unno.cloudfront.net/oPS.js?cid="+oDv[0];
-			document.head.appendChild(optimeraOpsScript);
-			})();
+			var oDv = ${JSON.stringify(this.oDv)};
+			var oVa = ${JSON.stringify(this.oVa)};
+			var oDevice = ${JSON.stringify(this.oDevice)};
 		`;
-		return script;
+
+		document.head.prepend(script);
+		utils.logger(logGroup, 'global variables loaded');
 	}
 
-	async loadScript(): Promise<void> {
-		document.head.prepend(this.getHardcodedScript());
+	async loadScoreFileScript(): Promise<void> {
+		const optimeraHost = window.location.host;
+		const optimeraPathName = window.location.pathname;
+		const url = `https://dyv1bugovvq1g.cloudfront.net/${CLIENT_ID}/${
+			optimeraHost + optimeraPathName
+		}.js`;
+
+		await utils.scriptLoader.loadScript(url);
+		utils.logger(logGroup, 'score file loaded');
+	}
+
+	// Variable 'oVa' needs to be updated as it gets overwritten after loading score file
+	async updateOvaVariable(): Promise<void> {
+		const conditionMet = await new utils.WaitFor(this.getWaitingCondition, 3, 100).until();
+		if (!conditionMet) {
+			return Promise.reject(new Error('oVa variable not updated'));
+		}
+
+		if (
+			this.oDevice &&
+			typeof window.oVa.device === 'object' &&
+			typeof window.oVa.device[this.oDevice] === 'object'
+		) {
+			this.oVa = window.oVa.device[this.oDevice];
+		} else {
+			this.oVa = window.oVa;
+		}
+
+		utils.logger(logGroup, 'oVa variable updated');
 		return Promise.resolve();
 	}
 
+	async loadOpsScript(): Promise<void> {
+		const url = `https://d15kdpgjg3unno.cloudfront.net/oPS.js?cid=${CLIENT_ID}`;
+
+		await utils.scriptLoader.loadScript(url);
+		utils.logger(logGroup, 'ops script loaded');
+	}
+
 	setTargeting(): void {
-		communicationService.on(eventsRepository.AD_ENGINE_STACK_START, () => {
-			if (!window.oVa) {
-				throw new Error(`Optimera 'oVa' variable is not defined`);
-			} else if (!window.oDv) {
-				throw new Error(`Optimera 'oDv' variable is not defined`);
-			}
-
-			const oVa = window.oVa;
-			const oDv = window.oDv;
-
-			for (let i = 1; i < oDv.length; i++) {
-				this.setSlotTargeting(oDv[i], oVa[oDv[i]]);
-			}
-		});
+		for (let i = 1; i < this.oDv.length; i++) {
+			this.setSlotTargeting(this.oDv[i], this.oVa[this.oDv[i]]);
+		}
+		utils.logger(logGroup, 'slot targeting set');
 	}
 
 	setSlotTargeting(slotName: string, value: string): void {
 		context.set(`slots.${slotName}.targeting.optimera`, value);
+	}
+
+	getWaitingCondition() {
+		if (!window.oVa || !window.oDv) {
+			return false;
+		}
+
+		for (let i = 1; i < window.oDv.length; i++) {
+			if (window.oVa[window.oDv[i]] !== ['NULL']) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
 
