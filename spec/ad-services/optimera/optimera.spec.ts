c8159660542e0fa1030f4fe1e69c96913ff0e76a
Szymon Nawrocki <snawrocki@fandom.com>
2022-05-09 16:10:27 +0200

    ADEN-11695 Add unit tests for Optimera

--- spec/ad-services/optimera/optimera.spec.ts
+++ spec/ad-services/optimera/optimera.spec.ts
@@ -1,7 +1,7 @@
 import { optimera } from '@wikia/ad-services';
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import { createSandbox } from 'sinon';
-import { context } from '../../../src/ad-engine/index';
+import { context, utils } from '../../../src/ad-engine/index';
 
 describe('Optimera', () => {
 	const sandbox = createSandbox();
@@ -9,7 +9,9 @@ describe('Optimera', () => {
 	let loadScriptStub;
 
 	beforeEach(() => {
-		loadScriptStub = sandbox.stub(optimera, 'loadScript').returns(Promise.resolve({} as any));
+		loadScriptStub = sandbox
+			.stub(utils.scriptLoader, 'loadScript')
+			.returns(Promise.resolve({} as any));
 		contextStub = sandbox.stub(context);
 	});
 
@@ -17,7 +19,7 @@ describe('Optimera', () => {
 		sandbox.restore();
 	});
 
-	it('loads optimera script when enabled', async () => {
+	it('loads script when enabled', async () => {
 		contextStub.get.withArgs('services.optimera.enabled').returns(true);
 
 		await optimera.call();
@@ -25,23 +27,11 @@ describe('Optimera', () => {
 		expect(loadScriptStub.called).to.equal(true);
 	});
 
-	it('returns rejected promise when disabled', async () => {
+	it('does not load script when disabled', async () => {
 		contextStub.get.withArgs('services.optimera.enabled').returns(false);
 
-		return await optimera.call().then(
-			() => assert(false),
-			() => assert(true),
-		);
-	});
-
-	it('setTargeting is called when script is laoded', async () => {
-		contextStub.get.withArgs('services.optimera.enabled').returns(true);
-
-		const setTargetingStub = sandbox.stub(optimera, 'setTargeting');
-
 		await optimera.call();
 
-		expect(loadScriptStub.called).to.equal(true);
-		expect(setTargetingStub.called).to.equal(true);
+		expect(loadScriptStub.called).to.equal(false);
 	});
 });
