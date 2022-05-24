c544b6144edae1b95dd765bf9fdc11f8c8ed850e
Tomasz Felczak <43118980+preludek@users.noreply.github.com>
2022-05-24 09:03:32 +0200

    ADEN-11780 add Eyeota service (#1423)
    * ADEN-11780 add eyeota ad-service behind icVar
    
    * ADEN-11780 unit tests
    
    * ADEN-11780 self CR
    
    * ADEN-11780 :see_no_evil: lint pls
    
    * ADEN-11780 self-CR 2
    
    * ADEN-11780
    
    * ADEN-11780 CR notes

---
+++ spec/ad-services/eyeota/eyeota.spec.ts
@@ -0,0 +1,47 @@
+import { eyeota } from '@wikia/ad-services';
+import { expect } from 'chai';
+import { createSandbox } from 'sinon';
+import { context, tcf, utils } from '../../../src/ad-engine/index';
+
+describe('Eyeota', () => {
+	const sandbox = createSandbox();
+	let loadScriptStub;
+
+	beforeEach(() => {
+		window.__tcfapi = window.__tcfapi as WindowTCF;
+
+		loadScriptStub = sandbox
+			.stub(utils.scriptLoader, 'loadScript')
+			.returns(Promise.resolve({} as any));
+
+		sandbox.stub(tcf, 'getTCData').returns(Promise.resolve({ tcString: 'test' }) as any);
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+		context.remove('services.eyeota.enabled');
+		delete window.__tcfapi;
+	});
+
+	it('is called when enabled in the context', async () => {
+		context.set('services.eyeota.enabled', true);
+
+		await eyeota.call();
+
+		expect(loadScriptStub.called).to.equal(true);
+	});
+
+	it('is not called when disabled in the context', async () => {
+		context.set('services.eyeota.enabled', false);
+
+		await eyeota.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+
+	it('constructs proper src', async () => {
+		const src = await eyeota.createScriptSource();
+
+		expect(src).to.equal('https://ps.eyeota.net/pixel?pid=r8rcb20&sid=fandom&gdpr=1&gdpr_consent=test&t=aj');
+	});
+});
