53f3b6cbf48625bb6c5bd4ebf1d477f285fcfd1c
Tomasz Felczak <43118980+preludek@users.noreply.github.com>
2022-05-31 10:46:07 +0200

    ADEN-11768 gam interstitial pls (#1428)
    Co-authored-by: Katarzyna Pudełko <bckatarzyna@gmail.com>

--- src/ad-tracking/click-tracker.ts
+++ src/ad-tracking/click-tracker.ts
@@ -30,6 +30,12 @@ class AdClickTracker {
 			this.addClickTrackingListeners(callback, adSlotName);
 		});
 
+		window.addEventListener('message', (event) => {
+			if (event.data.name === '[GAM iframe] Ad click') {
+				this.handleClickEvent(callback, slotService.get('interstitial'), event.data);
+			}
+		})
+
 		this.eventsToRegister.map((event) => this.addToTracking(event, callback));
 	}
 
