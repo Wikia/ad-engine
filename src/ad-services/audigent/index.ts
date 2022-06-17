8fbb0144e162cfb99abf9410e905f95b314c881c
Szymon Nawrocki <snawrocki@fandom.com>
2022-06-17 16:12:57 +0200

    ADEN-11954 Fix Audigent by waiting for its segments

--- src/ad-services/audigent/index.ts
+++ src/ad-services/audigent/index.ts
@@ -3,7 +3,6 @@ import { context, utils, externalLogger } from '@ad-engine/core';
 
 const logGroup = 'audigent';
 const DEFAULT_AUDIENCE_TAG_SCRIPT_URL = 'https://a.ad.gt/api/v1/u/matches/158';
-const DEFAULT_SEGMENTS_SCRIPT_URL = 'https://seg.ad.gt/api/v1/segments.js';
 
 class Audigent {
 	private isLoaded = false;
@@ -25,44 +24,47 @@ class Audigent {
 
 		const audienceTagScriptUrl =
 			context.get('services.audigent.audienceTagScriptUrl') || DEFAULT_AUDIENCE_TAG_SCRIPT_URL;
-		const segmentsScriptUrl =
-			context.get('services.audigent.segmentsScriptUrl') || DEFAULT_SEGMENTS_SCRIPT_URL;
 
 		if (!this.isLoaded) {
 			utils.logger(logGroup, 'loading');
 			context.set('targeting.AU_SEG', '-1');
 
-			utils.scriptLoader.loadScript(audienceTagScriptUrl, 'text/javascript', true, 'first');
-
 			utils.scriptLoader
-				.loadScript(segmentsScriptUrl, 'text/javascript', true, 'first')
+				.loadScript(audienceTagScriptUrl, 'text/javascript', true, 'first')
 				.then(() => {
+					const conditionMet = new utils.WaitFor(this.segmentsUpdated, 3, 100).until();
+					if (!conditionMet) {
+						return Promise.resolve(false);
+					}
+
 					this.setup();
 					communicationService.emit(eventsRepository.AUDIGENT_LOADED);
+					this.isLoaded = true;
 				});
-			this.isLoaded = true;
 		}
 	}
 
 	setup(): void {
-		if (typeof window['au_seg'] !== 'undefined') {
-			const au_segments = window['au_seg'].segments || [];
-			const limit = context.get('services.audigent.segmentLimit') || 0;
+		const au_segments = window['au_seg'].segments || [];
+		const limit = context.get('services.audigent.segmentLimit') || 0;
 
-			let segments = au_segments.length ? au_segments : 'no_segments';
+		let segments = au_segments.length ? au_segments : 'no_segments';
 
-			if (this.canSliceSegments(segments, limit)) {
-				segments = segments.slice(0, limit);
-			}
-
-			this.trackWithExternalLoggerIfEnabled(segments);
-
-			context.set('targeting.AU_SEG', segments);
+		if (this.canSliceSegments(segments, limit)) {
+			segments = segments.slice(0, limit);
 		}
+
+		this.trackWithExternalLoggerIfEnabled(segments);
+
+		context.set('targeting.AU_SEG', segments);
 	}
 
 	private canSliceSegments(segments: string | [], limit: number): boolean {
 		return limit > 0 && typeof segments !== 'string';
+	}
+
+	private segmentsUpdated(): boolean {
+		return typeof window['au_seg'] !== 'undefined';
 	}
 
 	private trackWithExternalLoggerIfEnabled(segments: string | []) {
