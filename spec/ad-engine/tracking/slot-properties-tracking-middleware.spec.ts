6ab9f862bf52d5dff0eaaa1b3fb92bf0c6c24da2
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2022-07-19 09:42:17 +0200

    ADEN-11994 Removing unused AdInfo and Video tracking parameters (#1464)
    Co-authored-by: Karol Wajs <90037542+kwfandom@users.noreply.github.com>
    Co-authored-by: soniackozubek <83713961+soniackozubek@users.noreply.github.com>

--- spec/ad-engine/tracking/slot-properties-tracking-middleware.spec.ts
+++ spec/ad-engine/tracking/slot-properties-tracking-middleware.spec.ts
@@ -9,7 +9,6 @@ describe('slot-properties-tracking-middleware', () => {
 	beforeEach(() => {
 		context.set('slots.foo.targeting', {
 			rv: 5,
-			wsi: 'ola1',
 		});
 		adSlot = new AdSlot({ id: 'foo' });
 		adSlot.advertiserId = '567';
@@ -41,10 +40,8 @@ describe('slot-properties-tracking-middleware', () => {
 			ad_status: 'success',
 			advertiser_id: '567',
 			creative_id: 123,
-			creative_size: '728x90',
 			kv_pos: 'foo',
 			kv_rv: 5,
-			kv_wsi: 'ola1',
 			order_id: 3,
 			previous: 'value',
 			product_lineitem_id: 789,
