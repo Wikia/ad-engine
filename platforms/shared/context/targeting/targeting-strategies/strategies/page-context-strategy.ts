a6f95b25ba09af047989a6d14ca42e453d2ade3b
Szymon Nawrocki <snawrocki@fandom.com>
2022-07-25 15:11:57 +0200

    ADEN-12028 Add '_p' prefix to page level tags

--- platforms/shared/context/targeting/targeting-strategies/strategies/page-context-strategy.ts
+++ platforms/shared/context/targeting/targeting-strategies/strategies/page-context-strategy.ts
@@ -11,30 +11,54 @@ export class PageContextStrategy extends CommonStrategy implements TargetingStra
 	execute(): Partial<Targeting> {
 		const wiki: MediaWikiAdsContext = context.get('wiki');
 
-		const targeting: Partial<Targeting> = {
+		let targeting: Partial<Targeting> = {
+			s1: utils.targeting.getRawDbName(this.context.site.siteName),
+			s2: this.getAdLayout(this.context.page.pageType || 'article'),
+		};
+
+		const pageContextTags = {
 			artid: this.context.page.articleId ? this.context.page.articleId.toString() : '',
 			age: this.context.page.tags?.age || [],
-			esrb: this.context.site.esrbRating,
 			gnre: this.context.page.tags?.gnre || [],
-			kid_wiki: this.context.site.directedAtChildren ? '1' : '0',
 			lang: this.context.page.lang || 'unknown',
 			media: this.context.page.tags?.media || [],
 			pform: this.context.page.tags?.pform || [],
 			pub: this.context.page.tags?.pub || [],
-			s0: this.context.site.vertical,
-			s0c: this.context.site.categories,
-			s1: utils.targeting.getRawDbName(this.context.site.siteName),
-			s2: this.getAdLayout(this.context.page.pageType || 'article'),
 			sex: this.context.page.tags?.sex || [],
 			theme: this.context.page.tags?.theme || [],
 			tv: this.context.page.tags?.tv || [],
 			wpage: this.context.page.pageName && this.context.page.pageName.toLowerCase(),
 		};
 
+		this.addPagePrefixToValues(pageContextTags);
+
+		const siteContextTags = {
+			esrb: this.context.site.esrbRating,
+			kid_wiki: this.context.site.directedAtChildren ? '1' : '0',
+			s0: this.context.site.vertical,
+			s0c: this.context.site.categories,
+		};
+
+		targeting = { ...targeting, ...pageContextTags, ...siteContextTags };
+
 		if (this.context.site.top1000) {
 			targeting.top = '1k';
 		}
 
 		return this.addCommonParams(targeting, wiki, this.skin);
+	}
+
+	addPagePrefixToValues(targeting: Partial<Targeting>): Partial<Targeting> {
+		for (const [key, value] of Object.entries(targeting)) {
+			if (Array.isArray(value) && value.length > 0) {
+				targeting[key] = value.map((val) => 'p_' + val);
+			}
+
+			if (typeof value === 'string') {
+				targeting[key] = 'p_' + value;
+			}
+		}
+
+		return targeting;
 	}
 }
