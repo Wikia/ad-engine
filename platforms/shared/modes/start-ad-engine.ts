4980f2cbe84e69f9fda819de2bed8e1b378803d7
Janik <djanik@fandom.com>
2022-07-25 08:02:56 +0200

    ADEN-12036 - Working on moving PPID initialisation

--- platforms/shared/modes/start-ad-engine.ts
+++ platforms/shared/modes/start-ad-engine.ts
@@ -2,40 +2,20 @@ import {
 	AdEngine,
 	AdSlot,
 	communicationService,
-	context,
-	eventsRepository,
 	utils,
 } from '@wikia/ad-engine';
+import {IdentityService} from "../../../src/ad-engine/services/identity-service";
 
 let adEngineInstance: AdEngine;
 
-function checkSilverSurferAvailability() {
-	return window.SilverSurferSDK?.isInitialized() && window.SilverSurferSDK?.requestUserPPID;
-}
-
-function setupPPID(): void {
-	if (checkSilverSurferAvailability() && context.get('services.ppid.enabled')) {
-		window.SilverSurferSDK.requestUserPPID();
-
-		communicationService.on(eventsRepository.IDENTITY_RESOLUTION_PPID_UPDATED, ({ ppid }) => {
-			setPPID(ppid);
-		});
-	}
-}
-
-function setPPID(ppid: string) {
-	const tag = window.googletag.pubads();
-	tag.setPublisherProvidedId(ppid);
-	context.set('targeting.ppid', ppid);
-}
-
 export function startAdEngine(inhibitors: Promise<any>[] = []): void {
 	if (!adEngineInstance) {
+		console.time('DJ: PPID Process')
+		IdentityService.getInstance().setupPPID();
 		const GPT_LIBRARY_URL = '//www.googletagservices.com/tag/js/gpt.js';
 
 		utils.logger('gpt-provider', 'loading GPT...');
 		utils.scriptLoader.loadScript(GPT_LIBRARY_URL);
-		setupPPID();
 
 		adEngineInstance = new AdEngine();
 		adEngineInstance.init(inhibitors);
