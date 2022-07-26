3a3e153550e7d5bca1bf881264d16ed87baaa386
Szymon Nawrocki <snawrocki@fandom.com>
2022-07-26 06:59:46 +0200

    ADEN-12028 Refactor targeting in page-level-strategy

---
+++ platforms/shared/context/targeting/targeting-strategies/interfaces/page-level-tags.ts
@@ -0,0 +1,8 @@
+export interface PageLevelTags {
+	gnre: string[];
+	media: string[];
+	pform: string[];
+	pub: string[];
+	theme: string[];
+	tv: string[];
+}
--- platforms/shared/context/targeting/targeting-strategies/strategies/page-context-strategy.ts
+++ platforms/shared/context/targeting/targeting-strategies/strategies/page-context-strategy.ts
@@ -2,6 +2,7 @@ import { TargetingStrategy } from '../interfaces/targeting-strategy';
 import { context, Targeting, utils } from '@wikia/ad-engine';
 import { CommonStrategy } from './common-strategy';
 import { Context } from '../models/context';
+import { PageLevelTags } from '../interfaces/page-level-tags';
 
 export class PageContextStrategy extends CommonStrategy implements TargetingStrategy {
 	constructor(private skin: string, private context: Context) {
@@ -12,34 +13,31 @@ export class PageContextStrategy extends CommonStrategy implements TargetingStra
 		const wiki: MediaWikiAdsContext = context.get('wiki');
 
 		let targeting: Partial<Targeting> = {
+			age: this.context.page.tags?.age || [],
+			artid: this.context.page.articleId ? this.context.page.articleId.toString() : '',
+			esrb: this.context.site.esrbRating,
+			kid_wiki: this.context.site.directedAtChildren ? '1' : '0',
+			lang: this.context.page.lang || 'unknown',
+			sex: this.context.page.tags?.sex || [],
+			s0: this.context.site.vertical,
+			s0c: this.context.site.categories,
 			s1: utils.targeting.getRawDbName(this.context.site.siteName),
 			s2: this.getAdLayout(this.context.page.pageType || 'article'),
-		};
-
-		const pageContextTags = {
-			artid: this.context.page.articleId ? this.context.page.articleId.toString() : '',
-			age: this.context.page.tags?.age || [],
-			gnre: this.context.page.tags?.gnre || [],
-			lang: this.context.page.lang || 'unknown',
-			media: this.context.page.tags?.media || [],
-			pform: this.context.page.tags?.pform || [],
-			pub: this.context.page.tags?.pub || [],
-			sex: this.context.page.tags?.sex || [],
-			theme: this.context.page.tags?.theme || [],
-			tv: this.context.page.tags?.tv || [],
 			wpage: this.context.page.pageName && this.context.page.pageName.toLowerCase(),
 		};
 
-		const siteContextTags = {
-			esrb: this.context.site.esrbRating,
-			kid_wiki: this.context.site.directedAtChildren ? '1' : '0',
-			s0: this.context.site.vertical,
-			s0c: this.context.site.categories,
+		const pageLevelTags: PageLevelTags = {
+			gnre: this.context.page.tags?.gnre || [],
+			media: this.context.page.tags?.media || [],
+			pform: this.context.page.tags?.pform || [],
+			pub: this.context.page.tags?.pub || [],
+			theme: this.context.page.tags?.theme || [],
+			tv: this.context.page.tags?.tv || [],
 		};
 
-		this.addPagePrefixToValues(pageContextTags);
+		this.addPagePrefixToValues(pageLevelTags);
 
-		targeting = { ...targeting, ...pageContextTags, ...siteContextTags };
+		targeting = { ...targeting, ...pageLevelTags };
 
 		if (this.context.site.top1000) {
 			targeting.top = '1k';
@@ -48,17 +46,13 @@ export class PageContextStrategy extends CommonStrategy implements TargetingStra
 		return this.addCommonParams(targeting, wiki, this.skin);
 	}
 
-	addPagePrefixToValues(targeting: Partial<Targeting>): Partial<Targeting> {
-		for (const [key, value] of Object.entries(targeting)) {
+	addPagePrefixToValues(tags: PageLevelTags): PageLevelTags {
+		for (const [key, value] of Object.entries(tags)) {
 			if (Array.isArray(value) && value.length > 0) {
-				targeting[key] = value.map((val) => 'p_' + val);
-			}
-
-			if (typeof value === 'string') {
-				targeting[key] = 'p_' + value;
+				tags[key] = value.map((val) => 'p_' + val);
 			}
 		}
 
-		return targeting;
+		return tags;
 	}
 }
