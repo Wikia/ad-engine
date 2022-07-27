a2275de800a097cff6b69c768ae471f6d12c7a35
Dominik Janik <larsire@users.noreply.github.com>
2022-07-27 13:16:26 +0200

    ADEN-12036 - Improved setting PPID process (#1481)
    * ADEN-12036 - Working on moving PPID initialisation
    
    * ADEN-12036 - Working on moving PPID initialisation
    
    * ADEN-12036 - Working on moving PPID initialisation
    
    * ADEN-12036 - Working on improving PPID initialisation
    
    * ADEN-12036 - Added mechanism to block possibility of infinite loop when SilverSurfer is not available
    
    * ADEN-12036 - Fixed unit tests
    
    * ADEN-12036 - Refactored postponeExecutionUntilSilverSurferLoads to use WaitFor
    
    Co-authored-by: Janik <djanik@fandom.com>
    Co-authored-by: soniackozubek <83713961+soniackozubek@users.noreply.github.com>

--- src/ad-engine/providers/gpt-provider.ts
+++ src/ad-engine/providers/gpt-provider.ts
@@ -5,11 +5,12 @@ import { AdSlot, Targeting } from '../models';
 import {
 	btfBlockerService,
 	context,
+	externalLogger,
 	slotDataParamsUpdater,
 	slotService,
 	trackingOptIn,
 } from '../services';
-import { defer, logger } from '../utils';
+import { defer, logger, WaitFor } from '../utils';
 import { GptSizeMap } from './gpt-size-map';
 import { setupGptTargeting } from './gpt-targeting';
 import { Provider } from './provider';
@@ -32,6 +33,28 @@ export function postponeExecutionUntilGptLoads(method: () => void): any {
 
 			return window.googletag.cmd.push(() => method.apply(this, args));
 		});
+	};
+}
+
+function postponeExecutionUntilSilverSurferLoads(method: () => void): any {
+	const SilverSurferAwaitTime = 100;
+	const SilverSurferAvailabilityTries = 3;
+
+	return function (...args: any): void {
+		new WaitFor(
+			() => window.SilverSurferSDK && window.SilverSurferSDK.isInitialized(),
+			SilverSurferAvailabilityTries,
+			0,
+			SilverSurferAwaitTime,
+		)
+			.until()
+			.then((isSilverSurferLoaded) => {
+				if (isSilverSurferLoaded) {
+					method.call(this, args);
+				} else {
+					externalLogger.log('SilverSurfer loading failed');
+				}
+			});
 	};
 }
 
@@ -211,17 +234,14 @@ export class GptProvider implements Provider {
 		});
 	}
 
-	checkSilverSurferAvailability() {
-		return window.SilverSurferSDK?.isInitialized() && window.SilverSurferSDK?.requestUserPPID;
-	}
-
+	@decorate(postponeExecutionUntilSilverSurferLoads)
 	setupPPID(): void {
-		if (this.checkSilverSurferAvailability() && context.get('services.ppid.enabled')) {
-			window.SilverSurferSDK.requestUserPPID();
-
+		if (window.SilverSurferSDK?.requestUserPPID && context.get('services.ppid.enabled')) {
 			communicationService.on(eventsRepository.IDENTITY_RESOLUTION_PPID_UPDATED, ({ ppid }) => {
 				this.setPPID(ppid);
 			});
+
+			window.SilverSurferSDK.requestUserPPID(context.get('services.ppidAdmsStorage.enabled'));
 		}
 	}
 
