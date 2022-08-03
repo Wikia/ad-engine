56998ad889ec2f28da53cf6510508abe4074b670
Janik <djanik@fandom.com>
2022-08-03 13:54:15 +0200

    ADEN-11166 - Cleaned up

--- src/ad-engine/pipeline/imps/partner-pipeline/partner-pipeline.ts
+++ src/ad-engine/pipeline/imps/partner-pipeline/partner-pipeline.ts
@@ -1,41 +1,16 @@
-import { Container, Injectable } from '@wikia/dependency-injection';
-import { PipelineAdapter, PipelineNext } from '../../pipeline-types';
+import { Injectable } from '@wikia/dependency-injection';
 import { PartnerStepUnion } from './partner-pipeline-types';
-
-@Injectable({ scope: 'Transient' })
-class PartnerPipelineAdapter implements PipelineAdapter<PartnerStepUnion, Promise<any>[]> {
-	async execute(
-		step: PartnerStepUnion,
-		payload: Promise<any>[],
-		next?: PipelineNext<Promise<any>[]>,
-	): Promise<Promise<any>[]> {
-		if (typeof step === 'function') {
-			const instance = step({});
-			instance.execute();
-			payload.push(instance.initialized);
-		} else {
-			step.execute();
-			payload.push(step.initialized);
-		}
-
-		return next(payload);
-	}
-}
 
 @Injectable({ scope: 'Transient' })
 export class PartnerPipeline {
 	steps: PartnerStepUnion[] = [];
-	constructor(container: Container) {
-		container.get(PartnerPipelineAdapter);
-	}
-
 	add(...steps: PartnerStepUnion[]): this {
 		this.steps = steps;
 
 		return this;
 	}
 
-	execute(payload: Promise<any>[] = []): Promise<any> {
+	execute(payload: Promise<void>[] = []): Promise<void[]> {
 		this.steps.forEach((step) => {
 			if (typeof step === 'function') {
 				const instance = step({});
