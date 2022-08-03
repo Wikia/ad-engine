16512f7dc44832ee8981be27508d1f85e107fa9d
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2022-08-03 15:39:45 +0200

    ADEN-12021 Expanding pgRubicon to all slots (#1485)
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

--- platforms/ucp-mobile/bidders/prebid/rubicon-pg.ts
+++ platforms/ucp-mobile/bidders/prebid/rubicon-pg.ts
@@ -3,8 +3,30 @@ export function getRubiconPGContext(): object {
 		enabled: false,
 		accountId: 7450,
 		slots: {
+			mobile_top_leaderboard: {
+				sizes: [[320, 50]],
+				siteId: '403054',
+				zoneId: '2256142',
+			},
 			mobile_in_content: {
 				sizes: [[300, 250]],
+				siteId: '403054',
+				zoneId: '2256142',
+			},
+			bottom_leaderboard: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				siteId: '403054',
+				zoneId: '2256142',
+			},
+			floor_adhesion: {
+				sizes: [
+					[300, 50],
+					[320, 50],
+					[320, 100],
+				],
 				siteId: '403054',
 				zoneId: '2256142',
 			},
