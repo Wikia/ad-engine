7bf1acbaf65565386bd519d044873f406ae3e4bf
Tomasz Felczak <43118980+preludek@users.noreply.github.com>
2022-08-05 11:08:50 +0200

    ADEN-11899 allow icp on sm UAP (#1491)
    * ADEN-11899 allow icp on sm UAP
    
    * ADEN-11899 mvoe SM higher in pipeline
    
    Co-authored-by: Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>

--- platforms/ucp-mobile/ucp-mobile-legacy-setup.ts
+++ platforms/ucp-mobile/ucp-mobile-legacy-setup.ts
@@ -21,9 +21,9 @@ export class UcpMobileLegacySetup {
 			UcpMobilePrebidConfigSetup,
 			UcpMobileA9ConfigSetup,
 			UcpMobileDynamicSlotsSetup,
-			BiddersStateSetup,
 			UcpMobileTemplatesSetup,
 			SequentialMessagingSetup, // SequentialMessagingSetup needs to be after *TemplatesSetup or UAP SM will break
+			BiddersStateSetup,
 			conditional(() => this.noAdsDetector.isAdsMode(), {
 				yes: UcpMobileAdsMode,
 				no: NoAdsMode,
