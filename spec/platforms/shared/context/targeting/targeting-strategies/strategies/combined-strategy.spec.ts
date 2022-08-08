89b564bbe2f8ef5c10b307ef2ac6ba7b751a9921
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2022-08-08 10:05:37 +0200

    ADEN-12041 Combine site and page tags (#1493)
    * ADEN-12041 initial commit with the new strategy
    
    * ADEN-12041 remove s0v and add first test
    
    * ADEN-12041 add a test for site tags only
    
    * ADEN-12041 add a test for page tags only and change logic so it passes
    
    * ADEN-12041 add a test for combined tags
    
    * ADEN-12041 extract addPagePrefixToValues() and reuse in the combined strategy
    
    * ADEN-12041 fix test so it can be run separately
    
    * ADEN-12041 bring back s0v
    
    * ADEN-12041 add more logging
    
    * ADEN-12041 make it possible to run the new strategy
    
    * ADEN-12041 page tags will never have sex, take it from site tags
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

---
+++ spec/platforms/shared/context/targeting/targeting-strategies/strategies/combined-strategy.spec.ts
@@ -0,0 +1,121 @@
+import { expect } from 'chai';
+
+import { context } from '../../../../../../../src/ad-engine';
+import {
+	Context,
+	Site,
+	Page,
+} from '../../../../../../../platforms/shared/context/targeting/targeting-strategies/models/context';
+import { CombinedStrategy } from '../../../../../../../platforms/shared/context/targeting/targeting-strategies/strategies/combined-strategy';
+
+describe('CombinedStrategy execution', () => {
+	beforeEach(() => {
+		context.set('geo.country', 'PL');
+		context.set('wiki.targeting.wikiDbName', 'test');
+		context.set('wiki.targeting.wikiVertical', 'test');
+	});
+
+	afterEach(() => {
+		context.set('geo.country', undefined);
+		context.set('wiki.targeting.wikiDbName', undefined);
+		context.set('wiki.targeting.wikiVertical', undefined);
+	});
+
+	const mockedSkin = 'test';
+	const defaultExpectedTargeting = {
+		age: [],
+		ar: '4:3',
+		artid: '666',
+		dmn: '',
+		esrb: 'ec',
+		geo: 'PL',
+		gnre: [],
+		hostpre: '',
+		is_mobile: '0',
+		kid_wiki: '1',
+		lang: 'pl',
+		media: [],
+		original_host: 'fandom',
+		pform: [],
+		pub: [],
+		s0: 'lifestyle',
+		s0c: [],
+		s0v: 'test',
+		s1: '_test',
+		s2: 'article',
+		sex: [],
+		skin: 'test',
+		theme: [],
+		tv: [],
+		uap: 'none',
+		uap_c: 'none',
+		wpage: 'test',
+	};
+
+	it('Returns empty tags when site and page tags are empty', function () {
+		const mockedContext: Context = new Context(
+			new Site([], true, 'ec', 'test', false, {}, 'lifestyle'),
+			new Page(666, 'pl', 666, 'test', 'all_ads', {}),
+		);
+
+		expect(new CombinedStrategy(mockedSkin, mockedContext).execute()).to.deep.eq(
+			defaultExpectedTargeting,
+		);
+	});
+
+	it('Returns tags when site tags are not empty and page tags are empty', function () {
+		const mockedSiteTags = {
+			gnre: ['test1', 'drama', 'comedy', 'horror'],
+			theme: ['test2', 'superheroes'],
+		};
+		const mockedContext: Context = new Context(
+			new Site([], true, 'ec', 'test', false, mockedSiteTags, 'lifestyle'),
+			new Page(666, 'pl', 666, 'test', 'all_ads', {}),
+		);
+		const expectedTargeting = { ...defaultExpectedTargeting, ...mockedSiteTags };
+
+		expect(new CombinedStrategy(mockedSkin, mockedContext).execute()).to.deep.eq(expectedTargeting);
+	});
+
+	it('Returns tags when site tags are empty and page tags are not empty', function () {
+		const mockedPageTags = {
+			gnre: ['test1', 'drama', 'comedy', 'horror'],
+			theme: ['test2', 'superheroes'],
+		};
+		const mockedContext: Context = new Context(
+			new Site([], true, 'ec', 'test', false, {}, 'lifestyle'),
+			new Page(666, 'pl', 666, 'test', 'all_ads', mockedPageTags),
+		);
+		const expectedTargeting = {
+			...defaultExpectedTargeting,
+			...{ gnre: ['p_test1', 'p_drama', 'p_comedy', 'p_horror'] },
+			...{ theme: ['p_test2', 'p_superheroes'] },
+		};
+
+		expect(new CombinedStrategy(mockedSkin, mockedContext).execute()).to.deep.eq(expectedTargeting);
+	});
+
+	it('Returns combined tags when site and page tags are not empty', function () {
+		const mockedSiteTags = {
+			gnre: ['test1', 'drama', 'comedy', 'horror'],
+			theme: ['test3', 'superheroes'],
+		};
+		const mockedPageTags = {
+			gnre: ['test2', 'drama', 'comedy', 'horror'],
+			theme: ['test4', 'superheroes'],
+		};
+		const mockedContext: Context = new Context(
+			new Site([], true, 'ec', 'test', false, mockedSiteTags, 'lifestyle'),
+			new Page(666, 'pl', 666, 'test', 'all_ads', mockedPageTags),
+		);
+		const expectedTargeting = {
+			...defaultExpectedTargeting,
+			...{
+				gnre: ['test1', 'drama', 'comedy', 'horror', 'p_test2', 'p_drama', 'p_comedy', 'p_horror'],
+			},
+			...{ theme: ['test3', 'superheroes', 'p_test4', 'p_superheroes'] },
+		};
+
+		expect(new CombinedStrategy(mockedSkin, mockedContext).execute()).to.deep.eq(expectedTargeting);
+	});
+});
