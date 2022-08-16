23be409f31e9352e0cd51065dfc38e7b9c5da3d5
Janik <dj@kazan.com.pl>
2022-08-16 09:04:08 +0200

    ADEN-12057 - Removed await for SS SDK

--- src/ad-services/ad-identity/index.ts
+++ src/ad-services/ad-identity/index.ts
@@ -1,18 +1,10 @@
 import { BaseServiceSetup, context } from '@ad-engine/core';
-import { logger, WaitFor } from '../../ad-engine/utils';
+import { logger } from '../../ad-engine/utils';
 
 class AdIdentity extends BaseServiceSetup {
 	logGroup = 'AdIdentity';
 	async setupPPID(): Promise<void> {
 		logger(this.logGroup, 'Awaiting for SS SDK');
-		const SilverSurferAwaitTime = 50;
-		const SilverSurferAvailabilityTries = 10;
-		await new WaitFor(
-			() => window.SilverSurferSDK && window.SilverSurferSDK.isInitialized(),
-			SilverSurferAvailabilityTries,
-			25,
-			SilverSurferAwaitTime,
-		);
 		logger(this.logGroup, 'SS SDK ready');
 		if (window.SilverSurferSDK?.requestUserPPID) {
 			window.SilverSurferSDK.requestUserPPID(context.get('services.ppidAdmsStorage.enabled'));
