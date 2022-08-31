b3b7cb6c6055d5d6e37163a874a1df77692d4409
Damian Sobczak <dsobczak@wikia-inc.com>
2022-08-31 10:35:13 +0200

    ADEN-12064 Dead Space campaign PoC

--- platforms/ucp-desktop/index.ts
+++ platforms/ucp-desktop/index.ts
@@ -9,3 +9,25 @@ window.RLQ.push(async () => {
 
 	platform.execute();
 });
+
+setTimeout(() => {
+	window.scroll({
+		top: 500,
+		behavior: 'smooth',
+	});
+
+	const shadow = document.createElement('div');
+	shadow.className = 'dsShadow';
+	document.body.prepend(shadow);
+
+	const tentacle1 = document.createElement('img');
+	tentacle1.src = 'https://i.imgur.com/fzCV3fJ.png';
+	tentacle1.className = 'dsTent1';
+	document.body.prepend(tentacle1);
+
+	setTimeout(() => {
+		shadow.style.opacity = '1';
+		tentacle1.style.top = '0';
+		tentacle1.style.left = '0';
+	}, 100);
+}, 5000);
