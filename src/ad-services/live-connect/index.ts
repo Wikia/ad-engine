80ff0a46d1b9328de05254491935db3746e097ad
Szymon Nawrocki <snawrocki@fandom.com>
2022-09-19 11:41:46 +0200

    ADEN-12189 Remove calls for 'age' and 'gender' LI calls

--- src/ad-services/live-connect/index.ts
+++ src/ad-services/live-connect/index.ts
@@ -17,8 +17,6 @@ const idConfigMapping: IdConfig[] = [
 	{ id: 'sha256', name: `${partnerName}-sha256`, params: { qf: '0.3', resolve: 'sha256' } },
 	{ id: 'md5', name: `${partnerName}-md5`, params: { qf: '0.3', resolve: 'md5' } },
 	{ id: 'sha1', name: `${partnerName}-sha1`, params: { qf: '0.3', resolve: 'sha1' } },
-	{ id: 'gender', name: `${partnerName}-gender`, params: { qf: '0.3', resolve: 'gender' } },
-	{ id: 'age', name: `${partnerName}-age`, params: { qf: '0.3', resolve: 'age' } },
 ];
 
 class LiveConnect extends BaseServiceSetup {
