6018ff600276ac585ee831636b71069ecc31bb2e
Janik <dj@kazan.com.pl>
2022-09-22 11:05:07 +0200

    ADEN-12139 - Started work on LiveRamp integration

--- src/ad-services/index.ts
+++ src/ad-services/index.ts
@@ -13,6 +13,7 @@ export * from './facebook-pixel';
 export * from './ias-publisher-optimization';
 export * from './instant-config';
 export * from './live-connect';
+export * from './live-ramp';
 export * from './nielsen';
 export * from './sailthru';
 export * from './stroer';
---
+++ src/ad-services/live-ramp/index.ts
@@ -0,0 +1,9 @@
+import { BaseServiceSetup } from '@ad-engine/core';
+
+class LiveRamp extends BaseServiceSetup {
+	call(): void {
+		return;
+	}
+}
+
+export const liveRamp = new LiveRamp();
