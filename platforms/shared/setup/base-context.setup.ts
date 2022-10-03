bea10e70479db7e2d89ff0003c80f8b31938dfe9
Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>
2022-10-03 10:26:10 +0200

    ADEN-12189 Add LI to N&S (#1563)
    * ADEN-12189 Integrate LiveConnect into f2
    
    * ADEN-12189 Remove calls for 'age' and 'gender' LI calls
    
    * ADEN-12189 Remove call for 'sha256'
    
    * ADEN-12189 Remove unnecessary types for LI params
    
    * ADEN-12189 Add LI caching to localStorage
    
    * ADEN-12189 LiveConnect - Caching strategies behind ICBM variable
    
    * ADEN-12189 LiveConnect - Fix unit tests
    
    * ADEN-12189 LiveConnect - Resolve only configs that are not cached
    
    * ADEN-12189 LiveConnect - Unify method names of universalStorage and localCache
    
    * ADEN-12189 LiveConnect - Small CR changes
    
    * ADEN-12189 LiveConnect - Fix shouldLoadScript() method
    
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

--- platforms/shared/setup/base-context.setup.ts
+++ platforms/shared/setup/base-context.setup.ts
@@ -166,6 +166,10 @@ export class BaseContextSetup implements DiProcess {
 			this.instantConfig.get('icInterventionTracking'),
 		);
 		context.set('services.liveConnect.enabled', this.instantConfig.get('icLiveConnect'));
+		context.set(
+			'services.liveConnect.cachingStrategy',
+			this.instantConfig.get('icLiveConnectCachingStrategy'),
+		);
 		context.set('services.nativo.enabled', this.instantConfig.get('icNativo'));
 		context.set('services.nielsen.enabled', this.instantConfig.get('icNielsen'));
 		context.set('services.sailthru.enabled', this.instantConfig.get('icSailthru'));
