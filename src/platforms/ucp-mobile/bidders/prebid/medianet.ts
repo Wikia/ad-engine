4cce10654f7ac867b4fdd2415c4a0453f99e35ef
Damian Sobczak <dsobczak@wikia-inc.com>
2022-10-11 10:12:53 +0200

    ADEN-12160 Adding Media.Net display configuration

---
+++ src/platforms/ucp-mobile/bidders/prebid/medianet.ts
@@ -0,0 +1,32 @@
+export function getMedianetContext(): object {
+	return {
+		enabled: false,
+		slots: {
+			top_leaderboard: {
+				sizes: [
+					[320, 50],
+					[320, 100],
+				],
+				cid: '8CU5JOKX4',
+				crid: '365183821',
+			},
+			top_boxad: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				cid: '8CU5JOKX4',
+				crid: '730485468',
+			},
+			floor_adhesion: {
+				sizes: [
+					[300, 50],
+					[320, 50],
+					[320, 100],
+				],
+				cid: '8CU5JOKX4',
+				crid: '476381130',
+			},
+		},
+	};
+}
