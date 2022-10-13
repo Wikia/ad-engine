266ea828e31dbfa1d2f9de647ae35dd21ca394ce
Damian Sobczak <dsobczak@wikia-inc.com>
2022-10-13 15:36:58 +0200

    ADEN-12161 Adding new bidder Ogury

---
+++ src/platforms/ucp-mobile/bidders/prebid/ogury.ts
@@ -0,0 +1,16 @@
+export function getOguryContext(): object {
+	return {
+		enabled: false,
+		slots: {
+			floor_adhesion: {
+				sizes: [
+					[300, 50],
+					[320, 50],
+					[320, 100],
+				],
+				adUnitId: '65c1e01f-4a93-4e25-a6dd-2d21e4685a0a',
+				assetKey: 'OGY-311D7E535854',
+			},
+		},
+	};
+}
