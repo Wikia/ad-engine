7e840fba88ce391c6f32ac5c6e1abaa7874de371
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2022-10-28 09:45:19 +0200

    Update ogury.ts

--- src/platforms/ucp-mobile/bidders/prebid/ogury.ts
+++ src/platforms/ucp-mobile/bidders/prebid/ogury.ts
@@ -8,7 +8,7 @@ export function getOguryContext(): object {
 					[320, 50],
 					[320, 100],
 				],
-				adUnitId: '65c1e01f-4a93-4e25-a6dd-2d21e4685a0a',
+				adUnitId: '164a1270-d078-013a-6f90-0d319559df69',
 				assetKey: 'OGY-311D7E535854',
 			},
 		},
