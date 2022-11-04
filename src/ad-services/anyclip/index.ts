e26d713ddded65dbedef2b5cea5d03e718fa84d2
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-11-04 18:34:01 +0100

    ADEN-12241 WIP: let's see if it works

--- src/ad-services/anyclip/index.ts
+++ src/ad-services/anyclip/index.ts
@@ -1,6 +1,7 @@
 import { context, utils } from '@ad-engine/core';
 
 const logGroup = 'Anyclip';
+const isSubscribeReady = () => typeof window['lreSubscribe'] !== 'undefined';
 
 class Anyclip {
 	get params(): Record<string, string> {
@@ -35,7 +36,42 @@ class Anyclip {
 			.then(() => {
 				incontentPlayerContainer.classList.remove('hide');
 				utils.logger(logGroup, 'ready');
+
+				this.waitForSubscribeReady().then((isSubscribeReady) => {
+					utils.logger(
+						logGroup,
+						'Anyclip global subscribe function set',
+						isSubscribeReady,
+						window['lreSubscribe'],
+					);
+					isSubscribeReady
+						? this.setupAnyclipListeners()
+						: utils.logger(logGroup, 'Anyclip global subscribe function set');
+				});
 			});
+	}
+
+	private setupAnyclipListeners() {
+		const subscribe = window['lreSubscribe'];
+
+		subscribe((data) => console.log('Anyclip WidgetLoad event data: ', data), 'WidgetLoad');
+		subscribe((data) => console.log('Anyclip adImpression event data: ', data), 'adImpression');
+		subscribe((data) => console.log('Anyclip adSkipped event data: ', data), 'adSkipped');
+		subscribe(
+			(data) => console.log('Anyclip adFirstQuartile event data: ', data),
+			'adFirstQuartile',
+		);
+		subscribe((data) => console.log('Anyclip adMidpoint event data: ', data), 'adMidpoint');
+		subscribe(
+			(data) => console.log('Anyclip adThirdQuartile event data: ', data),
+			'adThirdQuartile',
+		);
+		subscribe((data) => console.log('Anyclip adComplete event data: ', data), 'adComplete');
+		subscribe((data) => console.log('Anyclip adClick event data: ', data), 'adClick');
+	}
+
+	private waitForSubscribeReady(): Promise<boolean> {
+		return new utils.WaitFor(isSubscribeReady, 4, 250).until();
 	}
 }
 
