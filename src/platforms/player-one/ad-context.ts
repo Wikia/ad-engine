a44543ffeec52c8bc87b47757d30ee2d13c0ffb7
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-11-07 18:15:29 +0100

    ADEN-12344 simple setup to load GPT

---
+++ src/platforms/player-one/ad-context.ts
@@ -0,0 +1,8 @@
+export const basicContext = {
+	adUnitId: '/{custom.dfpId}/aw-{custom.site}/home',
+	custom: {
+		dfpId: '22309610186',
+		site: 'gamefaqs',
+	},
+	src: ['pp1'],
+};
--- src/platforms/player-one/player-one-platform.ts
+++ src/platforms/player-one/player-one-platform.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@wikia/dependency-injection';
-import { ProcessPipeline } from '@wikia/ad-engine';
+import { context, ProcessPipeline } from '@wikia/ad-engine';
+import { startAdEngine } from '@platforms/shared';
+
+import { basicContext } from './ad-context';
 
 @Injectable()
 export class PlayerOnePlatform {
 	constructor(private pipeline: ProcessPipeline) {}
 
 	execute(): void {
+		this.pipeline.add(
+			() => context.extend(basicContext),
+			() => {
+				startAdEngine();
+			},
+		);
+
 		this.pipeline.execute();
 	}
 }
