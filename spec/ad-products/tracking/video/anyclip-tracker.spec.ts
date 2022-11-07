a030a9c551a8bc409ed58e8352bacb34d3d3becf
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-11-07 16:48:46 +0100

    ADEN-12241 no magic numbers

--- spec/ad-products/tracking/video/anyclip-tracker.spec.ts
+++ spec/ad-products/tracking/video/anyclip-tracker.spec.ts
@@ -4,33 +4,34 @@ import { spy } from 'sinon';
 import { AnyclipTracker } from '@wikia/ad-products';
 
 describe('AnyclipTracker', () => {
+	const TEST_TIMEOUT = 10;
+	const TEST_RETRIES = 1;
+
 	afterEach(() => {
 		window['lreSubscribe'] = undefined;
 	});
 
-	it('uses global function to subscribe to AnyClip events', (done) => {
+	it('uses global function to subscribe to AnyClip events', () => {
 		const subscribeSpy = spy();
 		window['lreSubscribe'] = subscribeSpy;
 
-		const tracker = new AnyclipTracker(10, 1);
+		const tracker = new AnyclipTracker(TEST_TIMEOUT, TEST_RETRIES);
 		tracker.register();
 
 		setTimeout(() => {
 			expect(subscribeSpy.callCount).not.to.equal(0);
-			done();
-		}, 11);
+		}, TEST_TIMEOUT + 1);
 	});
 
-	it('does not do anything if there is no global function from AnyClip', (done) => {
+	it('does not do anything if there is no global function from AnyClip', () => {
 		const subscribeSpy = spy();
 
-		const tracker = new AnyclipTracker(10, 1);
+		const tracker = new AnyclipTracker(TEST_TIMEOUT, TEST_RETRIES);
 		tracker.register();
 
 		expect(subscribeSpy.callCount).to.equal(0);
 		setTimeout(() => {
 			expect(subscribeSpy.callCount).to.equal(0);
-			done();
-		}, 11);
+		}, TEST_TIMEOUT + 1);
 	});
 });
