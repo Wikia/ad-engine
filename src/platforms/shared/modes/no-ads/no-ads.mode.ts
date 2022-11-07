8e1e50000425f3a9b6aef1069ed53a4d94bcef90
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2022-11-07 13:31:32 +0100

    ADEN-12076 Disabling ads for logged users (#1593)
    * ADEN-12076 Sunsetting wiki.opts.pageType
    
    * ADEN-12076 Adding identity services to NoAdsMode
    
    * ADEN-12076 Adding no_ads skin do PPID
    
    Co-authored-by: Karol Wajs <90037542+kwfandom@users.noreply.github.com>

--- src/platforms/shared/modes/no-ads/no-ads.mode.ts
+++ src/platforms/shared/modes/no-ads/no-ads.mode.ts
@@ -1,15 +1,41 @@
-import { communicationService, DiProcess, jwpSetup } from '@wikia/ad-engine';
+import {
+	ats,
+	audigent,
+	communicationService,
+	DiProcess,
+	eventsRepository,
+	identityHub,
+	jwpSetup,
+	liveConnect,
+	liveRampPixel,
+	PartnerPipeline,
+	userIdentity,
+} from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
 import { NoAdsDetector } from '../../services/no-ads-detector';
 
 @Injectable()
 export class NoAdsMode implements DiProcess {
-	constructor(private noAdsDetector: NoAdsDetector) {}
+	constructor(private pipeline: PartnerPipeline, private noAdsDetector: NoAdsDetector) {}
 
 	execute(): void {
 		this.removeAdSlotsPlaceholders();
 		this.noAdsDetector.addReasons(window.ads.context.opts.noAdsReasons);
 		this.dispatchJWPlayerSetupAction();
+
+		this.pipeline
+			.add(
+				userIdentity,
+				liveRampPixel.setOptions({ dependencies: [userIdentity.initialized] }),
+				ats,
+				audigent,
+				liveConnect,
+				identityHub,
+			)
+			.execute()
+			.then(() => {
+				communicationService.emit(eventsRepository.AD_ENGINE_PARTNERS_READY);
+			});
 	}
 
 	private removeAdSlotsPlaceholders(): void {
