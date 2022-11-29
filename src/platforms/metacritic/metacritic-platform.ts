190ed100d12bb7bca8d3515532b6c9e4abc70b09
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-11-29 18:38:51 +0100

    ADEN-12466 add initial bundle for metacritic

---
+++ src/platforms/metacritic/metacritic-platform.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@wikia/dependency-injection';
+import { communicationService, context, eventsRepository, ProcessPipeline } from '@wikia/ad-engine';
+import { gptSetup } from '@platforms/shared';
+import { basicContext } from './ad-context';
+
+import { MetacriticSlotsContextSetup } from './setup/context/slots/metacritic-slots-context.setup';
+import { MetacriticDynamicSlotsSetup } from './setup/dynamic-slots/metacritic-dynamic-slots.setup';
+
+@Injectable()
+export class MetacriticPlatform {
+	constructor(private pipeline: ProcessPipeline) {}
+
+	execute(): void {
+		this.pipeline.add(
+			() => context.extend(basicContext),
+			// TODO: we need a CMP step here, so we won't call for ads unless we have a clear idea of the privacy policy of a visitor
+			// TODO: to decide if we want to call instant-config service for the first releases?
+			MetacriticDynamicSlotsSetup,
+			MetacriticSlotsContextSetup,
+			// TODO: add targeting setup once we have idea of page-level and slot-level targeting
+			() => communicationService.emit(eventsRepository.AD_ENGINE_CONFIGURED),
+			gptSetup.call,
+		);
+
+		this.pipeline.execute();
+	}
+}
---
+++ src/platforms/metacritic/setup/context/slots/metacritic-slots-context.setup.ts
@@ -0,0 +1,318 @@
+import { context, DiProcess } from '@wikia/ad-engine';
+import { Injectable } from '@wikia/dependency-injection';
+
+@Injectable()
+export class MetacriticSlotsContextSetup implements DiProcess {
+	execute(): void {
+		const slots = {
+			nav_ad_omni: {
+				defaultSizes: [
+					[5, 5],
+					[6, 6],
+				],
+				targeting: {
+					pos: 'nav',
+				},
+			},
+			leader_plus_top: {
+				defaultSizes: [
+					[728, 90],
+					[970, 250],
+					[970, 66],
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			leader_top: {
+				defaultSizes: [
+					[728, 90],
+					[970, 66],
+					[728, 91],
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			native_top: {
+				defaultSizes: [
+					[11, 11],
+					// fluid
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			mpu_plus_top: {
+				defaultSizes: [
+					[300, 250],
+					[300, 600],
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			mpu_bottom: {
+				defaultSizes: [[300, 250]],
+				targeting: {
+					pos: 'bottom',
+				},
+			},
+			leader_bottom: {
+				defaultSizes: [
+					[728, 90],
+					[970, 250],
+					[970, 66],
+				],
+				targeting: {
+					pos: 'bottom',
+				},
+			},
+			recirculation_ad: {
+				defaultSizes: [[300, 249]],
+				targeting: {
+					pos: 'recirculation',
+				},
+			},
+			leader_middle: {
+				defaultSizes: [
+					[728, 90],
+					[970, 66],
+					[728, 93],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			leader_middle2: {
+				defaultSizes: [
+					[728, 90],
+					[970, 66],
+					[728, 93],
+				],
+				targeting: {
+					pos: 'middle2',
+				},
+			},
+			incontent_plus_top: {
+				defaultSizes: [
+					[728, 90],
+					[970, 250],
+					[970, 66],
+					[5, 5],
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			mobile_nav_ad_omni: {
+				defaultSizes: [
+					[5, 5],
+					[6, 6],
+				],
+				targeting: {
+					pos: 'nav',
+				},
+			},
+			mobile_banner_top: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			mobile_native_top: {
+				defaultSizes: [
+					[11, 11],
+					// fluid
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			mobile_incontent_plus_top: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+					[5, 5],
+				],
+				targeting: {
+					pos: 'top',
+				},
+			},
+			mobile_banner_plus_middle: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_incontent_plus_middle2: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[5, 5],
+				],
+				targeting: {
+					pos: 'middle2',
+				},
+			},
+			mobile_mpu_banner_bottom: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+				],
+				targeting: {
+					pos: 'bottom',
+				},
+			},
+			mobile_gallery_banner_plus_1: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_2: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_3: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_4: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_5: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_6: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_7: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_8: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_9: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_10: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_11: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_12: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			mobile_gallery_banner_plus_13: {
+				defaultSizes: [
+					[300, 250],
+					[320, 50],
+					[320, 480],
+				],
+				targeting: {
+					pos: 'middle',
+				},
+			},
+			'mobile-nav-ad-plus-banner': {
+				defaultSizes: [
+					[5, 5],
+					[320, 50],
+				],
+				targeting: {
+					pos: 'nav',
+				},
+			},
+		};
+
+		context.set('slots', slots);
+	}
+}
