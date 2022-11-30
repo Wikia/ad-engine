e483323225587bd40348d6b50018392ce9923b8f
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-11-30 19:25:21 +0100

    ADEN-12487 enable our test Wikia Bidder on the main page of GameFAQs

---
+++ src/platforms/gamefaqs/bidders/wikia.ts
@@ -0,0 +1,19 @@
+export function getWikiaContext(): object {
+	return {
+		enabled: false,
+		slots: {
+			leader_plus_top: {
+				sizes: [[728, 90]],
+			},
+			mpu_top: {
+				sizes: [[300, 250]],
+			},
+			mpu_bottom: {
+				sizes: [[300, 250]],
+			},
+			leader_bottom: {
+				sizes: [[728, 90]],
+			},
+		},
+	};
+}
--- src/platforms/gamefaqs/setup/context/prebid/gamefaqs-prebid-config.setup.ts
+++ src/platforms/gamefaqs/setup/context/prebid/gamefaqs-prebid-config.setup.ts
@@ -8,11 +8,13 @@ import {
 import { Injectable } from '@wikia/dependency-injection';
 
 import { getAppnexusContext } from '../../../bidders/appnexus';
+import { getWikiaContext } from '../../../bidders/wikia';
 
 @Injectable()
 export class GamefaqsPrebidConfigSetup implements DiProcess {
 	execute(): void {
 		context.set('bidders.prebid.appnexus', getAppnexusContext());
+		context.set('bidders.prebid.wikia', getWikiaContext());
 
 		context.set('bidders.enabled', true);
 		context.set('bidders.prebid.enabled', true);
@@ -20,7 +22,9 @@ export class GamefaqsPrebidConfigSetup implements DiProcess {
 			'bidders.prebid.libraryUrl',
 			'//static.wikia.nocookie.net/fandom-ae-assets/prebid.js/v6.9.0/20221115.min.js',
 		);
+
 		context.set('bidders.prebid.appnexus.enabled', true);
+		context.set('bidders.prebid.appnexus.wikia', true);
 
 		this.registerListeners();
 	}
