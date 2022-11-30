da254867fc9c0d05a829491e117610ae202a5e27
Szymon Nawrocki <snawrocki@fandom.com>
2022-11-30 16:30:58 +0100

    ADEN-12448 Gamefaqs - Add TargetingSetup

---
+++ src/platforms/gamefaqs/setup/context/targeting/gamefaqs-config.ts
@@ -0,0 +1,18 @@
+export const gamefaqsConfig = {
+	targeting: {
+		seats: {
+			session: 16,
+			subsession: 4,
+		},
+		cookie: {
+			consolidate: true,
+			keyMap: {
+				ftag: 'ftag',
+				ttag: 'ttag',
+			},
+		},
+	},
+	timeouts: {
+		bidder: 1000,
+	},
+};
--- src/platforms/gamefaqs/setup/context/targeting/gamefaqs-targeting.setup.ts
+++ src/platforms/gamefaqs/setup/context/targeting/gamefaqs-targeting.setup.ts
@@ -1,53 +1,218 @@
-import { context, DiProcess } from '@wikia/ad-engine';
+import {
+	communicationService,
+	context,
+	CookieStorageAdapter,
+	DiProcess,
+	eventsRepository,
+} from '@wikia/ad-engine';
+import { gamefaqsConfig } from './gamefaqs-config';
+import isMatch from 'lodash/isMatch.js';
+
+interface CookieBasedTargetingParams {
+	ftag?: string;
+	ttag?: string;
+	pv?: string;
+	session?: string;
+	subses?: string;
+}
+
+interface TargetingParams extends CookieBasedTargetingParams {
+	con?: string;
+	franchise?: string;
+	game?: string;
+	genre?: string;
+	env?: string;
+	publisher?: string;
+	ptype?: string;
+	rdate?: string;
+	score?: string;
+	user?: string;
+	vguid?: string;
+}
 
 export class GamefaqsTargetingSetup implements DiProcess {
 	execute(): Promise<void> | void {
+		this.setSlParamForAdSlots();
+
 		context.set('targeting', {
 			...context.get('targeting'),
 			...this.getPageLevelTargeting(),
-			...this.getCommonTargetingParams(),
+			...this.getCookieBasedTargeting(),
 		});
 	}
 
 	getPageLevelTargeting() {
-		const targetParams = this.getTargetParams();
-
-		if (!targetParams) {
-			return;
-		}
-
-		return {
-			con: targetParams.con,
-			franchise: targetParams.franchise,
-			game: targetParams.game,
-			genre: targetParams.genre,
-			env: undefined,
-			publisher: targetParams.publisher,
-			rdate: targetParams.rdate,
-			score: targetParams.score,
-		};
-	}
-
-	getCommonTargetingParams() {
+		const targetParams = this.getMetadataTargetingParams();
 		const utagData = window.utag_data;
 
 		return {
-			ftag: undefined,
-			pos: undefined,
-			ptype: utagData.pageType,
-			pv: undefined,
-			session: undefined,
-			subses: undefined,
-			sl: undefined,
-			user: utagData.userType,
-			vguid: utagData.pageViewGuid,
+			con: targetParams?.con,
+			franchise: targetParams?.franchise,
+			game: targetParams?.game,
+			genre: targetParams?.genre,
+			publisher: targetParams?.publisher,
+			ptype: utagData?.pageType,
+			rdate: targetParams?.rdate,
+			score: targetParams?.score,
+			user: utagData?.userType,
+			vguid: utagData?.pageViewGuid,
 		};
 	}
 
-	getTargetParams() {
+	getMetadataTargetingParams(): TargetingParams {
 		const dataSettingsElement = document.head
 			.querySelector('[id=ad-settings]')
 			.getAttribute('data-settings');
 		return JSON.parse(dataSettingsElement).target_params;
+	}
+
+	// Transfered from: https://github.com/Wikia/player1-ads-adlibrary/blob/0df200c535adf3599c7de9e99b719953af2784e1/configs/global-config.js#L414-L435
+	setSlParamForAdSlots() {
+		communicationService.on(
+			eventsRepository.AD_ENGINE_SLOT_ADDED,
+			({ slot: adSlot }) => {
+				const slParams = [];
+
+				if (adSlot.getConfigProperty('lazyLoad')) {
+					slParams.push('LL');
+				}
+
+				if (gamefaqsConfig.timeouts.bidder) {
+					slParams.push(`T-${gamefaqsConfig.timeouts.bidder}`);
+				}
+
+				adSlot.config.targeting.sl =
+					adSlot.getSlotName() + (slParams.length > 0 ? '?' + slParams.join('|') : '');
+			},
+			false,
+		);
+	}
+
+	// Transfered from: https://github.com/Wikia/player1-ads-adlibrary/blob/0df200c535adf3599c7de9e99b719953af2784e1/core/targeting.js#L205
+	getCookieBasedTargeting() {
+		const cookieAdapter = new CookieStorageAdapter();
+		const browserSessionCookie = '_BB.bs';
+		const dailySessionCookie = '_BB.d';
+
+		// "consolidate" comes as true from the backend
+		//  https://github.com/Wikia/phoenix/blob/master/SiteBundle/Resources/js/src/ads/bidbarrel/adManager.js#L888
+		const consolidate = gamefaqsConfig.targeting.cookie.consolidate;
+		let surround = null;
+		const surroundCookie = cookieAdapter.getItem('surround');
+
+		if (surroundCookie) {
+			surround = surroundCookie.split('|');
+			if (consolidate) {
+				cookieAdapter.removeItem('surround');
+			}
+		} else {
+			const availableSessions = [
+				'a',
+				'b',
+				'c',
+				'd',
+				'e',
+				'f',
+				'g',
+				'h',
+				'i',
+				'j',
+				'k',
+				'l',
+				'm',
+				'n',
+				'o',
+				'p',
+				'q',
+				'r',
+				's',
+				't',
+				'u',
+				'v',
+				'w',
+				'x',
+				'y',
+				'z',
+			].slice(0, gamefaqsConfig.targeting.seats.session);
+			const session = availableSessions[Math.floor(Math.random() * availableSessions.length)];
+			const subsession = Math.floor(Math.random() * gamefaqsConfig.targeting.seats.subsession) + 1;
+			surround = [session, subsession];
+		}
+
+		const bbCookies = [
+			cookieAdapter.getItem(browserSessionCookie),
+			cookieAdapter.getItem(dailySessionCookie),
+		];
+
+		const existingCookies: CookieBasedTargetingParams = {
+			session: surround[0],
+			subses: surround[1],
+		};
+
+		const cookieKeyMapKeys = ['session', 'subses', 'ftag', 'ttag'];
+		const keyMap = gamefaqsConfig.targeting.cookie.keyMap;
+
+		for (let i = 0; i < cookieKeyMapKeys.length; i += 1) {
+			const key = cookieKeyMapKeys[i];
+			if (keyMap[key]) {
+				const value = cookieAdapter.getItem(keyMap[key]);
+				if (value) {
+					existingCookies[key] = value;
+					if (consolidate) {
+						cookieAdapter.removeItem(keyMap[key]);
+					}
+				}
+			}
+		}
+
+		const browserSessionData = bbCookies[0] ? JSON.parse(bbCookies[0]) : {};
+		const dailySessionData = bbCookies[1] ? JSON.parse(bbCookies[1]) : {};
+
+		const result = {
+			ttag: dailySessionData.ttag || existingCookies.ttag,
+			ftag: dailySessionData.ftag || existingCookies.ftag,
+			session: browserSessionData.session || existingCookies.session,
+			subses: browserSessionData.subses || existingCookies.subses,
+			pv: parseInt(dailySessionData.pv || existingCookies.pv || 0, 10),
+		};
+
+		if (!consolidate) {
+			cookieAdapter.removeItem(browserSessionCookie);
+			cookieAdapter.removeItem(dailySessionCookie);
+		}
+
+		if (typeof result.pv !== 'undefined') {
+			result.pv += 1;
+		}
+
+		if (consolidate) {
+			const bbBrowserSession = { session: result.session, subses: result.subses };
+			const bbDailySession = { ttag: result.ttag, ftag: result.ftag, pv: result.pv };
+
+			if (!isMatch(bbCookies[0], bbBrowserSession)) {
+				cookieAdapter.setItem(browserSessionCookie, JSON.stringify(bbBrowserSession));
+			}
+			if (!isMatch(bbCookies[1], bbDailySession)) {
+				cookieAdapter.setItem(dailySessionCookie, JSON.stringify(bbDailySession));
+			}
+		} else {
+			if (result.session !== existingCookies.session) {
+				cookieAdapter.setItem('session', result.session);
+			}
+			if (result.subses !== existingCookies.subses) {
+				cookieAdapter.setItem('subses', result.subses);
+			}
+		}
+
+		if (!consolidate) {
+			if (result.ftag !== existingCookies.ftag) {
+				cookieAdapter.setItem('ftag', result.ftag);
+			}
+			if (result.ttag !== existingCookies.ttag) {
+				cookieAdapter.setItem('ttag', result.ttag);
+			}
+		}
+
+		return result;
 	}
 }
