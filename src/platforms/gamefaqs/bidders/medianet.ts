f3de212206411bb3b5ae3d035c21a330e4e11163
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-12-01 18:33:04 +0100

    ADEN-12487 add Medianet bidder and leader_plus_top config for it

---
+++ src/platforms/gamefaqs/bidders/medianet.ts
@@ -0,0 +1,12 @@
+export function getMedianetContext(): object {
+	return {
+		enabled: false,
+		slots: {
+			leader_plus_top: {
+				sizes: [[728, 90]],
+				cid: '8CUIBM874',
+				crid: '773676927',
+			},
+		},
+	};
+}
--- src/platforms/gamefaqs/setup/context/prebid/gamefaqs-prebid-config.setup.ts
+++ src/platforms/gamefaqs/setup/context/prebid/gamefaqs-prebid-config.setup.ts
@@ -10,12 +10,16 @@ import { Injectable } from '@wikia/dependency-injection';
 import { getAppnexusContext } from '../../../bidders/appnexus';
 import { getWikiaContext } from '../../../bidders/wikia';
 import { getIndexExchangeContext } from '../../../bidders/index-exchange';
+import { getRubiconDisplayContext } from '../../../bidders/rubicon-display';
+import { getMedianetContext } from '../../../bidders/medianet';
 
 @Injectable()
 export class GamefaqsPrebidConfigSetup implements DiProcess {
 	execute(): void {
 		context.set('bidders.prebid.appnexus', getAppnexusContext());
 		context.set('bidders.prebid.indexExchange', getIndexExchangeContext());
+		context.set('bidders.prebid.medianet', getMedianetContext());
+		context.set('bidders.prebid.rubicon_display', getRubiconDisplayContext());
 		context.set('bidders.prebid.wikia', getWikiaContext());
 
 		context.set('bidders.enabled', true);
@@ -27,6 +31,8 @@ export class GamefaqsPrebidConfigSetup implements DiProcess {
 
 		context.set('bidders.prebid.appnexus.enabled', true);
 		context.set('bidders.prebid.indexExchange.enabled', true);
+		context.set('bidders.prebid.medianet.enabled', true);
+		context.set('bidders.prebid.rubicon_display.enabled', true);
 		context.set('bidders.prebid.wikia.enabled', true);
 
 		this.registerListeners();
