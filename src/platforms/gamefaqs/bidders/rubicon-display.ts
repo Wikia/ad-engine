cd9afaba054f070736de5bc744b7a52eaa1e3ed7
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-12-01 18:21:07 +0100

    ADEN-12487 add Rubicon bidder and leader_plus_top config for it

---
+++ src/platforms/gamefaqs/bidders/rubicon-display.ts
@@ -0,0 +1,13 @@
+export function getRubiconDisplayContext(): object {
+	return {
+		enabled: false,
+		accountId: '12556',
+		slots: {
+			leader_plus_top: {
+				sizes: [[728, 90]],
+				siteId: '110932',
+				zoneId: '2095012',
+			},
+		},
+	};
+}
