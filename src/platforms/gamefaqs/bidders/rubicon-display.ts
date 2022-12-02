f265a3b8fb26946442907cc1e22002126ee31d06
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-12-02 18:29:17 +0100

    ADEN-12487 leave a comment not to forget

--- src/platforms/gamefaqs/bidders/rubicon-display.ts
+++ src/platforms/gamefaqs/bidders/rubicon-display.ts
@@ -1,7 +1,7 @@
 export function getRubiconDisplayContext(): object {
 	return {
 		enabled: false,
-		accountId: '12556',
+		accountId: '12556', // it's different in https://github.com/Wikia/player1-ads-adlibrary/blob/REVSYS-1890_MoarPurge/configs/gamefaqs-rv/config.js#L36-L37
 		slots: {
 			leader_plus_top: {
 				sizes: [[728, 90]],
