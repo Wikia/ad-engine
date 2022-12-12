67d3e3bc10dcd79a689fbf45e6594e8c02cea062
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2022-12-12 16:53:20 +0100

    ADEN-12487 Switch GAM IDs in order to call for RV indirect ads (#1635)
    * ADEN-12487 switch GAM IDs in order to call for RV indirect ads
    
    * ADEN-12487 add prebid setup
    
    * ADEN-12487 enable our test Wikia Bidder on the main page of GameFAQs
    
    * ADEN-12487 fix typo
    
    * ADEN-12487 add IX bidder and leader_plus_top config for it
    
    * ADEN-12487 add Rubicon bidder and leader_plus_top config for it
    
    * ADEN-12487 add Medianet bidder and leader_plus_top config for it
    
    * ADEN-12487 leave a comment not to forget
    
    * ADEN-12487 extend config for Medianet
    
    * ADEN-12487 add query-string flag to enable AdEngine's prebid on demand
    
    * ADEN-12487 add leader_top to Wikia adapter
    
    * ADEN-12487 do not setup prebid bidders when prebid is disabled (CR changes)
    
    * ADEN-12487 add two more query-string switches in order to help with testing
    
    * ADEN-12487 clear the code after CR
    
    * ADEN-12504 Add proper src values (#1641)
    
    * ADEN-12504 add helper methods and set src=gamefaq
    
    * ADEN-12504 set src=comicvine and port helper methods
    
    * ADEN-12504 set src=gamespot and port helper methods
    
    * ADEN-12504 fix GameSpot platform
    
    * ADEN-12504 add CMP setup to GameSpot
    
    * ADEN-12504 add CMP setup to ComicVine
    
    * ADEN-12504 set src=giantbomb and port helper methods
    
    * ADEN-12504 fix GiantBomb platform
    
    * ADEN-12504 add CMP setup to GiantBomb
    
    * ADEN-12504 set src=metacritic and port helper methods
    
    * ADEN-12504 add CMP setup to Metacritic
    
    * ADEN-12504 update the value of src key for GameFAQs
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

---
+++ src/platforms/gamefaqs/bidders/medianet.ts
@@ -0,0 +1,34 @@
+export function getMedianetContext(): object {
+	return {
+		enabled: false,
+		slots: {
+			leader_plus_top: {
+				sizes: [
+					[728, 90],
+					[970, 66],
+					[970, 250],
+				],
+				cid: '8CUIBM874',
+				crid: '773676927',
+			},
+			mpu_top: {
+				sizes: [[300, 250]],
+				cid: '8CUIBM874',
+				crid: '128580044',
+			},
+			mpu_bottom: {
+				sizes: [[300, 250]],
+				cid: '8CUIBM874',
+				crid: '128580044',
+			},
+			leader_bottom: {
+				sizes: [
+					[728, 90],
+					[970, 66],
+				],
+				cid: '8CUIBM874',
+				crid: '773676927',
+			},
+		},
+	};
+}
