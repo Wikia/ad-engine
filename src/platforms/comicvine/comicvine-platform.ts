4b0e885003359ca23cb243552816b339e2c41ac9
Szymon Nawrocki <snawrocki@fandom.com>
2022-12-12 11:53:44 +0100

    ADEN-12491 Add TargetingSetup to Comicvine

--- src/platforms/comicvine/comicvine-platform.ts
+++ src/platforms/comicvine/comicvine-platform.ts
@@ -5,6 +5,7 @@ import { basicContext } from './ad-context';
 
 import { ComicvineSlotsContextSetup } from './setup/context/slots/comicvine-slots-context.setup';
 import { ComicvineDynamicSlotsSetup } from './setup/dynamic-slots/comicvine-dynamic-slots.setup';
+import { NewsAndRatingsTargetingSetup } from '../shared-news-and-ratings/context/targeting/news-and-ratings-targeting.setup';
 
 @Injectable()
 export class ComicvinePlatform {
@@ -15,6 +16,7 @@ export class ComicvinePlatform {
 			() => context.extend(basicContext),
 			// TODO: we need a CMP step here, so we won't call for ads unless we have a clear idea of the privacy policy of a visitor
 			// TODO: to decide if we want to call instant-config service for the first releases?
+			NewsAndRatingsTargetingSetup,
 			ComicvineDynamicSlotsSetup,
 			ComicvineSlotsContextSetup,
 			// TODO: add targeting setup once we have idea of page-level and slot-level targeting
