35b31277da96472fc4de50924f76786c31980596
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-12-14 17:51:01 +0100

    ADEN-12533 add bidders initial config to Gamespot bundle

--- src/platforms/gamespot/gamespot-platform.ts
+++ src/platforms/gamespot/gamespot-platform.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@wikia/dependency-injection';
 
 import {
+	bidders,
 	communicationService,
 	context,
 	eventsRepository,
 	utils,
 	ProcessPipeline,
 } from '@wikia/ad-engine';
-import { bootstrapAndGetConsent, gptSetup, InstantConfigSetup } from '@platforms/shared';
+import {
+	BiddersStateSetup,
+	bootstrapAndGetConsent,
+	gptSetup,
+	InstantConfigSetup,
+} from '@platforms/shared';
 
 import { basicContext } from './ad-context';
 import { GamespotSlotsContextSetup } from './setup/context/slots/gamespot-slots-context.setup';
 import { GamespotDynamicSlotsSetup } from './setup/dynamic-slots/gamespot-dynamic-slots.setup';
+import { GamespotPrebidConfigSetup } from './setup/context/prebid/gamespot-prebid-config.setup';
 
 @Injectable()
 export class GameSpotPlatform {
@@ -19,6 +26,7 @@ export class GameSpotPlatform {
 
 	execute(): void {
 		this.pipeline.add(
+			// setup
 			() => context.extend(basicContext),
 			() => context.set('custom.dfpId', this.shouldSwitchGamToRV() ? 22309610186 : 5441),
 			() => context.set('src', this.shouldSwitchSrcToTest() ? ['test'] : context.get('src')),
@@ -27,8 +35,16 @@ export class GameSpotPlatform {
 			InstantConfigSetup,
 			GamespotSlotsContextSetup,
 			GamespotDynamicSlotsSetup,
+			BiddersStateSetup,
+			GamespotPrebidConfigSetup,
 			// TODO: add targeting setup once we have idea of page-level and slot-level targeting
 			() => communicationService.emit(eventsRepository.AD_ENGINE_CONFIGURED),
+
+			// run
+			() =>
+				bidders
+					.call()
+					.then(() => communicationService.emit(eventsRepository.AD_ENGINE_PARTNERS_READY)),
 			gptSetup.call,
 		);
 
