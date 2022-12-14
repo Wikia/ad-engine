7e612b2c55a907e18c6fc38b83f5eae41c60b03d
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-12-14 18:03:10 +0100

    ADEN-12533 add bidders initial config to Giantbomb bundle

--- src/platforms/giantbomb/giantbomb-platform.ts
+++ src/platforms/giantbomb/giantbomb-platform.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@wikia/dependency-injection';
 
 import {
+	bidders,
 	communicationService,
 	context,
 	eventsRepository,
 	utils,
 	ProcessPipeline,
 } from '@wikia/ad-engine';
-import { bootstrapAndGetConsent, gptSetup, InstantConfigSetup } from '@platforms/shared';
+import {
+	BiddersStateSetup,
+	bootstrapAndGetConsent,
+	gptSetup,
+	InstantConfigSetup,
+} from '@platforms/shared';
 
 import { basicContext } from './ad-context';
 import { GiantbombSlotsContextSetup } from './setup/context/slots/giantbomb-slots-context.setup';
 import { GiantbombDynamicSlotsSetup } from './setup/dynamic-slots/giantbomb-dynamic-slots.setup';
+import { GiantbombPrebidConfigSetup } from './setup/context/prebid/giantbomb-prebid-config.setup';
 
 @Injectable()
 export class GiantbombPlatform {
@@ -19,6 +26,7 @@ export class GiantbombPlatform {
 
 	execute(): void {
 		this.pipeline.add(
+			// setup
 			() => context.extend(basicContext),
 			() => context.set('custom.dfpId', this.shouldSwitchGamToRV() ? 22309610186 : 5441),
 			() => context.set('src', this.shouldSwitchSrcToTest() ? ['test'] : context.get('src')),
@@ -27,8 +35,16 @@ export class GiantbombPlatform {
 			InstantConfigSetup,
 			GiantbombSlotsContextSetup,
 			GiantbombDynamicSlotsSetup,
+			BiddersStateSetup,
+			GiantbombPrebidConfigSetup,
 			// TODO: add targeting setup once we have idea of page-level and slot-level targeting
 			() => communicationService.emit(eventsRepository.AD_ENGINE_CONFIGURED),
+
+			// run
+			() =>
+				bidders
+					.call()
+					.then(() => communicationService.emit(eventsRepository.AD_ENGINE_PARTNERS_READY)),
 			gptSetup.call,
 		);
 
