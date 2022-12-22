79aa835dd1298998ef71b0d134efe99a586ef3fe
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-12-22 11:11:01 +0100

    ADEN-12533 add ads mode to Gamespot

--- src/platforms/gamespot/gamespot-platform.ts
+++ src/platforms/gamespot/gamespot-platform.ts
@@ -1,25 +1,14 @@
 import { Injectable } from '@wikia/dependency-injection';
 
-import {
-	bidders,
-	communicationService,
-	context,
-	eventsRepository,
-	utils,
-	ProcessPipeline,
-} from '@wikia/ad-engine';
-import {
-	BiddersStateSetup,
-	bootstrapAndGetConsent,
-	gptSetup,
-	InstantConfigSetup,
-} from '@platforms/shared';
+import { context, utils, ProcessPipeline } from '@wikia/ad-engine';
+import { BiddersStateSetup, bootstrapAndGetConsent, InstantConfigSetup } from '@platforms/shared';
 
 import { basicContext } from './ad-context';
 import { GamespotSlotsContextSetup } from './setup/context/slots/gamespot-slots-context.setup';
 import { GamespotDynamicSlotsSetup } from './setup/dynamic-slots/gamespot-dynamic-slots.setup';
 import { GamespotPrebidConfigSetup } from './setup/context/prebid/gamespot-prebid-config.setup';
 import { NewsAndRatingsTargetingSetup } from '../shared-news-and-ratings/context/targeting/news-and-ratings-targeting.setup';
+import { GamespotAdsMode } from './modes/gamespot-ads-mode';
 
 @Injectable()
 export class GameSpotPlatform {
@@ -27,7 +16,6 @@ export class GameSpotPlatform {
 
 	execute(): void {
 		this.pipeline.add(
-			// setup
 			() => context.extend(basicContext),
 			() => context.set('custom.dfpId', this.shouldSwitchGamToRV() ? 22309610186 : 5441),
 			() => context.set('src', this.shouldSwitchSrcToTest() ? ['test'] : context.get('src')),
@@ -39,15 +27,7 @@ export class GameSpotPlatform {
 			GamespotDynamicSlotsSetup,
 			BiddersStateSetup,
 			GamespotPrebidConfigSetup,
-			// TODO: add targeting setup once we have idea of page-level and slot-level targeting
-			() => communicationService.emit(eventsRepository.AD_ENGINE_CONFIGURED),
-
-			// run
-			() =>
-				bidders
-					.call()
-					.then(() => communicationService.emit(eventsRepository.AD_ENGINE_PARTNERS_READY)),
-			gptSetup.call,
+			GamespotAdsMode,
 		);
 
 		this.pipeline.execute();
