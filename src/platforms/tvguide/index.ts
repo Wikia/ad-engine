cbe68bf22a0f4ece7387f8c90aa910972a008f0e
Szymon Nawrocki <snawrocki@fandom.com>
2022-12-23 14:26:42 +0100

    ADEN-12446 Remove duplicated after merge  tvguide repo

--- src/platforms/tvguide/index.ts
+++
@@ -1,7 +0,0 @@
-import { Container } from '@wikia/dependency-injection';
-import { TvGuidePlatform } from './tvguide-platform';
-
-const container = new Container();
-const platform = container.get(TvGuidePlatform);
-
-platform.execute();
