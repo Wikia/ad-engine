5a4becbda7e54092f48add7d2db589f2fec7e1c6
Dominik Janik <6935667+domjanik@users.noreply.github.com>
2022-12-28 10:29:30 +0100

    ADEN-12401 - Removed mpaRating and esrbRating variables from context (#1658)
    * ADEN-12401 - Removed mpaRating and esrbRating variables
    
    * ADEN-12401 - Removed mpaRating and esrbRating variables
    
    * ADEN-12401 - Removed mpaRating and esrbRating variables
    
    * ADEN-12401 - fixed tests
    
    * ADEN-12401 - added missing tests
    
    * ADEN-12401 - added missing tests
    
    * ADEN-12401 - added missing tests
    
    Co-authored-by: Karol Wajs <90037542+kwfandom@users.noreply.github.com>

--- spec/platforms/shared/context/targeting/targeting-strategies/providers/common-tags.spec.ts
+++ spec/platforms/shared/context/targeting/targeting-strategies/providers/common-tags.spec.ts
@@ -21,7 +21,17 @@ describe('CommonTags', () => {
 
 	it('commonTags are returned correctly', function () {
 		const mockedContext: FandomContext = new FandomContext(
-			new Site([], true, 'ec', 'test', false, {}, mockedTaxonomy, 'general'),
+			new Site(
+				[],
+				true,
+				'test',
+				false,
+				{
+					mpa: ['general'],
+					esrb: ['ec'],
+				},
+				mockedTaxonomy,
+			),
 			new Page(666, 'pl', 666, 'test', 'article-test', {}, 546),
 		);
 
@@ -53,7 +63,17 @@ describe('CommonTags', () => {
 
 	it('should map empty taxonomy tags', function () {
 		const mockedContext: FandomContext = new FandomContext(
-			new Site([], true, 'ec', 'test', false, {}, [], 'general'),
+			new Site(
+				[],
+				true,
+				'test',
+				false,
+				{
+					mpa: ['general'],
+					esrb: ['ec'],
+				},
+				[],
+			),
 			new Page(666, 'pl', 666, 'test', 'article-test', {}, 546),
 		);
 
@@ -61,5 +81,79 @@ describe('CommonTags', () => {
 
 		expect(commonTags.s0).to.be.undefined;
 		expect(commonTags.s0v).to.be.undefined;
+	});
+
+	it('should create rating tag', function () {
+		const mockedContext: FandomContext = new FandomContext(
+			new Site(
+				[],
+				true,
+				'test',
+				false,
+				{
+					mpa: ['general'],
+					esrb: ['ec'],
+				},
+				[],
+			),
+			new Page(666, 'pl', 666, 'test', 'article-test', {}, 546),
+		);
+
+		const commonTags = new CommonTags(mockedSkin, mockedContext);
+
+		expect(commonTags.getCommonParams().rating).to.be.eq('esrb:ec,mpa:general');
+	});
+
+	it('should create rating tag with more than 1 ESRB value', function () {
+		const mockedContext: FandomContext = new FandomContext(
+			new Site(
+				[],
+				true,
+				'test',
+				false,
+				{
+					mpa: ['general'],
+					esrb: ['ec', 'mature'],
+				},
+				[],
+			),
+			new Page(666, 'pl', 666, 'test', 'article-test', {}, 546),
+		);
+
+		const commonTags = new CommonTags(mockedSkin, mockedContext);
+
+		expect(commonTags.getCommonParams().rating).to.be.eq('esrb:ec,esrb:mature,mpa:general');
+	});
+
+	it('should create rating tag with more than 1 MPA value', function () {
+		const mockedContext: FandomContext = new FandomContext(
+			new Site(
+				[],
+				true,
+				'test',
+				false,
+				{
+					mpa: ['general', 'pg'],
+					esrb: ['ec'],
+				},
+				[],
+			),
+			new Page(666, 'pl', 666, 'test', 'article-test', {}, 546),
+		);
+
+		const commonTags = new CommonTags(mockedSkin, mockedContext);
+
+		expect(commonTags.getCommonParams().rating).to.be.eq('esrb:ec,mpa:general,mpa:pg');
+	});
+
+	it('should create rating tag with no rating value', function () {
+		const mockedContext: FandomContext = new FandomContext(
+			new Site([], true, 'test', false, {}, []),
+			new Page(666, 'pl', 666, 'test', 'article-test', {}, 546),
+		);
+
+		const commonTags = new CommonTags(mockedSkin, mockedContext);
+
+		expect(commonTags.getCommonParams().rating).to.be.undefined;
 	});
 });
