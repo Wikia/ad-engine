cd698ae7a225aeab7f99c8cabfcd5e20a08d23c7
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2022-12-29 19:35:44 +0100

    ADEN-12463 replace big BaseContextSetup with a minimal one

--- src/platforms/gamefaqs/gamefaqs-platform.ts
+++ src/platforms/gamefaqs/gamefaqs-platform.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@wikia/dependency-injection';
 
-import { context, utils, ProcessPipeline } from '@wikia/ad-engine';
-import {
-	bootstrapAndGetConsent,
-	BiddersStateSetup,
-	InstantConfigSetup,
-	BaseContextSetup,
-} from '@platforms/shared';
+import { context, ProcessPipeline } from '@wikia/ad-engine';
+import { bootstrapAndGetConsent, BiddersStateSetup, InstantConfigSetup } from '@platforms/shared';
 
 import { basicContext } from './ad-context';
+import { BaseContextSetup } from '../shared-news-and-ratings/context/base-context-setup';
 import { GamefaqsAdsMode } from './modes/gamefaqs-ads-mode';
-import { GamefaqsSlotsContextSetup } from './setup/context/slots/gamefaqs-slots-context.setup';
 import { GamefaqsDynamicSlotsSetup } from './setup/dynamic-slots/gamefaqs-dynamic-slots.setup';
 import { GamefaqsPrebidConfigSetup } from './setup/context/prebid/gamefaqs-prebid-config.setup';
+import { GamefaqsSlotsContextSetup } from './setup/context/slots/gamefaqs-slots-context.setup';
 import { GamefaqsTargetingSetup } from './setup/context/targeting/gamefaqs-targeting.setup';
-import { NewsAndRatingsTargetingSetup } from '../shared-news-and-ratings/context/targeting/news-and-ratings-targeting.setup';
 import { LazyLoadedSlotsContextSetup } from '../shared-news-and-ratings/context/lazy-loaded-slots-context.setup';
+import { NewsAndRatingsTargetingSetup } from '../shared-news-and-ratings/context/targeting/news-and-ratings-targeting.setup';
 
 @Injectable()
 export class GamefaqsPlatform {
@@ -24,8 +20,6 @@ export class GamefaqsPlatform {
 	execute(): void {
 		this.pipeline.add(
 			() => context.extend(basicContext),
-			() => context.set('custom.dfpId', this.shouldSwitchGamToRV() ? 22309610186 : 5441),
-			() => context.set('src', this.shouldSwitchSrcToTest() ? ['test'] : context.get('src')),
 			// once we have Geo cookie set on varnishes we can parallel bootstrapAndGetConsent and InstantConfigSetup
 			() => bootstrapAndGetConsent(),
 			InstantConfigSetup,
@@ -41,13 +35,5 @@ export class GamefaqsPlatform {
 		);
 
 		this.pipeline.execute();
-	}
-
-	private shouldSwitchGamToRV() {
-		return utils.queryString.get('switch_to_rv_gam') === '1';
-	}
-
-	private shouldSwitchSrcToTest() {
-		return utils.queryString.get('switch_src_to_test') === '1';
 	}
 }
---
+++ src/platforms/shared-news-and-ratings/context/base-context-setup.ts
@@ -0,0 +1,32 @@
+import { context, DiProcess, InstantConfigService, utils } from '@wikia/ad-engine';
+import { Injectable } from '@wikia/dependency-injection';
+
+@Injectable()
+export class BaseContextSetup implements DiProcess {
+	constructor(protected instantConfig: InstantConfigService) {}
+
+	execute(): void {
+		context.set('custom.dfpId', this.shouldSwitchGamToRV() ? 22309610186 : 5441);
+		context.set('src', this.shouldSwitchSrcToTest() ? ['test'] : context.get('src'));
+
+		// identity
+		context.set('services.liveConnect.enabled', this.instantConfig.get('icLiveConnect'));
+		context.set(
+			'services.liveConnect.cachingStrategy',
+			this.instantConfig.get('icLiveConnectCachingStrategy'),
+		);
+
+		context.set('services.liveRampPixel.enabled', this.instantConfig.get('icLiveRampPixel'));
+
+		context.set('services.ppid.enabled', this.instantConfig.get('icPpid'));
+		context.set('services.ppidRepository', this.instantConfig.get('icPpidRepository'));
+	}
+
+	private shouldSwitchGamToRV() {
+		return utils.queryString.get('switch_to_rv_gam') === '1';
+	}
+
+	private shouldSwitchSrcToTest() {
+		return utils.queryString.get('switch_src_to_test') === '1';
+	}
+}
