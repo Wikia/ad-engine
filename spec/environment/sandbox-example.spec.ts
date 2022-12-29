d77763e9201ced38f387cfc3b4be1786928e250d
Damian Sobczak <dsobczak@wikia-inc.com>
2022-12-29 14:34:03 +0100

    ADEN-11164 Initial commit

--- spec/environment/sandbox-example.spec.ts
+++ spec/environment/sandbox-example.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 
 describe('Sandbox Example', () => {
 	it('should create test environment', () => {
-		// ToDo: implement Sandbox Helper after moving with Dependency Injection task
+		// ToDo: implement Sandbox Helper while going with Dependency Injection task
 		expect(2137).to.equal(2137);
 	});
 });
