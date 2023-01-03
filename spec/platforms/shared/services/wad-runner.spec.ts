0930bcfe043067f1f18a2e194fa351d5cab24352
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2023-01-03 11:08:15 +0100

    ADEN-12557 Add AB features to GameFAQs, Gamespot and TVGuide bundles (#1663)
    * ADEN-12557 WIP; check what we need to change to load GameFAQs BT
    
    * ADEN-12557 add debug query-string param to BAB
    
    * ADEN-12557 add domain blocking detection to WAD
    
    * ADEN-12557 bring back previously removed step
    
    * ADEN-12557 refactor war-runner and add tests
    
    * ADEN-12557 add possibility to switch BT loaders
    
    * ADEN-12557 refactor bab-detection.ts so it's testable and add tests for it
    
    * ADEN-12557 add unit tests for bt-rec service
    
    * ADEN-12557 add a test case for custom BT loader
    
    * ADEN-12557 CR change: rename doesBlockAdBlockExist() to blockAdBlockExists()
    
    * ADEN-12557 CR change: rename GameFaqsWadSetup to GamefaqsWadSetup
    
    * ADEN-12557 CR changes: clean up tests
    
    * ADEN-12557 fix injecting slots on GameFAQs after merging origin/dev
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>

---
+++ spec/platforms/shared/services/wad-runner.spec.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+import { BabDetection } from '@wikia/core';
+import { WadRunner } from '@wikia/platforms/shared';
+
+function createDetectionRunStub(returnValue: boolean) {
+	return new Promise((resolve) => {
+		resolve();
+	}).then(() => {
+		return returnValue;
+	});
+}
+
+describe('Wikia AdBlock Detector runner', () => {
+	const babDetectionStub = sinon.createStubInstance(BabDetection);
+	const onDetected = sinon.spy();
+
+	it('does not call onDetect callback when ad detection is disabled', async () => {
+		babDetectionStub.isEnabled.returns(false);
+
+		const wadRunner = new WadRunner(babDetectionStub, onDetected);
+		await wadRunner.call();
+
+		expect(onDetected.called).to.equal(false);
+	});
+
+	it('does not call onDetect callback when ad blocking is not detected', async () => {
+		const babDetectionRunStub = createDetectionRunStub(false);
+		babDetectionStub.isEnabled.returns(true);
+		babDetectionStub.run.returns(babDetectionRunStub);
+
+		const wadRunner = new WadRunner(babDetectionStub, onDetected);
+		await wadRunner.call();
+
+		expect(onDetected.called).to.equal(false);
+	});
+
+	it('does call onDetect callback when ad blocking is detected', async () => {
+		const babDetectionRunStub = createDetectionRunStub(true);
+		babDetectionStub.isEnabled.returns(true);
+		babDetectionStub.run.returns(babDetectionRunStub);
+
+		const wadRunner = new WadRunner(babDetectionStub, onDetected);
+		await wadRunner.call();
+
+		expect(onDetected.called).to.equal(true);
+	});
+});
