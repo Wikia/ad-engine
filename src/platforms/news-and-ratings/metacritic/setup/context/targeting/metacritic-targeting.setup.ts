74a59318f166c96a66e5c49b56c30b9f2f18ba6f
Szymon Nawrocki <snawrocki@fandom.com>
2023-01-04 12:10:28 +0100

    ADEN-12512 | N&R - Add verticalName to Metacritic

--- src/platforms/news-and-ratings/metacritic/setup/context/targeting/metacritic-targeting.setup.ts
+++ src/platforms/news-and-ratings/metacritic/setup/context/targeting/metacritic-targeting.setup.ts
@@ -3,9 +3,13 @@ import { context, DiProcess } from '@wikia/ad-engine';
 export class MetacriticTargetingSetup implements DiProcess {
 	execute(): void {
 		const targeting = {
-			s0: 'gaming' || 'ent', // TODO: Set vertical accordingly to the page
+			s0: this.getVerticalName(),
 		};
 
 		context.set('targeting', targeting);
+	}
+
+	getVerticalName(): 'gaming' | 'ent' {
+		return window.utag_data?.siteSection === 'games' ? 'gaming' : 'ent';
 	}
 }
