36443b4ddc5ecbb9103ec4dc6ab30b4b0fdcf082
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-01-10 11:14:23 +0100

    ADEN-12510 add mobile slots config to pubmatic

--- src/platforms/news-and-ratings/gamefaqs/bidders/pubmatic.ts
+++ src/platforms/news-and-ratings/gamefaqs/bidders/pubmatic.ts
@@ -37,9 +37,88 @@ export function getPubmaticContext(): object {
 				sizes: [[728, 90]],
 				ids: ['4856838'],
 			},
+			'mobile-native-top': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856839', '4856840'],
+			},
+			'mobile-incontent-ad': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856841', '4856842'],
+			},
+			'mobile-mpu-banner-bottom': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856843', '4856844'],
+			},
+			'mobile-native-plus-top': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856845', '4856846'],
+			},
+			'mobile-highimpact-plus': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856847', '4856848'],
+			},
+			'incontent-mobile-flex': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856849', '4856850'],
+			},
+			'mobile-banner': {
+				sizes: [[320, 50]],
+				ids: ['4856851'],
+			},
+			'mobile-mpu': {
+				sizes: [[300, 250]],
+				ids: ['4856852'],
+			},
 			'nav-ad-plus': {
 				sizes: [[728, 90]],
 				ids: ['4856853'],
+			},
+			'mobile-nav-ad-plus': {
+				sizes: [[320, 50]],
+				ids: ['4856854'],
+			},
+			'mobile-banner-plus-inc': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856855', '4856856'],
+			},
+			'mobile-incontent-ad-plus': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856857', '4856858'],
+			},
+			'mobile-banner-mpu': {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856859', '4856860'],
+			},
+			'incontent-ad-plus': {
+				sizes: [[728, 90]],
+				ids: ['4856861'],
 			},
 		},
 	};
