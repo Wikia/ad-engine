d759cf156eb4151cc904e30b6f268387c03c9f4b
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-01-10 11:05:31 +0100

    ADEN-12510 change sizes to numbers instead of strings

--- src/platforms/news-and-ratings/gamefaqs/bidders/pubmatic.ts
+++ src/platforms/news-and-ratings/gamefaqs/bidders/pubmatic.ts
@@ -4,41 +4,41 @@ export function getPubmaticContext(): object {
 		publisherId: '156260',
 		slots: {
 			'incontent-ad': {
-				sizes: [['728', '90']],
+				sizes: [[728, 90]],
 				ids: ['4856830'],
 			},
 			leader_plus_top: {
 				sizes: [
-					['728', '90'],
-					['970', '250'],
+					[728, 90],
+					[970, 250],
 				],
 				ids: ['4856831', '4856832'],
 			},
 			leader_top: {
-				sizes: [['728', '90']],
+				sizes: [[728, 90]],
 				ids: ['4856833'],
 			},
 			mpu_top: {
-				sizes: [['300', '250']],
+				sizes: [[300, 250]],
 				ids: ['4856834'],
 			},
 			mpu_plus_top: {
 				sizes: [
-					['300', '250'],
-					['300', '600'],
+					[300, 250],
+					[300, 600],
 				],
 				ids: ['4856835', '4856836'],
 			},
 			mpu_bottom: {
-				sizes: [['300', '250']],
+				sizes: [[300, 250]],
 				ids: ['4856837'],
 			},
 			leader_bottom: {
-				sizes: [['728', '90']],
+				sizes: [[728, 90]],
 				ids: ['4856838'],
 			},
 			'nav-ad-plus': {
-				sizes: [['728', '90']],
+				sizes: [[728, 90]],
 				ids: ['4856853'],
 			},
 		},
