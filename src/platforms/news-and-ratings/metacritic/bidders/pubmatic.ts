ae4ef66173220232872c18fccbb5f97b2b54a8a8
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-01-10 12:44:09 +0100

    ADEN-12510 add Pubmatic to Metacritic

---
+++ src/platforms/news-and-ratings/metacritic/bidders/pubmatic.ts
@@ -0,0 +1,182 @@
+export function getPubmaticContext(): object {
+	return {
+		enabled: false,
+		publisherId: '156260',
+		slots: {
+			leader_plus_top: {
+				sizes: [
+					[728, 90],
+					[970, 250],
+				],
+				ids: ['4856964', '4856965'],
+			},
+			leader_top: {
+				sizes: [[728, 90]],
+				ids: ['4856966'],
+			},
+			mpu_plus_top: {
+				sizes: [
+					[300, 250],
+					[300, 600],
+				],
+				ids: ['4856967', '4856968'],
+			},
+			mpu_bottom: {
+				sizes: [[300, 250]],
+				ids: ['4856969'],
+			},
+			leader_bottom: {
+				sizes: [
+					[728, 90],
+					[970, 250],
+				],
+				ids: ['4856970', '4856971'],
+			},
+			leader_middle: {
+				sizes: [[728, 90]],
+				ids: ['4856972'],
+			},
+			leader_middle2: {
+				sizes: [[728, 90]],
+				ids: ['4856973'],
+			},
+			incontent_plus_top: {
+				sizes: [
+					[728, 90],
+					[970, 250],
+				],
+				ids: ['4856974', '4856975'],
+			},
+			mobile_banner_top: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856976', '4856977'],
+			},
+			mobile_incontent_plus_top: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856978', '4856979'],
+			},
+			mobile_banner_plus_middle: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856980', '4856981'],
+			},
+			mobile_incontent_plus_middle2: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856982', '4856983'],
+			},
+			mobile_mpu_banner_bottom: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856984', '4856985'],
+			},
+			mobile_gallery_banner_plus_1: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856986', '4856987'],
+			},
+			mobile_gallery_banner_plus_2: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856988', '4856989'],
+			},
+			mobile_gallery_banner_plus_3: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856990', '4856991'],
+			},
+			mobile_gallery_banner_plus_4: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856992', '4856993'],
+			},
+			mobile_gallery_banner_plus_5: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856994', '4856995'],
+			},
+			mobile_gallery_banner_plus_6: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856996', '4856997'],
+			},
+			mobile_gallery_banner_plus_7: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4856998', '4856999'],
+			},
+			mobile_gallery_banner_plus_8: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4857000', '4857001'],
+			},
+			mobile_gallery_banner_plus_9: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4857002', '4857003'],
+			},
+			mobile_gallery_banner_plus_10: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4857004', '4857005'],
+			},
+			mobile_gallery_banner_plus_11: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4857006', '4857007'],
+			},
+			mobile_gallery_banner_plus_12: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4857008', '4857009'],
+			},
+			mobile_gallery_banner_plus_13: {
+				sizes: [
+					[300, 250],
+					[320, 50],
+				],
+				ids: ['4857010', '4857011'],
+			},
+			'mobile-nav-ad-plus-banner': {
+				sizes: [[320, 50]],
+				ids: ['4857012'],
+			},
+		},
+	};
+}
