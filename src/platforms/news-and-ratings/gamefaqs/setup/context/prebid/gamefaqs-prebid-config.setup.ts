5d08e529be242a0581679c6b02c47e65487605fe
Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>
2023-01-13 08:27:13 +0100

    ADEN-12623 Add Appnexus to News&Ratings (#1690)
    * ADEN-12510 remove previous bidders configuration for GameFAQs
    
    * ADEN-12510 add Pubmatic desktop config to GameFAQs
    
    * ADEN-12510 setup bidders in the right order
    
    * ADEN-12510 change sizes to numbers instead of strings
    
    * ADEN-12510 add mobile slots config to pubmatic
    
    * ADEN-12510 add Pubmatic to ComicVine
    
    * ADEN-12510 fix order of bidder setup steps
    
    * ADEN-12510 add Pubmatic to Gamespot
    
    * ADEN-12510 add Pubmatic to GiantBomb
    
    * ADEN-12510 add Pubmatic to Metacritic
    
    * ADEN-12510 add Pubmatic to TvGuide
    
    * ADEN-12510 minimise payload sent to Pubmatic based on the device type
    
    * ADEN-12623 N&R | Add Appnexus to GameFAQs
    
    * ADEN-12623 N&R | Add Appnexus to Comicvine
    
    * ADEN-12623 N&R | Add Appnexus to Gamespot
    
    * ADEN-12623 N&R | Add Appnexus to Giantbomb
    
    * ADEN-12623 N&R | Add Appnexus to Metacritic
    
    * ADEN-12623 N&R | Add Appnexus to TVGuide
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>

--- src/platforms/news-and-ratings/gamefaqs/setup/context/prebid/gamefaqs-prebid-config.setup.ts
+++ src/platforms/news-and-ratings/gamefaqs/setup/context/prebid/gamefaqs-prebid-config.setup.ts
@@ -7,18 +7,21 @@ import {
 	utils,
 } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
-
+import { getAppnexusContext } from '../../../bidders/appnexus';
 import { getPubmaticContext } from '../../../bidders/pubmatic';
 import { getWikiaContext } from '../../../bidders/wikia';
 
 @Injectable()
 export class GamefaqsPrebidConfigSetup implements DiProcess {
 	execute(): void {
+		const isDesktop = utils.client.isDesktop();
+
 		if (!this.isPrebidEnabled()) {
 			return;
 		}
 
-		context.set('bidders.prebid.pubmatic', getPubmaticContext(utils.client.isDesktop()));
+		context.set('bidders.prebid.appnexus', getAppnexusContext(isDesktop));
+		context.set('bidders.prebid.pubmatic', getPubmaticContext(isDesktop));
 		context.set('bidders.prebid.wikia', getWikiaContext());
 
 		this.registerListeners();
