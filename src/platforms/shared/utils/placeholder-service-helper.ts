e5422b55c185c095a39c8ee26408df464bf45c74
Damian Sobczak <dsobczak@wikia-inc.com>
2023-01-16 16:00:00 +0100

    ADEN-12620 CR fixes

--- src/platforms/shared/utils/placeholder-service-helper.ts
+++ src/platforms/shared/utils/placeholder-service-helper.ts
@@ -47,9 +47,13 @@ export class PlaceholderServiceHelper {
 		if (this.shouldHidePlaceholder) {
 			placeholder.classList.add('hide');
 
-			if (placeholder?.parentElement.className.includes('-ads-container')) {
-				placeholder.parentElement.classList.add('hide');
-			}
+			this.hideWrapperIfExists(placeholder);
+		}
+	};
+
+	hideWrapperIfExists = (placeholder: HTMLElement): void => {
+		if (placeholder?.parentElement?.className.includes('-ads-container')) {
+			placeholder.parentElement.classList.add('hide');
 		}
 	};
 
