ad71bd68f73be177480747e2a413347d6c4b3713
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-01-17 10:02:31 +0100

    ADEN-12624 add Megnite (Rubicon) to Metacritic

--- src/platforms/news-and-ratings/metacritic/setup/context/prebid/metacritic-prebid-config.setup.ts
+++ src/platforms/news-and-ratings/metacritic/setup/context/prebid/metacritic-prebid-config.setup.ts
@@ -9,8 +9,9 @@ import {
 import { Injectable } from '@wikia/dependency-injection';
 
 import { getAppnexusContext } from '../../../bidders/appnexus';
-import { getWikiaContext } from '../../../bidders/wikia';
 import { getPubmaticContext } from '../../../bidders/pubmatic';
+import { getRubiconDisplayContext } from '../../../bidders/rubicon-display';
+import { getWikiaContext } from '../../../bidders/wikia';
 
 @Injectable()
 export class MetacriticPrebidConfigSetup implements DiProcess {
@@ -23,6 +24,7 @@ export class MetacriticPrebidConfigSetup implements DiProcess {
 
 		context.set('bidders.prebid.appnexus', getAppnexusContext(isDesktop));
 		context.set('bidders.prebid.pubmatic', getPubmaticContext(isDesktop));
+		context.set('bidders.prebid.rubicon_display', getRubiconDisplayContext(isDesktop));
 		context.set('bidders.prebid.wikia', getWikiaContext());
 
 		this.registerListeners();
