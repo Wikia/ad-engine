17a8c2cbea3ba7ccc3f3667bca3e68bf27456f28
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-01-19 14:52:00 +0100

    ADEN-12625 add IX integration to ComicVine

--- src/platforms/news-and-ratings/comicvine/setup/context/prebid/comicvine-prebid-config.setup.ts
+++ src/platforms/news-and-ratings/comicvine/setup/context/prebid/comicvine-prebid-config.setup.ts
@@ -9,6 +9,7 @@ import {
 import { Injectable } from '@wikia/dependency-injection';
 
 import { getAppnexusContext } from '../../../bidders/appnexus';
+import { getIndexExchangeContext } from '../../../bidders/index-exchange';
 import { getPubmaticContext } from '../../../bidders/pubmatic';
 import { getRubiconDisplayContext } from '../../../bidders/rubicon-display';
 import { getWikiaContext } from '../../../bidders/wikia';
@@ -25,6 +26,7 @@ export class ComicvinePrebidConfigSetup implements DiProcess {
 		}
 
 		context.set('bidders.prebid.appnexus', getAppnexusContext(isDesktop));
+		context.set('bidders.prebid.indexExchange', getIndexExchangeContext(isDesktop));
 		context.set('bidders.prebid.pubmatic', getPubmaticContext(isDesktop));
 		context.set('bidders.prebid.rubicon_display', getRubiconDisplayContext(isDesktop));
 		context.set('bidders.prebid.wikia', getWikiaContext());
