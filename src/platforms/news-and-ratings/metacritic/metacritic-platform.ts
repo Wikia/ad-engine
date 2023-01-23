156c9f9f1f66ca60bdcf0fe9cd00b30cc281bd7a
Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>
2023-01-23 14:52:54 +0100

    ADEN-12675 Add NewsAndRatingsTargetingSetup to Metacritic (#1707)
    * ADEN-12675 Add NewsAndRatingsTargetingSetup to Metacritic
    
    * ADEN-12675 Add NewsAndRatingsTargetingSetup to TVGuide

--- src/platforms/news-and-ratings/metacritic/metacritic-platform.ts
+++ src/platforms/news-and-ratings/metacritic/metacritic-platform.ts
@@ -6,6 +6,7 @@ import { context, ProcessPipeline } from '@wikia/ad-engine';
 import {
 	NewsAndRatingsAdsMode,
 	NewsAndRatingsBaseContextSetup,
+	NewsAndRatingsTargetingSetup,
 	NewsAndRatingsWadSetup,
 } from '../shared';
 import { basicContext } from './ad-context';
@@ -27,6 +28,7 @@ export class MetacriticPlatform {
 			NewsAndRatingsBaseContextSetup,
 			NewsAndRatingsWadSetup,
 			MetacriticTargetingSetup,
+			NewsAndRatingsTargetingSetup,
 			MetacriticDynamicSlotsSetup,
 			MetacriticSlotsContextSetup,
 			MetacriticPrebidConfigSetup,
