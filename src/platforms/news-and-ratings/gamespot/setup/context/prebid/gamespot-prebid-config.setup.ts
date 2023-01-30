0570c2df1fb3f447aa05cfcf10f1ccae0972365e
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-01-30 15:12:37 +0100

    ADEN-12626 add Kargo to Gamespot

--- src/platforms/news-and-ratings/gamespot/setup/context/prebid/gamespot-prebid-config.setup.ts
+++ src/platforms/news-and-ratings/gamespot/setup/context/prebid/gamespot-prebid-config.setup.ts
@@ -10,6 +10,7 @@ import { Injectable } from '@wikia/dependency-injection';
 
 import { getAppnexusContext } from '../../../bidders/appnexus';
 import { getIndexExchangeContext } from '../../../bidders/index-exchange';
+import { getKargoContext } from '../../../bidders/kargo';
 import { getMedianetContext } from '../../../bidders/medianet';
 import { getPubmaticContext } from '../../../bidders/pubmatic';
 import { getRubiconDisplayContext } from '../../../bidders/rubicon-display';
@@ -28,6 +29,7 @@ export class GamespotPrebidConfigSetup implements DiProcess {
 
 		context.set('bidders.prebid.appnexus', getAppnexusContext(isDesktop));
 		context.set('bidders.prebid.indexExchange', getIndexExchangeContext(isDesktop));
+		context.set('bidders.prebid.kargo', getKargoContext(isDesktop));
 		context.set('bidders.prebid.medianet', getMedianetContext(isDesktop));
 		context.set('bidders.prebid.pubmatic', getPubmaticContext(isDesktop));
 		context.set('bidders.prebid.rubicon_display', getRubiconDisplayContext(isDesktop));
