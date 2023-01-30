503ad3363d87010f46b03c0a4cdc36cd3e83400c
Szymon Nawrocki <snawrocki@fandom.com>
2023-01-30 17:37:49 +0100

    ADEN-12630 Add YahooSspContext to Comicvine again..

--- src/platforms/news-and-ratings/comicvine/setup/context/prebid/comicvine-prebid-config.setup.ts
+++ src/platforms/news-and-ratings/comicvine/setup/context/prebid/comicvine-prebid-config.setup.ts
@@ -14,6 +14,7 @@ import { getMedianetContext } from '../../../bidders/medianet';
 import { getPubmaticContext } from '../../../bidders/pubmatic';
 import { getRubiconDisplayContext } from '../../../bidders/rubicon-display';
 import { getWikiaContext } from '../../../bidders/wikia';
+import { getYahooSspContext } from '../../../bidders/yahoossp';
 
 @Injectable()
 export class ComicvinePrebidConfigSetup implements DiProcess {
@@ -27,11 +28,12 @@ export class ComicvinePrebidConfigSetup implements DiProcess {
 		}
 
 		context.set('bidders.prebid.appnexus', getAppnexusContext(isDesktop));
-    context.set('bidders.prebid.indexExchange', getIndexExchangeContext(isDesktop));
+		context.set('bidders.prebid.indexExchange', getIndexExchangeContext(isDesktop));
 		context.set('bidders.prebid.medianet', getMedianetContext(isDesktop));
 		context.set('bidders.prebid.pubmatic', getPubmaticContext(isDesktop));
 		context.set('bidders.prebid.rubicon_display', getRubiconDisplayContext(isDesktop));
 		context.set('bidders.prebid.wikia', getWikiaContext());
+		context.set('bidders.prebid.yahoossp', getYahooSspContext(isDesktop));
 
 		this.registerListeners();
 	}
