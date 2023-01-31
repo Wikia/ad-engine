5e43898bba3a7bd041292f412c02dfd81b61a41c
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2023-01-31 14:44:12 +0100

    ADEN-12626 Add Kargo to N&R bundles (#1725)
    * ADEN-12626 add Kargo to ComicVine
    
    * ADEN-12626 add Kargo to GameFAQs
    
    * ADEN-12626 add Kargo to Gamespot
    
    * ADEN-12626 fix bad request from Kargo on Gamespot
    
    * ADEN-12626 add Kargo to GiantBomb
    
    * ADEN-12626 add Kargo to Metacritic
    
    * ADEN-12626 add Kargo to TV Guide
    
    * ADEN-12626 fix Kargo for ad units with multiple placement IDs
    
    * ADEN-12626 fix a typo
    
    ---------
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>
    Co-authored-by: Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>

--- src/platforms/news-and-ratings/gamespot/setup/context/prebid/gamespot-prebid-config.setup.ts
+++ src/platforms/news-and-ratings/gamespot/setup/context/prebid/gamespot-prebid-config.setup.ts
@@ -11,6 +11,7 @@ import { Injectable } from '@wikia/dependency-injection';
 import { getAppnexusContext } from '../../../bidders/appnexus';
 import { getCriteoContext } from '../../../bidders/criteo';
 import { getIndexExchangeContext } from '../../../bidders/index-exchange';
+import { getKargoContext } from '../../../bidders/kargo';
 import { getMedianetContext } from '../../../bidders/medianet';
 import { getPubmaticContext } from '../../../bidders/pubmatic';
 import { getRubiconDisplayContext } from '../../../bidders/rubicon-display';
@@ -31,6 +32,7 @@ export class GamespotPrebidConfigSetup implements DiProcess {
 		context.set('bidders.prebid.appnexus', getAppnexusContext(isDesktop));
 		context.set('bidders.prebid.criteo', getCriteoContext(isDesktop));
 		context.set('bidders.prebid.indexExchange', getIndexExchangeContext(isDesktop));
+		context.set('bidders.prebid.kargo', getKargoContext(isDesktop));
 		context.set('bidders.prebid.medianet', getMedianetContext(isDesktop));
 		context.set('bidders.prebid.pubmatic', getPubmaticContext(isDesktop));
 		context.set('bidders.prebid.rubicon_display', getRubiconDisplayContext(isDesktop));
