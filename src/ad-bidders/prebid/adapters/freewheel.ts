301a1e31b578f934e1bdf360e30aba4015e1452b
rafalgiemza <rgiemza@fandom.com>
2023-02-02 11:04:16 +0100

    ADEN-12216 | Add freewheel bider

---
+++ src/ad-bidders/prebid/adapters/freewheel.ts
@@ -0,0 +1,118 @@
+import { context, Dictionary, pbjsFactory, utils } from '@ad-engine/core';
+import { EXTENDED_MAX_CPM, PrebidAdapter } from '../prebid-adapter';
+
+const price = utils.queryString.get('wikia_video_adapter');
+const limit = parseInt(utils.queryString.get('wikia_adapter_limit'), 10) || 99;
+const timeout = parseInt(utils.queryString.get('wikia_adapter_timeout'), 10) || 100;
+const useRandomPrice = utils.queryString.get('wikia_adapter_random') === '1';
+
+export class Freewheel extends PrebidAdapter {
+	static bidderName = 'freewheel-ssp';
+	limit: number;
+	useRandomPrice: boolean;
+	timeout: number;
+	maxCpm = EXTENDED_MAX_CPM;
+
+	constructor(options) {
+		super(options);
+
+		this.enabled = !!price;
+		this.limit = limit;
+		this.timeout = timeout;
+		this.useRandomPrice = useRandomPrice;
+		this.isCustomBidAdapter = true;
+	}
+
+	static getVastUrl(width, height, slotName): string {
+		return utils.buildVastUrl(width / height, slotName, {
+			videoAdUnitId: context.get(`bidders.prebid.wikiaVideo.slots.${slotName}.videoAdUnitId`),
+			customParams: context.get(`bidders.prebid.wikiaVideo.slots.${slotName}.customParams`),
+		});
+	}
+
+	get bidderName(): string {
+		return Freewheel.bidderName;
+	}
+
+	prepareConfigForAdUnit(code): PrebidAdUnit {
+		return {
+			code,
+			mediaTypes: {
+				video: {
+					playerSize: [640, 480],
+					context: 'instream',
+				},
+			},
+			bids: [
+				{
+					bidder: this.bidderName,
+					labelAll: ['desktop'],
+					params: {
+						zoneId: 32563810, //only mandatory field
+						minPlayerSize: [100, 200],
+						maxPlayerSize: [1800, 1000],
+					},
+				},
+				{
+					bidder: this.bidderName,
+					labelAll: ['phone'],
+					params: {
+						zoneId: 32563826, //only mandatory field
+						minPlayerSize: [300, 50],
+						maxPlayerSize: [600, 350],
+					},
+				},
+			],
+		};
+	}
+
+	getSpec(): Dictionary<string | string[]> {
+		return {
+			code: this.bidderName,
+			supportedMediaTypes: ['video'],
+		};
+	}
+
+	getPrice(): number {
+		if (this.useRandomPrice) {
+			return Math.floor(Math.random() * 20);
+		}
+
+		return parseInt(price, 10) / 100;
+	}
+
+	callBids(bidRequest, addBidResponse, done): void {
+		this.addBids(bidRequest, addBidResponse, done);
+	}
+
+	addBids(bidRequest, addBidResponse, done): void {
+		setTimeout(async () => {
+			const pbjs: Pbjs = await pbjsFactory.init();
+
+			bidRequest.bids.forEach((bid) => {
+				if (this.limit === 0) {
+					return;
+				}
+
+				const bidResponse = pbjs.createBid(1);
+				const [width, height] = bid.sizes[0];
+				const slotName = bid.adUnitCode;
+
+				bidResponse.bidderCode = bidRequest.bidderCode;
+				bidResponse.cpm = this.getPrice();
+				bidResponse.creativeId = 'foo123_wikiaVideoCreativeId'; // TODO freewheel
+				bidResponse.ttl = 300;
+				bidResponse.mediaType = 'video';
+				bidResponse.width = width;
+				bidResponse.height = height;
+				bidResponse.vastUrl = Freewheel.getVastUrl(width, height, slotName);
+				bidResponse.videoCacheKey = '123foo_wikiaVideoCacheKey'; // TODO freewheel
+
+				addBidResponse(bid.adUnitCode, bidResponse);
+				this.limit -= 1;
+			});
+
+			done();
+		}, this.timeout);
+	}
+}
