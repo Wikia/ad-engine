4f0beae3aee7901918df256e06f7dd4b3b2f017e
Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
2023-02-02 10:55:05 +0100

    Update tvguide-platform.ts

--- src/platforms/news-and-ratings/tvguide/tvguide-platform.ts
+++ src/platforms/news-and-ratings/tvguide/tvguide-platform.ts
@@ -13,7 +13,7 @@ import { TvGuidePrebidConfigSetup } from './setup/context/prebid/tvguide-prebid-
 import { TvGuideSlotsContextSetup } from './setup/context/slots/tvguide-slots-context.setup';
 import { TvGuideTargetingSetup } from './setup/context/targeting/tvguide-targeting.setup';
 import { TvGuideDynamicSlotsSetup } from './setup/dynamic-slots/tvguide-dynamic-slots.setup';
-import { TvguideTemplatesSetup } from './templates/tvguide-templates.setup';
+import { TvGuideTemplatesSetup } from './templates/tvguide-templates.setup';
 
 @Injectable()
 export class TvGuidePlatform {
@@ -34,7 +34,7 @@ export class TvGuidePlatform {
 			TvGuideSlotsContextSetup,
 			TvGuidePrebidConfigSetup,
 			BiddersStateSetup,
-			TvguideTemplatesSetup,
+			TvGuideTemplatesSetup,
 			NewsAndRatingsAdsMode,
 		);
 
