a2df31590b9c77127802de72f77691a87d72c807
Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>
2023-02-03 13:33:51 +0100

    ADEN-12719 Add Audigent to N&R platforms (#1735)
    * ADEN-12719 Add Audigent to N&R platforms
    
    * [ADEN-12719] Grammar
    
    ---------
    
    Co-authored-by: Karol Wajs <90037542+kwfandom@users.noreply.github.com>

--- src/platforms/news-and-ratings/shared/modes/news-and-ratings-ads-mode.ts
+++ src/platforms/news-and-ratings/shared/modes/news-and-ratings-ads-mode.ts
@@ -1,5 +1,6 @@
 import { GptSetup, PlayerSetup, WadRunner } from '@platforms/shared';
 import {
+	Audigent,
 	Bidders,
 	Captify,
 	communicationService,
@@ -19,6 +20,7 @@ import { Injectable } from '@wikia/dependency-injection';
 export class NewsAndRatingsAdsMode implements DiProcess {
 	constructor(
 		private pipeline: PartnerPipeline,
+		private audigent: Audigent,
 		private bidders: Bidders,
 		private captify: Captify,
 		private confiant: Confiant,
@@ -40,6 +42,7 @@ export class NewsAndRatingsAdsMode implements DiProcess {
 				this.userIdentity,
 				this.liveRampPixel.setOptions({ dependencies: [this.userIdentity.initialized] }),
 				this.liveConnect,
+				this.audigent,
 				this.confiant,
 				this.iasPublisherOptimization,
 				this.captify,
