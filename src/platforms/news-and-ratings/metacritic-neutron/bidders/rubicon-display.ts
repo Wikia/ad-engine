df3457e0fc246aebdb461e123536050eb2d3d676
Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>
2023-02-06 13:00:32 +0100

    ADEN-12714 Add rest of the bidders to Metacritic News (metacritic-neutron) (#1740)
    * ADEN-12161 Add Appnexus to Metacritic News
    
    * ADEN-12161 Add Medianet to metacritic-neutron platform
    
    * ADEN-12161 Add Pubmatic to metacritic-neutron platform
    
    * ADEN-12161 N&R bidders - move 2 desktop slots out of mobile slots
    
    * ADEN-12161 Add Rubicon to metacritic-neutron platform
    
    * ADEN-12161 Refactor rubicon-display.ts to handle zoneId as array
    
    * ADEN-12714 Fix Rubicon zoneId format
    
    * ADEN-12714 Add video slot to metacritic-neutron slots context setup
    
    ---------
    
    Co-authored-by: Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
    Co-authored-by: Karol Wajs <90037542+kwfandom@users.noreply.github.com>

--- src/platforms/news-and-ratings/metacritic-neutron/bidders/rubicon-display.ts
+++ src/platforms/news-and-ratings/metacritic-neutron/bidders/rubicon-display.ts
@@ -1,93 +1,103 @@
-export function getIndexExchangeContext(isDesktop: boolean): object {
+export function getRubiconDisplayContext(isDesktop: boolean): object {
 	const desktopSlots = {
 		'omni-skybox-leader-nav': {
 			sizes: [[728, 90]],
-			siteId: ['936309'],
+			siteId: '462846',
+			zoneId: '2720056',
 		},
 		'omni-skybox-leaderboard-nav': {
 			sizes: [[728, 90]],
-			siteId: ['936311'],
+			siteId: '462846',
+			zoneId: '2720058',
 		},
 		'incontent-leader-plus-top': {
 			sizes: [
 				[970, 250],
 				[728, 90],
 			],
-			siteId: ['936316', '936319'],
+			siteId: '462846',
+			zoneId: ['2720064', '2720070'],
 		},
 		'incontent-leaderboard-top': {
 			sizes: [[728, 90]],
-			siteId: ['936319'],
+			siteId: '462846',
+			zoneId: '2720070',
 		},
 		'incontent-leader-plus-bottom': {
 			sizes: [
 				[728, 90],
 				[970, 250],
 			],
-			siteId: ['936315', '936310'],
+			siteId: '462846',
+			zoneId: ['2720064', '2720056'],
 		},
 		'incontent-leaderboard-bottom': {
 			sizes: [[728, 90]],
-			siteId: ['936317'],
+			siteId: '462846',
+			zoneId: '2720066',
 		},
 		'mpu-plus-top': {
 			sizes: [[300, 250]],
-			siteId: ['936312'],
+			siteId: '462846',
+			zoneId: '2720060',
 		},
 		'mpu-top': {
 			sizes: [[300, 250]],
-			siteId: ['936314'],
+			siteId: '462846',
+			zoneId: '2720062',
 		},
 		'mpu-middle': {
 			sizes: [[300, 250]],
-			siteId: ['936325'],
+			siteId: '462848',
+			zoneId: '2720078',
 		},
 		'mpu-bottom': {
 			sizes: [[300, 250]],
-			siteId: ['936327'],
-		},
-		video: {
-			siteId: '936594',
+			siteId: '462848',
+			zoneId: '2720080',
 		},
 	};
 
 	const mobileSlots = {
 		'mobile-omni-skybox-plus-nav': {
 			sizes: [[320, 50]],
-			siteId: ['936322'],
+			siteId: '462848',
+			zoneId: '2720074',
 		},
 		'mobile-banner-plus': {
 			sizes: [
 				[320, 50],
 				[300, 250],
 			],
-			siteId: ['936324', '936312'],
+			siteId: '462848',
+			zoneId: ['2720076', '2720060'],
 		},
 		'mobile-incontent-plus': {
 			sizes: [
 				[300, 250],
 				[320, 50],
 			],
-			siteId: ['936314', '936326'],
+			siteId: '462848',
+			zoneId: ['2720062', '2720078'],
 		},
 		'mobile-incontent-plus-bottom': {
 			sizes: [
 				[320, 50],
 				[300, 250],
 			],
-			siteId: ['936328', '936325'],
+			siteId: '462848',
+			zoneId: ['2720080', '2720078'],
 		},
 		'leaderboard-middle': {
 			sizes: [[728, 90]],
-			siteId: ['936318'],
-		},
-		video: {
-			siteId: '936611',
+			siteId: '462846',
+			zoneId: '2720068',
 		},
 	};
 
 	return {
 		enabled: false,
+		accountId: 7450,
 		slots: isDesktop ? desktopSlots : mobileSlots,
 	};
 }
