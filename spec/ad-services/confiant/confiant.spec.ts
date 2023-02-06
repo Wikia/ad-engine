7c3636b6798e8f30456e9a2f56b49f8e29e69c40
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2023-02-06 14:55:18 +0100

    ADEN-12739 Add new script for Confiant on N&R (#1741)
    * ADEN-12739 reorganise code and add unit tests
    
    * ADEN-12739 add possibility to overwrite default property ID for Confiant
    
    * ADEN-12739 enable Confiant new script on N&R
    
    ---------
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
    Co-authored-by: Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>

---
+++ spec/ad-services/confiant/confiant.spec.ts
@@ -0,0 +1,84 @@
+import { Confiant } from '@wikia/ad-services';
+import { context, InstantConfigService, utils } from '@wikia/core';
+import { expect } from 'chai';
+import { createSandbox } from 'sinon';
+
+describe('Confiant', () => {
+	const DEFAULT_CONFIANT_LIB_URL =
+		'//cdn.confiant-integrations.net/d-aIf3ibf0cYxCLB1HTWfBQOFEA/gpt_and_prebid/config.js';
+	const TEST_CONFIANT_LIB_URL =
+		'//cdn.confiant-integrations.net/TEST_PROPERTY_ID/gpt_and_prebid/config.js';
+
+	const sandbox = createSandbox();
+
+	let loadScriptStub, instantConfigStub;
+	let confiant: Confiant;
+
+	beforeEach(() => {
+		instantConfigStub = sandbox.createStubInstance(InstantConfigService);
+
+		loadScriptStub = sandbox
+			.stub(utils.scriptLoader, 'loadScript')
+			.returns(Promise.resolve({} as any));
+
+		confiant = new Confiant(instantConfigStub);
+	});
+
+	afterEach(() => {
+		sandbox.restore();
+		loadScriptStub.resetHistory();
+
+		context.remove('services.confiant.propertyId');
+	});
+
+	it('Confiant is called when enabled', async () => {
+		instantConfigStub.get.withArgs('icConfiant').returns(true);
+
+		await confiant.call();
+
+		expect(loadScriptStub.called).to.equal(true);
+	});
+
+	it('Confiant is loaded with default script', async () => {
+		instantConfigStub.get.withArgs('icConfiant').returns(true);
+
+		await confiant.call();
+
+		expect(loadScriptStub.calledWith(DEFAULT_CONFIANT_LIB_URL)).to.equal(true);
+	});
+
+	it('Confiant is loaded with selected propertyId in the script URL', async () => {
+		instantConfigStub.get.withArgs('icConfiant').returns(true);
+		context.set('services.confiant.propertyId', 'TEST_PROPERTY_ID');
+
+		await confiant.call();
+
+		expect(loadScriptStub.calledWith(TEST_CONFIANT_LIB_URL)).to.equal(true);
+	});
+
+	it('Confiant is loaded with default propertyId when the context one is an empty string', async () => {
+		instantConfigStub.get.withArgs('icConfiant').returns(true);
+		context.set('services.confiant.propertyId', '');
+
+		await confiant.call();
+
+		expect(loadScriptStub.calledWith(DEFAULT_CONFIANT_LIB_URL)).to.equal(true);
+	});
+
+	it('Confiant is loaded with default propertyId when the context one is undefined', async () => {
+		instantConfigStub.get.withArgs('icConfiant').returns(true);
+		context.set('services.confiant.propertyId', undefined);
+
+		await confiant.call();
+
+		expect(loadScriptStub.calledWith(DEFAULT_CONFIANT_LIB_URL)).to.equal(true);
+	});
+
+	it('Confiant is not called when disabled', async () => {
+		instantConfigStub.get.withArgs('icConfiant').returns(false);
+
+		await confiant.call();
+
+		expect(loadScriptStub.called).to.equal(false);
+	});
+});
