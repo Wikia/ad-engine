d6a078d5fcde845521faef07de17051065a0ddac
Jakub Banaś <jbanas@fandom.com>
2023-02-08 09:36:03 +0100

    ADEN-12720 cr fix

--- src/platforms/shared/setup/tracking-parameters.setup.ts
+++ src/platforms/shared/setup/tracking-parameters.setup.ts
@@ -3,52 +3,54 @@ import { Injectable } from '@wikia/dependency-injection';
 import Cookies from 'js-cookie';
 import { getMediaWikiVariable } from '../utils/get-media-wiki-variable';
 
-function getLegacyTrackingParameters(): ITrackingParameters {
-	const cookies = Cookies.get();
-	const wikiContext = {
-		beaconId:
-			getMediaWikiVariable('beaconId') ||
-			window.beaconId ||
-			window.beacon_id ||
-			cookies['wikia_beacon_id'],
-		pvNumber: getMediaWikiVariable('pvNumber') || window.pvNumber || cookies['pv_number'],
-		pvNumberGlobal:
-			getMediaWikiVariable('pvNumberGlobal') ||
-			window.pvNumberGlobal ||
-			cookies['pv_number_global'],
-		pvUID: getMediaWikiVariable('pvUID') || window.pvUID,
-		sessionId:
-			getMediaWikiVariable('sessionId') ||
-			window.sessionId ||
-			window.session_id ||
-			cookies['tracking_session_id'],
-	};
-
-	return wikiContext;
-}
-
-async function getNewTrackingParameters(): Promise<ITrackingParameters> {
-	await new utils.WaitFor(() => !!window.fandomContext.tracking, 10, 100).until();
-
-	const wikiContext = {
-		...window.fandomContext.tracking,
-		pvUID: getMediaWikiVariable('pvUID') || window.pvUID,
-	};
-
-	return wikiContext;
-}
-
-async function getTrackingParameters(legacyEnabled: boolean): Promise<ITrackingParameters> {
-	return legacyEnabled ? getLegacyTrackingParameters() : await getNewTrackingParameters();
-}
-
 @Injectable()
 export class TrackingParametersSetup implements DiProcess {
 	constructor(private instantConfig: InstantConfigService) {}
 
+	private getLegacyTrackingParameters(): ITrackingParameters {
+		const cookies = Cookies.get();
+		const wikiContext = {
+			beaconId:
+				getMediaWikiVariable('beaconId') ||
+				window.beaconId ||
+				window.beacon_id ||
+				cookies['wikia_beacon_id'],
+			pvNumber: getMediaWikiVariable('pvNumber') || window.pvNumber || cookies['pv_number'],
+			pvNumberGlobal:
+				getMediaWikiVariable('pvNumberGlobal') ||
+				window.pvNumberGlobal ||
+				cookies['pv_number_global'],
+			pvUID: getMediaWikiVariable('pvUID') || window.pvUID,
+			sessionId:
+				getMediaWikiVariable('sessionId') ||
+				window.sessionId ||
+				window.session_id ||
+				cookies['tracking_session_id'],
+		};
+
+		return wikiContext;
+	}
+
+	private async getNewTrackingParameters(): Promise<ITrackingParameters> {
+		await new utils.WaitFor(() => !!window.fandomContext.tracking, 10, 100).until();
+
+		const wikiContext = {
+			...window.fandomContext.tracking,
+			pvUID: getMediaWikiVariable('pvUID') || window.pvUID,
+		};
+
+		return wikiContext;
+	}
+
+	private async getTrackingParameters(legacyEnabled: boolean): Promise<ITrackingParameters> {
+		return legacyEnabled
+			? this.getLegacyTrackingParameters()
+			: await this.getNewTrackingParameters();
+	}
+
 	async execute() {
 		const legacyEnabled = !this.instantConfig.get('icDisableLegacyTrackingParameters', false);
-		const trackingParameters = await getTrackingParameters(legacyEnabled);
+		const trackingParameters = await this.getTrackingParameters(legacyEnabled);
 
 		context.set('wiki', { ...context.get('wiki'), ...trackingParameters });
 	}
--- src/platforms/shared/tracking/data-warehouse.ts
+++ src/platforms/shared/tracking/data-warehouse.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@wikia/dependency-injection';
 import { TrackingParams } from './models/tracking-params';
 
 const logGroup = 'data-warehouse-trackingParams';
-const eventUrl = 'https://beacon.wikia-services.com/"__track/special/trackingevent"';
+const eventUrl = 'https://beacon.wikia-services.com/__track/special/trackingevent';
 const videoEventUrl = 'https://beacon.wikia-services.com/__track/special/videoplayerevent';
 
 export interface TimeBasedParams {
