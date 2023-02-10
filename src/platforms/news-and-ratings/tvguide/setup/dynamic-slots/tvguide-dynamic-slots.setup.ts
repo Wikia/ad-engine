d2d01d71562da04994e6e242206e32d8ab7950b9
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2023-02-10 12:16:28 +0100

    ADEN-12667 Fix GPT errors on TV Guide (#1755)
    * ADEN-12667 remove GPT error for slots not existing in the config/context
    
    * ADEN-12667 remove GPT errors for incontents
    
    * ADEN-12667 extend slotInjector and use it with slotRepeater to repeat incontent-leader-plus-inc slots
    
    * Update README.md (#1756)
    
    * 128.0.1
    
    * ADEN-12667-cr Other idea of repeatable slots on TvGuide (#1757)
    
    * ADEN-12667-cr Other idea of repeatable slots on TvGuide
    
    * ADEN-12667 CR fixes
    
    * ADEN-12667 More CR fixes
    
    * ADEN-12667 Last CR
    
    ---------
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
    Co-authored-by: Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
    Co-authored-by: Jenkins <jenkins@fandom.com>

--- src/platforms/news-and-ratings/tvguide/setup/dynamic-slots/tvguide-dynamic-slots.setup.ts
+++ src/platforms/news-and-ratings/tvguide/setup/dynamic-slots/tvguide-dynamic-slots.setup.ts
@@ -1,8 +1,10 @@
 import {
+	AdSlot,
 	communicationService,
 	context,
 	DiProcess,
 	eventsRepository,
+	slotService,
 	utils,
 } from '@wikia/ad-engine';
 
@@ -26,6 +28,8 @@ export class TvGuideDynamicSlotsSetup implements DiProcess {
 	}
 
 	private injectSlots(adPlaceholders): void {
+		const pushedSlots = [];
+
 		adPlaceholders.forEach((placeholder) => {
 			const adWrapper = placeholder.firstElementChild;
 
@@ -34,10 +38,52 @@ export class TvGuideDynamicSlotsSetup implements DiProcess {
 			}
 
 			const adSlotName = adWrapper.getAttribute('data-ad');
+
+			if (pushedSlots.includes(adSlotName)) {
+				return;
+			}
+
+			if (context.get(`slots.${adSlotName}.repeat`)) {
+				this.setupRepeatableSlot(adSlotName);
+			}
+
+			pushedSlots.push(adSlotName);
 			adWrapper.id = adSlotName;
 
 			context.push('state.adStack', { id: adSlotName });
 		});
+	}
+
+	private setupRepeatableSlot(slotName, slotNameBase = '') {
+		communicationService.onSlotEvent(
+			AdSlot.STATUS_SUCCESS,
+			() => this.injectNextSlot(slotName, slotNameBase),
+			slotName,
+			true,
+		);
+		communicationService.onSlotEvent(
+			AdSlot.STATUS_COLLAPSE,
+			() => this.injectNextSlot(slotName, slotNameBase),
+			slotName,
+			true,
+		);
+	}
+
+	private injectNextSlot(slotName, slotNameBase = '') {
+		const adSlot = slotService.get(slotName);
+		const nextIndex = adSlot.getConfigProperty('repeat.index') + 1;
+		const nextSlotName = `${slotNameBase || slotName}-${nextIndex}`;
+		const nextSlotPlace = document.querySelector(
+			`.c-adDisplay_container > div[data-ad="${slotNameBase || slotName}"]:not(.gpt-ad)`,
+		);
+
+		if (!nextSlotPlace) {
+			return;
+		}
+
+		this.setupRepeatableSlot(nextSlotName, slotNameBase || slotName);
+		nextSlotPlace.id = nextSlotName;
+		context.push('state.adStack', { id: nextSlotName });
 	}
 
 	// TODO: This is temporary workaround. Change it for the proper event informing that ad placeholders
