9cb255555fbac53eb88c435c359a40de45b0c4d3
Jakub Bieńkowski <jbienkowski@fandom.com>
2023-02-14 13:18:56 +0100

    ADEN-12746 Cleanup

--- src/platforms/test-page/index.ts
+++
@@ -1,7 +0,0 @@
-import { Container } from '@wikia/dependency-injection';
-import { TestPagePlatform } from './test-page-platform';
-
-const container = new Container();
-const platform = container.get(TestPagePlatform);
-
-platform.execute();
