dfd58bcb863a8980d47f5c351a7de63572f49c77
rafalgiemza <rgiemza@fandom.com>
2023-03-24 14:15:10 +0100

    ADEN-12878 | Integrate A9 video prebid on N&R

--- src/platforms/shared/bidders/bidders-state.setup.ts
+++ src/platforms/shared/bidders/bidders-state.setup.ts
@@ -1,4 +1,11 @@
-import { context, Dictionary, DiProcess, InstantConfigService, utils } from '@wikia/ad-engine';
+import {
+	context,
+	Dictionary,
+	DiProcess,
+	InstantConfigService,
+	targetingService,
+	utils,
+} from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
 
 @Injectable()
@@ -36,15 +43,12 @@ export class BiddersStateSetup implements DiProcess {
 	}
 
 	setupBidders(): void {
-		const hasFeaturedVideo =
-			context.get('custom.hasFeaturedVideo') || context.get('slots.video.isVideo');
-
 		if (this.instantConfig.get('icA9Bidder')) {
 			context.set('bidders.a9.enabled', true);
 			context.set('bidders.coppaA9', this.instantConfig.get('icCoppaA9'));
 			context.set(
 				'bidders.a9.videoEnabled',
-				this.instantConfig.get('icA9VideoBidder') && hasFeaturedVideo,
+				this.instantConfig.get('icA9VideoBidder') && isAnyVideo(),
 			);
 		}
 
@@ -69,6 +73,16 @@ export class BiddersStateSetup implements DiProcess {
 			'bidders.enabled',
 			context.get('bidders.prebid.enabled') || context.get('bidders.a9.enabled'),
 		);
+
+		function isAnyVideo(): boolean {
+			// Is there is a FeaturedVideo on UCP?
+			const hasFeaturedVideo = context.get('custom.hasFeaturedVideo');
+
+			// Is there a video on News&Ratings?
+			const ptypeIncludesVideo = targetingService.get('ptype')?.includes('video_');
+
+			return Boolean(hasFeaturedVideo || ptypeIncludesVideo);
+		}
 	}
 
 	private enableIfApplicable(name: string, icKey: string): void {
