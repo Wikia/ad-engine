07d43b53b5048ec4da4378a5024ca7065285a985
awesolowski <awesolowski@fandom.com>
2023-03-30 16:27:31 +0200

    ADEN-12854 - change constant declaration from static to readonly - CR suggestion

--- src/core/utils/outbound-traffic-restrict.ts
+++ src/core/utils/outbound-traffic-restrict.ts
@@ -1,19 +1,17 @@
 import { context } from '../services';
 
 class OutboundTrafficRestrict {
-	private static MAX = 100;
-	private static DEFAULT_THRESHOLD = 2;
+	private readonly MAX = 100;
+	private readonly DEFAULT_THRESHOLD = 2;
 
 	public isOutboundTrafficAllowed(serviceName = 'default'): boolean {
 		return (
-			this.getSeed() <
-			(context.get(`services.${serviceName}.threshold`) ||
-				OutboundTrafficRestrict.DEFAULT_THRESHOLD)
+			this.getSeed() < (context.get(`services.${serviceName}.threshold`) || this.DEFAULT_THRESHOLD)
 		);
 	}
 
 	private getSeed(): number {
-		return Math.random() * OutboundTrafficRestrict.MAX;
+		return Math.random() * this.MAX;
 	}
 }
 
