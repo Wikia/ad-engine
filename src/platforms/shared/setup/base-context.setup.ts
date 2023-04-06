6628e77901c0c1166ee4e6573bf2a9a3e3a2142c
Damian Sobczak <dsobczak@wikia-inc.com>
2023-04-06 09:25:15 +0200

    ADEN-12849 Adding icVariable

--- src/platforms/shared/setup/base-context.setup.ts
+++ src/platforms/shared/setup/base-context.setup.ts
@@ -65,6 +65,10 @@ export class BaseContextSetup implements DiProcess {
 	}
 
 	private setOptionsContext(): void {
+		if (!this.instantConfig.get('icIncontentHeadersExperiment')) {
+			context.set('templates.incontentAnchorSelector', '.mw-parser-output > h2');
+		}
+
 		context.set('options.performanceAds', this.instantConfig.get('icPerformanceAds'));
 		context.set('options.stickyTbExperiment', this.instantConfig.get('icStickyTbExperiment'));
 		context.set(
