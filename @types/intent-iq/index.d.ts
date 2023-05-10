9608ccd470ceda8eec39b15a0ba6d2399c97741b
Jakub Bieńkowski <124168110+jbienkowski-fandom@users.noreply.github.com>
2023-05-10 11:34:47 +0200

    ADEN-12643 Implementation of IntentIQ integration (#1910)
    * WIP
    
    * ADEN-12643 Initial implementation of IntentIQ integration
    
    * ADEN-12643 Fixes
    
    * ADEN-12643 do not wait for the promise to resolve
    
    * ADEN-12643 Add unit tests
    
    * ADEN-12643 AB test 90/10
    
    * ADEN-12643 Use final scripts' locations
    
    * ADEN-12643 Take user consents into consideration
    
    * ADEN-12643 Fix unit tests

---
+++ @types/intent-iq/index.d.ts
@@ -0,0 +1,46 @@
+/**
+ * IntentIQ configuration object.
+ *
+ * @see: https://prebid-rtb.documents.intentiq.com/integration/iiq-js-integration
+ */
+interface IntentIQConfig {
+	partner: number;
+	pbjs?: Pbjs;
+	timeoutInMillis?: number;
+	ABTestingConfigurationSource?: string;
+	abPercentage?: number;
+	manualWinReportEnabled?: boolean;
+	partnerClientId?: string;
+	partnerClientIdType?: number;
+	callback?: (data: any) => void;
+	browserBlackList?: string;
+	domainName?: string;
+}
+
+/**
+ * IntentIQ report data object.
+ *
+ * @see: https://prebid-rtb.documents.intentiq.com/integration/getting-started-with-rtpo-win-report
+ */
+interface IntentIQReportData {
+	biddingPlatformId: number;
+	partnerAuctionId?: string;
+	bidderCode: string;
+	prebidAuctionId?: string;
+	cpm: number;
+	currency: string;
+	originalCpm?: number;
+	originalCurrency?: string;
+	status?: string;
+	placementId?: string;
+}
+
+type IntentIqObject = {
+	new (config: IntentIQConfig): IntentIqObject;
+	reportExternalWin(data: IntentIQReportData): void;
+	intentIqConfig?: {
+		abTesting?: {
+			currentTestGroup: string;
+		};
+	};
+};
---
+++ spec/platforms/shared/utils/instant-config.setup.spec.ts
@@ -0,0 +1,56 @@
+import { Audigent } from '@wikia/ad-services';
+import { context, InstantConfigService, pbjsFactory } from '@wikia/core';
+import { Binding, Container } from '@wikia/dependency-injection';
+import { InstantConfigSetup } from '@wikia/platforms/shared';
+import { expect } from 'chai';
+import { SinonSpy, SinonStub } from 'sinon';
+
+describe('InstantConfigSetup', () => {
+	let container;
+	const instantConfigService = new InstantConfigService();
+	let containerBindStub: SinonStub;
+	let containerValueSpy: SinonSpy;
+
+	beforeEach(() => {
+		container = new Container();
+		global.sandbox.stub(InstantConfigService.prototype, 'init').resolves(instantConfigService);
+		containerValueSpy = global.sandbox.spy();
+		containerBindStub = global.sandbox.stub(container, 'bind').returns({
+			value: containerValueSpy,
+		} as unknown as Binding<any>);
+	});
+
+	it('should bind InstantConfigService to container', async () => {
+		global.sandbox
+			.stub(InstantConfigService.prototype, 'get')
+			.withArgs('icPrebid')
+			.returns(false)
+			.withArgs('icAudigent')
+			.returns(false);
+		const instantConfigSetup = new InstantConfigSetup(container);
+
+		await instantConfigSetup.execute();
+
+		expect(containerBindStub.calledWithExactly(InstantConfigService)).to.be.true;
+		expect(containerValueSpy.calledWithExactly(instantConfigService)).to.be.true;
+	});
+
+	it('should preloadLibraries', async () => {
+		global.sandbox
+			.stub(InstantConfigService.prototype, 'get')
+			.withArgs('icPrebid')
+			.returns(true)
+			.withArgs('icAudigent')
+			.returns(true);
+		const contextSetStub = global.sandbox.stub(context, 'set');
+		const pbjsFactoryInitStub = global.sandbox.stub(pbjsFactory, 'init');
+		const audigentLoadSegmentLibraryStub = global.sandbox.stub(Audigent, 'loadSegmentLibrary');
+		const instantConfigSetup = new InstantConfigSetup(container);
+
+		await instantConfigSetup.execute();
+
+		expect(contextSetStub.calledOnceWith('bidders.prebid.libraryUrl')).to.be.true;
+		expect(pbjsFactoryInitStub.calledOnce).to.be.true;
+		expect(audigentLoadSegmentLibraryStub.calledOnce).to.be.true;
+	});
+});
