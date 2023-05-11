e59ad9b72be2107ac71808e393db0463f450fe31
Janik <dj@kazan.com.pl>
2023-05-11 13:03:50 +0200

    ADEN-13087 - Fixed problem with AgeGate variable in non-kid wikis

--- src/core/utils/is-coppa-subject.ts
+++ src/core/utils/is-coppa-subject.ts
@@ -2,19 +2,19 @@ import { utils } from '../index';
 import { context, CookieStorageAdapter } from '../services';
 
 export function isCoppaSubject(): boolean {
-	const directedAtChildren = context.get('wiki.targeting.directedAtChildren');
-	if (context.get('services.ageGateHandling') && directedAtChildren) {
+	const wikiDirectedAtChildren = context.get('wiki.targeting.directedAtChildren');
+	if (context.get('services.ageGateHandling') && wikiDirectedAtChildren) {
 		try {
 			const cookieStorage = new CookieStorageAdapter();
 			const ageGateResult = cookieStorage.getItem('age_gate');
 			if (ageGateResult) {
 				const [dialogShown, adult] = ageGateResult.split('|');
-				return dialogShown === '1' ? adult === '0' : directedAtChildren;
+				return dialogShown === '1' ? adult === '0' : wikiDirectedAtChildren;
 			}
 		} catch (e) {
 			utils.logger('age-gate', 'Error while reading age gate cookie');
 		}
 	}
 
-	return directedAtChildren;
+	return wikiDirectedAtChildren;
 }
