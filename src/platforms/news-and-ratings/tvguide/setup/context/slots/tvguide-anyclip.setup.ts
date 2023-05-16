12a3b590ac2a634bd6baa76fb6eaf22f456814a1
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-05-16 13:38:48 +0200

    VIDEO-524 add listings page to Anyclip applicable and insert placeholders

--- src/platforms/news-and-ratings/tvguide/setup/context/slots/tvguide-anyclip.setup.ts
+++ src/platforms/news-and-ratings/tvguide/setup/context/slots/tvguide-anyclip.setup.ts
@@ -1,17 +1,66 @@
-import { context, DiProcess, targetingService, utils } from '@wikia/ad-engine';
+import {
+	communicationService,
+	context,
+	DiProcess,
+	eventsRepository,
+	targetingService,
+	utils,
+} from '@wikia/ad-engine';
 
 export class TvGuideAnyclipSetup implements DiProcess {
 	private logGroup = 'Anyclip';
 
 	execute(): void {
+		const pname = targetingService.get('pname');
+
 		context.set('services.anyclip.isApplicable', () => {
-			const pname = targetingService.get('pname');
 			const isApplicable = this.isApplicable(pname);
 
 			utils.logger(this.logGroup, 'isApplicable: ', isApplicable, pname);
 
 			return isApplicable;
 		});
+
+		if (this.shouldPlayerBeIncontent(pname)) {
+			communicationService.on(
+				eventsRepository.PLATFORM_AD_PLACEMENT_READY,
+				({ placementId }) => {
+					if (placementId === 'leader-top') {
+						utils.logger(this.logGroup, 'inserting placeholders');
+						this.insertPlaceholders();
+					}
+				},
+				false,
+			);
+		}
+	}
+
+	private insertPlaceholders(): void {
+		const listingScheduleRow = document.querySelector('.c-tvListingsSchedule_video');
+		listingScheduleRow.appendChild(this.buildRow());
+	}
+
+	private buildRow(): HTMLElement {
+		const listingScheduleRow = document.createElement('div');
+		listingScheduleRow.classList.add('c-tvListingsVideo-container', 'ae-anyclip-row');
+
+		const listingVideoContainer = document.createElement('div');
+		listingVideoContainer.classList.add('c-tvListingsVideo-container', 'ae-anyclip-container');
+
+		const videoElement = document.createElement('div');
+		videoElement.classList.add('c-tvListingsVideo_media', 'g-outer-spacing-bottom-medium');
+		videoElement.id = 'incontent_player';
+
+		listingVideoContainer.appendChild(videoElement);
+		listingScheduleRow.appendChild(listingVideoContainer);
+
+		return listingScheduleRow;
+	}
+
+	shouldPlayerBeIncontent(pname: string): boolean {
+		const incontentPnames = ['listings/main'];
+
+		return this.isApplicable(pname) && incontentPnames.indexOf(pname) !== -1;
 	}
 
 	isApplicable(pname: string): boolean {
