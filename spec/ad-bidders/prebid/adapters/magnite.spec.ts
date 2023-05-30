081461f79194adaf4f552230bc0b9113613a0801
preludek <tomasz.felczak89@gmail.com>
2023-05-30 10:33:03 +0200

    ADEN-13228 add spec

---
+++ spec/ad-bidders/prebid/adapters/magnite.spec.ts
@@ -0,0 +1,40 @@
+import { Magnite } from '@wikia/ad-bidders/prebid/adapters/magnite';
+import { expect } from 'chai';
+
+describe('Magnite bidder adapter', () => {
+	it('can be enabled', () => {
+		const magnite = new Magnite({
+			enabled: true,
+			slots: {},
+		});
+
+		expect(magnite.enabled).to.equal(true);
+	});
+
+	it('prepareAdUnits returns data in correct shape', () => {
+		const magnite = new Magnite({
+			enabled: true,
+			slots: {
+				top_boxad: {
+					sizes: [[300, 250]],
+				},
+			},
+		});
+
+		expect(magnite.prepareAdUnits()).to.deep.equal([
+			{
+				code: 'top_boxad',
+				mediaTypes: {
+					banner: {
+						sizes: [[300, 250]],
+					},
+				},
+				bids: [
+					{
+						bidder: 'mgnipbs',
+					},
+				],
+			},
+		]);
+	});
+});
