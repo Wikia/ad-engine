5f1d31eb8c72ca5cc29e00b3c4cb2f1330b16d4a
Dominik Janik <6935667+domjanik@users.noreply.github.com>
2023-05-31 11:24:43 +0200

    ADEN-12562 - moving Ad Partners to IE (#1947)
    * ADEN-12562 - Removed partners
    
    * ADEN-12562 - Removed partners
    
    * Fixes after merge
    
    * ADEN-12562 - Fixed problem with audigent tags
    
    * ADEN-12562 - Fixed problems with partners
    
    ---------
    
    Co-authored-by: Jakub Bieńkowski <jbienkowski@fandom.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

--- src/platforms/shared/tracking/tracking.setup.ts
+++ src/platforms/shared/tracking/tracking.setup.ts
@@ -47,7 +47,6 @@ export class TrackingSetup {
 		this.interventionTracker();
 		this.adClickTracker();
 		this.ctaTracker();
-		this.identityTracker();
 		this.keyValsTracker();
 		this.adSizeTracker.init();
 	}
@@ -159,22 +158,6 @@ export class TrackingSetup {
 
 	private interventionTracker(): void {
 		interventionTracker.register();
-	}
-
-	private identityTracker(): void {
-		communicationService.on(
-			eventsRepository.IDENTITY_PARTNER_DATA_OBTAINED,
-			(eventInfo) => {
-				this.dwTracker.track(
-					{
-						partner_name: eventInfo.payload.partnerName,
-						partner_identity_id: eventInfo.payload.partnerIdentityId,
-					},
-					trackingUrls.IDENTITY_INFO,
-				);
-			},
-			false,
-		);
 	}
 
 	private keyValsTracker(): void {
