10f5f09dedb084f6b4fa334799b71d280a282611
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2023-06-01 18:14:35 +0200

    Revert "VIDEO-524 Inject Anyclip in-content player on the TV Guide listing page"

--- spec/platforms/news-and-ratings/shared/setup/dynamic-slots/news-and-ratings-anyclip.setup.spec.ts
+++ spec/platforms/news-and-ratings/shared/setup/dynamic-slots/news-and-ratings-anyclip.setup.spec.ts
@@ -52,14 +52,6 @@ describe('Anyclip setup', () => {
 
 			expect(setup.isApplicable('/news/an-article/')).to.be.false;
 		});
-
-		it("is applicable when it's a listing page'", () => {
-			const setup: NewsAndRatingsAnyclipSetup = new NewsAndRatingsAnyclipSetup(
-				slotsDefinitionRepository,
-			);
-
-			expect(setup.isApplicable('listings/main')).to.be.true;
-		});
 	});
 
 	describe('execute()', () => {
