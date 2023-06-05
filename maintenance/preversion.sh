3dd570dbcc19493dab32a271081095a4650d9148
Janik <dj@kazan.com.pl>
2023-06-05 16:32:18 +0200

    test cleanup

--- maintenance/preversion.sh
+++ maintenance/preversion.sh
@@ -1,12 +1,12 @@
-##!/usr/bin/env bash
-#set -e
-#
-#CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-#
-#if [ $CURRENT_BRANCH != "dev" ]; then
-#	echo -e "\033[31mYou're not on dev branch\033[0m ($CURRENT_BRANCH)"
-#	exit 1
-#fi
-#
-#npm run test
-#npm run lint
+#!/usr/bin/env bash
+set -e
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $CURRENT_BRANCH != "dev" ]; then
+	echo -e "\033[31mYou're not on dev branch\033[0m ($CURRENT_BRANCH)"
+	exit 1
+fi
+
+npm run test
+npm run lint
