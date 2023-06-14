bb51081deaeb989096f6e87e435a8a3cbb9c22b3
awesolowski <awesolowski@fandom.com>
2023-06-14 08:28:18 +0200

    ADEN-13031 - upgrades: - small fix, to make sure, slot to destroy exist on close (for user whi sign-in)

--- src/platforms/ucp-desktop/setup/dynamic-slots/specific-handler/gallery-lightbox-handler.ts
+++ src/platforms/ucp-desktop/setup/dynamic-slots/specific-handler/gallery-lightbox-handler.ts
@@ -78,7 +78,12 @@ export default class GalleryLightboxHandler {
 					return;
 				}
 
-				slotService.get(placementId).destroy();
+				const gallerySlot = slotService.get(this.slotName);
+				if (!gallerySlot) {
+					return;
+				}
+
+				gallerySlot.destroy();
 			},
 			false,
 		);
