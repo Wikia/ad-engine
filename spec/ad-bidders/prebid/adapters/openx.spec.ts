4beeed923715473e3bc65b0b611bb5ec94683e40
Lukasz Nawrotek <lnawrotek@fandom.com>
2023-06-19 15:29:28 +0200

    ADEN-12960

--- spec/ad-bidders/prebid/adapters/openx.spec.ts
+++ spec/ad-bidders/prebid/adapters/openx.spec.ts
@@ -1,4 +1,5 @@
 import { Openx } from '@wikia/ad-bidders/prebid/adapters/openx';
+import { context } from '@wikia/core';
 import { expect } from 'chai';
 
 describe('Openx bidder adapter', () => {
@@ -42,6 +43,41 @@ describe('Openx bidder adapter', () => {
 						params: {
 							unit: 11223344,
 							delDomain: 'wikia.com',
+						},
+					},
+				],
+			},
+		]);
+	});
+
+	it('prepareAdUnits for mobile video returns data in correct shape', () => {
+		const openx = new Openx({
+			enabled: true,
+			delDomain: 'wikia.com',
+			slots: {
+				featured: {
+					unit: '123456',
+				},
+			},
+		});
+		context.set('slots.featured.isVideo', true);
+
+		expect(openx.prepareAdUnits()).to.deep.equal([
+			{
+				code: 'featured',
+				mediaTypes: {
+					video: {
+						context: 'instream',
+						mimes: ['video/mp4', 'video/x-flv'],
+						playerSize: [640, 480],
+					},
+				},
+				bids: [
+					{
+						bidder: 'openx',
+						params: {
+							delDomain: 'wikia.com',
+							unit: '123456',
 						},
 					},
 				],
