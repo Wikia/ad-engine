c32dd34b99489f8039acfe7a86dcc971cd290efb
Jakub Bieńkowski <jbienkowski@fandom.com>
2023-06-28 23:32:26 +0200

    ADEN-13257 LiveRamp ATS 2.0

--- src/ad-bidders/prebid/ats/ats.ts
+++ src/ad-bidders/prebid/ats/ats.ts
@@ -5,31 +5,39 @@ const logGroup = 'ATS';
 
 export class Ats extends BaseServiceSetup {
 	private isLoaded = false;
-	private atsScriptUrl =
-		'https://ats-wrapper.privacymanager.io/ats-modules/2be88350-78eb-4214-96b8-3a489d2aa7d6/ats.js';
+	private launchpadScriptUrl =
+		'https://launchpad-wrapper.privacymanager.io/2f928425-1fbe-4680-b67a-c5f5ad831378/launchpad-liveramp.js';
+	private launchpadBundleScriptUrl =
+		'https://launchpad.privacymanager.io/latest/launchpad.bundle.js';
 
 	call(): Promise<void> {
 		if (!this.isEnabled('bidders.liveRampATS.enabled')) {
 			utils.logger(logGroup, 'disabled');
-			return Promise.resolve();
+			return;
 		}
 
 		utils.logger(logGroup, 'enabled');
 
-		if (!this.isLoaded) {
-			const userEmailHashes: [string, string, string] = context.get('wiki.opts.userEmailHashes');
-			const atsScript = utils.scriptLoader.loadScript(this.atsScriptUrl);
+		if (this.isLoaded) {
+			return;
+		}
 
-			if (!Array.isArray(userEmailHashes) || userEmailHashes?.length !== 3) {
-				return Promise.resolve();
-			}
+		const userEmailHashes: [string, string, string] = context.get('wiki.opts.userEmailHashes');
+		if (!Array.isArray(userEmailHashes) || userEmailHashes?.length !== 3) {
+			utils.logger(logGroup, userEmailHashes);
+			return;
+		}
+		const [md5, sha1, sha256] = userEmailHashes;
+		const launchpadScript = utils.scriptLoader.loadScript(this.launchpadScriptUrl);
+		const launchpadBundleScript = utils.scriptLoader.loadScript(this.launchpadBundleScriptUrl);
 
-			const [md5, sha1, sha256] = userEmailHashes;
-
-			return atsScript.then(() => {
+		return Promise.all([launchpadScript, launchpadBundleScript])
+			.then(() => this.waitForAts())
+			.then(() => {
 				const consentType = context.get('options.geoRequiresConsent') ? 'gdpr' : 'ccpa';
 				const consentString =
 					consentType === 'gdpr' ? Cookies.get('euconsent-v2') : Cookies.get('usprivacy');
+
 				(window as any).ats.setAdditionalData({
 					consentType,
 					consentString,
@@ -41,6 +49,9 @@ export class Ats extends BaseServiceSetup {
 
 				this.isLoaded = true;
 			});
-		}
+	}
+
+	private waitForAts(): Promise<boolean> {
+		return new utils.WaitFor(() => (window as any).ats !== undefined, 10, 50).until();
 	}
 }
