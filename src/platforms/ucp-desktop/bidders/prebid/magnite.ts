c97de12e78daf03c152311a907aacc0f4bceae6e
preludek <tomasz.felczak89@gmail.com>
2023-07-02 20:17:16 +0200

    ADEN-13228 add tlb and tb to magnite context

--- src/platforms/ucp-desktop/bidders/prebid/magnite.ts
+++ src/platforms/ucp-desktop/bidders/prebid/magnite.ts
@@ -3,11 +3,25 @@ export function getMagniteContext(): object {
 		enabled: false,
 		accountId: 7450,
 		slots: {
+			top_leaderboard: {
+				sizes: [
+					[728, 90],
+					[970, 250],
+				],
+				targeting: {
+					loc: ['top'],
+				},
+				position: 'atf',
+			},
 			top_boxad: {
 				sizes: [
 					[300, 250],
 					[300, 600],
 				],
+				targeting: {
+					loc: ['top'],
+				},
+				position: 'atf',
 			},
 		},
 	};
