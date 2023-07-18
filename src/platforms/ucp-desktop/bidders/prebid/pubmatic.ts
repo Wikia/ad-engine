1315dde891b748cb0cfec8daa50de555278924e0
Adam Wesołowski <124166784+adi-wesolowski-fandom@users.noreply.github.com>
2023-07-18 12:29:56 +0200

    ADEN-13301 - bidders fo gallery slot (#2026)
    * ADEN-13301 - bidders fo gallery slot
    - upgrade context setup (bidder-alias)
    - add bidders setup:
      - appnexus
      - index-exchange
      - pubmatic
      - rubicon
      - kargo (created new file + upgrade prebid congif setup)
    
    * ADEN-13301 - fixes after CR
    
    ---------
    
    Co-authored-by: lnawrotek <117349052+lnawrotek@users.noreply.github.com>
    Co-authored-by: Damian Sobczak <35136242+dmnsobczak@users.noreply.github.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

--- src/platforms/ucp-desktop/bidders/prebid/pubmatic.ts
+++ src/platforms/ucp-desktop/bidders/prebid/pubmatic.ts
@@ -44,6 +44,10 @@ export function getPubmaticContext(): object {
 				],
 				ids: ['/5441/BOTTOM_LEADERBOARD_728x90@728x90', '/5441/BOTTOM_LEADERBOARD_970x250@970x250'],
 			},
+			fandom_dt_galleries: {
+				sizes: [[720, 90]],
+				ids: ['/5441/FANDOM_DT_GALLERIES@728x90', 5244133],
+			},
 		},
 	};
 }
