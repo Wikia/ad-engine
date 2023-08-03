5f3f7c81d111a990bf489d0417ef2c2948bcd6b7
awesolowski <awesolowski@fandom.com>
2023-08-03 15:49:37 +0200

    ADEN-13047 - webads - configure bidders for fandom (desktop+mobile)

--- src/platforms/ucp-desktop/bidders/prebid/webads.ts
+++ src/platforms/ucp-desktop/bidders/prebid/webads.ts
@@ -8,7 +8,37 @@ export function getWebadsContext(): object {
 					[970, 250],
 				],
 				placementId: '64a7e9b6c87e0b22eed4d57f_64a690add98d1515f8a3a126',
-				accountId: '64a690add98d1515f8a3a11e',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			top_boxad: {
+				sizes: [
+					[300, 250],
+					[300, 600],
+				],
+				placementId: '648079843898112402b6dfdc_647766563ab5cfa9ef3e1d60',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			incontent_leaderboard: {
+				sizes: [[728, 90]],
+				placementId: '648079843898112402b6dfdc_6477665d2f9119a5bd3e1d61',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			incontent_boxad: {
+				sizes: [[300, 250]],
+				placementId: '648079843898112402b6dfdc_6477666a1996aa19643e1d62',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			bottom_leaderboard: {
+				sizes: [
+					[728, 90],
+					[970, 250],
+				],
+				placementId: '648079843898112402b6dfdc_647766799d24213a613e1d64',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			featured: {
+				placementId: '648079843898112402b6dfdc_6477667219823c5f0b3e1d63',
+				accountId: '647765b7705d4fca3b3e1d58',
 			},
 		},
 	};
--- src/platforms/ucp-mobile/bidders/prebid/webads.ts
+++ src/platforms/ucp-mobile/bidders/prebid/webads.ts
@@ -1,6 +1,32 @@
 export function getWebadsContext(): object {
 	return {
 		enabled: false,
-		slots: {},
+		slots: {
+			top_leaderboard: {
+				sizes: [
+					[320, 50],
+					[320, 100],
+				],
+				placementId: '648079843898112402b6dfdc_647766970ee778cdfd3e1d66',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			mobile_prefooter: {
+				sizes: [[300, 250]],
+				placementId: '648079843898112402b6dfdc_6477668ef53dfce3373e1d65',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			floor_adhesion: {
+				sizes: [
+					[320, 50],
+					[320, 100],
+				],
+				placementId: '648079843898112402b6dfdc_6477669cfd7c540d4d3e1d67',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+			featured: {
+				placementId: '648079843898112402b6dfdc_647766a916faafcac33e1d68',
+				accountId: '647765b7705d4fca3b3e1d58',
+			},
+		},
 	};
 }
