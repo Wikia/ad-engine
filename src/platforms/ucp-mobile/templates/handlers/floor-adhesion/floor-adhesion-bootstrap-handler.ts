640f63be62da5079e1ede3a426b2ac5e868c457e
rafalgiemza <rgiemza@fandom.com>
2023-08-07 12:53:17 +0200

    ADEN-13414 | Fix floor_adhesion on mobile

--- src/platforms/ucp-mobile/templates/handlers/floor-adhesion/floor-adhesion-bootstrap-handler.ts
+++ src/platforms/ucp-mobile/templates/handlers/floor-adhesion/floor-adhesion-bootstrap-handler.ts
@@ -24,7 +24,7 @@ export class FloorAdhesionBootstrapHandler implements TemplateStateHandler {
 	}
 
 	async onLeave(): Promise<void> {
-		document.getElementById('floor_adhesion_anchor').classList.remove(HIDDEN_AD_CLASS);
+		document.getElementById('floor_adhesion_anchor').classList.remove('hide');
 		this.adSlot.show();
 	}
 }
