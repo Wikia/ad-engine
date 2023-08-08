a8b8b4ca4f3e1ab13aaa3cce81cbb46c20038b0b
Szymon Nawrocki <snawrocki@fandom.com>
2023-08-08 16:55:14 +0200

    TACO-18 Id5 AB testing - Save id5_group in localStorage

--- src/ad-bidders/prebid/id5/id5.ts
+++ src/ad-bidders/prebid/id5/id5.ts
@@ -1,7 +1,6 @@
-import { context, targetingService, utils } from '@ad-engine/core';
+import { context, targetingService, UniversalStorage, utils } from '@ad-engine/core';
 import { UserIdConfig } from '../index';
 
-const logGroup = 'Id5';
 interface Id5Config extends UserIdConfig {
 	params: {
 		abTesting: {
@@ -11,7 +10,25 @@ interface Id5Config extends UserIdConfig {
 	};
 }
 
+type id5GroupValue = '1' | '0';
+const logGroup = 'Id5';
+
 class Id5 {
+	private storage;
+	private id5GroupKey = 'id5_group';
+
+	constructor() {
+		this.storage = new UniversalStorage();
+	}
+
+	private isEnabled(): boolean {
+		return (
+			context.get('bidders.prebid.id5') &&
+			!context.get('options.optOutSale') &&
+			!utils.isCoppaSubject()
+		);
+	}
+
 	getConfig(): Id5Config {
 		if (!this.isEnabled()) {
 			utils.logger(logGroup, 'disabled');
@@ -37,48 +54,66 @@ class Id5 {
 			},
 		};
 
-		// Configure A/B testing
-		const id5AbValue = context.get('bidders.prebid.id5AbValue');
-
-		if (id5AbValue && id5AbValue !== 0) {
-			utils.logger(logGroup, 'A/B testing enabled', 'value', id5AbValue);
-			config.params.abTesting.enabled = true;
-			config.params.abTesting.controlGroupPct = id5AbValue;
-		} else {
-			utils.logger(logGroup, 'A/B testing disabled');
-		}
+		this.configureAbTestingParameters(config);
 
 		utils.logger(logGroup, 'config', config);
 
 		return config;
 	}
 
-	async setupAbTesting(pbjs: Pbjs): Promise<void> {
-		const controlGroup = await this.getControlGroup(pbjs);
-		utils.logger(logGroup, 'Control group', controlGroup);
+	private configureAbTestingParameters(config: Id5Config): void {
+		const id5AbValue = context.get('bidders.prebid.id5AbValue');
 
-		this.setControlGroupAsTargetingParam(controlGroup);
-	}
+		if (id5AbValue && typeof id5AbValue === 'number') {
+			config.params.abTesting.enabled = true;
+			config.params.abTesting.controlGroupPct = id5AbValue;
 
-	private async getControlGroup(pbjs: Pbjs): Promise<boolean> {
-		await new utils.WaitFor(() => pbjs.getUserIds()?.id5id?.ext !== undefined, 10, 100).until();
-
-		return pbjs.getUserIds()?.id5id?.ext?.abTestingControlGroup;
-	}
-
-	private setControlGroupAsTargetingParam(controlGroup: boolean): void {
-		if (controlGroup !== undefined && typeof controlGroup === 'boolean') {
-			targetingService.set('id5_group', controlGroup === true ? '1' : '0');
-			utils.logger(logGroup, 'Control group set as targeting param');
+			utils.logger(logGroup, 'A/B testing enabled', 'value', id5AbValue);
+		} else {
+			utils.logger(logGroup, 'A/B testing disabled');
 		}
 	}
 
-	private isEnabled(): boolean {
-		return (
-			context.get('bidders.prebid.id5') &&
-			!context.get('options.optOutSale') &&
-			!utils.isCoppaSubject()
-		);
+	async setupAbTesting(pbjs: Pbjs): Promise<void> {
+		const controlGroup =
+			this.getControlGroupFromStorage() || (await this.getControlGroupFromPbjsObject(pbjs));
+
+		utils.logger(logGroup, 'Control group', controlGroup);
+
+		if (!controlGroup) {
+			return;
+		}
+
+		this.saveInStorage(this.id5GroupKey, controlGroup);
+		this.setControlGroupAsTargetingParam(controlGroup);
+	}
+
+	private getControlGroupFromStorage(): id5GroupValue {
+		const storageValue = this.storage.getItem(this.id5GroupKey);
+
+		if (storageValue !== null && typeof storageValue === 'number') {
+			return storageValue === 1 ? '1' : '0';
+		}
+	}
+
+	private async getControlGroupFromPbjsObject(pbjs: Pbjs): Promise<id5GroupValue> {
+		await new utils.WaitFor(() => pbjs.getUserIds()?.id5id?.ext !== undefined, 10, 20).until();
+
+		return pbjs.getUserIds()?.id5id?.ext?.abTestingControlGroup === true ? '1' : '0';
+	}
+
+	private saveInStorage(key: string, value: string) {
+		if (this.storage.getItem(key) !== null) {
+			return;
+		}
+
+		this.storage.setItem(this.id5GroupKey, value);
+		utils.logger(logGroup, key, 'saved in storage', value);
+	}
+
+	private setControlGroupAsTargetingParam(controlGroup: id5GroupValue): void {
+		targetingService.set(this.id5GroupKey, controlGroup);
+		utils.logger(logGroup, 'Control group set as targeting param', controlGroup);
 	}
 }
 
