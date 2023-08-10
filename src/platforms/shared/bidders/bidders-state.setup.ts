bd80fb6cecc46235ce6be3d1306b2bbe926b0d20
Szymon Nawrocki <74318406+szymon-nawrocki@users.noreply.github.com>
2023-08-10 15:40:44 +0200

    TACO-18 Configure AB testing for ID5 (#2068)
    * TACO-18 Configure AB testing for ID5
    
    * TACO-18 Add ID5Config interface and add unit tests for ab testing
    
    * TACO-18 Id5 AB testing - set targeting key val
    
    * TACO-18 Id5 AB testing - set controlGroup to 0 or 1
    
    * TACO-18 Id5 AB testing - Save id5_group in localStorage
    
    * TACO-18 Id5 AB testing - Add early return if Control group is undefined
    
    * TACO-18 Id5 AB testing - Add early return if Control group is undefined
    
    * TACO-18 Id5 AB testing - Add additional check for enabled
    
    * TACO-18 Id5 AB testing - Change values 1 0 to A B
    
    * TACO-18 Id5 AB testing - Add unit tests
    
    * TACO-18 Refactor setting A/B parameters
    
    * TACO-18 If Id5 is disabled exit the code
    
    ---------
    
    Co-authored-by: Dominik Janik <6935667+domjanik@users.noreply.github.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

--- src/platforms/shared/bidders/bidders-state.setup.ts
+++ src/platforms/shared/bidders/bidders-state.setup.ts
@@ -64,6 +64,7 @@ export class BiddersStateSetup implements DiProcess {
 
 			context.set('bidders.prebid.intentIQ', this.instantConfig.get('icPrebidIntentIQ', false));
 			context.set('bidders.prebid.id5', this.instantConfig.get('icPrebidId5', false));
+			context.set('bidders.prebid.id5AbValue', this.instantConfig.get('icPrebidId5AB', 0));
 		}
 
 		context.set(
