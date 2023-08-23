2bc774ee166f73d5e21f0653ae55b5195232e572
Pavel Bulayeu <34650035+verhopara@users.noreply.github.com>
2023-08-23 08:49:57 +0200

    [ADEN-13071] Added force BT behavior with disabled WAS ad block check

---
+++ src/ad-services/bt-force/index.ts
@@ -0,0 +1,40 @@
+import { context, utils } from '@ad-engine/core';
+
+const logGroup = 'bt-force-loader';
+
+/**
+ * BT service handler
+ */
+class BTForce {
+	/**
+	 * Runs BT force rec service and injects code
+	 */
+	async run(): Promise<void> {
+		if (!this.isEnabled()) {
+			utils.logger(logGroup, 'disabled');
+			return Promise.resolve();
+		}
+
+		utils.logger(logGroup, 'loading');
+		await this.loadScript().then(() => {
+			utils.logger(logGroup, 'ready');
+		});
+	}
+
+	/**
+	 * Checks if BT rec is enabled
+	 */
+	isEnabled(): boolean {
+		return context.get('options.wad.btForce');
+	}
+
+	/**
+	 * Injects BT script
+	 */
+	private loadScript(): Promise<Event> {
+		const btLibraryUrl = 'https://btloader.com/tag?o=5199505043488768&upapi=true';
+		return utils.scriptLoader.loadScript(btLibraryUrl, true, 'first');
+	}
+}
+
+export const btForce = new BTForce();
--- src/platforms/shared/services/wad-runner.ts
+++ src/platforms/shared/services/wad-runner.ts
@@ -2,14 +2,19 @@ import {
 	babDetection,
 	BaseServiceSetup,
 	btfBlockerService,
+	btForce,
 	btRec,
 	context,
 } from '@wikia/ad-engine';
 import { trackBab } from '../tracking/bab-tracker';
 
 const defaultOnDetect = () => {
-	btfBlockerService.finishFirstCall();
-	btRec.run();
+	if (context.get('options.wad.btForce')) {
+		btForce.run();
+	} else {
+		btfBlockerService.finishFirstCall();
+		btRec.run();
+	}
 };
 
 export class WadRunner extends BaseServiceSetup {
@@ -17,17 +22,22 @@ export class WadRunner extends BaseServiceSetup {
 	public onDetected: () => void = defaultOnDetect;
 
 	async call(): Promise<void> {
-		if (!this.detector.isEnabled()) {
-			return Promise.resolve();
-		}
-
-		const isBabDetected = await this.detector.run();
-		context.set('options.wad.blocking', isBabDetected);
-
-		trackBab(isBabDetected);
-
-		if (isBabDetected) {
+		if (this.instantConfig.get('icBTForce')) {
+			context.set('options.wad.btForce', true);
 			this.onDetected();
+		} else {
+			if (!this.detector.isEnabled()) {
+				return Promise.resolve();
+			}
+
+			const isBabDetected = await this.detector.run();
+			context.set('options.wad.blocking', isBabDetected);
+
+			trackBab(isBabDetected);
+
+			if (isBabDetected) {
+				this.onDetected();
+			}
 		}
 	}
 }
