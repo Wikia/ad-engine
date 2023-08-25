aafccd2b2f08acc4a9d9435575f4454b2978fceb
rafalgiemza <rgiemza@fandom.com>
2023-08-25 09:31:02 +0200

    ADEN-13511 | handle no ad case - hide ad when user rejected gdpr - otherwise big ugly box will show up

--- src/platforms/shared/handlers/gallery-lightbox-handler.ts
+++ src/platforms/shared/handlers/gallery-lightbox-handler.ts
@@ -37,6 +37,7 @@ export class GalleryLightboxAdsHandler {
 		communicationService.onSlotEvent(
 			AdSlotStatus.STATUS_COLLAPSE,
 			() => {
+				this.hideMobileGalleryAdPlaceholder();
 				this.refreshLock = true;
 			},
 			this.slotName,
