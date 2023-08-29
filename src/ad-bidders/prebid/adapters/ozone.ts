8626023953ffbc4fc5a8d9cba16a8ee1d5df645e
Janik <dj@kazan.com.pl>
2023-08-29 09:59:21 +0200

    TACO-36 - Adjusted Ozone config

--- src/ad-bidders/prebid/adapters/ozone.ts
+++ src/ad-bidders/prebid/adapters/ozone.ts
@@ -1,11 +1,22 @@
+import { context, Dictionary, targetingService } from '@ad-engine/core';
 import { PrebidAdapter } from '../prebid-adapter';
+import { PrebidAdSlotConfig } from '../prebid-models';
 
 export class Ozone extends PrebidAdapter {
 	static bidderName = 'ozone';
 	dcn: string;
+	customTargeting: Dictionary;
 
 	constructor(options) {
 		super(options);
+		this.customTargeting = {
+			s1: [
+				context.get('wiki.targeting.wikiIsTop1000')
+					? targetingService.get('s1') || ''
+					: 'not a top1k wiki',
+			],
+			lang: [targetingService.get('wikiLanguage') || targetingService.get('lang') || 'en'],
+		};
 		this.dcn = options.dcn;
 	}
 
@@ -13,7 +24,7 @@ export class Ozone extends PrebidAdapter {
 		return Ozone.bidderName;
 	}
 
-	prepareConfigForAdUnit(code, { sizes, pos }): PrebidAdUnit {
+	prepareConfigForAdUnit(code, { sizes, pos }: PrebidAdSlotConfig): PrebidAdUnit {
 		return {
 			code: code,
 			mediaTypes: {
@@ -27,8 +38,17 @@ export class Ozone extends PrebidAdapter {
 					params: {
 						publisherId: 'OZONETEST001',
 						siteId: '4204204201',
-						placementId: '0420420421',
+						placementId: '8000000326',
 						pos,
+						customData: [
+							{
+								settings: {},
+								targeting: {
+									page_type: 'article',
+									...this.getTargeting(code, this.customTargeting),
+								},
+							},
+						],
 					},
 				},
 			],
