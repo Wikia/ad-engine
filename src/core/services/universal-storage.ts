47876124b64ccf15cfba17393d2d9659dfe13542
Pavel Bulayeu <34650035+verhopara@users.noreply.github.com>
2023-08-30 09:25:33 +0200

    [ADEN-13071] Code review update

--- src/core/services/universal-storage.ts
+++ src/core/services/universal-storage.ts
@@ -1,5 +1,7 @@
 import { Dictionary } from '../models';
+import { logger } from '../utils';
 
+const logGroup = 'universal-storage';
 export interface StorageProvider {
 	getItem(key: string): string;
 	setItem(key: string, input: string, timeToLiveMs?: number): void;
@@ -7,15 +9,54 @@ export interface StorageProvider {
 	clear(): void;
 }
 
+export type StorageAdapter = () => StorageProvider;
+
 export interface Storage<T> {
 	getItem(key: string): T | string;
 	setItem(key: string, input: Dictionary | string, timeToLiveMs?: number): boolean;
 }
 
+class InMemoryStorage implements StorageProvider {
+	private storage: Dictionary = {};
+	clear(): void {
+		this.storage = {};
+	}
+
+	getItem(key: string): string {
+		return this.storage[key];
+	}
+
+	removeItem(key: string): void {
+		delete this.storage[key];
+	}
+
+	setItem(key: string, input: string): void {
+		this.storage[key] = input;
+	}
+}
 export class UniversalStorage implements Storage<any> {
 	private fallbackStorage: Dictionary = {};
+	private provider: StorageProvider;
 
-	constructor(private provider: StorageProvider = window.localStorage) {}
+	constructor(storageProvider: StorageAdapter = () => window.localStorage) {
+		let useFallback = false;
+		try {
+			// Chrome throws error even during simple attempt to access local or session storage if they are disabled
+			// So before setItem / removeItem test let's try setup provider first
+			this.provider = storageProvider();
+			// Next test storage
+			// In case of local / session storage are not available it will throw error during setItem attempt
+			if (!this.isAvailable()) {
+				useFallback = true;
+			}
+		} catch {
+			useFallback = true;
+		}
+		if (useFallback) {
+			logger(logGroup, `StorageProvider doesn't work, fallback to the InMemoryProvider`);
+			this.provider = new InMemoryStorage();
+		}
+	}
 
 	isAvailable(): boolean {
 		try {
@@ -47,7 +88,7 @@ export class UniversalStorage implements Storage<any> {
 		try {
 			this.provider.setItem(key, value, timeToLiveMs);
 		} catch (e) {
-			console.warn(`Item ${key} wasn't set in the storage`, e);
+			logger(logGroup, `Item ${key} wasn't set in the storage`, e);
 			this.fallbackStorage[key] = value;
 		}
 		return true;
