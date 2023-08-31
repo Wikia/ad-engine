27fbe6a6ae509ad496fcf8ecc433373204448c55
Janik <dj@kazan.com.pl>
2023-08-31 08:28:30 +0200

    TACO-35 - Removed pos param

--- src/platforms/ucp-desktop/bidders/prebid/ozone.ts
+++ src/platforms/ucp-desktop/bidders/prebid/ozone.ts
@@ -7,33 +7,27 @@ export function getOzoneContext(): object {
 					[728, 90],
 					[970, 250],
 				],
-				pos: 'OZ_top_leaderboard',
 			},
 			top_boxad: {
 				sizes: [
 					[300, 250],
 					[300, 600],
 				],
-				pos: 'OZ_top_boxad',
 			},
 			incontent_leaderboard: {
 				sizes: [[728, 90]],
-				pos: 'OZ_incontent_leaderboard',
 			},
 			fandom_dt_galleries: {
 				sizes: [[728, 90]],
-				pos: 'OZ_fandom_dt_galleries',
 			},
 			incontent_boxad_1: {
 				sizes: [[300, 250]],
-				pos: 'OZ_incontent_boxad',
 			},
 			bottom_leaderboard: {
 				sizes: [
 					[728, 90],
 					[970, 250],
 				],
-				pos: 'OZ_bottom_leaderboard',
 			},
 		},
 	};
--- src/platforms/ucp-mobile/bidders/prebid/ozone.ts
+++ src/platforms/ucp-mobile/bidders/prebid/ozone.ts
@@ -7,36 +7,30 @@ export function getOzoneContext(): object {
 					[728, 90],
 					[970, 250],
 				],
-				pos: 'OZ_mobile_top_leaderboard',
 			},
 			top_boxad: {
 				sizes: [
 					[300, 250],
 					[300, 600],
 				],
-				pos: 'OZ_top_boxad',
 			},
 			gallery_leaderboard: {
 				sizes: [
 					[320, 100],
 					[320, 50],
 				],
-				pos: 'OZ_fandom_mw_galleries',
 			},
 			incontent_boxad_1: {
 				sizes: [[300, 250]],
-				pos: 'OZ_incontent_boxad',
 			},
 			bottom_leaderboard: {
 				sizes: [[300, 250]],
-				pos: 'OZ_mobile_prefooter',
 			},
 			floor_adhesion: {
 				sizes: [
 					[320, 50],
 					[320, 100],
 				],
-				pos: 'OZ_floor_adhesion',
 			},
 		},
 	};
