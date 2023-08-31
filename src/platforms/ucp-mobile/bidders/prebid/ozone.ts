34e3f38c23495535929108b521a1418548dafe82
Janik <dj@kazan.com.pl>
2023-08-31 08:25:47 +0200

    TACO-35 - Added pos param

--- src/platforms/ucp-mobile/bidders/prebid/ozone.ts
+++ src/platforms/ucp-mobile/bidders/prebid/ozone.ts
@@ -7,36 +7,36 @@ export function getOzoneContext(): object {
 					[728, 90],
 					[970, 250],
 				],
-				name: 'OZ_mobile_top_leaderboard',
+				pos: 'OZ_mobile_top_leaderboard',
 			},
 			top_boxad: {
 				sizes: [
 					[300, 250],
 					[300, 600],
 				],
-				name: 'OZ_top_boxad',
+				pos: 'OZ_top_boxad',
 			},
 			gallery_leaderboard: {
 				sizes: [
 					[320, 100],
 					[320, 50],
 				],
-				name: 'OZ_fandom_mw_galleries',
+				pos: 'OZ_fandom_mw_galleries',
 			},
 			incontent_boxad_1: {
 				sizes: [[300, 250]],
-				name: 'OZ_incontent_boxad',
+				pos: 'OZ_incontent_boxad',
 			},
 			bottom_leaderboard: {
 				sizes: [[300, 250]],
-				name: 'OZ_mobile_prefooter',
+				pos: 'OZ_mobile_prefooter',
 			},
 			floor_adhesion: {
 				sizes: [
 					[320, 50],
 					[320, 100],
 				],
-				name: 'OZ_floor_adhesion',
+				pos: 'OZ_floor_adhesion',
 			},
 		},
 	};
