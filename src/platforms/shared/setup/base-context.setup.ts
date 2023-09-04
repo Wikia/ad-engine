6f99e03fc1ad804433ed7997986f3bdf6efb3653
lnawrotek <117349052+lnawrotek@users.noreply.github.com>
2023-09-04 17:31:40 +0200

    Update src/platforms/shared/setup/base-context.setup.ts
    Co-authored-by: Adam Wesołowski <124166784+adi-wesolowski-fandom@users.noreply.github.com>

--- src/platforms/shared/setup/base-context.setup.ts
+++ src/platforms/shared/setup/base-context.setup.ts
@@ -190,7 +190,7 @@ export class BaseContextSetup implements DiProcess {
 			'templates.sizeOverwritingMap',
 			universalAdPackage.UAP_ADDITIONAL_SIZES.companionSizes,
 		);
-		context.set('bidders.s2s.bidders', this.instantConfig.get('icPrebidS2sBidders'));
+		context.set('bidders.s2s.bidders', this.instantConfig.get('icPrebidS2sBidders', []));
 		context.set('bidders.s2s.enabled', this.instantConfig.get('icPrebidS2s'));
 	}
 
