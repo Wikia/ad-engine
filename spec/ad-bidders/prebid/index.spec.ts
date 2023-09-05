52f8a36e2f6b596c4f6352e6a0a1d40cf9ca6ccf
Lukasz Nawrotek <lnawrotek@fandom.com>
2023-09-05 14:21:50 +0200

    ADEN-13228

--- spec/ad-bidders/prebid/index.spec.ts
+++ spec/ad-bidders/prebid/index.spec.ts
@@ -1,5 +1,5 @@
 import { PrebidProvider } from '@wikia/ad-bidders/prebid';
-import { TargetingService, targetingService } from '@wikia/core';
+import { context, TargetingService, targetingService } from '@wikia/core';
 import { expect } from 'chai';
 import { SinonStubbedInstance } from 'sinon';
 import { stubPbjs } from '../../core/services/pbjs.stub';
@@ -53,6 +53,50 @@ describe('PrebidProvider bidder', () => {
 
 			expect(keys.length).to.equal(2);
 			expect(keys).to.deep.equal(['hb_bidder', 'hb_pb']);
+		});
+	});
+
+	describe('s2s', () => {
+		afterEach(() => {
+			context.remove('bidders.s2s.enabled');
+			context.remove('bidders.s2s.bidders');
+		});
+
+		it('is disabled', () => {
+			const prebid = new PrebidProvider(bidderConfig);
+
+			expect(prebid.prebidConfig['s2sConfig']).to.equal(undefined);
+		});
+
+		it('is enabled', () => {
+			context.set('bidders.s2s.enabled', true);
+			context.set('bidders.s2s.bidders', ['test']);
+			const prebid = new PrebidProvider(bidderConfig);
+
+			expect(prebid.prebidConfig['s2sConfig']).to.not.equal(undefined);
+		});
+
+		it('has correct config', () => {
+			context.set('bidders.s2s.enabled', true);
+			context.set('bidders.s2s.bidders', ['test', 'test1']);
+			const prebid = new PrebidProvider(bidderConfig);
+
+			expect(prebid.prebidConfig['s2sConfig']).to.deep.equal([
+				{
+					accountId: 7450,
+					bidders: ['test', 'test1'],
+					defaultVendor: 'rubicon',
+					coopSync: true,
+					userSyncLimit: 8,
+					allowUnknownBidderCodes: true,
+					extPrebid: {
+						extPrebidBidders: {
+							test: { wrappername: '7450_test' },
+							test1: { wrappername: '7450_test1' },
+						},
+					},
+				},
+			]);
 		});
 	});
 });
--- src/platforms/shared/setup/base-context.setup.ts
+++ src/platforms/shared/setup/base-context.setup.ts
@@ -191,7 +191,7 @@ export class BaseContextSetup implements DiProcess {
 			universalAdPackage.UAP_ADDITIONAL_SIZES.companionSizes,
 		);
 		context.set('bidders.s2s.bidders', this.instantConfig.get('icPrebidS2sBidders', []));
-		context.set('bidders.s2s.enabled', this.instantConfig.get('icPrebidS2s'));
+		context.set('bidders.s2s.enabled', this.instantConfig.get('icPrebidS2sBidders', []).length > 0);
 	}
 
 	private setupStickySlotContext(): void {
