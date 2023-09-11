16cd204551a532ef7c2fa74e31e620df2ff90764
Pavel Bulayeu <34650035+verhopara@users.noreply.github.com>
2023-09-11 23:11:55 +0200

    [ADEN-13071] Added side units render so we will have left rail ads

--- src/ad-services/bt-force/index.ts
+++ src/ad-services/bt-force/index.ts
@@ -15,6 +15,8 @@ class BTForce {
 			return Promise.resolve();
 		}
 
+		this.insertSideUnits();
+
 		utils.logger(logGroup, 'loading');
 		await this.loadScript().then(() => {
 			utils.logger(logGroup, 'ready');
@@ -34,6 +36,48 @@ class BTForce {
 	private loadScript(): Promise<Event> {
 		const btLibraryUrl = 'https://btloader.com/tag?o=5199505043488768&upapi=true';
 		return utils.scriptLoader.loadScript(btLibraryUrl, true, 'first');
+	}
+
+	private insertSideUnits(): void {
+		if (
+			context.get('state.isMobile') ||
+			document.body.classList.contains('is-content-expanded') ||
+			document.documentElement.classList.contains('is-content-expanded')
+		) {
+			return;
+		}
+
+		const containerMaxWidth = 1236;
+		const minSideAdWidth = 90;
+		const sideUnitsCalculatedPadding = 60;
+		const globalNavigationWidth = 66;
+		const sideContainersSpace =
+			2 * minSideAdWidth + globalNavigationWidth + sideUnitsCalculatedPadding;
+		const resizableContainer = document.querySelector('.page.has-right-rail, .mainpage .page');
+
+		if (
+			!resizableContainer ||
+			(window.document.body.scrollWidth || 0) < containerMaxWidth + sideContainersSpace
+		) {
+			return;
+		}
+
+		const sideBTleft = document.createElement('div');
+		const sideBTright = document.createElement('div');
+
+		sideBTleft.className = 'side-bt-container left';
+		sideBTright.className = 'side-bt-container right';
+
+		resizableContainer.prepend(sideBTleft, sideBTright);
+
+		const wrapperLeft = document.createElement('div');
+		const wrapperRight = document.createElement('div');
+
+		wrapperLeft.id = 'btbgleft';
+		wrapperRight.id = 'btbgright';
+
+		sideBTleft.append(wrapperLeft);
+		sideBTright.append(wrapperRight);
 	}
 }
 
