d88e0a1f96266af14d54eecaab9121314b52e86a
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-09-12 17:40:34 +0200

    COTECH-691 extend [Ad Engine] Setup JWPlayer action

--- src/platforms/shared/setup/player.setup.ts
+++ src/platforms/shared/setup/player.setup.ts
@@ -12,13 +12,30 @@ const logGroup = 'player-setup';
 export class PlayerSetup extends BaseServiceSetup {
 	call() {
 		const showAds = !context.get('options.wad.blocking');
+		const strategyRulesEnabled = true;
 
 		if (showAds) {
 			utils.logger(logGroup, 'with ads');
-			new JWPlayerManager().manage();
+
+			if (!strategyRulesEnabled) new JWPlayerManager().manage();
 		} else {
 			utils.logger(logGroup, 'ad block detected, without ads');
 		}
-		communicationService.dispatch(jwpSetup({ showAds: showAds, autoplayDisabled: false }));
+
+		if (strategyRulesEnabled) {
+			const vastUrl = utils.buildVastUrl(16 / 9, 'featured', {
+				videoAdUnitId:
+					'/5441/wka1b.VIDEO/featured/desktop/ucp_desktop-fandom-fv-article/_project43-life',
+				correlator: Math.round(Math.random() * 10000000000),
+				vpos: 'preroll',
+				customParams: 'src=test&pos=featured&rv=1',
+			});
+
+			communicationService.dispatch(
+				jwpSetup({ showAds: showAds, autoplayDisabled: false, vastUrl, strategyRulesEnabled }),
+			);
+		} else {
+			communicationService.dispatch(jwpSetup({ showAds: showAds, autoplayDisabled: false }));
+		}
 	}
 }
