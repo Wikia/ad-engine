634ff79ea9712134ba706a5eaff8c5fa37b4e997
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-09-13 13:28:53 +0200

    COTECH-691 clean up the code a bit

--- src/platforms/shared/setup/player.setup.ts
+++ src/platforms/shared/setup/player.setup.ts
@@ -17,32 +17,47 @@ export class PlayerSetup extends BaseServiceSetup {
 		if (showAds) {
 			utils.logger(logGroup, 'with ads');
 
-			if (!strategyRulesEnabled) new JWPlayerManager().manage();
+			if (!strategyRulesEnabled) {
+				new JWPlayerManager().manage();
+			} else {
+				utils.logger(
+					logGroup,
+					'JWP Strategy Rules enabled - AdEngine does not control ads in JWP anymore',
+				);
+			}
 		} else {
 			utils.logger(logGroup, 'ad block detected, without ads');
 		}
 
 		if (strategyRulesEnabled) {
-			const slotName = 'featured';
-			const src = context.get('src');
-			const vastUrl = utils.buildVastUrl(16 / 9, slotName, {
-				videoAdUnitId: utils.stringBuilder.build(context.get('slots.featured.videoAdUnit'), {
-					slotConfig: {
-						group: 'VIDEO',
-						adProduct: 'featured',
-						slotNameSuffix: '',
-					},
-				}),
-				correlator: Math.round(Math.random() * 10000000000),
-				vpos: 'preroll',
-				customParams: `src=${src}&pos=${slotName}&rv=1`,
-			});
-
 			communicationService.dispatch(
-				jwpSetup({ showAds: showAds, autoplayDisabled: false, vastUrl, strategyRulesEnabled }),
+				jwpSetup({
+					showAds: showAds,
+					autoplayDisabled: false,
+					vastUrl: this.generateVastUrlForJWPlayer(),
+					strategyRulesEnabled,
+				}),
 			);
 		} else {
 			communicationService.dispatch(jwpSetup({ showAds: showAds, autoplayDisabled: false }));
 		}
+	}
+
+	private generateVastUrlForJWPlayer() {
+		const slotName = 'featured';
+		const src = context.get('src');
+
+		return utils.buildVastUrl(16 / 9, slotName, {
+			videoAdUnitId: utils.stringBuilder.build(context.get('slots.featured.videoAdUnit'), {
+				slotConfig: {
+					group: 'VIDEO',
+					adProduct: 'featured',
+					slotNameSuffix: '',
+				},
+			}),
+			correlator: Math.round(Math.random() * 10000000000),
+			vpos: 'preroll',
+			customParams: `src=${src}&pos=${slotName}&rv=1`,
+		});
 	}
 }
