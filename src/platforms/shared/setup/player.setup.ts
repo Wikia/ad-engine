c7fc5bc420da0021af6fbb4fc02a91c1adacb2d4
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-09-13 13:15:45 +0200

    COTECH-691 make the ad unit and part of targeting dynamic

--- src/platforms/shared/setup/player.setup.ts
+++ src/platforms/shared/setup/player.setup.ts
@@ -23,12 +23,19 @@ export class PlayerSetup extends BaseServiceSetup {
 		}
 
 		if (strategyRulesEnabled) {
-			const vastUrl = utils.buildVastUrl(16 / 9, 'featured', {
-				videoAdUnitId:
-					'/5441/wka1b.VIDEO/featured/desktop/ucp_desktop-fandom-fv-article/_project43-life',
+			const slotName = 'featured';
+			const src = context.get('src');
+			const vastUrl = utils.buildVastUrl(16 / 9, slotName, {
+				videoAdUnitId: utils.stringBuilder.build(context.get('slots.featured.videoAdUnit'), {
+					slotConfig: {
+						group: 'VIDEO',
+						adProduct: 'featured',
+						slotNameSuffix: '',
+					},
+				}),
 				correlator: Math.round(Math.random() * 10000000000),
 				vpos: 'preroll',
-				customParams: 'src=test&pos=featured&rv=1',
+				customParams: `src=${src}&pos=${slotName}&rv=1`,
 			});
 
 			communicationService.dispatch(
