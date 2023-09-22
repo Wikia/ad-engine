5c33e743495fa4b312178056077185df6a4cc809
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-09-22 17:03:19 +0200

    COTECH-691 fix unit test after adding AdSlot in player.setup.ts

--- spec/platforms/shared/setup/player.setup.spec.ts
+++ spec/platforms/shared/setup/player.setup.spec.ts
@@ -34,12 +34,21 @@ describe('PlayerSetup', () => {
 		context.set('src', 'test');
 		context.set('slots.featured.videoAdUnit', '/5441/test/vast/ad/unit');
 		context.set('options.video.enableStrategyRules', true);
+		const expectedDispatchArg = {
+			showAds: false,
+			autoplayDisabled: false,
+			vastUrl:
+				'https://pubads.g.doubleclick.net/gampad/ads?output=vast&env=vp&gdfp_req=1&impl=s&unviewed_position_start=1&url=about%3Ablank&description_url=about%3Ablank&correlator=1319648926&iu=/5441/test/vast/ad/unit&sz=640x480&cust_params=src%3Dtest%26pos%3Dfeatured%26rv%3D1&vpos=preroll&rdp=0',
+			strategyRulesEnabled: true,
+			type: '[Ad Engine] Setup JWPlayer',
+			__global: true,
+		};
 
 		dispatch = global.sandbox.spy(communicationService, 'dispatch');
 		const playerSetup = new PlayerSetup();
 		playerSetup.call();
 
-		expect(dispatch.callCount).to.equal(1);
-		expect(dispatch.firstCall.args[0].strategyRulesEnabled).to.be.true;
+		expect(dispatch.withArgs(expectedDispatchArg).calledOnce);
+		expect(dispatch.lastCall.args[0].strategyRulesEnabled).to.be.true;
 	});
 });
