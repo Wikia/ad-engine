772d842e2bde3ff63ed7b707b90e8f9c80bcd118
preludek <tomasz.felczak89@gmail.com>
2023-09-25 08:31:20 +0200

    ADEN-13608 add metacritic setup

---
+++ src/platforms/news-and-ratings/metacritic/bidders/magniteS2s.ts
@@ -0,0 +1,172 @@
+export function getMagniteS2sContext(isDesktop: boolean): object {
+	const desktopSlots = {
+		leader_plus_top: {
+			sizes: [
+				[728, 90],
+				[970, 250],
+			],
+		},
+		leader_top: {
+			sizes: [[728, 90]],
+		},
+		mpu_plus_top: {
+			sizes: [
+				[300, 250],
+				[300, 600],
+			],
+		},
+		mpu_bottom: {
+			sizes: [[300, 250]],
+		},
+		leader_bottom: {
+			sizes: [
+				[728, 90],
+				[970, 250],
+			],
+		},
+		leader_middle: {
+			sizes: [[728, 90]],
+		},
+		leader_middle2: {
+			sizes: [[728, 90]],
+		},
+		incontent_plus_top: {
+			sizes: [
+				[728, 90],
+				[970, 250],
+			],
+		},
+		nav_ad_omni: {
+			sizes: [
+				[728, 90],
+				[970, 250],
+			],
+		},
+	};
+
+	const mobileSlots = {
+		mobile_banner_top: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_incontent_plus_top: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_banner_plus_middle: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_incontent_plus_middle2: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_mpu_banner_bottom: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_1: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_2: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_3: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_4: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_5: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_6: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_7: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_8: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_9: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_10: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_11: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_12: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		mobile_gallery_banner_plus_13: {
+			sizes: [
+				[300, 250],
+				[320, 50],
+			],
+		},
+		'mobile-nav-ad-plus-banner': {
+			sizes: [[320, 50]],
+		},
+		mobile_nav_ad_omni: {
+			sizes: [
+				[300, 100],
+				[320, 50],
+			],
+		},
+	};
+
+	return {
+		enabled: false,
+		accountId: 7450,
+		slots: isDesktop ? desktopSlots : mobileSlots,
+	};
+}
