212988a378173953b198d1fea8110d1b86723701
Lukasz Nawrotek <lnawrotek@fandom.com>
2023-09-25 14:38:05 +0200

    ADEN-13615

--- src/platforms/news-and-ratings/metacritic-neutron/setup/context/targeting/metacritic-neutron-targeting.setup.ts
+++ src/platforms/news-and-ratings/metacritic-neutron/setup/context/targeting/metacritic-neutron-targeting.setup.ts
@@ -3,11 +3,21 @@ import { context, DiProcess, targetingService } from '@wikia/ad-engine';
 export class MetacriticNeutronTargetingSetup implements DiProcess {
 	execute(): void {
 		const targeting = {
-			s0: 'ent',
+			s0: this.getS0(),
 			s1: 'metacritic-neutron',
 			skin: `metacritic_neutron_${context.get('state.isMobile') ? 'mobile' : 'desktop'}`,
 		};
 
 		targetingService.extend(targeting);
+	}
+
+	private getS0(): string {
+		const path = window.location.pathname;
+
+		if (path.startsWith('/game/') || path.startsWith('/browse/game/')) {
+			return 'gaming';
+		}
+
+		return 'ent';
 	}
 }
