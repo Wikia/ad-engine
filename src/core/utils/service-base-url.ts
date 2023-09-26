7029bc138a601c59ed9b9a6673d1f8558bb14b71
Jakub Bieńkowski <jbienkowski@fandom.com>
2023-09-26 11:46:23 +0200

    Fix getServicesBaseURL util

--- src/core/utils/service-base-url.ts
+++ src/core/utils/service-base-url.ts
@@ -1,5 +1,19 @@
 export function getServicesBaseURL() {
-	return document.currentScript && 'src' in document.currentScript
-		? new URL(document.currentScript.src).origin + '/'
-		: 'https://services.fandom.com/';
+	const fandomDomains = [
+		'fandom.com',
+		'tvguide.com',
+		'metacritic.com',
+		'gamespot.com',
+		'giantbomb.com',
+		'muthead.com',
+		'futhead.com',
+		'fandom-ae-assets.s3.amazonaws.com',
+		'fanatical.com',
+	];
+
+	return fandomDomains.find((domain) => window.location.hostname.includes(domain))
+		? 'https://services.fandom.com/'
+		: 'https://services.fandom-dev.' +
+				(location.hostname.match(/(?!\.)(pl|us)$/) || ['us'])[0] +
+				'/';
 }
