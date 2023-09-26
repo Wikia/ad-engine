221aed6160e492ff9a64809ad7a2ff76a244e6e4
awesolowski <awesolowski@fandom.com>
2023-09-26 12:22:03 +0200

    ADEN-13519 - structure change (page type at the end)

--- src/platforms/news-and-ratings/metacritic-neutron/ad-context.ts
+++ src/platforms/news-and-ratings/metacritic-neutron/ad-context.ts
@@ -1,7 +1,7 @@
 export const basicContext = {
 	// based on https://github.com/Wikia/player1-ads-adlibrary/blob/REVSYS-1890_MoarPurge/docs/guides/getting-started.md#the-dfp-path-property
 	adUnitId:
-		'/{custom.dfpId}/{custom.pageType}/{custom.device}{custom.region}-{custom.property}{custom.pagePath}',
+		'/{custom.dfpId}/{custom.device}{custom.region}-{custom.property}{custom.pagePath}/{custom.pageType}',
 	custom: {
 		dfpId: '5441',
 		region: 'aw',
