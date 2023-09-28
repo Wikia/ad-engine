27e7f5f289f938c591103147483dfea5b21df956
preludek <tomasz.felczak89@gmail.com>
2023-09-28 14:04:47 +0200

    ADEN-13608 remap gamefaqs slots

--- src/platforms/news-and-ratings/gamefaqs/bidders/magniteS2s.ts
+++ src/platforms/news-and-ratings/gamefaqs/bidders/magniteS2s.ts
@@ -1,111 +1,60 @@
 export function getMagniteS2sContext(isDesktop: boolean): object {
 	const desktopSlots = {
-		'incontent-ad': {
-			sizes: [[728, 90]],
-		},
-		leader_plus_top: {
+		'gamefaqs_dt_ic-lb': {
 			sizes: [
 				[728, 90],
 				[970, 250],
+				[970, 66],
 			],
 		},
-		leader_top: {
-			sizes: [[728, 90]],
+		'gamefaqs_dt_bottom-lb': {
+			sizes: [
+				[728, 90],
+				[970, 250],
+				[970, 66],
+			],
 		},
-		mpu_top: {
-			sizes: [[300, 250]],
+		'gamefaqs_dt_ic-boxad': {
+			sizes: [
+				[300, 250],
+				[300, 600],
+				[160, 600],
+			],
 		},
-		mpu_plus_top: {
+		gamefaqs_dt_top_boxad: {
 			sizes: [
 				[300, 250],
 				[300, 600],
 			],
 		},
-		mpu_bottom: {
-			sizes: [[300, 250]],
-		},
-		leader_bottom: {
-			sizes: [[728, 90]],
-		},
-		'nav-ad-plus': {
-			sizes: [[728, 90]],
-		},
-		'skybox-nav': {
-			sizes: [
-				[728, 90],
-				[970, 250],
-			],
-		},
 	};
 
 	const mobileSlots = {
-		'mobile-native-top': {
+		gamefaqs_adhesion_mw: {
 			sizes: [
-				[300, 250],
 				[320, 50],
+				[320, 100],
 			],
 		},
-		'mobile-incontent-ad': {
+		gamefaqs_mw_bottom_lb: {
 			sizes: [
-				[300, 250],
 				[320, 50],
+				[320, 100],
 			],
 		},
-		'mobile-mpu-banner-bottom': {
+		'gamefaqs_mw_ic-boxad': {
 			sizes: [
-				[300, 250],
 				[320, 50],
+				[320, 100],
 			],
 		},
-		'mobile-native-plus-top': {
+		'gamefaqs_mw_ic-lb': {
 			sizes: [
-				[300, 250],
 				[320, 50],
+				[320, 100],
 			],
 		},
-		'mobile-highimpact-plus': {
-			sizes: [
-				[300, 250],
-				[320, 50],
-			],
-		},
-		'incontent-mobile-flex': {
-			sizes: [
-				[300, 250],
-				[320, 50],
-			],
-		},
-		'mobile-banner': {
-			sizes: [[320, 50]],
-		},
-		'mobile-mpu': {
-			sizes: [[300, 250]],
-		},
-		'mobile-nav-ad-plus': {
-			sizes: [[320, 50]],
-		},
-		'mobile-banner-plus-inc': {
-			sizes: [
-				[300, 250],
-				[320, 50],
-			],
-		},
-		'mobile-incontent-ad-plus': {
-			sizes: [
-				[300, 250],
-				[320, 50],
-			],
-		},
-		'mobile-banner-mpu': {
-			sizes: [
-				[300, 250],
-				[320, 50],
-			],
-		},
-		'incontent-ad-plus': {
-			sizes: [[728, 90]],
-		},
-		'mobile-skybox-nav': {
+		gamefaqs_mw_top_boxad: {
 			sizes: [
 				[320, 50],
 				[320, 100],
