7935d6021b9bf20fc832368b20daf178398acd70
preludek <tomasz.felczak89@gmail.com>
2023-09-29 10:55:03 +0200

    ADEN-13549 added unit tests

--- spec/platforms/shared/tracking/batch-processor.spec.ts
+++ spec/platforms/shared/tracking/batch-processor.spec.ts
@@ -1,11 +1,24 @@
 import { BatchProcessor } from '@wikia/platforms/shared/tracking/batch-processor';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 const eventsArray = [{ url: '1111' }, { url: '2222' }, { url: '3333' }, { url: '4444' }];
 
 describe('batch-processor', () => {
-	describe('batch tasks', async () => {
-		it('split tasks into smaller batches', async () => {
+	let clock;
+
+	before(() => {
+		clock = sinon.useFakeTimers({
+			toFake: ['setInterval'],
+		});
+	});
+
+	after(() => {
+		clock.restore();
+	});
+
+	describe('batch tasks', () => {
+		it('split tasks into smaller batches', () => {
 			const batchProcessor = new BatchProcessor(eventsArray, 3, 0);
 
 			expect(batchProcessor.batchedTasks.length).to.be.eq(2);
@@ -17,7 +30,7 @@ describe('batch-processor', () => {
 			expect(batchProcessor.batchedTasks[1]).to.deep.equal([{ url: '4444' }]);
 		});
 
-		it('do not split when batch smaller than array size', async () => {
+		it('do not split when batch smaller than array size', () => {
 			const batchProcessor = new BatchProcessor(eventsArray, 5, 0);
 
 			expect(batchProcessor.batchedTasks.length).to.be.eq(1);
@@ -27,6 +40,20 @@ describe('batch-processor', () => {
 				{ url: '3333' },
 				{ url: '4444' },
 			]);
+		});
+	});
+
+	describe('dispatch events in batches', async () => {
+		const sendEventSpy = sinon.spy();
+
+		it('events sent count', async () => {
+			const batchProcessor = new BatchProcessor(eventsArray, 2, 1);
+
+			await batchProcessor.dispatchEventsWithTimeout(sendEventSpy);
+
+			clock.tick(3);
+
+			expect(sendEventSpy.callCount).to.be.eq(4);
 		});
 	});
 });
