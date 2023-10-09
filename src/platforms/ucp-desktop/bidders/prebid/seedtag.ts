6fcc53634945313ba58b31c399cb04a94411407a
awesolowski <awesolowski@fandom.com>
2023-10-09 14:07:02 +0200

    ADEN-13006 - add new bidder: seedtag - add other slots on ucp desktop

--- src/platforms/ucp-desktop/bidders/prebid/seedtag.ts
+++ src/platforms/ucp-desktop/bidders/prebid/seedtag.ts
@@ -1,6 +1,6 @@
 export function getSeedtagContext(): object {
 	return {
-		enabled: true,
+		enabled: false,
 		slots: {
 			top_leaderboard: {
 				sizes: [
@@ -9,6 +9,46 @@ export function getSeedtagContext(): object {
 				],
 				publisherId: '7908-0833-01',
 				adUnitId: '29832730',
+				placement: 'inBanner',
+			},
+			top_boxad: {
+				sizes: [
+					[300, 250],
+					[300, 600],
+				],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832737',
+				placement: 'inBanner',
+			},
+			incontent_leaderboard: {
+				sizes: [[728, 90]],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832784',
+				placement: 'inBanner',
+			},
+			incontent_boxad: {
+				sizes: [[300, 50]],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832792',
+				placement: 'inArticle',
+			},
+			bottom_leaderboard: {
+				sizes: [
+					[728, 90],
+					[970, 250],
+				],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832715',
+				placement: 'inScreen',
+			},
+			floor_adhesion: {
+				sizes: [
+					[320, 50],
+					[320, 100],
+				],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832715',
+				placement: 'inScreen',
 			},
 		},
 	};
