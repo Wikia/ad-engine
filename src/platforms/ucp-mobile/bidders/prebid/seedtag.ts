64542a16399aa9e56cd273ef84856fdab0c3f6f1
awesolowski <awesolowski@fandom.com>
2023-10-10 11:03:07 +0200

    ADEN-13006 - add mobile

---
+++ src/platforms/ucp-mobile/bidders/prebid/seedtag.ts
@@ -0,0 +1,31 @@
+export function getSeedtagContext(): object {
+	return {
+		enabled: false,
+		slots: {
+			top_leaderboard: {
+				sizes: [
+					[320, 50],
+					[320, 100],
+				],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832764',
+				placement: 'inBanner',
+			},
+			mobile_prefooter: {
+				sizes: [[300, 250]],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832758',
+				placement: 'inBanner',
+			},
+			floor_adhesion: {
+				sizes: [
+					[320, 50],
+					[320, 100],
+				],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832715',
+				placement: 'inScreen',
+			},
+		},
+	};
+}
