cda4237d0ccdf7445518f809be66795cb0b33279
awesolowski <awesolowski@fandom.com>
2023-10-11 13:59:15 +0200

    ADEN-13006 - update mobile setup

--- src/platforms/ucp-mobile/bidders/prebid/seedtag.ts
+++ src/platforms/ucp-mobile/bidders/prebid/seedtag.ts
@@ -2,7 +2,7 @@ export function getSeedtagContext(): object {
 	return {
 		enabled: false,
 		slots: {
-			top_leaderboard: {
+			mobile_top_leaderboard: {
 				sizes: [
 					[320, 50],
 					[320, 100],
@@ -25,6 +25,11 @@ export function getSeedtagContext(): object {
 				publisherId: '7908-0833-01',
 				adUnitId: '29832715',
 				placement: 'inScreen',
+			},
+			featured: {
+				sizes: [[0, 0]],
+				publisherId: '7908-0833-01',
+				adUnitId: '29832808',
 			},
 		},
 	};
