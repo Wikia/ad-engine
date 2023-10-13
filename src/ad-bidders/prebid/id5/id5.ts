9e98a0adaf010742d811e773e54dc9ab0724031b
Jakub Banaś <jbanas@fandom.com>
2023-10-13 14:26:47 +0200

    TACO-212 add iiq start/done events (#2190)
    * TACO-212 add iiq start/done events
    
    * TACO-212 add id5 start/done events
    
    * TACO-212 cr
    
    ---------
    
    Co-authored-by: Dominik Janik <6935667+domjanik@users.noreply.github.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

--- src/ad-bidders/prebid/id5/id5.ts
+++ src/ad-bidders/prebid/id5/id5.ts
@@ -1,3 +1,4 @@
+import { communicationService, eventsRepository } from '@ad-engine/communication';
 import { context, targetingService, UniversalStorage, utils } from '@ad-engine/core';
 import { UserIdConfig } from '../index';
 
@@ -37,6 +38,7 @@ class Id5 {
 		}
 
 		utils.logger(logGroup, 'enabled');
+		communicationService.emit(eventsRepository.ID5_START);
 
 		const id5AbValue: number = context.get('bidders.prebid.id5AbValue');
 		if (id5AbValue) {
