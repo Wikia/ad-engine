3beb03175488893c634dbf4661904b2a7a79cb6f
Adam Wesołowski <124166784+adi-wesolowski-fandom@users.noreply.github.com>
2023-10-16 15:24:34 +0200

    ADEN-13543 - move partners at the end, right after event: TLB rendered (#2189)
    * ADEN-13543 - move partners at the end, right after event: TLB rendered
    
    * ADEN-13543 - add safe-switch timeout to fire partners even when TLB-render event is not present or taking too long
    
    * ADEN-13543 - undo openweb removing from desktop-ads-mode and do some cleaning
    
    * ADEN-13543 - remove recently added experian + remove partners from mobile pipleline
    
    * ADEN-13543 - upgrades after CR
    
    ---------
    
    Co-authored-by: lnawrotek <117349052+lnawrotek@users.noreply.github.com>
    Co-authored-by: Jakub Banaś <jbanas@fandom.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>

--- src/platforms/ucp-mobile/ucp-mobile-platform.ts
+++ src/platforms/ucp-mobile/ucp-mobile-platform.ts
@@ -14,6 +14,7 @@ import {
 	SequentialMessagingSetup,
 	TrackingParametersSetup,
 	TrackingSetup,
+	UcpPartnersSetup,
 	UcpTargetingSetup,
 } from '@platforms/shared';
 import {
@@ -70,6 +71,7 @@ export class UcpMobilePlatform {
 			UcpMobileExperimentsSetup,
 			TrackingSetup,
 			AdEngineRunnerSetup,
+			UcpPartnersSetup,
 			() => communicationService.emit(eventsRepository.AD_ENGINE_CONFIGURED),
 		);
 
