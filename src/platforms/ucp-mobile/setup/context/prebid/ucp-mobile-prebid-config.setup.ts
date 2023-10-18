10c91651c98b569bad100fe2f0e1c7170bcf8cfd
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-10-18 14:28:30 +0200

    COTECH-787 rename new function and make it not override slots configs for bidders

--- src/platforms/ucp-mobile/setup/context/prebid/ucp-mobile-prebid-config.setup.ts
+++ src/platforms/ucp-mobile/setup/context/prebid/ucp-mobile-prebid-config.setup.ts
@@ -21,26 +21,28 @@ import { getWebadsContext } from '../../../bidders/prebid/webads';
 import { getWikiaContext } from '../../../bidders/prebid/wikia';
 import { getWikiaVideoContext } from '../../../bidders/prebid/wikia-video';
 
-function toggleVideoSlotsBids(bidderContext) {
+function filterVideoBids(bidderContext) {
 	const hasFeaturedVideo = context.get('custom.hasFeaturedVideo');
 	const bidConfigSlotNames = Object.keys(bidderContext.slots);
-	const hasBidderVideoSlots =
-		bidderContext.slots &&
-		(bidConfigSlotNames.includes('featured') || bidConfigSlotNames.includes('incontent_player'));
+	const hasFeaturedBidConfig = bidderContext.slots && bidConfigSlotNames.includes('featured');
+	const hasIncontentPlayerBidConfig =
+		bidderContext.slots && bidConfigSlotNames.includes('incontent_player');
 
-	if (hasBidderVideoSlots && hasFeaturedVideo) {
+	if (hasFeaturedVideo && hasIncontentPlayerBidConfig) {
 		const newVideoSlotsConfig = {
-			featured: { ...bidderContext.slots['featured'] },
+			...bidderContext.slots,
 		};
+		delete newVideoSlotsConfig['incontent_player'];
 
 		return {
 			...bidderContext,
 			slots: newVideoSlotsConfig,
 		};
-	} else if (hasBidderVideoSlots) {
+	} else if (!hasFeaturedVideo && hasFeaturedBidConfig) {
 		const newVideoSlotsConfig = {
-			incontent_player: { ...bidderContext.slots['incontent_player'] },
+			...bidderContext.slots,
 		};
+		delete newVideoSlotsConfig['featured'];
 
 		return {
 			...bidderContext,
@@ -54,25 +56,25 @@ function toggleVideoSlotsBids(bidderContext) {
 @Injectable()
 export class UcpMobilePrebidConfigSetup implements DiProcess {
 	execute(): void {
-		context.set('bidders.prebid.appnexus', toggleVideoSlotsBids(getAppnexusContext()));
-		context.set('bidders.prebid.appnexusAst', toggleVideoSlotsBids(getAppnexusAstContext()));
-		context.set('bidders.prebid.freewheel', toggleVideoSlotsBids(getFreewheelContext()));
-		context.set('bidders.prebid.gumgum', toggleVideoSlotsBids(getGumgumContext()));
-		context.set('bidders.prebid.indexExchange', toggleVideoSlotsBids(getIndexExchangeContext()));
-		context.set('bidders.prebid.kargo', toggleVideoSlotsBids(getKargoContext()));
-		context.set('bidders.prebid.mgnipbs', toggleVideoSlotsBids(getMagniteS2sContext()));
-		context.set('bidders.prebid.medianet', toggleVideoSlotsBids(getMedianetContext()));
-		context.set('bidders.prebid.nobid', toggleVideoSlotsBids(getNobidContext()));
-		context.set('bidders.prebid.ogury', toggleVideoSlotsBids(getOguryContext()));
-		context.set('bidders.prebid.openx', toggleVideoSlotsBids(getOpenXContext()));
-		context.set('bidders.prebid.ozone', toggleVideoSlotsBids(getOzoneContext()));
-		context.set('bidders.prebid.pubmatic', toggleVideoSlotsBids(getPubmaticContext()));
-		context.set('bidders.prebid.relevantdigital', toggleVideoSlotsBids(getWebadsContext()));
-		context.set('bidders.prebid.rubicon', toggleVideoSlotsBids(getRubiconContext()));
-		context.set('bidders.prebid.rubicon_display', toggleVideoSlotsBids(getRubiconDisplayContext()));
-		context.set('bidders.prebid.triplelift', toggleVideoSlotsBids(getTripleliftContext()));
-		context.set('bidders.prebid.verizon', toggleVideoSlotsBids(getVerizonContext()));
-		context.set('bidders.prebid.wikia', toggleVideoSlotsBids(getWikiaContext()));
-		context.set('bidders.prebid.wikiaVideo', toggleVideoSlotsBids(getWikiaVideoContext()));
+		context.set('bidders.prebid.appnexus', filterVideoBids(getAppnexusContext()));
+		context.set('bidders.prebid.appnexusAst', filterVideoBids(getAppnexusAstContext()));
+		context.set('bidders.prebid.freewheel', filterVideoBids(getFreewheelContext()));
+		context.set('bidders.prebid.gumgum', filterVideoBids(getGumgumContext()));
+		context.set('bidders.prebid.indexExchange', filterVideoBids(getIndexExchangeContext()));
+		context.set('bidders.prebid.kargo', filterVideoBids(getKargoContext()));
+		context.set('bidders.prebid.mgnipbs', filterVideoBids(getMagniteS2sContext()));
+		context.set('bidders.prebid.medianet', filterVideoBids(getMedianetContext()));
+		context.set('bidders.prebid.nobid', filterVideoBids(getNobidContext()));
+		context.set('bidders.prebid.ogury', filterVideoBids(getOguryContext()));
+		context.set('bidders.prebid.openx', filterVideoBids(getOpenXContext()));
+		context.set('bidders.prebid.ozone', filterVideoBids(getOzoneContext()));
+		context.set('bidders.prebid.pubmatic', filterVideoBids(getPubmaticContext()));
+		context.set('bidders.prebid.relevantdigital', filterVideoBids(getWebadsContext()));
+		context.set('bidders.prebid.rubicon', filterVideoBids(getRubiconContext()));
+		context.set('bidders.prebid.rubicon_display', filterVideoBids(getRubiconDisplayContext()));
+		context.set('bidders.prebid.triplelift', filterVideoBids(getTripleliftContext()));
+		context.set('bidders.prebid.verizon', filterVideoBids(getVerizonContext()));
+		context.set('bidders.prebid.wikia', filterVideoBids(getWikiaContext()));
+		context.set('bidders.prebid.wikiaVideo', filterVideoBids(getWikiaVideoContext()));
 	}
 }
