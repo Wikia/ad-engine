014b5a62948f0e2b8c4e45f9eb5aadb1c363dd2a
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-10-18 10:51:55 +0200

    COTECH-787 add simple tests that guards events names

---
+++ spec/ad-services/anyclip/anyclip-bids-refresher.spec.ts
@@ -0,0 +1,41 @@
+import { AnyclipBidsRefresher } from '@wikia/ad-services/anyclip/anyclip-bids-refresher';
+import { AdSlotEvent, slotService } from '@wikia/core';
+
+import { expect } from 'chai';
+import { spy, stub } from 'sinon';
+
+describe('AnyclipBidsRefresher', () => {
+	let serviceSlotGetStub;
+	const fakeIncontentPlayerSlot = {
+		emit: spy(),
+	};
+
+	beforeEach(() => {
+		serviceSlotGetStub = stub(slotService, 'get' as any);
+		serviceSlotGetStub.returns(fakeIncontentPlayerSlot);
+	});
+
+	afterEach(() => {
+		window['testSubscribeFunc'] = undefined;
+		serviceSlotGetStub.restore();
+	});
+
+	it('does not emit events to refresh bids when the subscribe function is not called', () => {
+		const refresher = new AnyclipBidsRefresher('testSubscribeFunc');
+		refresher.trySubscribingBidRefreshing();
+		expect(fakeIncontentPlayerSlot.emit.callCount).equal(0);
+	});
+
+	it('emits events to refresh bids when the subscribe function is called', () => {
+		const fakeSubscribe = (callback) => callback();
+		window['testSubscribeFunc'] = fakeSubscribe;
+
+		const refresher = new AnyclipBidsRefresher('testSubscribeFunc');
+		refresher.trySubscribingBidRefreshing();
+
+		expect(fakeIncontentPlayerSlot.emit.getCalls()[0].firstArg).equal(
+			AdSlotEvent.VIDEO_AD_IMPRESSION,
+		);
+		expect(fakeIncontentPlayerSlot.emit.getCalls()[1].firstArg).equal(AdSlotEvent.VIDEO_AD_USED);
+	});
+});
