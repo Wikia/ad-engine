f9c816ff17e5d7f7e37dbb72408e77439baa0244
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-10-18 11:13:06 +0200

    COTECH-787 add red tests for video slots in bidders config for ucp-mobile

---
+++ spec/platforms/ucp-mobile/setup/prebid/ucp-mobile-prebid-config.setup.spec.ts
@@ -0,0 +1,31 @@
+import { context } from '@wikia/core';
+import { UcpMobilePrebidConfigSetup } from '@wikia/platforms/ucp-mobile/setup/context/prebid/ucp-mobile-prebid-config.setup';
+
+import { expect } from 'chai';
+
+describe('ucp-mobile prebid setup', () => {
+	after(() => {
+		context.remove('custom.hasFeaturedVideo');
+		context.remove('bidders.prebid');
+	});
+
+	it('filters out featured slot when JWP is not on the page', () => {
+		context.set('custom.hasFeaturedVideo', false);
+		const setup = new UcpMobilePrebidConfigSetup();
+
+		setup.execute();
+
+		expect(context.get('bidders.prebid.appnexusAst.slots.incontent_player')).to.exist;
+		expect(context.get('bidders.prebid.appnexusAst.slots.featured')).to.not.exist;
+	});
+
+	it('filters out incontent_player slot when JWP is on the page', () => {
+		context.set('custom.hasFeaturedVideo', true);
+		const setup = new UcpMobilePrebidConfigSetup();
+
+		setup.execute();
+
+		expect(context.get('bidders.prebid.appnexusAst.slots.incontent_player')).to.not.exist;
+		expect(context.get('bidders.prebid.appnexusAst.slots.featured')).to.exist;
+	});
+});
