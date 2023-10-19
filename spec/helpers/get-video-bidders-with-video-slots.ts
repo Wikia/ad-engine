775c323e524d9d41299a6bdf7c8bb83c385c6323
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-10-19 16:21:22 +0200

    COTECH-787 add typing in the helper function

--- spec/helpers/get-video-bidders-with-video-slots.ts
+++ spec/helpers/get-video-bidders-with-video-slots.ts
@@ -1,4 +1,6 @@
-export function getVideoBiddersWithVideoSlots(biddersConfig) {
+import { PrebidConfig } from '@wikia/ad-bidders/prebid/prebid-models';
+
+export function getVideoBiddersWithVideoSlots(biddersConfig: PrebidConfig) {
 	return Object.keys(biddersConfig).filter(
 		(configKey) =>
 			biddersConfig[configKey]?.slots?.featured ||
