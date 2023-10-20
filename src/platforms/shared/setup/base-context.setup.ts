46c1ef873b1f2580a3fd0c4d84cb51000182b5d7
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-10-20 12:22:29 +0200

    COTECH-800 extract video related settings to a separate method

--- src/platforms/shared/setup/base-context.setup.ts
+++ src/platforms/shared/setup/base-context.setup.ts
@@ -76,6 +76,19 @@ export class BaseContextSetup implements DiProcess {
 		);
 
 		context.set(
+			'options.floorAdhesionNumberOfViewportsFromTopToPush',
+			this.instantConfig.get('icFloorAdhesionViewportsToStart'),
+		);
+		context.set('options.rotatorDelay', this.instantConfig.get('icRotatorDelay', {}));
+		context.set('options.maxDelayTimeout', this.instantConfig.get('icAdEngineDelay', 2000));
+		context.set('options.delayEvents', this.instantConfig.get('icDelayEvents'));
+
+		this.setVideoContext();
+		this.setWadContext();
+	}
+
+	private setVideoContext(): void {
+		context.set(
 			'options.video.playAdsOnNextVideo',
 			!!this.instantConfig.get('icFeaturedVideoAdsFrequency'),
 		);
@@ -96,13 +109,6 @@ export class BaseContextSetup implements DiProcess {
 			'options.video.forceVideoAdsOnAllVideosExceptSponsored',
 			this.instantConfig.get('icFeaturedVideoForceVideoAdsEverywhereExceptSponsoredVideo'),
 		);
-
-		context.set(
-			'options.floorAdhesionNumberOfViewportsFromTopToPush',
-			this.instantConfig.get('icFloorAdhesionViewportsToStart'),
-		);
-		context.set('options.rotatorDelay', this.instantConfig.get('icRotatorDelay', {}));
-		context.set('options.maxDelayTimeout', this.instantConfig.get('icAdEngineDelay', 2000));
 		context.set('options.jwpMaxDelayTimeout', this.instantConfig.get('icUAPJWPlayerDelay', 0));
 		context.set('options.video.iasTracking.enabled', this.instantConfig.get('icIASVideoTracking'));
 		context.set('options.video.isUAPJWPEnabled', this.instantConfig.get('icUAPJWPlayer'));
@@ -114,9 +120,6 @@ export class BaseContextSetup implements DiProcess {
 			'options.video.comscoreJwpTracking',
 			this.instantConfig.get('icComscoreJwpTracking'),
 		);
-		context.set('options.delayEvents', this.instantConfig.get('icDelayEvents'));
-
-		this.setWadContext();
 	}
 
 	private setInContentExperiment(): void {
