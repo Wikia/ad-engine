bf95653f344bb5a0d11cb69d23388a16429af829
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-11-07 17:25:17 +0100

    COTECH-800 fix unit tests

--- spec/ad-services/connatix/connatix.spec.ts
+++ spec/ad-services/connatix/connatix.spec.ts
@@ -1,10 +1,30 @@
 import { Connatix } from '@wikia/ad-services';
+import { communicationService, EventOptions } from '@wikia/communication';
+import { CommunicationService } from '@wikia/communication/communication-service';
 import { context } from '@wikia/core';
-import sinon from 'sinon';
+import sinon, { SinonStubbedInstance } from 'sinon';
 import { makePlayerInjectorSpy } from './test-doubles/player-injector-spy';
 
 describe('Connatix', () => {
-	it('Connatix is called', async () => {
+	let communicationServiceStub: SinonStubbedInstance<CommunicationService>;
+
+	beforeEach(() => {
+		communicationServiceStub = global.sandbox.stub(communicationService);
+		communicationServiceStub.on.callsFake(
+			(event: EventOptions, callback: (payload?: any) => void) => {
+				const payload = { isLoaded: false };
+				callback(payload);
+			},
+		);
+	});
+
+	afterEach(() => {
+		global.sandbox.restore();
+
+		context.remove('services.connatix.enabled');
+	});
+
+	it('is called when there is no Fan Takeover', async () => {
 		context.set('services.connatix.enabled', true);
 		const playerInjectorSpy = makePlayerInjectorSpy();
 
@@ -14,7 +34,23 @@ describe('Connatix', () => {
 		sinon.assert.calledWith(playerInjectorSpy.insertPlayerContainer, connatix.cid);
 	});
 
-	it('Connatix is disabled', async () => {
+	it('is called when there is a Fan Takeover', async () => {
+		context.set('services.connatix.enabled', false);
+		const playerInjectorSpy = makePlayerInjectorSpy();
+
+		const connatix = new Connatix(null, null, playerInjectorSpy);
+		await connatix.call();
+		communicationServiceStub.on.callsFake(
+			(event: EventOptions, callback: (payload?: any) => void) => {
+				const payload = { isLoaded: true };
+				callback(payload);
+			},
+		);
+
+		sinon.assert.notCalled(playerInjectorSpy.insertPlayerContainer);
+	});
+
+	it('is called when disabled', async () => {
 		context.set('services.connatix.enabled', false);
 		const playerInjectorSpy = makePlayerInjectorSpy();
 
