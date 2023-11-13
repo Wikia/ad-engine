e607df76c582b79449755a88407cd7a2749e62be
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-11-13 13:58:19 +0100

    Revert "Revert "COTECH-800 Bring back Connatix player (#2209)" (#2231)"
    This reverts commit 284a9d4e7365568efbd5a091a131a59628db7474.

---
+++ src/ad-services/connatix/index.ts
@@ -0,0 +1,44 @@
+import { communicationService, eventsRepository, UapLoadStatus } from '@ad-engine/communication';
+import { BaseServiceSetup, context, utils } from '@ad-engine/core';
+import { ConnatixPlayer } from './connatix-player';
+import { initConnatixHeadScript } from './head-script';
+import { PlayerInjector, PlayerInjectorInterface } from './player-injector';
+
+export const logGroup = 'connatix';
+
+export class Connatix extends BaseServiceSetup {
+	get cid(): string {
+		return context.get('services.connatix.cid');
+	}
+
+	isEnabled(): boolean {
+		return !!context.get('services.connatix.enabled') && !!this.cid;
+	}
+
+	constructor(
+		protected instantConfig,
+		protected globalTimeout,
+		private playerInjector: PlayerInjectorInterface,
+	) {
+		super();
+
+		const connatixPlayer = new ConnatixPlayer();
+		this.playerInjector = playerInjector ? playerInjector : new PlayerInjector(connatixPlayer);
+	}
+
+	async call(): Promise<void> {
+		if (!this.isEnabled()) {
+			utils.logger(logGroup, 'Connatix player is disabled');
+			return Promise.resolve();
+		}
+
+		communicationService.on(eventsRepository.AD_ENGINE_UAP_LOAD_STATUS, (action: UapLoadStatus) => {
+			if (!action.isLoaded) {
+				utils.logger(logGroup, 'No Fan Takeover loaded - injecting Connatix player');
+				initConnatixHeadScript(this.cid);
+				utils.logger(logGroup, 'Connatix head script is ready');
+				this.playerInjector.insertPlayerContainer(this.cid);
+			}
+		});
+	}
+}
