32901d01f0fadeb08b1c60252e4d5f1cb7d2e171
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-11-21 18:06:35 +0100

    COTECH-832 WIP: make the tagless VAST request more dynamic

--- src/platforms/shared/services/tagless-request-setup.ts
+++ src/platforms/shared/services/tagless-request-setup.ts
@@ -1,20 +1,43 @@
-import { BaseServiceSetup, utils } from '@wikia/ad-engine';
-
-const logGroup = 'tagless-request';
+import {
+	BaseServiceSetup,
+	context,
+	getAdUnitString,
+	targetingService,
+	utils,
+} from '@wikia/ad-engine';
 
 export class TaglessRequestSetup extends BaseServiceSetup {
+	private logGroup = 'tagless-request';
+
 	async call(): Promise<void> {
+		const pageTargeting = targetingService.dump();
+
 		const videoTaglessRequestUrl = utils.buildVastUrl(16 / 9, 'video', {
-			videoAdUnitId:
-				'/5441/wka1b.VIDEO/featured/smartphone/ucp_mobile-fandom-fv-article/_project43-life',
-			customParams: 'npa=0&artid=355&&s1=_project43&cid=adeng-uap-jwp&src=test&pos=featured',
+			videoAdUnitId: getAdUnitString('featured', {
+				...context.get('slots.featured'),
+				slotNameSuffix: '',
+			}),
+			customParams: `src=${context.get('src')}&pos=featured&cid=${pageTargeting['cid']}`,
 		});
 
-		utils.logger(logGroup, 'Sending a tagless request: ', videoTaglessRequestUrl);
-		await fetch(videoTaglessRequestUrl).then((res) =>
-			utils.logger(logGroup, 'Response received', res),
-		);
+		utils.logger(this.logGroup, 'Sending a tagless request: ', videoTaglessRequestUrl);
+		await fetch(videoTaglessRequestUrl)
+			.then((res) => res.blob())
+			.then((resBlob) => resBlob.text())
+			.then((text) => {
+				try {
+					const parser = new DOMParser();
+					const xml = parser.parseFromString(text, 'text/xml');
+					const ads = xml.getElementsByTagName('Ad');
+					const firstReturnedAdId = ads[0]?.id;
+					utils.logger(this.logGroup, firstReturnedAdId);
 
-		return Promise.resolve();
+					return Promise.resolve(firstReturnedAdId);
+				} catch (e) {
+					utils.logger(this.logGroup, 'No XML available - not a VAST response from the ad server?');
+
+					return Promise.resolve(null);
+				}
+			});
 	}
 }
