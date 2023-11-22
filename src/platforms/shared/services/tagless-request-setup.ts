5d412780616b2f3a9a37d7d18c7bc675bd0da3bc
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2023-11-22 12:47:37 +0100

    COTECH-832 copy syncing key-vals logic from JWP inhibitor

--- src/platforms/shared/services/tagless-request-setup.ts
+++ src/platforms/shared/services/tagless-request-setup.ts
@@ -3,11 +3,18 @@ import {
 	context,
 	getAdUnitString,
 	targetingService,
+	universalAdPackage,
 	utils,
 } from '@wikia/ad-engine';
 
+interface ParsedCampaignData {
+	lineItemId: string;
+	creativeId: string;
+}
+
 export class TaglessRequestSetup extends BaseServiceSetup {
 	private logGroup = 'tagless-request';
+	private syncedVideoLines;
 
 	async call(): Promise<void> {
 		if (!this.isEnabled('icTaglessRequestEnabled')) {
@@ -15,34 +22,65 @@ export class TaglessRequestSetup extends BaseServiceSetup {
 			return Promise.resolve(null);
 		}
 
+		const videoTaglessRequestUrl = this.buildTaglessVideoRequest();
+		utils.logger(this.logGroup, 'Sending a tagless request: ', videoTaglessRequestUrl);
+
+		await fetch(videoTaglessRequestUrl)
+			.then((res) => res.blob())
+			.then((resBlob) => resBlob.text())
+			.then((text) => this.handleTaglessResponse(text))
+			.then((ad: ParsedCampaignData) => this.updateDisplayAdsTargetingIfSyncedCampaign(ad));
+	}
+
+	private handleTaglessResponse(text: string) {
+		try {
+			const firstReturnedAdId = this.getFirstAdFromTaglessResponse(text);
+			utils.logger(this.logGroup, firstReturnedAdId);
+
+			return Promise.resolve(firstReturnedAdId);
+		} catch (e) {
+			utils.logger(this.logGroup, 'No XML available - not a VAST response from the ad server?');
+
+			return Promise.resolve(null);
+		}
+	}
+
+	private updateDisplayAdsTargetingIfSyncedCampaign(ad: ParsedCampaignData) {
+		if (!this.syncedVideoLines) {
+			this.syncedVideoLines = context.get('options.video.uapJWPLineItemIds') || [];
+		}
+
+		const { lineItemId, creativeId } = ad;
+
+		if (lineItemId && creativeId && this.syncedVideoLines.includes(lineItemId)) {
+			universalAdPackage.updateSlotsTargeting(lineItemId, creativeId);
+			utils.logger(this.logGroup, 'video ad is from UAP:JWP campaign - updating key-vals');
+		} else {
+			utils.logger(this.logGroup, 'video ad is not from UAP:JWP campaign');
+		}
+	}
+
+	private buildTaglessVideoRequest() {
 		const pageTargeting = targetingService.dump();
 
-		const videoTaglessRequestUrl = utils.buildVastUrl(16 / 9, 'video', {
+		return utils.buildVastUrl(16 / 9, 'video', {
 			videoAdUnitId: getAdUnitString('featured', {
 				...context.get('slots.featured'),
 				slotNameSuffix: '',
 			}),
 			customParams: `src=${context.get('src')}&pos=featured&cid=${pageTargeting['cid']}`,
 		});
+	}
 
-		utils.logger(this.logGroup, 'Sending a tagless request: ', videoTaglessRequestUrl);
-		await fetch(videoTaglessRequestUrl)
-			.then((res) => res.blob())
-			.then((resBlob) => resBlob.text())
-			.then((text) => {
-				try {
-					const parser = new DOMParser();
-					const xml = parser.parseFromString(text, 'text/xml');
-					const ads = xml.getElementsByTagName('Ad');
-					const firstReturnedAdId = ads[0]?.id;
-					utils.logger(this.logGroup, firstReturnedAdId);
+	private getFirstAdFromTaglessResponse(response: string) {
+		const parser = new DOMParser();
+		const xml = parser.parseFromString(response, 'text/xml');
+		const lineItemId = xml.getElementsByTagName('Ad');
+		const creativeId = xml.getElementsByTagName('Creative');
 
-					return Promise.resolve(firstReturnedAdId);
-				} catch (e) {
-					utils.logger(this.logGroup, 'No XML available - not a VAST response from the ad server?');
-
-					return Promise.resolve(null);
-				}
-			});
+		return {
+			lineItemId: lineItemId[0]?.id,
+			creativeId: creativeId[0]?.id,
+		};
 	}
 }
