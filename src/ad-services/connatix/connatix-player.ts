a8b109b07fc09e3d2a4060c0e8c579d2aaa0cd70
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2023-11-24 17:11:30 +0100

    COTECH-819 Bring back Connatix to mobile and make it work like Anyclip (#2246)
    * COTECH-819 add Connatix player to ucp-mobile
    
    * COTECH-819 change player and render IDs for mobile
    
    * COTECH-819 remove misleading config property
    
    * COTECH-819 make Anyclip and Connatix Fan Takeover logic similar
    
    * COTECH-819 lazy-load Connatix on mobile
    
    * COTECH-819 change the way we load Connatix and copy floor_adhesion logic from Anyclip
    
    * COTECH-819 "fix" unit tests
    
    * COTECH-819 add bids refreshing
    
    * COTECH-819 add types in callback and describe tests better
    
    * COTECH-819 add two more test cases for after render
    
    ---------
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>

--- src/ad-services/connatix/connatix-player.ts
+++ src/ad-services/connatix/connatix-player.ts
@@ -1,11 +1,30 @@
 export interface ConnatixPlayerInterface {
-	get(playerId: string, renderId: string): any;
+	get(playerId: string, renderId: string, renderCallback: (error, player) => void): any;
+}
+
+export enum ConnatixPlayerType {
+	OutStream = 0,
+	InStream = 1,
+}
+
+export interface ConnatixPlayerApi {
+	destroy: () => void;
+	disableAdvertising: () => void;
+	disableFloatingMode: () => void;
+	enableAdvertising: () => void;
+	enableFloatingMode: () => void;
+	getPlayerType: () => ConnatixPlayerType;
+	on: (eventName: string, callback: (eventData: unknown) => void) => void;
 }
 
 export class ConnatixPlayer implements ConnatixPlayerInterface {
-	get(playerId: string, renderId: string): any {
+	get(
+		playerId: string,
+		renderId: string,
+		renderCallback: (error, player: ConnatixPlayerApi) => void,
+	): any {
 		return window.cnx.cmd.push(() => {
-			window.cnx({ playerId: playerId }).render(renderId);
+			window.cnx({ playerId: playerId }).render(renderId, renderCallback);
 		});
 	}
 }
