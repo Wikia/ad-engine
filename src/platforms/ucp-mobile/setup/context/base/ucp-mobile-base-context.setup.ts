d202cda8d8f5d0d6ccdffb3461b902744893f774
Mateusz Kłakus <mklakus@fandom.com>
2023-11-27 09:26:03 +0100

    ADEN-12076 - ucp_mobile no_ads for logged in gamepedia users

--- src/platforms/ucp-mobile/setup/context/base/ucp-mobile-base-context.setup.ts
+++ src/platforms/ucp-mobile/setup/context/base/ucp-mobile-base-context.setup.ts
@@ -7,6 +7,13 @@ export class UcpMobileBaseContextSetup extends BaseContextSetup {
 	execute(): void {
 		super.execute();
 
+		const isGamepedia = window.ads.context.opts.isGamepedia;
+		const isLoggedIn = window.mw.config.get('wgUserId');
+
+		if (isGamepedia && isLoggedIn) {
+			this.noAdsDetector.addReason('gamepedia_loggedin_mobile_no_ads');
+		}
+
 		context.set(
 			'custom.serverPrefix',
 			utils.geoService.isProperCountry(['AU', 'NZ']) ? 'vm1b' : 'wka1b',
