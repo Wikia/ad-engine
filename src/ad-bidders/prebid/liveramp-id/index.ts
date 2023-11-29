b7d59b903fbf91f1e0414c4b7f1a5da085a222c8
Adam Wesołowski <124166784+adi-wesolowski-fandom@users.noreply.github.com>
2023-11-29 11:58:31 +0100

    Aden 13619 2 (#2258)
    * ADEN-13619 - bring back (liveramp)ATS
    
    * ADEN-13619 - bring back (liveramp)ATS - add missed (by git) spec.file
    
    * ADEN-13619 - bring back (liveramp)ATS - add missed files
    
    * ADEN-13619 - bring back liveramp(&)ATS - all of it + refactor + fix unit tests
    
    * ADEN-13619 remove redundant class
    
    * ADEN-13619 rename ats to id
    
    * ADEN-13619 add another condition for enable ATS analytics
    
    * ADEN-13619 remove await
    
    ---------
    
    Co-authored-by: preludek <tomasz.felczak89@gmail.com>
    Co-authored-by: Tomasz Felczak <43118980+preludek@users.noreply.github.com>

---
+++ src/ad-bidders/prebid/liveramp-id/index.ts
@@ -0,0 +1,42 @@
+import { context, utils } from '@ad-engine/core';
+import { UserIdConfig } from '../index';
+
+const logGroup = 'LiveRamp';
+
+export abstract class LiveRampIdTypes {
+	static PLACEMENT_ID = '2161';
+	static ENVELOPE_STORAGE_NAME = 'idl_env';
+}
+
+class LiveRampId {
+	getConfig(): UserIdConfig {
+		if (!this.isEnabled()) {
+			utils.logger(logGroup, 'disabled');
+			return;
+		}
+
+		utils.logger(logGroup, 'enabled');
+		return {
+			name: 'identityLink',
+			params: {
+				pid: LiveRampIdTypes.PLACEMENT_ID,
+			},
+			storage: {
+				type: 'html5',
+				name: LiveRampIdTypes.ENVELOPE_STORAGE_NAME,
+				expires: 1,
+				refreshInSeconds: 1800,
+			},
+		};
+	}
+
+	private isEnabled(): boolean {
+		return (
+			context.get('bidders.liveRampId.enabled') &&
+			!context.get('options.optOutSale') &&
+			!utils.isCoppaSubject()
+		);
+	}
+}
+
+export const liveRampId = new LiveRampId();
