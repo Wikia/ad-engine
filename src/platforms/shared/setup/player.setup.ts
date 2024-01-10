e221e1d5f485244bf2adc8d444604b743e070715
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2024-01-10 15:26:06 +0100

    Restore strategy rules in video player setup

--- src/platforms/shared/setup/player.setup.ts
+++ src/platforms/shared/setup/player.setup.ts
@@ -1,4 +1,5 @@
 import {
+	AdSlot,
 	BaseServiceSetup,
 	communicationService,
 	context,
@@ -7,6 +8,7 @@ import {
 	JWPlayerManager,
 	jwpSetup,
 	Optimizely,
+	slotService,
 	utils,
 	VastResponseData,
 	VastTaglessRequest,
@@ -14,6 +16,17 @@ import {
 import { Injectable } from '@wikia/dependency-injection';
 
 const logGroup = 'player-setup';
+
+const OPTIMIZELY_STRATEGY_RULES_EXPERIMENT = {
+	EXPERIMENT_ENABLED: 'strategy_rules',
+	EXPERIMENT_VARIANT: 'strategy_rules_variant',
+};
+
+const OPTIMIZELY_STRATEGY_RULES_EXPERIMENT_VARIANTS = {
+	DISABLED: 'strategy_rules_disabled',
+	ENABLED: 'strategy_rules_enabled',
+	UNDEFINED: 'strategy_rules_undefined',
+};
 
 @Injectable()
 export class PlayerSetup extends BaseServiceSetup {
@@ -29,9 +42,12 @@ export class PlayerSetup extends BaseServiceSetup {
 	}
 
 	async call() {
-		const showAds = !context.get('options.wad.blocking');
+		this.setupOptimizelyExperiment();
 
-		if (showAds) {
+		const showAds = !context.get('options.wad.blocking');
+		const strategyRulesEnabled = context.get('options.video.enableStrategyRules');
+
+		if (showAds && !strategyRulesEnabled) {
 			utils.logger(logGroup, 'JWP with ads controlled by AdEngine enabled');
 
 			const vastResponse: VastResponseData =
@@ -43,6 +59,7 @@ export class PlayerSetup extends BaseServiceSetup {
 			if (vastResponse?.xml) {
 				displayAndVideoAdsSyncContext.setVastRequestedBeforePlayer();
 			}
+
 			this.jwpManager.manage();
 			communicationService.dispatch(
 				jwpSetup({
@@ -51,15 +68,61 @@ export class PlayerSetup extends BaseServiceSetup {
 					vastXml: vastResponse?.xml,
 				}),
 			);
+		} else if (strategyRulesEnabled) {
+			utils.logger(
+				logGroup,
+				'JWP Strategy Rules enabled - AdEngine does not control ads in JWP anymore',
+			);
+			this.jwpManager.manage();
+			communicationService.dispatch(
+				jwpSetup({
+					showAds,
+					autoplayDisabled: false,
+					vastUrl: this.generateVastUrlForJWPlayer(),
+					strategyRulesEnabled,
+				}),
+			);
 		} else {
 			utils.logger(logGroup, 'ad block detected, without ads');
 			this.jwpManager.manage();
 			communicationService.dispatch(
 				jwpSetup({
-					showAds: false,
+					showAds,
 					autoplayDisabled: false,
 				}),
 			);
 		}
+	}
+
+	private generateVastUrlForJWPlayer(): string {
+		const aspectRatio = 16 / 9;
+		const slotName = 'featured';
+		const position = 'preroll';
+
+		const adSlot = slotService.get(slotName) || new AdSlot({ id: slotName });
+		if (!slotService.get(slotName)) {
+			slotService.add(adSlot);
+		}
+
+		return utils.buildVastUrl(aspectRatio, adSlot.getSlotName(), { vpos: position });
+	}
+
+	private setupOptimizelyExperiment() {
+		this.optimizely.addVariantToTargeting(
+			OPTIMIZELY_STRATEGY_RULES_EXPERIMENT,
+			OPTIMIZELY_STRATEGY_RULES_EXPERIMENT_VARIANTS.UNDEFINED,
+		);
+
+		const variant = this.optimizely.getVariant(OPTIMIZELY_STRATEGY_RULES_EXPERIMENT);
+
+		if (!variant) {
+			return;
+		}
+
+		this.optimizely.addVariantToTargeting(OPTIMIZELY_STRATEGY_RULES_EXPERIMENT, variant);
+		context.set(
+			'options.video.enableStrategyRules',
+			variant === OPTIMIZELY_STRATEGY_RULES_EXPERIMENT_VARIANTS.ENABLED,
+		);
 	}
 }
