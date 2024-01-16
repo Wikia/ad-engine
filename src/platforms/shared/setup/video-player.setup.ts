b6927eca3d064bd6241289f2165553ef5151ebef
David Iacovone <diacovone@fandom.com>
2024-01-16 15:56:50 +0100

    COTECH-993 added video-player.setup.ts

---
+++ src/platforms/shared/setup/video-player.setup.ts
@@ -0,0 +1,22 @@
+import {BaseServiceSetup, communicationService, context, eventsRepository, utils} from "@wikia/ad-engine";
+import {iasVideoTracker} from "../../../ad-products/video/porvata/plugins/ias/ias-video-tracker";
+import {Injectable} from "@wikia/dependency-injection";
+
+const logGroup = 'player-setup';
+
+@Injectable()
+export class VideoPlayerSetup extends BaseServiceSetup {
+
+    async execute(): Promise<void> {
+        communicationService.on(eventsRepository.PLAYER_READY, () => {
+            this.loadIasTrackerIfEnabled();
+        })
+    }
+
+    private loadIasTrackerIfEnabled(): void {
+        if (context.get('options.video.iasTracking.enabled')) {
+            utils.logger(logGroup, 'Loading IAS tracker for video player')
+            iasVideoTracker.load();
+        }
+    }
+}
