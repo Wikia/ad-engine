e0e7f83e429e61dca6f3e643257e0f424576c3dd
Lukasz Nawrotek <lnawrotek@fandom.com>
2024-01-26 13:21:50 +0100

    ADEN-13829

--- src/platforms/ucp-mobile/setup/experiments/ucp-mobile-top-boxad-experiment.ts
+++ src/platforms/ucp-mobile/setup/experiments/ucp-mobile-top-boxad-experiment.ts
@@ -7,6 +7,7 @@ import {
 	utils,
 } from '@wikia/ad-engine';
 import { Injectable } from '@wikia/dependency-injection';
+import { UcpMobileTopBoxadParagraphHelper } from './utils/ucp-mobile-top-boxad-paragraph-helper';
 
 interface TopBoxadConfigExperiment {
 	anchorSelector: string;
@@ -27,67 +28,89 @@ export class UcpMobileTopBoxadExperiment {
 		insertMethod: 'after',
 	};
 
-	constructor(private instantConfig: InstantConfigService, private openWeb: OpenWeb) {}
+	constructor(
+		private instantConfig: InstantConfigService,
+		private openWeb: OpenWeb,
+		private paragraphHelper: UcpMobileTopBoxadParagraphHelper,
+	) {}
 
 	public getConfig(): TopBoxadConfigExperiment {
-		if (this.isHome()) {
-			utils.logger(logGroup, 'Home page');
-			this.addToTargeting('top_boxad_default');
-
-			return this.defaultHomeConfig;
-		} else if (this.isExperimentEnabled()) {
+		if (this.isExperimentEnabled() && !this.isHome()) {
+			utils.logger(logGroup, 'New topbox_ad config');
 			return this.getExperimentConfig();
 		}
 
-		utils.logger(logGroup, 'Default config');
+		utils.logger(logGroup, this.isHome() ? 'Home page' : 'Default config');
+		this.addToTargeting('top_boxad_default');
+		return this.isHome() ? this.defaultHomeConfig : this.defaultNonHomeConfig;
+	}
+
+	private getExperimentConfig(): TopBoxadConfigExperiment {
+		const paragraphSelector = this.paragraphHelper.getParagraphSelector();
+		const paragraphs = Array.from(document.querySelectorAll(paragraphSelector));
+		const firstParagraph = this.paragraphHelper.getFirstParagraph(paragraphs);
+
+		if (!this.paragraphHelper.existParagraph(firstParagraph)) {
+			utils.logger(logGroup, 'First paragraph does not exist');
+			this.addToTargeting('top_boxad_default');
+			return this.defaultNonHomeConfig;
+		}
+
+		if (this.isBeforeFirstH2(firstParagraph.element)) {
+			return this.getBeforeFirstH2Config(firstParagraph);
+		}
+
+		if (this.paragraphHelper.isLargeParagraph(firstParagraph.element)) {
+			return this.getLargeParagraphConfig(firstParagraph);
+		}
+
+		const secondParagraph = this.paragraphHelper.getSecondParagraph(
+			paragraphs,
+			firstParagraph.nthOfType,
+		);
+
+		if (this.paragraphHelper.existParagraph(secondParagraph)) {
+			return this.getSecondParagraphConfig(secondParagraph);
+		}
+
 		this.addToTargeting('top_boxad_default');
 		return this.defaultNonHomeConfig;
 	}
 
-	private getExperimentConfig(): TopBoxadConfigExperiment {
-		const paragraphs = Array.from(document.querySelectorAll('.mw-parser-output > p'));
-		const firstParagraph = this.getFirstParagraph(paragraphs);
+	private getSecondParagraphConfig(secondParagraph: { element: Element; nthOfType: number }) {
+		utils.logger(logGroup, 'Short first <p>, ad after second <p>');
+		this.addToTargeting('top_boxad_second_paragraph');
 
-		if (!this.existParagraph(firstParagraph)) {
-			utils.logger(logGroup, 'First paragraph does not exist');
-			this.addToTargeting('top_boxad_default');
+		return this.prepareConfig(
+			`${this.paragraphHelper.getNthParagraphSelector(secondParagraph.nthOfType)}, ${
+				this.defaultAnchorSelector
+			}`,
+			'after',
+		);
+	}
 
-			return this.defaultNonHomeConfig;
-		} else if (this.isBeforeFirstH2(firstParagraph.element)) {
-			utils.logger(logGroup, 'One <p> before <h2>');
-			this.addToTargeting('top_boxad_one_paragraph');
+	private getLargeParagraphConfig(firstParagraph: { element: Element; nthOfType: number }) {
+		utils.logger(logGroup, 'Large first <p>');
+		this.addToTargeting('top_boxad_large_paragraph');
 
-			return this.prepareConfig(
-				`${this.getParagraphSelector(firstParagraph.nthOfType)}, ${this.defaultAnchorSelector}`,
-				'after',
-			);
-		}
+		return this.prepareConfig(
+			`${this.paragraphHelper.getNthParagraphSelector(firstParagraph.nthOfType)}, ${
+				this.defaultAnchorSelector
+			}`,
+			'after',
+		);
+	}
 
-		if (this.isLargeParagraph(firstParagraph.element)) {
-			utils.logger(logGroup, 'Large first <p>');
-			this.addToTargeting('top_boxad_large_paragraph');
+	private getBeforeFirstH2Config(firstParagraph: { element: Element; nthOfType: number }) {
+		utils.logger(logGroup, 'One <p> before <h2>');
+		this.addToTargeting('top_boxad_one_paragraph');
 
-			return this.prepareConfig(
-				`${this.getParagraphSelector(firstParagraph.nthOfType)}, ${this.defaultAnchorSelector}`,
-				'after',
-			);
-		} else {
-			const secondParagraph = this.getSecondParagraph(paragraphs, firstParagraph.nthOfType);
-
-			if (this.existParagraph(secondParagraph)) {
-				utils.logger(logGroup, 'Short first <p>, ad after second <p>');
-				this.addToTargeting('top_boxad_second_paragraph');
-
-				return this.prepareConfig(
-					`${this.getParagraphSelector(secondParagraph.nthOfType)}, ${this.defaultAnchorSelector}`,
-					'after',
-				);
-			}
-		}
-
-		this.addToTargeting('top_boxad_default');
-
-		return this.defaultNonHomeConfig;
+		return this.prepareConfig(
+			`${this.paragraphHelper.getNthParagraphSelector(firstParagraph.nthOfType)}, ${
+				this.defaultAnchorSelector
+			}`,
+			'after',
+		);
 	}
 
 	private addToTargeting(experimentGroup: string) {
@@ -121,10 +144,6 @@ export class UcpMobileTopBoxadExperiment {
 		return context.get('wiki.targeting.pageType') === 'home';
 	}
 
-	private getParagraphSelector(nthOfType) {
-		return `.mw-parser-output > p:nth-of-type(${nthOfType})`;
-	}
-
 	private isBeforeFirstH2(element: Element) {
 		const firstH2 = document.querySelector('.mw-parser-output > h2');
 
@@ -135,40 +154,5 @@ export class UcpMobileTopBoxadExperiment {
 		const tocElement = document.querySelector('.mw-parser-output > #toc');
 
 		return element.nextElementSibling == tocElement && tocElement.nextElementSibling == firstH2;
-	}
-
-	private getParagraphData(
-		paragraphs: Element[],
-		condition: (element: Element, index: number) => boolean,
-	) {
-		const index = paragraphs.findIndex(condition);
-
-		return {
-			element:
-				index >= 0 ? document.querySelector(this.getParagraphSelector(index + 1)) : undefined,
-			nthOfType: index >= 0 ? index + 1 : undefined,
-		};
-	}
-
-	private getFirstParagraph(paragraphs: Element[]) {
-		return this.getParagraphData(paragraphs, (element) => element.textContent.trim() !== '');
-	}
-
-	private getSecondParagraph(paragraphs: Element[], firstOfTypeIndex: number) {
-		return this.getParagraphData(
-			paragraphs,
-			(element, index) => element.textContent.trim() !== '' && index !== firstOfTypeIndex - 1,
-		);
-	}
-
-	private isLargeParagraph(paragraph: Element) {
-		return (
-			paragraph.textContent &&
-			(paragraph.textContent.length >= 400 || paragraph.clientHeight >= 350)
-		);
-	}
-
-	private existParagraph(paragraph: { element: any; nthOfType: any }) {
-		return paragraph.element && paragraph.nthOfType;
 	}
 }
