6f4702144b3578424c220780a768beb422b100e1
Steven Ng <sng@fandom.com>
2024-01-29 17:47:28 -0800

    fix Gamespot Targeting Setup tests

--- spec/platforms/news-and-ratings/gamespot/setup/context/targeting/gamespot-targeting.setup.spec.ts
+++ spec/platforms/news-and-ratings/gamespot/setup/context/targeting/gamespot-targeting.setup.spec.ts
@@ -1,10 +1,19 @@
 import { GamespotTargetingSetup } from '@wikia/platforms/news-and-ratings/gamespot/setup/context/targeting/gamespot-targeting.setup';
 import { expect } from 'chai';
 
+class MockDataLayer {
+	events = [];
+	add(event) {
+		this.events.push(event);
+	}
+	find(predicate) {
+		return this.events.find(predicate);
+	}
+}
+
 describe('Gamespot Targeting Setup', () => {
 	beforeEach(() => {
-		window.dataLayer = {};
-		window.dataLayer.data = undefined;
+		window.dataLayer = new MockDataLayer();
 	});
 
 	describe('getVerticalName', () => {
@@ -36,7 +45,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=news when topicName is not available', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = { siteSection: 'news' };
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'news' },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -48,7 +60,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=news when topicName is empty array', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = { siteSection: 'news' };
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'news' },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -60,10 +75,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=news when topicName array include "Games"', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'news',
-				topicName: ['Games'],
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'news', topicName: ['Games'] },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -75,11 +90,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=news when topicName does not include "Games" but contentTopicId does', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'news',
-				topicName: ['Tech'],
-				contentTopicName: 'gaming-tech',
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'news', topicName: ['Tech'], contentTopicName: 'gaming-tech' },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -91,10 +105,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "ent" on siteSection=news when topicName array does not include "Games"', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'news',
-				topicName: ['TV'],
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'news', topicName: ['Tech'] },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -106,10 +120,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=reviews when topicName array include "Games"', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'reviews',
-				topicName: ['Games'],
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'reviews', topicName: ['Games'] },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -121,10 +135,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=reviews when topicName array include "Game Review"', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'reviews',
-				topicName: ['Games'],
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'reviews', topicName: ['Games'] },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -136,10 +150,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "ent" on siteSection=reviews when topicName array does not include "Games"', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'reviews',
-				topicName: ['TV'],
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'reviews', topicName: ['TV'] },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -151,10 +165,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=galleries when topicName array include "Games"', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'galleries',
-				topicName: ['Games'],
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'galleries', topicName: ['Games'] },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -166,10 +180,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "ent" on siteSection=galleries when topicName array does not include "Games"', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {
-				siteSection: 'galleries',
-				topicName: ['TV'],
-			};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'galleries', topicName: ['TV'] },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -181,7 +195,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" on siteSection=reviews when topicName is not available', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = { siteSection: 'news' };
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'news' },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -193,7 +210,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" when siteSection does not exist', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = {};
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: {},
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
@@ -205,7 +225,10 @@ describe('Gamespot Targeting Setup', () => {
 		it('returns "gaming" when siteSection is different than news and entertainment', () => {
 			// given
 			const gamespotTargetingSetup = new GamespotTargetingSetup();
-			window.dataLayer.data = { siteSection: 'different' };
+			window.dataLayer.add({
+				event: 'Pageview',
+				data: { siteSection: 'different' },
+			});
 
 			//when
 			const verticalName = gamespotTargetingSetup.getVerticalName();
