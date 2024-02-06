8b34dfd7f61b72f81ea615e624a4b5319bf3ac30
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2024-02-06 19:14:50 +0100

    COTECH-1035 define just one experiment for no incontent player

--- src/core/video/no-incontent-player-experiment.ts
+++ src/core/video/no-incontent-player-experiment.ts
@@ -1,21 +1,13 @@
 import defineExperiment from '@fandom/pathfinder-lite/experiments/defineExperiment';
 import getExperiment from '@fandom/pathfinder-lite/experiments/getExperiment';
 
-const NO_INCONTENT_PLAYER_EXPERIMENT_DESKTOP = defineExperiment({
+const NO_INCONTENT_PLAYER_EXPERIMENT = defineExperiment({
 	name: 'no-incontent-player-experiment-desktop',
-	buckets: ['A', 'B', 'C', 'D'],
-});
-
-const NO_INCONTENT_PLAYER_EXPERIMENT_MOBILE = defineExperiment({
-	name: 'no-incontent-player-experiment-mobile',
-	buckets: ['W', 'X', 'Y', 'Z'],
+	buckets: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], // 10.93%
 });
 
 export function isNoInContentVideoVariationActive() {
-	const experiment = getExperiment([
-		NO_INCONTENT_PLAYER_EXPERIMENT_DESKTOP,
-		NO_INCONTENT_PLAYER_EXPERIMENT_MOBILE,
-	]);
+	const experiment = getExperiment([NO_INCONTENT_PLAYER_EXPERIMENT]);
 
 	return !!experiment;
 }
