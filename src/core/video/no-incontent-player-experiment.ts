2fbd5fa4e7d6037beec6c04e4d8e33f8a5c1498a
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2024-02-06 19:32:57 +0100

    COTECH-1035 add a variable to disable no incontent player experiment

--- src/core/video/no-incontent-player-experiment.ts
+++ src/core/video/no-incontent-player-experiment.ts
@@ -1,13 +1,17 @@
 import defineExperiment from '@fandom/pathfinder-lite/experiments/defineExperiment';
 import getExperiment from '@fandom/pathfinder-lite/experiments/getExperiment';
+import { context } from '../../index';
 
 const NO_INCONTENT_PLAYER_EXPERIMENT = defineExperiment({
-	name: 'no-incontent-player-experiment-desktop',
+	name: 'no-incontent-player-experiment',
 	buckets: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], // 10.93%
 });
 
 export function isNoInContentVideoVariationActive() {
-	const experiment = getExperiment([NO_INCONTENT_PLAYER_EXPERIMENT]);
+	const isExperimentActive = !!getExperiment([NO_INCONTENT_PLAYER_EXPERIMENT]);
+	const forceDisabledExperiment = !!context.get(
+		'options.video.experiments.disabled.no_incontent_player',
+	);
 
-	return !!experiment;
+	return isExperimentActive && !forceDisabledExperiment;
 }
