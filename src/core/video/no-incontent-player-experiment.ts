f3b3eb8f60e9890bee1eb42f4d5f0a666da86bf4
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2024-02-07 14:28:20 +0100

    COTECH-1035 reorganise experiment groups

--- src/core/video/no-incontent-player-experiment.ts
+++ src/core/video/no-incontent-player-experiment.ts
@@ -1,17 +1,65 @@
 import defineExperiment from '@fandom/pathfinder-lite/experiments/defineExperiment';
 import getExperiment from '@fandom/pathfinder-lite/experiments/getExperiment';
+import { Experiment } from '@fandom/pathfinder-lite/types';
 import { context } from '../../index';
 
-const NO_INCONTENT_PLAYER_EXPERIMENT = defineExperiment({
-	name: 'no-incontent-player-experiment',
-	buckets: ['A', 'B', 'C', 'D'], // 6.25%
-});
+export const incontentPlayerRemovalVariants = {
+	desktopControl: 'incontent-player-removal-experiment-desktop-control',
+	desktopActive: 'incontent-player-removal-experiment-desktop-active',
+	mobileControl: 'incontent-player-removal-experiment-mobile-control',
+	mobileActive: 'incontent-player-removal-experiment-mobile-active',
+};
+
+type RunningExperimentType = Experiment | null | undefined;
+
+const getMobileExperiments = () => [
+	defineExperiment({
+		name: incontentPlayerRemovalVariants.mobileControl,
+		buckets: ['C', 'D', 'E'],
+	}),
+	defineExperiment({
+		name: incontentPlayerRemovalVariants.mobileActive,
+		buckets: ['a', 'b', 'c'],
+	}),
+];
+
+const getDesktopExperiments = () => [
+	defineExperiment({
+		name: incontentPlayerRemovalVariants.desktopControl,
+		buckets: ['a', 'b', 'c', 'd'],
+	}),
+	defineExperiment({
+		name: incontentPlayerRemovalVariants.desktopActive,
+		buckets: ['4', '5', '6', '7'],
+	}),
+];
+
+const getAllExperiments = () => {
+	return context.get('state.isMobile') ? getMobileExperiments() : getDesktopExperiments();
+};
+
+let runningExperiment: RunningExperimentType;
+
+const getRunningExperiment = (): RunningExperimentType => {
+	if (typeof runningExperiment === 'undefined') {
+		runningExperiment = getExperiment(getAllExperiments());
+	}
+
+	return runningExperiment;
+};
+
+const isExperimentActive = (experimentName: string): boolean => {
+	return getRunningExperiment()?.name === experimentName;
+};
 
 export function isNoInContentVideoVariationActive() {
-	const isExperimentActive = !!getExperiment([NO_INCONTENT_PLAYER_EXPERIMENT]);
+	const isMobile = context.get('state.isMobile');
+	const isActive = isMobile
+		? isExperimentActive(incontentPlayerRemovalVariants.mobileActive)
+		: isExperimentActive(incontentPlayerRemovalVariants.desktopActive);
 	const forceDisabledExperiment = !!context.get(
 		'options.video.experiments.disabled.no_incontent_player',
 	);
 
-	return isExperimentActive && !forceDisabledExperiment;
+	return isActive && !forceDisabledExperiment;
 }
