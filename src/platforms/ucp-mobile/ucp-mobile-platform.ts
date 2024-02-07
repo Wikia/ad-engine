ba71475fe6357900ad95efe3b33cb259008d6a8c
preludek <tomasz.felczak89@gmail.com>
2024-02-07 12:57:26 +0100

    ADEN-13831 remove bidders from fandom mobile

--- src/platforms/ucp-mobile/ucp-mobile-platform.ts
+++ src/platforms/ucp-mobile/ucp-mobile-platform.ts
@@ -1,6 +1,4 @@
 import {
-	BiddersStateSetup,
-	BiddersTargetingUpdater,
 	ConsentManagementPlatformSetup,
 	ensureGeoCookie,
 	InstantConfigSetup,
@@ -30,9 +28,7 @@ import {
 import { Injectable } from '@wikia/dependency-injection';
 import { basicContext } from './ad-context';
 import { UcpMobileAdsMode } from './modes/ucp-mobile-ads.mode';
-import { UcpMobileA9ConfigSetup } from './setup/context/a9/ucp-mobile-a9-config.setup';
 import { UcpMobileBaseContextSetup } from './setup/context/base/ucp-mobile-base-context.setup';
-import { UcpMobilePrebidConfigSetup } from './setup/context/prebid/ucp-mobile-prebid-config.setup';
 import { UcpMobileSlotsContextSetup } from './setup/context/slots/ucp-mobile-slots-context.setup';
 import { UcpMobileDynamicSlotsSetup } from './setup/dynamic-slots/ucp-mobile-dynamic-slots.setup';
 import { UcpMobileTemplatesSetup } from './templates/ucp-mobile-templates.setup';
@@ -61,12 +57,8 @@ export class UcpMobilePlatform {
 			IdentitySetup,
 			UcpTargetingSetup,
 			LoadTimesSetup,
-			UcpMobilePrebidConfigSetup,
-			UcpMobileA9ConfigSetup,
 			UcpMobileDynamicSlotsSetup,
 			UcpMobileTemplatesSetup,
-			BiddersStateSetup,
-			BiddersTargetingUpdater,
 			LabradorSetup,
 			conditional(() => this.noAdsDetector.isAdsMode(), {
 				yes: UcpMobileAdsMode,
