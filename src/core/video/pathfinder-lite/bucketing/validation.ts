b1b9e2dd00513b267fd3f3101fdd7a1835782585
Andrzej Łukaszewski <nandy-andy@users.noreply.github.com>
2024-02-08 15:45:33 +0100

    COTECH-1035 Enable no incontent player experiment via Pathfinder (#2329)
    * COTECH-1035 add pathfinder-lite to deps
    
    * COTECH-1035 replace Optimizely with Pathfinder-lite
    
    * COTECH-1035 define just one experiment for no incontent player
    
    * COTECH-1035 add a variable to disable no incontent player experiment
    
    * COTECH-1035 update buckets for no incontent player experiment
    
    * COTECH-1035 update pathfinder-lite dependency
    
    * COTECH-1025 pass token as env variable
    
    * COTECH-1025 rename github npm token
    
    * COTECH-1035 update remaining workflows
    
    * COTECH-1035 revert PR labeler action change
    
    * COTECH-1035 reorganise experiment groups
    
    * COTECH-1035 rename variables
    
    * COTECH-1035 rename on/off helper function
    
    * COTECH-1035 clean up .npmrc and change token name
    
    * COTECH-1035 use right package and change buckets for mobile
    
    * COTECH-1035 rename env and secret variable
    
    * COTECH-1035 rename the file and go back to smaller code
    
    * COTECH-1035 add artifactory setup back to .npmrc
    
    * Revert "COTECH-1035 add artifactory setup back to .npmrc"
    
    This reverts commit b6112865cb8273ceef64ed3805fff2a23530269c.
    
    * COTECH-1035 update the buckets
    
    * COTECH-1035 | use pathfinder-lite copied code (#2333)
    
    * Clean up npmrc
    
    * Use copied pathfinder-lite
    
    * Remove pathfinder-lite tests
    
    * Fix getting beacon id
    
    * Tracking
    
    ---------
    
    Co-authored-by: Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
    Co-authored-by: Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
    Co-authored-by: Karol Tatała <ktatala@wikia-inc.com>
    Co-authored-by: Karol Tatała <ktatala@fandom.com>

---
+++ src/core/video/pathfinder-lite/bucketing/validation.ts
@@ -0,0 +1,50 @@
+import { BucketType } from '../types';
+
+export const DEFAULT_BUCKET_INDEX = 0;
+export const ALLOWED_BUCKET_CHAR = /[0-9a-zA-Z_-]/;
+export const ALLOWED_BUCKETS_STRING = /[0-9a-zA-Z_-]+/;
+export const CONTROL_BUCKETS_CHAR = /[a-d]/;
+
+/** Validates whether a bucket is not part of Control buckets nor isn't a restricted value */
+export function validateBucket(buckets: string[]): BucketType {
+	if (buckets.map((bucket) => !ALLOWED_BUCKET_CHAR.test(bucket)).some(Boolean)) {
+		return BucketType.Invalid;
+	}
+
+	if (buckets.map((bucket) => CONTROL_BUCKETS_CHAR.test(bucket)).some(Boolean)) {
+		return BucketType.Control;
+	}
+
+	return BucketType.Valid;
+}
+
+export function validateCustomBucket(validateFunc: () => boolean) {
+	if (typeof validateFunc !== 'function') {
+		return BucketType.Invalid;
+	}
+
+	if (validateFunc()) {
+		return BucketType.Valid;
+	}
+
+	return BucketType.Invalid;
+}
+
+/** Get list of overlapping buckets */
+export function checkForBucketOverlap(buckets: Array<string>): Array<string> {
+	// no buckets, nothing to do
+	if (!buckets?.length) {
+		return [];
+	}
+
+	// merge all strings, sort, return if we have two or more symbols next to each other, but trim them to 1st character each
+	const matches = buckets
+		.filter(String)
+		.map((bucket) => bucket.split(''))
+		.reduce((acc, val) => acc.concat(val), [])
+		.sort()
+		.join('')
+		.match(/(.)\1+/g);
+
+	return matches ? matches.map((match) => match[0]) : [];
+}
