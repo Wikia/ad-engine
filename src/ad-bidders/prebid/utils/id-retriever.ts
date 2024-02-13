3bcc207ccd26ae3c199415d8ac65074b3c089e21
Janik <dj@kazan.com.pl>
2024-02-13 09:49:00 +0100

    TACO-374 - CR Fixes

--- src/ad-bidders/prebid/utils/id-retriever.ts
+++ src/ad-bidders/prebid/utils/id-retriever.ts
@@ -20,12 +20,12 @@ export class IdRetriever {
 	// Where P is Present, A is Absent and x is empty placeholder for new IDs
 	async generateBoiString() {
 		const idSources: string[] = (await this.getIds()).map((id) => id.source);
-		const idString = 'x'.repeat(this.ID_STRING_LENGTH);
+		const idString = new Array(this.ID_STRING_LENGTH).fill('x');
 		Object.keys(this.ID_MAP).forEach((idTouple) => {
 			idString[idTouple] = idSources.includes(this.ID_MAP[idTouple]) ? 'P' : 'A';
 		});
 		utils.logger(this.logGroup, 'Generated BOI string ', idString);
-		return idString;
+		return idString.join('');
 	}
 
 	static get(): IdRetriever {
