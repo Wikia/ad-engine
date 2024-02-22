0aa597631de48fbe67cd496b774b06c59d732f40
pawelforfa <pforfa@fandom.com>
2024-02-22 11:43:01 -0500

    ADEN-13830 :: Removed the News & Ratings platform and the associated tests/specs

--- src/platforms/news-and-ratings/comicvine/index.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from '@wikia/dependency-injection';
-import { ComicvinePlatform } from './comicvine-platform';
-import './styles.scss';
-
-const container = new Container();
-const platform = container.get(ComicvinePlatform);
-
-platform.execute();
