ccbb03680cc3932926e9172bc03c340e18abd9a3
Andrzej 'nAndy' Łukaszewski <nandy@wikia-inc.com>
2024-02-28 15:47:43 +0100

    Revert "COTECH-1050 Change player setup for Connatix so the tentpole is more stable (#2365)"
    This reverts commit 5a933f5ad8a8e06508b1afabc2f028aad09bb014.

--- spec/platforms/shared/setup/player.setup.spec.ts
+++ spec/platforms/shared/setup/player.setup.spec.ts
@@ -1,9 +1,4 @@
-import {
-	JWPlayerManager,
-	VastResponseData,
-	VastTaglessRequest,
-	videoDisplayTakeoverSynchronizer,
-} from '@wikia/ad-products';
+import { JWPlayerManager, VastResponseData, VastTaglessRequest } from '@wikia/ad-products';
 import { communicationService, eventsRepository } from '@wikia/communication';
 import { context, displayAndVideoAdsSyncContext, InstantConfigService } from '@wikia/core';
 import { PlayerSetup } from '@wikia/platforms/shared';
@@ -14,8 +9,6 @@ describe('PlayerSetup', () => {
 	const MOCKED_VAST_AD_UNIT = '/5441/cnx-test/vast/ad/unit';
 
 	let dispatchSpy: SinonSpy;
-	let displayVideoSyncSpy: SinonSpy;
-	let getSyncTimeoutSpy: SinonSpy;
 	let instantConfigStub: SinonStubbedInstance<InstantConfigService>;
 	let jwpManagerStub: SinonStubbedInstance<JWPlayerManager>;
 	let vastTaglessRequestStub;
@@ -39,8 +32,6 @@ describe('PlayerSetup', () => {
 
 	beforeEach(() => {
 		dispatchSpy = global.sandbox.spy(communicationService, 'dispatch');
-		displayVideoSyncSpy = global.sandbox.spy(videoDisplayTakeoverSynchronizer, 'resolve');
-		getSyncTimeoutSpy = global.sandbox.spy(displayAndVideoAdsSyncContext, 'getSyncTimeout');
 	});
 
 	afterEach(() => {
@@ -52,149 +43,80 @@ describe('PlayerSetup', () => {
 		context.remove('vast.adUnitId');
 	});
 
-	describe('JWPlayer', () => {
-		it('should dispatch jwpSetup action without VAST XML when tagless request is not enabled', async () => {
-			context.set('src', 'test');
-			context.set('slots.featured.videoAdUnit', MOCKED_VAST_AD_UNIT);
-			context.set('options.video.displayAndVideoAdsSyncEnabled', false);
-			const expectedDispatchArg = {
-				showAds: true,
-				autoplayDisabled: false,
-				vastXml: undefined,
-				type: '[Ad Engine] Setup JWPlayer',
-				__global: true,
-			};
-			vastTaglessRequestStub.getVast = () => Promise.resolve(undefined);
+	it('should dispatch jwpSetup action without VAST XML when tagless request is not enabled', async () => {
+		context.set('src', 'test');
+		context.set('slots.featured.videoAdUnit', MOCKED_VAST_AD_UNIT);
+		context.set('options.video.displayAndVideoAdsSyncEnabled', false);
+		const expectedDispatchArg = {
+			showAds: true,
+			autoplayDisabled: false,
+			vastXml: undefined,
+			type: '[Ad Engine] Setup JWPlayer',
+			__global: true,
+		};
+		vastTaglessRequestStub.getVast = () => Promise.resolve(undefined);
 
-			await subject.call();
+		await subject.call();
 
-			expect(dispatchSpy.calledOnceWith(expectedDispatchArg)).to.be.true;
-		});
-
-		it('should dispatch jwpSetup action with VAST XML when tagless request is enabled', async () => {
-			global.sandbox.stub(displayAndVideoAdsSyncContext, 'isSyncEnabled').returns(true);
-			global.sandbox.stub(displayAndVideoAdsSyncContext, 'isTaglessRequestEnabled').returns(true);
-			const mockedVastXML =
-				'<?xml version="1.0" encoding="UTF-8"?><VAST xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd" version="4.0"></VAST>';
-			const response: VastResponseData = { xml: mockedVastXML, lineItemId: '', creativeId: '' };
-			const expectedDispatchArg = {
-				showAds: true,
-				autoplayDisabled: false,
-				vastXml: mockedVastXML,
-				type: '[Ad Engine] Setup JWPlayer',
-				__global: true,
-			};
-			vastTaglessRequestStub.getVast = () => Promise.resolve(response);
-
-			await subject.call();
-
-			expect(dispatchSpy.calledOnce).to.be.true;
-			expect(dispatchSpy.firstCall.args[0]).to.deep.equal(expectedDispatchArg);
-		});
-
-		it('should dispatch jwpSetup action with showAds flag and without VAST XML when adblock detected', async () => {
-			context.set('options.wad.blocking', true);
-			const expectedDispatchArg = {
-				showAds: false,
-				autoplayDisabled: false,
-				type: '[Ad Engine] Setup JWPlayer',
-				__global: true,
-			};
-
-			await subject.call();
-
-			expect(dispatchSpy.calledOnceWith(expectedDispatchArg)).to.be.true;
-			expect(dispatchSpy.firstCall.args[0].showAds).to.be.false;
-			expect(dispatchSpy.firstCall.args[0].vastXml).to.be.undefined;
-		});
+		expect(dispatchSpy.calledOnceWith(expectedDispatchArg)).to.be.true;
 	});
 
-	describe('Connatix', () => {
-		it('should dispatch video setup action when Connatix is enabled', async () => {
-			instantConfigStub.get.withArgs('icFeaturedVideoPlayer').returns('connatix');
-			context.set('slots.featured.videoAdUnit', MOCKED_VAST_AD_UNIT);
-			context.set('vast.adUnitId', MOCKED_VAST_AD_UNIT);
-			const slotName = 'featured';
-			const expectedDispatchArg = {
-				showAds: true,
-				autoplayDisabled: false,
-				videoAdUnitPath: MOCKED_VAST_AD_UNIT,
-				targetingParams: 'pos=featured&rv=1&player=cnx',
-				vastXml: undefined,
-				type: '[Video] Setup done',
-				__global: true,
-			};
+	it('should dispatch jwpSetup action with VAST XML when tagless request is enabled', async () => {
+		global.sandbox.stub(displayAndVideoAdsSyncContext, 'isSyncEnabled').returns(true);
+		global.sandbox.stub(displayAndVideoAdsSyncContext, 'isTaglessRequestEnabled').returns(true);
+		const mockedVastXML =
+			'<?xml version="1.0" encoding="UTF-8"?><VAST xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd" version="4.0"></VAST>';
+		const response: VastResponseData = { xml: mockedVastXML, lineItemId: '', creativeId: '' };
+		const expectedDispatchArg = {
+			showAds: true,
+			autoplayDisabled: false,
+			vastXml: mockedVastXML,
+			type: '[Ad Engine] Setup JWPlayer',
+			__global: true,
+		};
+		vastTaglessRequestStub.getVast = () => Promise.resolve(response);
 
-			await subject.call();
-			communicationService.emit(eventsRepository.BIDDERS_BIDDING_DONE, { slotName });
+		await subject.call();
 
-			expect(dispatchSpy.called).to.be.true;
-			expect(dispatchSpy.lastCall.args[0]).to.deep.equal(expectedDispatchArg);
-		});
+		expect(dispatchSpy.calledOnce).to.be.true;
+		expect(dispatchSpy.firstCall.args[0]).to.deep.equal(expectedDispatchArg);
+	});
 
-		it('should dispatch video setup action with VAST XML when tagless request is enabled', async () => {
-			global.sandbox.stub(displayAndVideoAdsSyncContext, 'isSyncEnabled').returns(true);
-			global.sandbox.stub(displayAndVideoAdsSyncContext, 'isTaglessRequestEnabled').returns(true);
-			instantConfigStub.get.withArgs('icFeaturedVideoPlayer').returns('connatix');
-			context.set('slots.featured.videoAdUnit', MOCKED_VAST_AD_UNIT);
-			context.set('vast.adUnitId', MOCKED_VAST_AD_UNIT);
-			const mockedVastXML =
-				'<?xml version="1.0" encoding="UTF-8"?><VAST xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast.xsd" version="4.0"></VAST>';
-			const response: VastResponseData = { xml: mockedVastXML, lineItemId: '', creativeId: '' };
-			const slotName = 'featured';
-			const expectedDispatchArg = {
-				showAds: true,
-				autoplayDisabled: false,
-				videoAdUnitPath: MOCKED_VAST_AD_UNIT,
-				targetingParams: 'pos=featured&rv=1&player=cnx',
-				vastXml: mockedVastXML,
-				type: '[Video] Setup done',
-				__global: true,
-			};
-			vastTaglessRequestStub.getVast = () => Promise.resolve(response);
+	it('should dispatch jwpSetup action with showAds flag and without VAST XML when adblock detected', async () => {
+		context.set('options.wad.blocking', true);
+		const expectedDispatchArg = {
+			showAds: false,
+			autoplayDisabled: false,
+			type: '[Ad Engine] Setup JWPlayer',
+			__global: true,
+		};
 
-			await subject.call();
-			communicationService.emit(eventsRepository.BIDDERS_BIDDING_DONE, { slotName });
+		await subject.call();
 
-			expect(dispatchSpy.called).to.be.true;
-			expect(dispatchSpy.lastCall.args[0]).to.deep.equal(expectedDispatchArg);
-		});
+		expect(dispatchSpy.calledOnceWith(expectedDispatchArg)).to.be.true;
+		expect(dispatchSpy.firstCall.args[0].showAds).to.be.false;
+		expect(dispatchSpy.firstCall.args[0].vastXml).to.be.undefined;
+	});
 
-		describe('Connatix and video+display sync', () => {
-			it('should get video+display sync timeout on play event when no adImpression event happened', async () => {
-				PlayerSetup.resolveVideoDisplaySyncBasedOnPlayerEvent('play', undefined, undefined);
+	it('should dispatch video setup action when Connatix is enabled', async () => {
+		instantConfigStub.get.withArgs('icFeaturedVideoPlayer').returns('connatix');
+		context.set('slots.featured.videoAdUnit', MOCKED_VAST_AD_UNIT);
+		context.set('vast.adUnitId', MOCKED_VAST_AD_UNIT);
+		const slotName = 'featured';
+		const expectedDispatchArg = {
+			showAds: true,
+			autoplayDisabled: false,
+			videoAdUnitPath: MOCKED_VAST_AD_UNIT,
+			targetingParams: 'pos=featured&rv=1&player=cnx',
+			vastXml: undefined,
+			type: '[Video] Setup done',
+			__global: true,
+		};
 
-				expect(getSyncTimeoutSpy.called).to.be.true;
-			});
+		await subject.call();
+		communicationService.emit(eventsRepository.BIDDERS_BIDDING_DONE, { slotName });
 
-			it('should resolve video+display sync on adError event', () => {
-				PlayerSetup.resolveVideoDisplaySyncBasedOnPlayerEvent('adError', undefined, undefined);
-
-				expect(getSyncTimeoutSpy.called).to.be.false;
-				expect(displayVideoSyncSpy.called).to.be.true;
-			});
-
-			it('should resolve video+display sync on adImpression event', () => {
-				const mockedAdImpressionState = {
-					vastParams: {
-						lineItemId: '123',
-						creativeId: '456',
-					},
-				};
-				const mockedAdSlot = {
-					setStatus: () => {},
-					emit: () => {},
-				};
-
-				PlayerSetup.resolveVideoDisplaySyncBasedOnPlayerEvent(
-					'adImpression',
-					mockedAdImpressionState,
-					mockedAdSlot,
-				);
-
-				expect(getSyncTimeoutSpy.called).to.be.false;
-				expect(displayVideoSyncSpy.called).to.be.true;
-			});
-		});
+		expect(dispatchSpy.called).to.be.true;
+		expect(dispatchSpy.lastCall.args[0]).to.deep.equal(expectedDispatchArg);
 	});
 });
