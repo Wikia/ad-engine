a15cd88bdba3c7fb627236352d445881487be025
preludek <tomasz.felczak89@gmail.com>
2024-03-07 08:27:20 +0100

    ADEN-13916 platforms gone

--- src/platforms/news-and-ratings/metacritic/index.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from '@wikia/dependency-injection';
-import { MetacriticPlatform } from './metacritic-platform';
-import './styles.scss';
-
-const container = new Container();
-const platform = container.get(MetacriticPlatform);
-
-platform.execute();
