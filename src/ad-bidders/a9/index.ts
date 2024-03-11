5e8cfab9c09bdd120be46bea8586dd9b7a443bcf
Jakub Bieńkowski <124168110+jbienkowski-fandom@users.noreply.github.com>
2024-03-11 13:34:47 +0100

    TACO-341 Support for OneTrust on FCP (#2295)
    * TACO-341 Do not use cookies, stick to USP API and TCF API
    
    * TACO-341 Do not use cookies, stick to USP API and TCF API
    
    * TACO-341 Fix TCF API not available when fetching A9 bids
    
    * TACO-341 Minor changes to GlobalContextService
    
    * TACO-341 Increase GDPR CMP timeout in Apstag to match the one in Prebid
    
    * TACO-341 Final apstag fixes
    
    * TACO-341 - Fixed problems with multiple logs
    
    * TACO-341 Custom debounce instead of dedicated onMany method
    
    * TACO-341 Simplify Apstag unit tests
    
    * TACO-341 debounce unit test
    
    ---------
    
    Co-authored-by: psmagiel <149783083+psmagiel@users.noreply.github.com>
    Co-authored-by: Sonia Kozubek <83713961+soniackozubek@users.noreply.github.com>
    Co-authored-by: Janik <dj@kazan.com.pl>

--- src/ad-bidders/a9/index.ts
+++ src/ad-bidders/a9/index.ts
@@ -108,23 +108,14 @@ export class A9Provider extends BidderProvider {
 
 			await this.apstag.init();
 
-			communicationService.on(
-				eventsRepository.AD_ENGINE_CONSENT_UPDATE,
-				(consents: GdprConsentPayload & CcpaSignalPayload) =>
-					this.apstag.sendHEM(this.apstag.getRecord(), consents),
-				false,
-			);
-
-			communicationService.on(
-				eventsRepository.AD_ENGINE_CONSENT_READY,
-				(consents: GdprConsentPayload & CcpaSignalPayload) => {
-					const record = this.apstag.getRecord();
-					if (record) {
-						this.apstag.sendHEM(record, consents);
-					}
-				},
-				false,
-			);
+			const listener = utils.withDebounce((consents: GdprConsentPayload & CcpaSignalPayload) => {
+				const record = this.apstag.getRecord();
+				if (record) {
+					this.apstag.sendHEM(record, consents);
+				}
+			}, 500);
+			communicationService.on(eventsRepository.AD_ENGINE_CONSENT_UPDATE, listener, false);
+			communicationService.on(eventsRepository.AD_ENGINE_CONSENT_READY, listener, true);
 
 			if (!trackingOptIn.isOptedIn() || trackingOptIn.isOptOutSale()) {
 				utils.logger(logGroup, 'A9 was initialized without consents');
