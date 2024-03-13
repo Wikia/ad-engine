e21aa3c2a7ea9c1989c1754907bd8251455253ea
pgalicia <pgalicia@fandom.com>
2024-03-13 15:25:12 -0700

    ADEN-13654: Clean code

--- src/platforms/ucp-desktop/setup/dynamic-slots/ucp-desktop-dynamic-slots.setup.ts
+++ src/platforms/ucp-desktop/setup/dynamic-slots/ucp-desktop-dynamic-slots.setup.ts
@@ -55,9 +55,9 @@ export class UcpDesktopDynamicSlotsSetup implements DiProcess {
 			insertSlots([this.slotsDefinitionRepository.getFloorAdhesionConfig()]);
 			slotService.enable('floor_adhesion');
 		} else if (this.ucpDesktopFloorAdhesionExperiment.isFloorAdhesionShowing()) {
-			communicationService.on(eventsRepository.AD_ENGINE_UAP_NTC_LOADED, () => {
-				return insertSlots([this.slotsDefinitionRepository.getFloorAdhesionConfig()]);
-			});
+			communicationService.on(eventsRepository.AD_ENGINE_UAP_NTC_LOADED, () =>
+				insertSlots([this.slotsDefinitionRepository.getFloorAdhesionConfig()]),
+			);
 		}
 
 		communicationService.on(eventsRepository.RAIL_READY, () => {
