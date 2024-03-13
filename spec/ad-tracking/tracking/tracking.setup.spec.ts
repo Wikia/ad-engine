c01c121de219374256a1bb05d4204b41955d0bbe
preludek <tomasz.felczak89@gmail.com>
2024-03-13 10:45:54 +0100

    ADEN-refactor-tracking move and fix specs

---
+++ spec/ad-tracking/tracking/tracking.setup.spec.ts
@@ -0,0 +1,71 @@
+import { BaseTrackingSetup, DataWarehouseTracker, trackingUrls } from '@wikia/ad-tracking';
+import { LabradorTracker } from '@wikia/ad-tracking/trackers/labrador-tracker';
+import { InstantConfigService, targetingService } from '@wikia/core';
+import { expect } from 'chai';
+import { SinonSpy } from 'sinon';
+
+describe('TrackingSetup', () => {
+	const dwTracker = new DataWarehouseTracker();
+	const labradorTracker = new LabradorTracker(dwTracker);
+	const instantConfig = new InstantConfigService({
+		appName: 'testapp',
+	});
+	let trackSpy: SinonSpy;
+
+	beforeEach(() => {
+		trackSpy = global.sandbox.spy(dwTracker, 'track');
+	});
+
+	afterEach(() => {
+		targetingService.clear();
+	});
+
+	it('should track keyvals', (done) => {
+		// given
+		targetingService.clear();
+		targetingService.set('key1', 'value1');
+		const trackingSetup = new BaseTrackingSetup(labradorTracker, dwTracker, instantConfig);
+
+		// when
+		trackingSetup.execute();
+
+		// then
+		setTimeout(() => {
+			targetingService.clear();
+			expect(
+				trackSpy
+					.getCall(trackSpy.callCount - 1)
+					.calledWithExactly({ keyvals: '{"key1":"value1"}' }, trackingUrls.KEY_VALS),
+			).to.be.true;
+			done();
+		});
+	});
+
+	it('should track Google Topics API when it is available', (done) => {
+		// given
+		targetingService.clear();
+		targetingService.set('ppid', 'ppid');
+		targetingService.set('topics_available', '1');
+		// @ts-expect-error Feature Policy API is not available in TS
+		global.window.document.featurePolicy = {
+			allowsFeature: (feature: string) => feature === 'browsing-topics',
+		};
+		// @ts-expect-error Google Topics API is not available in TS
+		global.window.document.browsingTopics = () => Promise.resolve(['topic1', 'topic2']);
+		const trackingSetup = new BaseTrackingSetup(labradorTracker, dwTracker, instantConfig);
+
+		// when
+		trackingSetup.execute();
+
+		// then
+		setTimeout(() => {
+			targetingService.clear();
+			expect(
+				trackSpy
+					.getCall(trackSpy.callCount - 1)
+					.calledWithExactly({ ppid: 'ppid', topic: '["topic1","topic2"]' }, trackingUrls.TOPICS),
+			).to.be.true;
+			done();
+		});
+	});
+});
