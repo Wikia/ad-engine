13198e694b6a75750ebd11d8790f0b585db870f3
Karol Tatała <ktatala@fandom.com>
2024-03-20 14:29:51 +0100

    Remove redundant file

--- src/newfile.ts
+++
@@ -1,5 +0,0 @@
-// @ts-strict-ignore
-const element = document.getElementById('ID');
-
-// this line should not cause `TS18047: element is possibly null` error as strict mode is disabled
-console.log(element.classList);
